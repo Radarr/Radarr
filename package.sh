631cf776f6c14cdf20b39c1787fa502e56348fe2
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-03 15:54:15 +0100

    Travis now automatically pushes a build to a server.

--- package.sh
+++ package.sh
@@ -1,4 +1,18 @@
-VERSION=$1
+if [ $# -eq 0 ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != false ]; then
+    echo "Need to supply version argument" && exit;
+  fi
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  #VERSION="`date +%H:%M:%S`"
+  VERSION="15-11-15"
+  YEAR="`date +%Y`"
+  MONTH="`date +%m`"
+  DAY="`date +%d`"
+else
+  VERSION=$1
+fi
 outputFolder='./_output'
 outputFolderMono='./_output_mono'
 outputFolderOsx='./_output_osx'
@@ -7,3 +21,25 @@ outputFolderOsxApp='./_output_osx_app'
 cp -r $outputFolder Radarr_Windows_$VERSION
 cp -r $outputFolderMono Radarr_Mono_$VERSION
 cp -r $outputFolderOsxApp Radarr_OSX_$VERSION
+
+zip -r Radarr_Windows_$VERSION.zip Radarr_Windows_$VERSION >& /dev/null
+zip -r Radarr_Mono_$VERSION.zip Radarr_Mono_$VERSION >& /dev/null
+zip -r Radarr_OSX_$VERSION.zip Radarr_OSX_$VERSION >& /dev/null
+
+ftp -n ftp.leonardogalli.ch << END_SCRIPT
+quote USER $FTP_USER
+quote PASS $FTP_PASS
+mkdir builds
+cd builds
+mkdir $YEAR
+cd $YEAR
+mkdir $MONTH
+cd $MONTH
+mkdir $DAY
+cd $DAY
+binary
+put Radarr_Windows_$VERSION.zip
+put Radarr_Mono_$VERSION.zip
+put Radarr_OSX_$VERSION.zip
+quit
+END_SCRIPT
