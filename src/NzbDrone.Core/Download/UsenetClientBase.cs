af74ccca1c9400cbaa23a799ee7e8014b27a49db
AeonLucid <aeonlucid@outlook.com>
2017-01-05 00:03:57 +0100

    Remove confusing abstract method from UsenetClientBase.

--- src/NzbDrone.Core/Download/UsenetClientBase.cs
+++ src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -30,12 +30,7 @@ namespace NzbDrone.Core.Download
         
         public override DownloadProtocol Protocol => DownloadProtocol.Usenet;
 
-        protected abstract string AddFromNzbFile(RemoteEpisode remoteEpisode, string filename, byte[] fileContent);
-
-        protected virtual string AddFromNzbFile(RemoteMovie remoteMovie, string filename, byte[] fileContents)
-        {
-            throw new NotImplementedException();
-        }
+        protected abstract string AddFromNzbFile(RemoteMovie remoteMovie, string filename, byte[] fileContents);
 
         public override string Download(RemoteEpisode remoteEpisode)
         {
