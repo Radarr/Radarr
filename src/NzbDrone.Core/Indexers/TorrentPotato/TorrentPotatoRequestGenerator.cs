7a5fe59dbf63ad285741dbda35aa081804f3df1e
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-05 17:12:46 +0100

    Added TorrentPotato Indexer.

---
+++ src/NzbDrone.Core/Indexers/TorrentPotato/TorrentPotatoRequestGenerator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.IndexerSearch.Definitions;
+
+namespace NzbDrone.Core.Indexers.TorrentPotato
+{
+    public class TorrentPotatoRequestGenerator : IIndexerRequestGenerator
+    {
+
+        public TorrentPotatoSettings Settings { get; set; }
+
+        public TorrentPotatoRequestGenerator()
+        {
+
+        }
+
+        public virtual IndexerPageableRequestChain GetRecentRequests()
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests("list", null, null));
+
+            return pageableRequests;
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SingleEpisodeSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests("search", searchCriteria.Series.TvdbId, "S{0:00}E{1:00}", searchCriteria.SeasonNumber, searchCriteria.EpisodeNumber));
+
+            return pageableRequests;
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SeasonSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests("search", searchCriteria.Series.TvdbId, "S{0:00}", searchCriteria.SeasonNumber));
+
+            return pageableRequests;
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(DailyEpisodeSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests("search", searchCriteria.Series.TvdbId, "\"{0:yyyy MM dd}\"", searchCriteria.AirDate));
+
+            return pageableRequests;
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(AnimeEpisodeSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SpecialEpisodeSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            foreach (var queryTitle in searchCriteria.EpisodeQueryTitles)
+            {
+                var query = queryTitle.Replace('+', ' ');
+                query = System.Web.HttpUtility.UrlEncode(query);
+
+                pageableRequests.Add(GetPagedRequests("search", searchCriteria.Series.TvdbId, query));
+            }
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<IndexerRequest> GetPagedRequests(string mode, int? tvdbId, string query, params object[] args)
+        {
+            var requestBuilder = new HttpRequestBuilder(Settings.BaseUrl)
+                .Accept(HttpAccept.Json);
+
+            requestBuilder.AddQueryParam("passkey", Settings.Passkey);
+            requestBuilder.AddQueryParam("user", Settings.User);
+            requestBuilder.AddQueryParam("imdbid", "tt0076759"); //For now just search for Star Wars.
+
+
+
+            yield return new IndexerRequest(requestBuilder.Build());
+        }
+
+        private IEnumerable<IndexerRequest> GetMovieRequest(MovieSearchCriteria searchCriteria)
+        {
+            var requestBuilder = new HttpRequestBuilder(Settings.BaseUrl)
+                 .Accept(HttpAccept.Json);
+
+            requestBuilder.AddQueryParam("passkey", Settings.Passkey);
+            requestBuilder.AddQueryParam("user", Settings.User);
+
+            if (searchCriteria.Movie.ImdbId.IsNotNullOrWhiteSpace())
+            {
+                requestBuilder.AddQueryParam("imdbid", searchCriteria.Movie.ImdbId);
+            }
+            else
+            {
+                requestBuilder.AddQueryParam("search", searchCriteria.Movie.Title);
+            }
+
+            yield return new IndexerRequest(requestBuilder.Build());
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(MovieSearchCriteria searchCriteria)
+        {
+            
+
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetMovieRequest(searchCriteria));
+
+            return pageableRequests;
+
+            
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Indexers/TorrentPotato/TorrentPotatoSettings.cs
@@ -0,0 +1,38 @@
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Indexers.TorrentPotato
+{
+    public class TorrentPotatoSettingsValidator : AbstractValidator<TorrentPotatoSettings>
+    {
+        public TorrentPotatoSettingsValidator()
+        {
+            RuleFor(c => c.BaseUrl).ValidRootUrl();
+        }
+    }
+
+    public class TorrentPotatoSettings : IProviderConfig
+    {
+        private static readonly TorrentPotatoSettingsValidator Validator = new TorrentPotatoSettingsValidator();
+
+        public TorrentPotatoSettings()
+        {
+            BaseUrl = "";
+        }
+
+        [FieldDefinition(0, Label = "API URL", HelpText = "URL to TorrentPotato api.")]
+        public string BaseUrl { get; set; }
+        
+        [FieldDefinition(1, Label = "Username", HelpText = "The username you use at your indexer.")]
+        public string User { get; set; }
+
+        [FieldDefinition(2, Label = "Passkey", HelpText = "The password you use at your Indexer,")]
+        public string Passkey { get; set; }
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
