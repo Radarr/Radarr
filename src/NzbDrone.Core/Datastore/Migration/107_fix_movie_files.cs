8fa43fb9f74a98f42c38a94c455190cc786c1334
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-05 14:36:58 +0100

    Fix for Path column in MovieFiles table.

---
+++ src/NzbDrone.Core/Datastore/Migration/107_fix_movie_files.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(107)]
+    public class fix_movie_files : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Alter.Table("MovieFiles").AlterColumn("Path").AsString().Nullable(); //Should be deleted, but to much work, ¯\_(ツ)_/¯
+        }
+    }
+}
