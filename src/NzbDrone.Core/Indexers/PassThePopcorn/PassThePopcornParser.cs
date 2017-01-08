d1372f4d00a43493bb76759cdda20c542bc371e4
Devin Buhl <devin.kray@gmail.com>
2017-01-08 14:26:36 -0500

    Added PassThePopcorn indexe

---
+++ src/NzbDrone.Core/Indexers/PassThePopcorn/PassThePopcornParser.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers.Exceptions;
+using NzbDrone.Core.Parser.Model;
+using System;
+
+namespace NzbDrone.Core.Indexers.PassThePopcorn
+{
+    public class PassThePopcornParser : IParseIndexerResponse
+    {
+        private readonly PassThePopcornSettings _settings;
+
+        public PassThePopcornParser(PassThePopcornSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public IList<ReleaseInfo> ParseResponse(IndexerResponse indexerResponse)
+        {
+            var torrentInfos = new List<ReleaseInfo>();
+
+            if (indexerResponse.HttpResponse.StatusCode != HttpStatusCode.OK)
+            {
+                throw new IndexerException(indexerResponse,
+                    "Unexpected response status {0} code from API request",
+                    indexerResponse.HttpResponse.StatusCode);
+            }
+
+            var jsonResponse = JsonConvert.DeserializeObject<PassThePopcornResponse>(indexerResponse.Content);
+
+            var responseData = jsonResponse.Movies;
+            if (responseData == null)
+            {
+                throw new IndexerException(indexerResponse,
+                    "Indexer API call response missing result data");
+            }
+
+            foreach (var result in responseData)
+            {
+                foreach (var torrent in result.Torrents)
+                {
+                    var id = torrent.Id;
+                    torrentInfos.Add(new TorrentInfo()
+                    {
+                        Guid = string.Format("PassThePopcorn-{0}", id),
+                        Title = torrent.ReleaseName,
+                        Size = Int64.Parse(torrent.Size),
+                        DownloadUrl = GetDownloadUrl(id, jsonResponse.AuthKey, jsonResponse.PassKey),
+                        InfoUrl = GetInfoUrl(result.GroupId, id),
+                        Seeders = Int32.Parse(torrent.Seeders),
+                        Peers = Int32.Parse(torrent.Leechers) + Int32.Parse(torrent.Seeders),
+                        PublishDate = torrent.UploadTime.ToUniversalTime()
+                    });
+                }
+            }
+
+            return torrentInfos.ToArray();
+        }
+
+        private string GetDownloadUrl(int torrentId, string authKey, string passKey)
+        {
+            var url = new HttpUri(_settings.BaseUrl)
+                .CombinePath("/torrents.php")
+                .AddQueryParam("action", "download")
+                .AddQueryParam("id", torrentId)
+                .AddQueryParam("authkey", authKey)
+                .AddQueryParam("torrent_pass", passKey);
+
+            return url.FullUri;
+        }
+
+        private string GetInfoUrl(string groupId, int torrentId)
+        {
+            var url = new HttpUri(_settings.BaseUrl)
+                .CombinePath("/torrents.php")
+                .AddQueryParam("id", groupId)
+                .AddQueryParam("torrentid", torrentId);
+
+            return url.FullUri;
+
+        }
+    }
+}
