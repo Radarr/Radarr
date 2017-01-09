2ed0738b301467b23b42c46a32a5062dd132c5fe
Devin Buhl <onedr0p@users.noreply.github.com>
2017-01-09 08:30:15 -0500

    Added PassThePopcorn indexer (#64)
    * Added PassThePopcorn indexe
    
    * Added checkboxes for Gold and Checked torrents thanks @evulhotdog
    
    * Sorted movie releases by golden -> checked -> upload date
    
    * Refactored logic.
    
    * Added flags at the end of torrent name for golden / approved
    
    * Updated PTP GOlden to be the popcorn emoji
    
    * Revert "Updated PTP GOlden to be the popcorn emoji"
    
    This reverts commit c6cf0f5fc520b84f43548afe558c0797e0866fbf.
    
    * Opps, hopefully build will pass now.
    
    * Move PTP props to new subclass.

---
+++ src/NzbDrone.Core/Indexers/PassThePopcorn/PassThePopcornApi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NzbDrone.Core.Indexers.PassThePopcorn
+{
+    public class Director
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class Torrent
+    {
+        public int Id { get; set; }
+        public string Quality { get; set; }
+        public string Source { get; set; }
+        public string Container { get; set; }
+        public string Codec { get; set; }
+        public string Resolution { get; set; }
+        public bool Scene { get; set; }
+        public string Size { get; set; }
+        public DateTime UploadTime { get; set; }
+        public string RemasterTitle { get; set; }
+        public string Snatched { get; set; }
+        public string Seeders { get; set; }
+        public string Leechers { get; set; }
+        public string ReleaseName { get; set; }
+        public bool Checked { get; set; }
+        public bool GoldenPopcorn { get; set; }
+    }
+
+    public class Movie
+    {
+        public string GroupId { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Cover { get; set; }
+        public List<string> Tags { get; set; }
+        public List<Director> Directors { get; set; }
+        public string ImdbId { get; set; }
+        public int TotalLeechers { get; set; }
+        public int TotalSeeders { get; set; }
+        public int TotalSnatched { get; set; }
+        public long MaxSize { get; set; }
+        public string LastUploadTime { get; set; }
+        public List<Torrent> Torrents { get; set; }
+    }
+
+    public class PassThePopcornResponse
+    {
+        public string TotalResults { get; set; }
+        public List<Movie> Movies { get; set; }
+        public string Page { get; set; }
+        public string AuthKey { get; set; }
+        public string PassKey { get; set; }
+    }
+
+}
---
+++ src/NzbDrone.Core/Indexers/PassThePopcorn/PassThePopcornRequestGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NzbDrone.Common.Http;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.IndexerSearch.Definitions;
+
+namespace NzbDrone.Core.Indexers.PassThePopcorn
+{
+    public class PassThePopcornRequestGenerator : IIndexerRequestGenerator
+    {
+        public PassThePopcornSettings Settings { get; set; }
+
+        public virtual IndexerPageableRequestChain GetRecentRequests()
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetRequest(null));
+
+            return pageableRequests;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(MovieSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(searchCriteria.Movie.ImdbId));
+            return pageableRequests;
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SingleEpisodeSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(AnimeEpisodeSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SpecialEpisodeSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(DailyEpisodeSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        public virtual IndexerPageableRequestChain GetSearchRequests(SeasonSearchCriteria searchCriteria)
+        {
+            return new IndexerPageableRequestChain();
+        }
+
+        private IEnumerable<IndexerRequest> GetRequest(string searchParameters)
+        {
+            var request = new IndexerRequest(string.Format("{0}/torrents.php?json=noredirect&searchstr={1}", Settings.BaseUrl.Trim().TrimEnd('/'), searchParameters), HttpAccept.Json);
+
+            foreach (var cookie in HttpHeader.ParseCookies(Settings.Cookie))
+            {
+                request.HttpRequest.Cookies[cookie.Key] = cookie.Value;
+            }
+
+            yield return request;
+        }
+    }
+}
