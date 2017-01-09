03156d62f61c643986c131fd5e4891b395fe02e5
Devin Buhl <onedr0p@users.noreply.github.com>
2017-01-09 12:28:08 -0500

    Remove some indexers and fixed HDBits (#79)
    * Update HDBitsRequestGenerator.cs
    
    * Removed torrentleech, btn and bitmetv

---
+++ src/NzbDrone.Core/Datastore/Migration/111_remove_bitmetv.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(111)]
+    public class remove_bitmetv : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Delete.FromTable("Indexers").Row(new { Implementation = "BitMeTv" });
+        }
+    }
+}
