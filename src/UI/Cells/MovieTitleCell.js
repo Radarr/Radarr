e4f7aa52dfcb5e019db2cfd5f158fa8e5f17a19c
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-10 15:15:15 +0100

    History now correctly shows movie title. Fixes #92

--- src/UI/Cells/MovieTitleCell.js
+++ src/UI/Cells/MovieTitleCell.js
@@ -4,8 +4,4 @@ module.exports = TemplatedCell.extend({
     className : 'series-title-cell',
     template  : 'Cells/SeriesTitleTemplate',
 
-    // render : function() {
-    //     this.$el.html(this.model.get("movie").get("title")); //Hack, but somehow handlebar helper does not work.
-    //     return this;
-    // }
 });
