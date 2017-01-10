683bda49d84cd4f935abd0b1ed10e11f3b212c83
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-10 16:51:56 +0100

    Movie Editor works now. Fixes #99.

---
+++ src/NzbDrone.Api/Movies/MovieEditorModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using NzbDrone.Api.Extensions;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Api.Movie
+{
+    public class MovieEditorModule : NzbDroneApiModule
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieEditorModule(IMovieService movieService)
+            : base("/movie/editor")
+        {
+            _movieService = movieService;
+            Put["/"] = Movie => SaveAll();
+        }
+
+        private Response SaveAll()
+        {
+            var resources = Request.Body.FromJson<List<MovieResource>>();
+
+            var Movie = resources.Select(MovieResource => MovieResource.ToModel(_movieService.GetMovie(MovieResource.Id))).ToList();
+
+            return _movieService.UpdateMovie(Movie)
+                                 .ToResource()
+                                 .AsResponse(HttpStatusCode.Accepted);
+        }
+    }
+}
