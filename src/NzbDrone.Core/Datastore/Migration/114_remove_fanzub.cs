cd68eea79070f515769012b95ef9cebdf6a7700d
Devin Buhl <devin.kray@gmail.com>
2017-01-11 09:40:42 -0500

    removed indexer Fanzub - site shutdown

---
+++ src/NzbDrone.Core/Datastore/Migration/114_remove_fanzub.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(114)]
+    public class remove_fanzub : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Delete.FromTable("Indexers").Row(new { Implementation = "Fanzub" });
+        }
+    }
+}
