6d2f81e3ed8fe14cb1849ded82fc7e37f105a630
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-17 14:28:30 +0100

    Remove - as replacement for :

--- src/NzbDrone.Core/Organizer/FileNameBuilder.cs
+++ src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -318,7 +318,7 @@ namespace NzbDrone.Core.Organizer
         {
             string result = name;
             string[] badCharacters = { "\\", "/", "<", ">", "?", "*", ":", "|", "\"" };
-            string[] goodCharacters = { "+", "+", "", "", "!", "-", "-", "", "" };
+            string[] goodCharacters = { "+", "+", "", "", "!", "-", "", "", "" };
 
             for (int i = 0; i < badCharacters.Length; i++)
             {
