43d904d20b1ec99aa55ed973acd8c9fc94a46d24
Devin Buhl <devin.kray@gmail.com>
2017-01-23 18:06:42 -0500

    added trakt user list importing

---
+++ src/NzbDrone.Core/NetImport/Trakt/TraktRequestGenerator.cs
@@ -0,0 +1,35 @@
+﻿using NzbDrone.Common.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.NetImport.Trakt
+{
+    public class TraktRequestGenerator : INetImportRequestGenerator
+    {
+        public TraktSettings Settings { get; set; }
+
+        public virtual NetImportPageableRequestChain GetMovies()
+        {
+            var pageableRequests = new NetImportPageableRequestChain();
+
+            pageableRequests.Add(GetMovies(null));
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<NetImportRequest> GetMovies(string searchParameters)
+        {
+            // https://api.trakt.tv/users/timdturner/lists/custom1/items/movies
+            // trakt-api-version = 2
+            // trakt-api-key = 657bb899dcb81ec8ee838ff09f6e013ff7c740bf0ccfa54dd41e791b9a70b2f0
+
+            var request = new NetImportRequest($"{Settings.Link.Trim()}{Settings.Username.Trim()}/lists/{Settings.Listname.Trim()}/items/movies", HttpAccept.Json);
+            request.HttpRequest.Headers.Add("trakt-api-version", "2");
+            request.HttpRequest.Headers.Add("trakt-api-key", "657bb899dcb81ec8ee838ff09f6e013ff7c740bf0ccfa54dd41e791b9a70b2f0");
+
+            yield return request;
+        }
+    }
+}
