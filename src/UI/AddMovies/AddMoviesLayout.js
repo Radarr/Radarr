8b3b46b724631622b26e771aedc0741b04f2f4cc
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-23 16:04:52 +0100

    Added easy to use List Selection for manual import use later. The place where this resides will change.

--- src/UI/AddMovies/AddMoviesLayout.js
+++ src/UI/AddMovies/AddMoviesLayout.js
@@ -5,57 +5,70 @@ var RootFolderLayout = require('./RootFolders/RootFolderLayout');
 var ExistingMoviesCollectionView = require('./Existing/AddExistingMovieCollectionView');
 var AddMoviesView = require('./AddMoviesView');
 var ProfileCollection = require('../Profile/ProfileCollection');
+var ListCollection = require("../Settings/NetImport/NetImportCollection");
 var RootFolderCollection = require('./RootFolders/RootFolderCollection');
 require('../Movies/MoviesCollection');
 
 module.exports = Marionette.Layout.extend({
-    template : 'AddMovies/AddMoviesLayoutTemplate',
+		template : 'AddMovies/AddMoviesLayoutTemplate',
 
-    regions : {
-        workspace : '#add-movies-workspace'
-    },
+		regions : {
+				workspace : '#add-movies-workspace'
+		},
 
-    events : {
-        'click .x-import'  : '_importMovies',
-        'click .x-add-new' : '_addMovies',
-        'click .x-show-existing' : '_toggleExisting'
-    },
+		events : {
+				'click .x-import'  : '_importMovies',
+				'click .x-add-new' : '_addMovies',
+				'click .x-show-existing' : '_toggleExisting'
+		},
 
-    attributes : {
-        id : 'add-movies-screen'
-    },
+		attributes : {
+				id : 'add-movies-screen'
+		},
 
-    initialize : function() {
-        ProfileCollection.fetch();
-        RootFolderCollection.fetch().done(function() {
-            RootFolderCollection.synced = true;
-        });
-    },
+		initialize : function() {
+				ProfileCollection.fetch();
+				RootFolderCollection.fetch().done(function() {
+						RootFolderCollection.synced = true;
+				});
+				this.templateHelpers = {}
+				this.listCollection = new ListCollection();
+				this.templateHelpers.lists = this.listCollection.toJSON();
 
-    _toggleExisting : function(e) {
-        var showExisting = e.target.checked;
-        
-        vent.trigger(vent.Commands.ShowExistingCommand, {
-            showExisting: showExisting
-        });
-    },
+				this.listenTo(this.listCollection, 'all', this._listsUpdated);
+				this.listCollection.fetch();
 
-    onShow : function() {
-        this.workspace.show(new AddMoviesView());
-    },
+		},
 
-    _folderSelected : function(options) {
-        vent.trigger(vent.Commands.CloseModalCommand);
-        this.workspace.show(new ExistingMoviesCollectionView({ model : options.model }));
-    },
+		_toggleExisting : function(e) {
+				var showExisting = e.target.checked;
 
-    _importMovies : function() {
-        this.rootFolderLayout = new RootFolderLayout();
-        this.listenTo(this.rootFolderLayout, 'folderSelected', this._folderSelected);
-        AppLayout.modalRegion.show(this.rootFolderLayout);
-    },
+				vent.trigger(vent.Commands.ShowExistingCommand, {
+						showExisting: showExisting
+				});
+		},
 
-    _addMovies : function() {
-        this.workspace.show(new AddMoviesView());
-    }
+		onShow : function() {
+				this.workspace.show(new AddMoviesView());
+		},
+
+		_listsUpdated : function() {
+			this.templateHelpers.lists = this.listCollection.toJSON();
+			this.render();
+		},
+
+		_folderSelected : function(options) {
+				vent.trigger(vent.Commands.CloseModalCommand);
+				this.workspace.show(new ExistingMoviesCollectionView({ model : options.model }));
+		},
+
+		_importMovies : function() {
+				this.rootFolderLayout = new RootFolderLayout();
+				this.listenTo(this.rootFolderLayout, 'folderSelected', this._folderSelected);
+				AppLayout.modalRegion.show(this.rootFolderLayout);
+		},
+
+		_addMovies : function() {
+				this.workspace.show(new AddMoviesView());
+		}
 });
