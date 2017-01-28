5e9a79afe8251d192364dc71b4ae2f3c549e49eb
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-01-28 17:35:51 +0100

    Quality of an existing movie file can now be edited.

---
+++ src/UI/Movies/Files/EditFileCell.js
@@ -0,0 +1,22 @@
+var vent = require('vent');
+var Backgrid = require('backgrid');
+
+module.exports = Backgrid.Cell.extend({
+		className : 'edit-episode-file-cell',
+
+		events : {
+				'click' : '_onClick'
+		},
+
+		render : function() {
+				this.$el.empty();
+				this.$el.html('<i class="icon-sonarr-edit" title="Edit information about this file."></i>');
+
+				return this;
+		},
+
+		_onClick : function() {
+				var self = this;
+				vent.trigger(vent.Commands.EditFileCommand, { file : this.model });
+		}
+});
