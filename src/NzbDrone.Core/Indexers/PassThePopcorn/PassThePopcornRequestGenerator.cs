6af6da16b4aec5c1c29ab6fe1ac14920332cdf30
Devin Buhl <devin.kray@gmail.com>
2017-01-30 01:48:28 -0500

    Use username, password and passkey for passthepopcorn

--- src/NzbDrone.Core/Indexers/PassThePopcorn/PassThePopcornRequestGenerator.cs
+++ src/NzbDrone.Core/Indexers/PassThePopcorn/PassThePopcornRequestGenerator.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using Newtonsoft.Json.Linq;
 using NzbDrone.Common.Http;
-using NzbDrone.Common.Serializer;
 using NzbDrone.Core.IndexerSearch.Definitions;
+using RestSharp;
 
 namespace NzbDrone.Core.Indexers.PassThePopcorn
 {
@@ -54,14 +54,42 @@ namespace NzbDrone.Core.Indexers.PassThePopcorn
 
         private IEnumerable<IndexerRequest> GetRequest(string searchParameters)
         {
-            var request = new IndexerRequest(string.Format("{0}/torrents.php?json=noredirect&searchstr={1}", Settings.BaseUrl.Trim().TrimEnd('/'), searchParameters), HttpAccept.Json);
+            var request =
+                new IndexerRequest(
+                    $"{Settings.BaseUrl.Trim().TrimEnd('/')}/torrents.php?json=noredirect&searchstr={searchParameters}",
+                    HttpAccept.Json);
 
-            foreach (var cookie in HttpHeader.ParseCookies(Settings.Cookie))
+            var cookies = GetPTPCookies();
+            foreach (var cookie in cookies)
             {
-                request.HttpRequest.Cookies[cookie.Key] = cookie.Value;
+                request.HttpRequest.Cookies[cookie.Name] = cookie.Value;
             }
 
             yield return request;
         }
+
+        private IList<RestResponseCookie> GetPTPCookies()
+        {
+            var client = new RestClient(Settings.BaseUrl.Trim().TrimEnd('/'));
+            var request = new RestRequest("/ajax.php?action=login", Method.POST);
+            request.AddParameter("username", Settings.Username);
+            request.AddParameter("password", Settings.Password);
+            request.AddParameter("passkey", Settings.Passkey);
+            request.AddParameter("keeplogged", "1");
+            request.AddParameter("login", "Log In!");
+            request.AddHeader("Content-Type", "multipart/form-data");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            var jsonResponse = JObject.Parse(content);
+            if (content != null && (string)jsonResponse["Result"] != "Error")
+            {
+                return response.Cookies;
+            }
+
+            throw new Exception("Error connecting to PTP invalid username, password, or passkey");
+        }
+
     }
 }
