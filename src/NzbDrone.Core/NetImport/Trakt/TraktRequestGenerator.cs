8f49af19a83dbf4ac8b219fde4fa0562694738f9
geogolem <geogolem@gmail.com>
2017-02-05 00:49:08 -0500

    basic support for trakt authorization and private lists this will work as long as you supply a valid auth_key that has been provided by trakt-api for use with radar's trakt api key and content related to the user's account
    the problem is these auth_keys are only valid for some time
    and then need to be refreshed with a refresh key.
    
    i haven't implemented the refresh part totally yet..
    
    also, the app is able to obtain the auth key and refresh key for the
    user
    but right now, radarr expects you to obtain it by other means
    
    please refer to the trakt api definition here: http://docs.trakt.apiary.io/#reference/authentication-oauth

--- src/NzbDrone.Core/NetImport/Trakt/TraktRequestGenerator.cs
+++ src/NzbDrone.Core/NetImport/Trakt/TraktRequestGenerator.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace NzbDrone.Core.NetImport.Trakt
 {
@@ -60,11 +63,54 @@ namespace NzbDrone.Core.NetImport.Trakt
                     break;
             }
 
+            /*
+            if (Settings.Refreshtoken ! = null) //if a refreshToken exists
+            {
+                TimeSpan span = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0,DateTimeKind.Utc));
+                double unixTime = span.TotalSeconds;
+                bool tokenExpired = false; //TokenCreatedAt + TokenExpiresIn < unixTime()
+                if (tokenExpired)
+                {
+                    var url = Settings.Link.Trim();
+                    url = url + "/oauth/token";
+
+                    string postData = "refresh_token=" + Settings.Refreshtoken.Trim();
+                    postData += "&client_id=" + Settings.Authtoken.Trim();
+                    postData += "&client_secret=b16be19076b515553bb830141e08729c1d987fe686b3c3bc0316ba4382c2b810";
+                    postData += "&redirect_uri=urn:ietf:wg:oauth:2.0:oob";
+                    postData += "&grant_type=refresh_token";
+
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                    HttpWebRequest rquest = (HttpWebRequest)WebRequest.Create(url);
+                    rquest.Method = "POST";
+                    rquest.Headers.ContentType = "application/json";
+                    using (var st = rquest.GetRequestStream())
+                        st.Write(byteArray, 0, byteArray.Length);
+                    var rsponse = (HttpWebResponse)rquest.GetResponse();
+                    var rsponseString = new StreamReader(rsponse.GetResponseStream()).ReadToEnd();
+                    rsponse.Close();
+                    dynamic j1 = JObject.Parse(rsponseString);
+
+                    //these need to be updated and need to but so far dont know how
+                    //this shoud only be done if the post request was successful.
+                    Settings.Authtoken = j1.access_token;
+                    Settings.Refreshtoken = j1.refresh_token;
+                    TokenCreatedAt = j1.created_at; //convert to double
+                    TokenExpiresIn = j1.expires_in; //convert to double
+                }
+            }
+            */
+
             var request = new NetImportRequest($"{link}", HttpAccept.Json);
             request.HttpRequest.Headers.Add("trakt-api-version", "2");
             request.HttpRequest.Headers.Add("trakt-api-key", "657bb899dcb81ec8ee838ff09f6e013ff7c740bf0ccfa54dd41e791b9a70b2f0");
+            if (Settings.Authtoken != null)
+            {
+                request.HttpRequest.Headers.Add("Authorization", "Bearer " + Settings.Authtoken.Trim());
+            }
 
-            yield return request;
+                yield return request;
         }
     }
 }
