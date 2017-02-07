2450147f5a170fc09bc86248dfdaa2b9d6a6f7eb
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-02-07 13:53:36 +0100

    Added spinner feedback for tmdbid cell.

--- src/UI/AddMovies/BulkImport/TmdbIdCell.js
+++ src/UI/AddMovies/BulkImport/TmdbIdCell.js
@@ -15,29 +15,48 @@ module.exports = NzbDroneCell.extend({
         render : function() {
             this.$el.empty();
 
-            this.$el.html('<input type="text" class="x-tmdbId tmdbId-input form-control" value="' + this.cellValue.get('tmdbId') + '" />');
-            
+            this.$el.html('<i class="icon-sonarr-info hidden"></i><input type="text" class="x-tmdbId tmdbId-input form-control" value="' + this.cellValue.get('tmdbId') + '" />');
+
             return this;
         },
 
         _updateId : function() {
-            var data = this.$el.find('.x-tmdbId').val();
+						var field = this.$el.find('.x-tmdbId');
+            var data = field.val();
 
             var promise = $.ajax({
                 url  : window.NzbDrone.ApiRoot + '/movies/lookup/tmdb?tmdbId=' + data,
                 type : 'GET',
             });
 
-            //this.$(this.ui.grab).spinForPromise(promise);
+						//field.spinForPromise(promise);
+
+						field.prop("disabled", true)
+
+						var icon = this.$(".icon-sonarr-info");
+
+						icon.removeClass("hidden");
+
+            icon.spinForPromise(promise);
             var _self = this;
             var cacheMonitored = this.model.get('monitored');
-            promise.success(function(response) {            
+						var cacheProfile = this.model.get("profileId");
+						var cachePath = this.model.get("path");
+						var cacheFile = this.model.get("movieFile");
+						var cacheRoot = this.model.get("rootFolderPath");
+
+            promise.success(function(response) {
                 _self.model.set(response);
                 _self.model.set('monitored', cacheMonitored); //reset to the previous monitored value
+								_self.model.set('profileId', cacheProfile);
+								_self.model.set('path', cachePath);
+								_self.model.set('movieFile', cacheFile); // may be unneccessary.
+								field.prop("disabled", false)
             });
 
             promise.error(function(request, status, error) {
                 console.error("Status: " + status, "Error: " + error);
+								field.prop("disabled", false)
             });
         }
 });
