6216366681c26cdebaca4307d97ad3de0f1aff54
Devin Buhl <devin.kray@gmail.com>
2017-02-06 19:30:25 -0500

    change hadoken to use radarr label

--- src/NzbDrone.Core/Download/Clients/Hadouken/HadoukenSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Hadouken/HadoukenSettings.cs
@@ -28,7 +28,7 @@ namespace NzbDrone.Core.Download.Clients.Hadouken
         {
             Host = "localhost";
             Port = 7070;
-            Category = "sonarr-tv";
+            Category = "radarr";
         }
 
         [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
