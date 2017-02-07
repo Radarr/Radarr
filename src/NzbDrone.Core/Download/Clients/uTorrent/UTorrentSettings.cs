f6128870990bf0423880efea954f93ace6c5e351
Devin Buhl <devin.kray@gmail.com>
2017-02-06 19:27:13 -0500

    change utorrent default label to radarr

--- src/NzbDrone.Core/Download/Clients/uTorrent/UTorrentSettings.cs
+++ src/NzbDrone.Core/Download/Clients/uTorrent/UTorrentSettings.cs
@@ -23,7 +23,7 @@ namespace NzbDrone.Core.Download.Clients.UTorrent
         {
             Host = "localhost";
             Port = 9091;
-            TvCategory = "tv-sonarr";
+            TvCategory = "radarr";
         }
 
         [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
