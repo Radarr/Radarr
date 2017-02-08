4ca1f226431ee723299d862275734c0af2ba5036
Devin Buhl <devin.kray@gmail.com>
2017-02-08 10:13:27 -0500

    Add try/catch block

--- src/NzbDrone.Core/Indexers/AwesomeHD/AwesomeHDRssParser.cs
+++ src/NzbDrone.Core/Indexers/AwesomeHD/AwesomeHDRssParser.cs
@@ -32,60 +32,64 @@ namespace NzbDrone.Core.Indexers.AwesomeHD
                     indexerResponse.HttpResponse.StatusCode);
             }
 
-            // Hacky ¯\_(ツ)_/¯
-            XmlDocument doc = new XmlDocument();
-            // doc.LoadXml(indexerResponse.Content);
-
-            var xdoc = XDocument.Parse(indexerResponse.Content);
-
-            var searchResults = xdoc.Descendants("searchresults").Select(x => new
+            try
             {
-                AuthKey = x.Element("authkey").Value,
-            }).FirstOrDefault();
-
-            var torrents = xdoc.Descendants("torrent")
-            .Select(x => new
-            {
-                Id = x.Element("id").Value,
-                Name = x.Element("name").Value,
-                Year = x.Element("year").Value,
-                GroupId = x.Element("groupid").Value,
-                Time = DateTime.Parse(x.Element("time").Value),
-                UserId = x.Element("userid").Value,
-                Size = long.Parse(x.Element("size").Value),
-                Snatched = x.Element("snatched").Value,
-                Seeders = x.Element("seeders").Value,
-                Leechers = x.Element("leechers").Value,
-                ReleaseGroup = x.Element("releasegroup").Value,
-                Resolution = x.Element("resolution").Value,
-                Media = x.Element("media").Value,
-                Format = x.Element("format").Value,
-                Encoding = x.Element("encoding").Value,
-                AudioFormat = x.Element("audioformat").Value,
-                AudioBitrate = x.Element("audiobitrate").Value,
-                AudioChannels = x.Element("audiochannels").Value,
-                Subtitles = x.Element("subtitles").Value,
-                EncodeStatus = x.Element("encodestatus").Value,
-                Freeleech = x.Element("freeleech").Value,
-            }).ToList();
-
-            foreach (var torrent in torrents)
-            {
-                var id = torrent.Id;
-                var title = $"{torrent.Name}.{torrent.Year}.{torrent.Resolution}.{torrent.Media}.{torrent.Encoding}.{torrent.AudioFormat}-{torrent.ReleaseGroup}";
-
-                torrentInfos.Add(new TorrentInfo()
+                var xdoc = XDocument.Parse(indexerResponse.Content);
+                var searchResults = xdoc.Descendants("searchresults").Select(x => new
                 {
-                    Guid = string.Format("AwesomeHD-{0}", id),
-                    Title = title,
-                    Size = torrent.Size,
-                    DownloadUrl = GetDownloadUrl(id, searchResults.AuthKey, _settings.Passkey),
-                    InfoUrl = GetInfoUrl(torrent.GroupId, id),
-                    Seeders = int.Parse(torrent.Seeders),
-                    Peers = int.Parse(torrent.Leechers) + int.Parse(torrent.Seeders),
-                    PublishDate = torrent.Time.ToUniversalTime()
-                });
+                    AuthKey = x.Element("authkey").Value,
+                }).FirstOrDefault();
+
+                var torrents = xdoc.Descendants("torrent")
+                .Select(x => new
+                {
+                    Id = x.Element("id").Value,
+                    Name = x.Element("name").Value,
+                    Year = x.Element("year").Value,
+                    GroupId = x.Element("groupid").Value,
+                    Time = DateTime.Parse(x.Element("time").Value),
+                    UserId = x.Element("userid").Value,
+                    Size = long.Parse(x.Element("size").Value),
+                    Snatched = x.Element("snatched").Value,
+                    Seeders = x.Element("seeders").Value,
+                    Leechers = x.Element("leechers").Value,
+                    ReleaseGroup = x.Element("releasegroup").Value,
+                    Resolution = x.Element("resolution").Value,
+                    Media = x.Element("media").Value,
+                    Format = x.Element("format").Value,
+                    Encoding = x.Element("encoding").Value,
+                    AudioFormat = x.Element("audioformat").Value,
+                    AudioBitrate = x.Element("audiobitrate").Value,
+                    AudioChannels = x.Element("audiochannels").Value,
+                    Subtitles = x.Element("subtitles").Value,
+                    EncodeStatus = x.Element("encodestatus").Value,
+                    Freeleech = x.Element("freeleech").Value,
+                }).ToList();
+
+                foreach (var torrent in torrents)
+                {
+                    var id = torrent.Id;
+                    var title = $"{torrent.Name}.{torrent.Year}.{torrent.Resolution}.{torrent.Media}.{torrent.Encoding}.{torrent.AudioFormat}-{torrent.ReleaseGroup}";
+
+                    torrentInfos.Add(new TorrentInfo()
+                    {
+                        Guid = string.Format("AwesomeHD-{0}", id),
+                        Title = title,
+                        Size = torrent.Size,
+                        DownloadUrl = GetDownloadUrl(id, searchResults.AuthKey, _settings.Passkey),
+                        InfoUrl = GetInfoUrl(torrent.GroupId, id),
+                        Seeders = int.Parse(torrent.Seeders),
+                        Peers = int.Parse(torrent.Leechers) + int.Parse(torrent.Seeders),
+                        PublishDate = torrent.Time.ToUniversalTime()
+                    });
+                }
             }
+            catch (XmlException)
+            {
+                throw new IndexerException(indexerResponse,
+                   "An error occurred while processing feed, feed invalid");
+            }
+
 
             return torrentInfos.OrderByDescending(o => ((dynamic)o).Seeders).ToArray();
         }
