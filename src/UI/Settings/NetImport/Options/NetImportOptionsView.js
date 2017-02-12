bca7158710c58cc7d5998621137e407842dfe0e1
geogolem <geogolem@gmail.com>
2017-02-12 06:47:42 -0500

    cleanup code a bit

--- src/UI/Settings/NetImport/Options/NetImportOptionsView.js
+++ src/UI/Settings/NetImport/Options/NetImportOptionsView.js
@@ -6,8 +6,8 @@ var params = new URLSearchParams(window.location.search.slice(1));
 var oauth=params.get('oauth');
 var refresh=params.get('refresh');
 var q = window.location;
-var url = q.protocol+'//'+q.hostname+(q.port ? ':' + q.port : '')+'/settings/netimport';
-history.pushState('object', 'title', url);
+var callback_url = q.protocol+'//'+q.hostname+(q.port ? ':' + q.port : '')+'/settings/netimport';
+history.pushState('object', 'title', callback_url);
 var view = Marionette.ItemView.extend({
 	template : 'Settings/NetImport/Options/NetImportOptionsViewTemplate',
 	events : {
@@ -34,8 +34,8 @@ var view = Marionette.ItemView.extend({
 
 	_resetTraktTokens : function() {
 		if (window.confirm("You will now be taken to trakt.tv for authentication.\nYou will then be directed back here.\nDon't forget to click save when you get back!")){
-		var l = window.location;
-		var callback_url=l.protocol+'//'+l.hostname+(l.port ? ':' +l.port : '')+'/settings/netimport';
+		//var q = window.location;
+		//var callback_url=l.protocol+'//'+l.hostname+(l.port ? ':' +l.port : '')+'/settings/netimport';
 		window.location='https://api.couchpota.to/authorize/trakt/?target='+callback_url;
 		}
 	}
