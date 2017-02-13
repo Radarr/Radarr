5d3ae82bf387669571c4fb90f12b21888d34f0ee
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-02-13 15:51:15 +0100

    Update CommonVersionInfo.cs
    Just to kickstart appveyor

--- src/Common/CommonVersionInfo.cs
+++ src/Common/CommonVersionInfo.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System.Reflection;
-using System;
 
 [assembly: AssemblyVersion("0.1.0.*")]
