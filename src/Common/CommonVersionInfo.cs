9d7dce0bfa77ecbcfc119d47c64376cd5767c04d
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-02-13 15:41:36 +0100

    Update CommonVersionInfo.cs

--- src/Common/CommonVersionInfo.cs
+++ src/Common/CommonVersionInfo.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System.Reflection;
+using System;
 
 [assembly: AssemblyVersion("0.1.0.*")]
