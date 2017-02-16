363048e68edf3a0b0fc7d8eebbde045f9ff5c7f0
Devin Buhl <onedr0p@users.noreply.github.com>
2017-02-16 13:29:21 -0500

    Roll back some code on Net Import (#772)

--- src/NzbDrone.Core/NetImport/CouchPotato/CouchPotatoImport.cs
+++ src/NzbDrone.Core/NetImport/CouchPotato/CouchPotatoImport.cs
@@ -16,12 +16,30 @@ namespace NzbDrone.Core.NetImport.CouchPotato
     public class CouchPotatoImport : HttpNetImportBase<CouchPotatoSettings>
     {
         public override string Name => "CouchPotato";
-        public override bool Enabled => false;
+        public override bool Enabled => true;
         public override bool EnableAuto => false;
 
         public CouchPotatoImport(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
             : base(httpClient, configService, parsingService, logger)
         { }
+
+        /*public new virtual IEnumerable<ProviderDefinition> DefaultDefinitions
+        {
+            get
+            {
+                var config = (CouchPotatoSettings)new CouchPotatoSettings();
+                config.Link = "http://localhost";
+                config.Port = "5050";
+
+                yield return new NetImportDefinition
+                {
+                    Name = "Localhost",
+                    Enabled = config.Validate().IsValid && Enabled,
+                    Implementation = GetType().Name,
+                    Settings = config
+                };
+            }
+        }*/
 
         public override INetImportRequestGenerator GetRequestGenerator()
         {
--- src/NzbDrone.Core/NetImport/StevenLu/StevenLuImport.cs
+++ src/NzbDrone.Core/NetImport/StevenLu/StevenLuImport.cs
@@ -16,8 +16,8 @@ namespace NzbDrone.Core.NetImport.StevenLu
     public class StevenLuImport : HttpNetImportBase<StevenLuSettings>
     {
         public override string Name => "StevenLu";
-        public override bool Enabled => false;
-        public override bool EnableAuto => false;
+        public override bool Enabled => true;
+        public override bool EnableAuto => true;
 
         public StevenLuImport(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
             : base(httpClient, configService, parsingService, logger)
