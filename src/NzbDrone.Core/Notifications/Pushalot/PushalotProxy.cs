136721f26571a00405d27f503fae37905a47836c
hotio <hotio@users.noreply.github.com>
2017-02-19 15:58:53 +0100

    Update notification logos

--- src/NzbDrone.Core/Notifications/Pushalot/PushalotProxy.cs
+++ src/NzbDrone.Core/Notifications/Pushalot/PushalotProxy.cs
@@ -29,11 +29,11 @@ namespace NzbDrone.Core.Notifications.Pushalot
             var client = RestClientFactory.BuildClient(URL);
             var request = BuildRequest();
 
-            request.AddParameter("Source", "Sonarr");
+            request.AddParameter("Source", "Radarr");
 
             if (settings.Image)
             {
-                request.AddParameter("Image", "https://raw.githubusercontent.com/Sonarr/Sonarr/develop/Logo/128.png");                
+                request.AddParameter("Image", "https://raw.githubusercontent.com/Radarr/Radarr/develop/Logo/128.png");                
             }
 
             request.AddParameter("Title", title);
