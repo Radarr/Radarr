210b24a700110dd3a9cc5335ce3d9f7eca50090d
geogolem <geogolem@gmail.com>
2017-02-22 23:08:28 -0500

    add buttons in Wanted/Missing view

--- src/NzbDrone.Core/Tv/MovieRepository.cs
+++ src/NzbDrone.Core/Tv/MovieRepository.cs
@@ -196,15 +196,6 @@ namespace NzbDrone.Core.Tv
         {
             return Query.Where(pagingSpec.FilterExpression)
                              .AndWhere(m => m.MovieFileId == 0)
-                             //note: this doesnt respect the availability delay that is used for RssSync
-                             //should be .AndWhere (m=> m.IsAvailable(delay)) where delay corresponds to AvailabilityDelay on Indexers Settings page
-                             //but that doesnt seem to work
-			                 //TODO: will need to update for preDB for now treat PreDB like Released
-                             .AndWhere(m=> 
-                             (m.MinimumAvailability==MovieStatusType.Released && m.Status >=MovieStatusType.Released) ||
-                             (m.MinimumAvailability==MovieStatusType.InCinemas && m.Status >= MovieStatusType.InCinemas) ||
-                             (m.MinimumAvailability==MovieStatusType.Announced && m.Status >=MovieStatusType.Announced) ||
-                             (m.MinimumAvailability==MovieStatusType.PreDB && m.Status >= MovieStatusType.Released))
                              .OrderBy(pagingSpec.OrderByClause(), pagingSpec.ToSortDirection())
                              .Skip(pagingSpec.PagingOffset())
                              .Take(pagingSpec.PageSize);
