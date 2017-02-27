06bd6db6011de7eff91c32fd7a9c14998e939f2b
Ross Valler <RossValler@users.noreply.github.com>
2017-02-27 17:21:44 -0500

    Expose more information to the Webhook notification (#935)
    * Fix/implement Webhook notifications
    
    * Expose more information (specifically TMDB ID)

--- src/NzbDrone.Core/Notifications/Webhook/WebhookRemoteMovie.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookRemoteMovie.cs
@@ -5,21 +5,27 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookRemoteMovie
     {
+        public int TmdbId { get; set; }
         public string ImdbId { get; set; }
         public string Title { get; set; }
+        public int Year { get; set; }
 
         public WebhookRemoteMovie() { }
 
         public WebhookRemoteMovie(RemoteMovie remoteMovie)
         {
+            TmdbId = remoteMovie.Movie.TmdbId;
             ImdbId = remoteMovie.Movie.ImdbId;
-            Title = remoteMovie.Release.Title;
+            Title = remoteMovie.Movie.Title;
+            Year = remoteMovie.Movie.Year;
         }
 
         public WebhookRemoteMovie(Movie movie)
         {
+            TmdbId = movie.TmdbId;
             ImdbId = movie.ImdbId;
             Title = movie.Title;
+            Year = movie.Year;
         }
     }
 }
