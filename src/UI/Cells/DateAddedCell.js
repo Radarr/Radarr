9a4182400f6ef19a42fb43610945e784970d040a
Bill Szeliga <bszeliga@connectionseducation.com>
2017-02-27 22:23:52 -0500

    use utility functions to format time

--- src/UI/Cells/DateAddedCell.js
+++ src/UI/Cells/DateAddedCell.js
@@ -1,4 +1,6 @@
 var TemplatedCell = require('./TemplatedCell');
+var UiSettingsModel = require('../Shared/UiSettingsModel');
+var FormatHelpers = require('../Shared/FormatHelpers');
 var moment = require('moment');
 
 module.exports = TemplatedCell.extend({
@@ -7,8 +9,11 @@ module.exports = TemplatedCell.extend({
     render : function() {
       this.$el.html("");
 
-      if (this.model.get("added")) {
-          this.$el.html(moment(this.model.get("added")).format('LLL'));
+      var dateAdded = this.model.get("added");
+
+      if (dateAdded) {
+          var time = '{0} at {1}'.format(FormatHelpers.relativeDate(dateAdded), moment(dateAdded).format(UiSettingsModel.time(true, false)));
+          this.$el.html(time);
       }
 
       return this;
