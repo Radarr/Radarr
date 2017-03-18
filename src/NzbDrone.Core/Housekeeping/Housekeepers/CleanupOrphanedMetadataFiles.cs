14e4e581b59b71761da43fbed5a9e07d1dd1947b
Devin Buhl <devin.kray@gmail.com>
2017-03-17 23:45:10 -0400

    Revert "Clean up the cleanup'er (housekeeping)"
    This reverts commit c03c13d9241a0f59f118cd9ffccba5646a3f52ca.

--- src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedMetadataFiles.cs
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedMetadataFiles.cs
@@ -13,45 +13,45 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
 
         public void Clean()
         {
-            //DeleteOrphanedBySeries();
-            //DeleteOrphanedByEpisodeFile();
-            //DeleteWhereEpisodeFileIsZero();
+            DeleteOrphanedBySeries();
+            DeleteOrphanedByEpisodeFile();
+            DeleteWhereEpisodeFileIsZero();
         }
 
-        //private void DeleteOrphanedBySeries()
-        //{
-        //    var mapper = _database.GetDataMapper();
+        private void DeleteOrphanedBySeries()
+        {
+            var mapper = _database.GetDataMapper();
 
-        //    mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
-        //                             WHERE Id IN (
-        //                             SELECT MetadataFiles.Id FROM MetadataFiles
-        //                             LEFT OUTER JOIN Series
-        //                             ON MetadataFiles.SeriesId = Series.Id
-        //                             WHERE Series.Id IS NULL)");
-        //}
+            mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
+                                     WHERE Id IN (
+                                     SELECT MetadataFiles.Id FROM MetadataFiles
+                                     LEFT OUTER JOIN Series
+                                     ON MetadataFiles.SeriesId = Series.Id
+                                     WHERE Series.Id IS NULL)");
+        }
 
-        //private void DeleteOrphanedByEpisodeFile()
-        //{
-        //    var mapper = _database.GetDataMapper();
+        private void DeleteOrphanedByEpisodeFile()
+        {
+            var mapper = _database.GetDataMapper();
 
-        //    mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
-        //                             WHERE Id IN (
-        //                             SELECT MetadataFiles.Id FROM MetadataFiles
-        //                             LEFT OUTER JOIN EpisodeFiles
-        //                             ON MetadataFiles.EpisodeFileId = EpisodeFiles.Id
-        //                             WHERE MetadataFiles.EpisodeFileId > 0
-        //                             AND EpisodeFiles.Id IS NULL)");
-        //}
+            mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
+                                     WHERE Id IN (
+                                     SELECT MetadataFiles.Id FROM MetadataFiles
+                                     LEFT OUTER JOIN EpisodeFiles
+                                     ON MetadataFiles.EpisodeFileId = EpisodeFiles.Id
+                                     WHERE MetadataFiles.EpisodeFileId > 0
+                                     AND EpisodeFiles.Id IS NULL)");
+        }
 
-        //private void DeleteWhereEpisodeFileIsZero()
-        //{
-        //    var mapper = _database.GetDataMapper();
+        private void DeleteWhereEpisodeFileIsZero()
+        {
+            var mapper = _database.GetDataMapper();
 
-        //    mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
-        //                             WHERE Id IN (
-        //                             SELECT Id FROM MetadataFiles
-        //                             WHERE Type IN (2, 5)
-        //                             AND EpisodeFileId = 0)");
-        //}
+            mapper.ExecuteNonQuery(@"DELETE FROM MetadataFiles
+                                     WHERE Id IN (
+                                     SELECT Id FROM MetadataFiles
+                                     WHERE Type IN (2, 5)
+                                     AND EpisodeFileId = 0)");
+        }
     }
 }
