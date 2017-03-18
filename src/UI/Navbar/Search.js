1b29b89bf126e0c73e5ab36993ec4cf12fb86450
thejacer87 <jacebennest87@gmail.com>
2017-03-17 21:53:09 -0700

    New movie search (#1212)
    * add movie search empty template (#1149)
    
    * hooked up new route in controller (#1149)

--- src/UI/Navbar/Search.js
+++ src/UI/Navbar/Search.js
@@ -14,7 +14,7 @@ var substringMatcher = function() {
         var matches = _.select(FullMovieCollection.toJSON(), function(series) {
             return series.title.toLowerCase().indexOf(q.toLowerCase()) > -1;
         });
-        cb(matches);
+        cb(matches);    
     };
 };
 
@@ -27,6 +27,11 @@ $.fn.bindSearch = function() {
         name       : 'series',
         displayKey : function(series) {
            return series.title + ' (' + series.year + ')';
+        },
+        templates  : {
+          empty : function(input) {
+            return '<div class="tt-dataset-series"><span class="tt-suggestions" style="display: block;"><div class="tt-suggestion"><p style="white-space: normal;"><a class="no-movies-found" href="/addmovies/search/' + input.query + '">Search for "' + input.query + '"</a></p></div></span></div>'
+          },
         },
         source     : substringMatcher()
     });
