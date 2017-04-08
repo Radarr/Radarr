31ca0002d7d17f1359dbed3f8c5936128ce94b67
TCBWZA <duncan.macmillan@tcbw.biz>
2017-04-08 18:46:28 +0200

    Change process name and rename assembly

--- src/NzbDrone.Common/Processes/ProcessProvider.cs
+++ src/NzbDrone.Common/Processes/ProcessProvider.cs
@@ -35,8 +35,8 @@ namespace NzbDrone.Common.Processes
     {
         private readonly Logger _logger;
 
-        public const string NZB_DRONE_PROCESS_NAME = "NzbDrone";
-        public const string NZB_DRONE_CONSOLE_PROCESS_NAME = "NzbDrone.Console";
+        public const string NZB_DRONE_PROCESS_NAME = "Lidarr";
+        public const string NZB_DRONE_CONSOLE_PROCESS_NAME = "Lidarr.Console";
 
         public ProcessProvider(Logger logger)
         {
