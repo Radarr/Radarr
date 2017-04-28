d72b9d16d380ccf23cbe6a894b164906a7c3998b
randellhodges <rhodges@taxfodder.com>
2017-04-28 08:52:41 -0500

    Added advanced configuration options to support filtering Categories, Codecs, and Medium to the HDBits indexer.

--- src/NzbDrone.Core/Indexers/HDBits/HDBitsRequestGenerator.cs
+++ src/NzbDrone.Core/Indexers/HDBits/HDBitsRequestGenerator.cs
@@ -59,6 +59,10 @@ namespace NzbDrone.Core.Indexers.HDBits
             query.Username = Settings.Username;
             query.Passkey = Settings.ApiKey;
 
+            query.Category = Settings.GetIntList(Settings.Categories);
+            query.Codec = Settings.GetIntList(Settings.Codec);
+            query.Medium = Settings.GetIntList(Settings.Medium);
+
             // Require Internal only if came from RSS sync
             if (Settings.RequireInternal && query.ImdbInfo == null)
             {
--- src/NzbDrone.Core/Indexers/HDBits/HDBitsSettings.cs
+++ src/NzbDrone.Core/Indexers/HDBits/HDBitsSettings.cs
@@ -1,7 +1,11 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Linq;
+using FluentValidation;
 using NzbDrone.Core.Annotations;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
+using System.Linq.Expressions;
+using FluentValidation.Results;
 
 namespace NzbDrone.Core.Indexers.HDBits
 {
@@ -38,9 +42,68 @@ namespace NzbDrone.Core.Indexers.HDBits
         [FieldDefinition(4, Label = "Require Internal", Type = FieldType.Checkbox, HelpText = "Require Internal releases for release to be accepted.")]
         public bool RequireInternal { get; set; }
 
+        [FieldDefinition(5, Label = "Categories", Advanced = true, HelpText = "A comma delimited list of integers. Options: 1=Movie, 2=TV, 3=Documentary, 4=Music, 5=Sport, 6=Audio, 7=XXX, 8=Misc/Demo. Example: 1,3")]
+        public string Categories { get; set; }
+
+        [FieldDefinition(6, Label = "Codec", Advanced = true, HelpText = "A comma delimited list of integers. Options: 1=h264, 2=Mpeg2, 3=VC1, 4=Xvid. Example: 1,2")]
+        public string Codec { get; set; }
+
+        [FieldDefinition(7, Label = "Medium", Advanced = true, HelpText = "A comma delimited list of integers. Options: 1=BluRay, 3=Encode, 4=Capture, 5=Remux, 6=WebDL. Example: 3,4,6")]
+        public string Medium { get; set; }
+
+        public int[] GetIntList(string input)
+        {
+            if (!String.IsNullOrWhiteSpace(input))
+                return input.Split(',').Select(t => int.Parse(t.Trim())).ToArray();
+
+            return new int[0];
+        }
+
         public NzbDroneValidationResult Validate()
         {
-            return new NzbDroneValidationResult(Validator.Validate(this));
+            var results = Validator.Validate(this);
+
+            Validate<string, HdBitsCategory>(results, () => this.Categories);
+            Validate<string, HdBitsCodec>(results, () => this.Codec);
+            Validate<string, HdBitsMedium>(results, () => this.Medium);
+
+            return new NzbDroneValidationResult(results);
+        }
+
+        private void Validate<T, K>(ValidationResult results, Expression<Func<T>> expr) where K: struct
+        {
+            var propertyName = ((MemberExpression)expr.Body).Member.Name;
+            var propertyValue = expr.Compile()() as string;
+
+            // Nothing selected means no filtering, which is valid
+            if (String.IsNullOrWhiteSpace(propertyValue))
+                return;
+
+            try
+            {
+                // I am not using ToIntList because that assumes the data is valid. This way doesn't
+                // assume that and we try to identify specifically if a value is not an integer.
+
+                var choices = Enum.GetValues(typeof(K)).Cast<K>().ToLookup(k => Convert.ToInt32(k));
+                var candidates = propertyValue.Split(',').Select(t => t.Trim());
+
+                foreach (var candidate in candidates)
+                {
+                    if (int.TryParse(candidate, out int v))
+                    {
+                        if (!choices.Contains(v))
+                            results.Errors.Add(new ValidationFailure(propertyName, $"\"{candidate}\" is not a valid choice", candidate));
+                    }
+                    else
+                    {
+                        results.Errors.Add(new ValidationFailure(propertyName, $"\"{candidate}\" is not a valid number", candidate));
+                    }
+                }
+            }
+            catch
+            {
+                results.Errors.Add(new ValidationFailure(propertyName, "Uknown error validating property"));
+            }
         }
     }
 
