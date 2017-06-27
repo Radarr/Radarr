1e5931c1c8f66e6b351d2e1e1977392b9e842113
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-06-27 12:34:23 +0200

    First bits

---
+++ src/NzbDrone.Core/Movies/AlternativeTitle.cs
@@ -0,0 +1,32 @@
+﻿namespace NzbDrone.Core.Movies
+{
+    public class AlternativeTitle
+    {
+        public SourceType SourceType { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string CleanTitle { get; set; }
+        public int SourceId { get; set; }
+        public int Votes { get; set; }
+        public int VoteCount { get; set; }
+
+        public bool IsTrusted(int minVotes = 3)
+        {
+            switch (SourceType)
+            {
+                case SourceType.TMDB:
+                    return Votes >= minVotes;
+                default:
+                    return true;
+            }   
+        }
+    }
+
+    public enum SourceType
+    {
+        TMDB = 0,
+        Mappings = 1,
+        User = 2,
+        Indexer = 3
+    }
+}
