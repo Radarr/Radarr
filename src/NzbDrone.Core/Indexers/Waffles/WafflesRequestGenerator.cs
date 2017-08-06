5556989324b7ccfda880aacf3097c7247e982577
Qstick <qstick@gmail.com>
2017-08-05 22:17:44 -0400

    Implement Waffles Indexer (#34)
    * Implement Waffles Indexer
    
    Implement Waffles Indexer
    
    * Remove Exception Class
    
    Remove Exception Class
    
    * Fix csproj file
    
    Fix csproj file
    
    * Add Test Fixture
    
    Add Test Fixture
    
    * Split line due to length for readability
    
    Split line due to length for readability

---
+++ src/NzbDrone.Core/Indexers/Waffles/WafflesRequestGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
+using System.Text;
+using System;
+using NzbDrone.Core.IndexerSearch.Definitions;
+
+namespace NzbDrone.Core.Indexers.Waffles
+{
+    public class WafflesRequestGenerator : IIndexerRequestGenerator
+    {
+        public WafflesSettings Settings { get; set; }
+        
+        public virtual IndexerPageableRequestChain GetRecentRequests()
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests(null));
+
+            return pageableRequests;
+        }
+
+        [System.Obsolete("Sonarr TV Stuff -- Shouldn't be needed for Lidarr")]
+        public virtual IndexerPageableRequestChain GetSearchRequests(SingleEpisodeSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException ();
+        }
+
+        [System.Obsolete("Sonarr TV Stuff -- Shouldn't be needed for Lidarr")]
+        public virtual IndexerPageableRequestChain GetSearchRequests(SeasonSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.Obsolete("Sonarr TV Stuff -- Shouldn't be needed for Lidarr")]
+        public virtual IndexerPageableRequestChain GetSearchRequests(DailyEpisodeSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.Obsolete("Sonarr TV Stuff -- Shouldn't be needed for Lidarr")]
+        public virtual IndexerPageableRequestChain GetSearchRequests(AnimeEpisodeSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.Obsolete("Sonarr TV Stuff -- Shouldn't be needed for Lidarr")]
+        public virtual IndexerPageableRequestChain GetSearchRequests(SpecialEpisodeSearchCriteria searchCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(AlbumSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests(string.Format("&q=artist:{0} album:{1}",searchCriteria.Artist.Name,searchCriteria.Album.Title)));
+
+            return pageableRequests;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(ArtistSearchCriteria searchCriteria)
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests(string.Format("&q=artist:{0}", searchCriteria.Artist.Name)));
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<IndexerRequest> GetPagedRequests(string query)
+        { 
+
+            var url = new StringBuilder();
+        
+            url.AppendFormat("{0}/browse.php?rss=1&c0=1&uid={1}&passkey={2}", Settings.BaseUrl.Trim().TrimEnd('/'), Settings.UserId, Settings.RssPasskey);
+
+            if (query.IsNotNullOrWhiteSpace())
+            {
+                url.AppendFormat(query);
+            }
+
+            yield return new IndexerRequest(url.ToString(), HttpAccept.Rss);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Indexers/Waffles/WafflesSettings.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.Indexers.Waffles
+{
+    public class WafflesSettingsValidator : AbstractValidator<WafflesSettings>
+    {
+        public WafflesSettingsValidator()
+        {
+            RuleFor(c => c.BaseUrl).ValidRootUrl();
+            RuleFor(c => c.UserId).NotEmpty();
+            RuleFor(c => c.RssPasskey).NotEmpty();
+        }
+    }
+
+    public class WafflesSettings : IProviderConfig
+    {
+        private static readonly WafflesSettingsValidator Validator = new WafflesSettingsValidator();
+
+        public WafflesSettings()
+        {
+            BaseUrl = "https://www.waffles.ch";
+        }
+
+        [FieldDefinition(0, Label = "Website URL")]
+        public string BaseUrl { get; set; }
+
+        [FieldDefinition(1, Label = "UserId")]
+        public string UserId { get; set; }
+
+        [FieldDefinition(2, Label = "RSS Passkey")]
+        public string RssPasskey { get; set; }
+
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
