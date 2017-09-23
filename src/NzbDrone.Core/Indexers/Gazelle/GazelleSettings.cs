1322633d0d71801ab4bd1579f94a2b83e3a8b965
Qstick <qstick@gmail.com>
2017-09-22 22:48:15 -0400

    Add Support for Gazelle based indexers

---
+++ src/NzbDrone.Core/Indexers/Gazelle/GazelleSettings.cs
@@ -0,0 +1,45 @@
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+using System.Text.RegularExpressions;
+
+namespace NzbDrone.Core.Indexers.Gazelle
+{
+    public class GazelleSettingsValidator : AbstractValidator<GazelleSettings>
+    {
+        public GazelleSettingsValidator()
+        {
+            RuleFor(c => c.BaseUrl).ValidRootUrl();
+            RuleFor(c => c.Username).NotEmpty();
+            RuleFor(c => c.Password).NotEmpty();
+        }
+    }
+
+    public class GazelleSettings : IProviderConfig
+    {
+        private static readonly GazelleSettingsValidator Validator = new GazelleSettingsValidator();
+
+        public GazelleSettings()
+        {
+
+        }
+
+        public string AuthKey;
+        public string PassKey;
+
+        [FieldDefinition(0, Label = "URL", Advanced = true, HelpText = "Do not change this unless you know what you're doing. Since your cookie will be sent to that host.")]
+        public string BaseUrl { get; set; }
+
+        [FieldDefinition(1, Label = "Username", HelpText = "Username")]
+        public string Username { get; set; }
+
+        [FieldDefinition(2, Label = "Password", Type = FieldType.Password, HelpText = "Password")]
+        public string Password { get; set; }
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
