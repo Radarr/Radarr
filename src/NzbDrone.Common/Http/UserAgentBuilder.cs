Sonarr/Sonarr

New: Better platform detection specifically for Non-Windows Systems

src/NzbDrone.Common/Http/UserAgentBuilder.cs
@@ -2,19 +2,33 @@ using NzbDrone.Common.EnvironmentInfo;
 
 namespace NzbDrone.Common.Http
 {
-    public static class UserAgentBuilder
+    public interface IUserAgentBuilder
     {
-        public static string UserAgent { get; private set; }
-        public static string UserAgentSimplified { get; private set; }
+        string GetUserAgent(bool simplified = false);
+    }
 
-        static UserAgentBuilder()
+    public class UserAgentBuilder : IUserAgentBuilder
+    {
+        private readonly string _userAgentSimplified;
+        private readonly string _userAgent;
+
+        public string GetUserAgent(bool simplified)
         {
-            UserAgent = string.Format("Sonarr/{0} ({1} {2})",
-                BuildInfo.Version,
-                OsInfo.Os, OsInfo.Version.ToString(2));
+            if (simplified)
+            {
+                return _userAgentSimplified;
+            }
 
-            UserAgentSimplified = string.Format("Sonarr/{0}",
-                BuildInfo.Version.ToString(2));
+            return _userAgent;
+        }
+
+        public UserAgentBuilder(IOsInfo osInfo)
+        {
+            var osName = osInfo.Name.ToLower();
+            var osVersion = osInfo.Version.ToLower();
+
+            _userAgent = $"Sonarr/{BuildInfo.Version} ({osName} {osVersion})";
+            _userAgentSimplified = $"Sonarr/{BuildInfo.Version.ToString(2)}";
         }
     }
 }

src/NzbDrone.Core/Rest/RestClientFactory.cs
@@ -7,12 +7,11 @@ namespace NzbDrone.Core.Rest
     {
         public static RestClient BuildClient(string baseUrl)
         {
-            var restClient = new RestClient(baseUrl);
+            var restClient = new RestClient(baseUrl)
+            {
+                UserAgent = $"Sonarr/{BuildInfo.Version} ({OsInfo.Os})"
+            };
 
-            restClient.UserAgent = string.Format("Sonarr/{0} (RestSharp/{1}; {2}/{3})",
-                BuildInfo.Version,
-                restClient.GetType().Assembly.GetName().Version,
-                OsInfo.Os, OsInfo.Version.ToString(2));
 
             return restClient;
         }

Radarr/Radarr

(cherry picked from commit ad7d571b24f05866961eece1d0d80582f0c0d57a)

src/NzbDrone.Common/Http/UserAgentBuilder.cs
@@ -2,19 +2,33 @@ using NzbDrone.Common.EnvironmentInfo;
 
 namespace NzbDrone.Common.Http
 {
-    public static class UserAgentBuilder
+    public interface IUserAgentBuilder
     {
-        public static string UserAgent { get; private set; }
-        public static string UserAgentSimplified { get; private set; }
+        string GetUserAgent(bool simplified = false);
+    }
 
-        static UserAgentBuilder()
+    public class UserAgentBuilder : IUserAgentBuilder
+    {
+        private readonly string _userAgentSimplified;
+        private readonly string _userAgent;
+
+        public string GetUserAgent(bool simplified)
         {
-            UserAgent = string.Format("Radarr/{0} ({1} {2})",
-                BuildInfo.Version,
-                OsInfo.Os, OsInfo.Version.ToString(2));
+            if (simplified)
+            {
+                return _userAgentSimplified;
+            }
 
-            UserAgentSimplified = string.Format("Radarr/{0}",
-                BuildInfo.Version.ToString(2));
+            return _userAgent;
+        }
+
+        public UserAgentBuilder(IOsInfo osInfo)
+        {
+            var osName = osInfo.Name.ToLower();
+            var osVersion = osInfo.Version.ToLower();
+
+            _userAgent = $"Radarr/{BuildInfo.Version} ({osName} {osVersion})";
+            _userAgentSimplified = $"Radarr/{BuildInfo.Version.ToString(2)}";
         }
     }
 }

src/NzbDrone.Core/Rest/RestClientFactory.cs
@@ -7,12 +7,10 @@ namespace NzbDrone.Core.Rest
     {
         public static RestClient BuildClient(string baseUrl)
         {
-            var restClient = new RestClient(baseUrl);
-
-            restClient.UserAgent = string.Format("Radarr/{0} (RestSharp/{1}; {2}/{3})",
-                BuildInfo.Version,
-                restClient.GetType().Assembly.GetName().Version,
-                OsInfo.Os, OsInfo.Version.ToString(2));
+            var restClient = new RestClient(baseUrl)
+            {
+                UserAgent = $"Radarr/{BuildInfo.Version} ({OsInfo.Os})"
+            };
 
             return restClient;
         }