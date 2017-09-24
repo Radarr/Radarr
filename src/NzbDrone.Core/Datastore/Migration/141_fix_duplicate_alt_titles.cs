25b0a52ba6851ddaab6c89b0a940e9ccfba7eebb
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-09-24 16:17:43 +0200

    Fixed: Alternative Titles appearing multiple times. Duplicate alt titles will be deleted. Fixes #2040

---
+++ src/NzbDrone.Core/Datastore/Migration/141_fix_duplicate_alt_titles.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Collections.Generic;
+using System.Data;
+using FluentMigrator;
+using Newtonsoft.Json.Linq;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(141)]
+    public class fix_duplicate_alt_titles : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.WithConnection(RenameUrlToBaseUrl);
+            Alter.Table("AlternativeTitles").AlterColumn("CleanTitle").AsString().Unique();
+        }
+
+        private void RenameUrlToBaseUrl(IDbConnection conn, IDbTransaction tran)
+        {
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.Transaction = tran;
+                cmd.CommandText = "DELETE FROM AlternativeTitles WHERE rowid NOT IN ( SELECT MIN(rowid) FROM AlternativeTitles GROUP BY CleanTitle )";
+
+                cmd.ExecuteNonQuery();
+                
+            }
+        }
+    }
+}
