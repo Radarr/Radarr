d99fd151e34255c0ef352ea31db1da984be74c9e
Qstick <qstick@gmail.com>
2017-09-27 21:35:09 -0400

    Fix regression in Indexer Edit Modal

--- frontend/src/Settings/Indexers/Indexers/EditIndexerModalConnector.js
+++ frontend/src/Settings/Indexers/Indexers/EditIndexerModalConnector.js
@@ -10,7 +10,7 @@ function createMapDispatchToProps(dispatch, props) {
 
   return {
     dispatchClearPendingChanges() {
-      dispatch(clearPendingChanges)({ section });
+      dispatch(clearPendingChanges({ section }));
     },
 
     dispatchCancelTestIndexer() {
