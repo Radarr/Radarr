4c916f91865f64b0c81684b9f3c2c253d0077a6d
Qstick <qstick@gmail.com>
2017-10-07 23:26:45 -0400

    Fix HealthCheck Warning for Artist Folders with 'ro'

--- src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -1,33 +1,33 @@
-﻿using System.Linq;
+using System.Linq;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
-using NzbDrone.Core.Tv;
+using NzbDrone.Core.Music;
 
 namespace NzbDrone.Core.HealthCheck.Checks
 {
     public class MountCheck : HealthCheckBase
     {
         private readonly IDiskProvider _diskProvider;
-        private readonly ISeriesService _seriesService;
+        private readonly IArtistService _artistService;
 
-        public MountCheck(IDiskProvider diskProvider, ISeriesService seriesService)
+        public MountCheck(IDiskProvider diskProvider, IArtistService artistService)
         {
             _diskProvider = diskProvider;
-            _seriesService = seriesService;
+            _artistService = artistService;
         }
 
         public override HealthCheck Check()
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
-            var mounts = _seriesService.GetAllSeries()
-                                       .Select(series => _diskProvider.GetMount(series.Path))
+            var mounts = _artistService.GetAllArtists()
+                                       .Select(artist => _diskProvider.GetMount(artist.Path))
+                                       .Where(m => m != null && m.MountOptions != null && m.MountOptions.IsReadOnly)
                                        .DistinctBy(m => m.RootDirectory)
-                                       .Where(m => m.MountOptions != null && m.MountOptions.IsReadOnly)
                                        .ToList();
 
             if (mounts.Any())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Error, "Mount containing a series path is mounted read-only: " + string.Join(",", mounts.Select(m => m.Name)), "#series-mount-ro");
+                return new HealthCheck(GetType(), HealthCheckResult.Error, "Mount containing a artist path is mounted read-only: " + string.Join(",", mounts.Select(m => m.Name)), "#artist-mount-ro");
             }
 
             return new HealthCheck(GetType());
