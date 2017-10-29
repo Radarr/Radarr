99ab61fac667f02791d440178039f18731498676
Qstick <qstick@gmail.com>
2017-10-28 23:49:54 -0400

    Fixed: Slower daemon startup loop if Lidarr runs into non-recoverable errors

--- src/NzbDrone.Common/EnvironmentInfo/AppFolderFactory.cs
+++ src/NzbDrone.Common/EnvironmentInfo/AppFolderFactory.cs
@@ -3,6 +3,7 @@ using System.Security.AccessControl;
 using System.Security.Principal;
 using NLog;
 using NzbDrone.Common.Disk;
+using NzbDrone.Common.Exceptions;
 using NzbDrone.Common.Instrumentation;
 
 namespace NzbDrone.Common.EnvironmentInfo
@@ -35,6 +36,11 @@ namespace NzbDrone.Common.EnvironmentInfo
             if (OsInfo.IsWindows)
             {
                 SetPermissions();
+            }
+
+            if (!_diskProvider.FolderWritable(_appFolderInfo.AppDataFolder))
+            {
+                throw new LidarrStartupException("AppFolder {0} is not writable", _appFolderInfo.AppDataFolder);
             }
         }
 
