26935bd82fcee15d6224b6f91d6c16f941407ff5
Qstick <qstick@gmail.com>
2017-11-16 13:35:11 -0500

    Fixed: Flaky Housekeeper Tests

--- src/NzbDrone.Core.Test/Housekeeping/Housekeepers/FixFutureDownloadClientStatusTimesFixture.cs
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/FixFutureDownloadClientStatusTimesFixture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using FizzWare.NBuilder;
@@ -60,7 +60,7 @@ namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
             Mocker.GetMock<IDownloadClientStatusRepository>()
                   .Verify(v => v.UpdateMany(
                           It.Is<List<DownloadClientStatus>>(i => i.All(
-                              s => s.InitialFailure.Value < DateTime.UtcNow))
+                              s => s.InitialFailure.Value <= DateTime.UtcNow))
                       )
                   );
         }
@@ -85,7 +85,7 @@ namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
             Mocker.GetMock<IDownloadClientStatusRepository>()
                   .Verify(v => v.UpdateMany(
                           It.Is<List<DownloadClientStatus>>(i => i.All(
-                              s => s.MostRecentFailure.Value < DateTime.UtcNow))
+                              s => s.MostRecentFailure.Value <= DateTime.UtcNow))
                       )
                   );
         }
