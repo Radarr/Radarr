532e14040bae5e954f3fc17e35ee97e06b79c88b
Qstick <qstick@gmail.com>
2017-12-14 23:49:07 -0500

    Fix: DatabaseCorruptException should slow startup loop

--- src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
+++ src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using NzbDrone.Common.Exceptions;
 
 namespace NzbDrone.Core.Datastore
 {
-    public class CorruptDatabaseException : NzbDroneException
+    public class CorruptDatabaseException : LidarrStartupException
     {
         public CorruptDatabaseException(string message, params object[] args) : base(message, args)
         {
