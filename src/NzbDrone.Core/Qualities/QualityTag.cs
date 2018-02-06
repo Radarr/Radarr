1a19f1366f70831d9b950b620dc9982ed38b3b97
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-02-06 17:32:17 +0100

    First impl.

---
+++ src/NzbDrone.Core/Qualities/QualityTag.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.Qualities
+{
+    public class QualityTag
+    {
+        public string Raw { get; set; }
+        public TagType TagType { get; set; }
+        public TagModifier TagModifier { get; set; }
+        public object Value { get; set; }
+        
+        public QualityTag(string raw)
+        {
+            Raw = raw;
+        }
+
+        public bool DoesItMatch(ParsedMovieInfo movieInfo)
+        {
+            switch (TagType)
+            {
+                case TagType.Edition:
+                    string stringValue = (string) Value;
+                    return movieInfo.Edition.Contains(stringValue.Remove(' ').ToLower());
+                default:
+                    return false;
+            }
+        }
+
+    }
+
+    public enum TagType
+    {
+        Resolution = 1,
+        Source = 2,
+        Modifiers = 4,
+        Edition = 8,
+        Language = 16,
+        Custom = 32
+    }
+
+    [Flags]
+    public enum TagModifier
+    {
+        Regex = 1,
+        Not = 2, // Do not match
+        AbsolutelyRequired = 4
+    }
+}
