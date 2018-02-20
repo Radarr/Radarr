1ff9ac9e50eda15bb1d5970265f9ffca2d4b13ba
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-02-20 22:13:50 +0100

    Made some ui additions, such as a legend and grouping of quality tag matches.

--- src/UI/Quality/MatchesCell.js
+++ src/UI/Quality/MatchesCell.js
@@ -1,8 +1,48 @@
 var TemplatedCell = require('../Cells/TemplatedCell');
+var _ = require('underscore');
 
 module.exports = TemplatedCell.extend({
     className : 'matches-cell',
-    template  : 'Quality/MatchesCell'
+    template  : 'Quality/MatchesCell',
+    _orig : TemplatedCell.prototype.initialize,
+
+    initialize : function() {
+        this._orig.apply(this, arguments);
+        var groups = {};
+        _.each(this.cellValue.attributes, function(value, key, obj){
+           var groupKey = key[0];
+           switch (groupKey)
+           {
+               case "s":
+                   groupKey = "Source";
+                       break;
+               case "r":
+                   groupKey = "Resolution";
+                   break;
+               case "m":
+                   groupKey = "Modifier";
+                   break;
+               case "c":
+                   groupKey = "Custom";
+                   break;
+               case "l":
+                   groupKey = "Language";
+                   break;
+               case "e":
+                   groupKey = "Edition";
+                   break;
+           }
 
 
+           if (groups[groupKey] === undefined) {
+               groups[groupKey] = { matches : [], ok : false};
+           }
+            groups[groupKey].matches.push({ "value" : key.toUpperCase(), "ok" : value});
+            if (value === true) {
+                groups[groupKey].ok = true;
+            }
+        });
+
+        this.cellValue.attributes.groups = groups;
+    }
 });
