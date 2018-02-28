59a176f03b1527433c22fa18f0b4ce9fe32eec4d
Qstick <qstick@gmail.com>
2018-02-27 22:43:16 -0500

    Fixed: Temp Workaround for Appveyor Cake Issue

--- build-appveyor.ps1
+++ build-appveyor.ps1
@@ -180,5 +180,5 @@ if (!(Test-Path $CAKE_EXE)) {
 
 # Start Cake
 Write-Host "Running build script..."
-Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
+Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -settings_skipverification=true -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
 exit $LASTEXITCODE
