c08c2eb2f82550f3e408d9412358b0d1d56ba92c
Qstick <qstick@gmail.com>
2018-03-17 10:55:16 -0400

    Fixed: Jumpbar uses incorrect property

--- frontend/src/Utilities/Array/getIndexOfFirstCharacter.js
+++ frontend/src/Utilities/Array/getIndexOfFirstCharacter.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 export default function getIndexOfFirstCharacter(items, character) {
   return _.findIndex(items, (item) => {
-    const firstCharacter = item.sortTitle.charAt(0);
+    const firstCharacter = item.sortName.charAt(0);
 
     if (character === '#') {
       return !isNaN(firstCharacter);
