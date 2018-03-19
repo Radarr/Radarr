f3e1f376fae766762446466876b54c077bc082c3
Qstick <qstick@gmail.com>
2018-03-18 21:22:23 -0400

    Updated DB Migration to 145, PTP is 144

--- src/NzbDrone.Core/Datastore/Migration/145_banner_to_fanart.cs
+++ src/NzbDrone.Core/Datastore/Migration/145_banner_to_fanart.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(144)]
+    [Migration(145)]
     public class banner_to_fanart : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
