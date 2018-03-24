f7c48f7f5555b6928a89ead688570d1f7e700a91
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-24 15:53:09 +0100

    Fixed: Revised handling of cookies in case of redirects.

--- src/NzbDrone.Common/Http/HttpRequest.cs
+++ src/NzbDrone.Common/Http/HttpRequest.cs
@@ -13,6 +13,7 @@ namespace NzbDrone.Common.Http
             Url = new HttpUri(url);
             Headers = new HttpHeader();
             AllowAutoRedirect = true;
+            StoreRequestCookie = true;
             Cookies = new Dictionary<string, string>();
             
             if (!RuntimeInfoBase.IsProduction)
@@ -37,6 +38,7 @@ namespace NzbDrone.Common.Http
         public bool ConnectionKeepAlive { get; set; }
         public bool LogResponseContent { get; set; }
         public Dictionary<string, string> Cookies { get; private set; }
+        public bool StoreRequestCookie { get; set; }
         public bool StoreResponseCookie { get; set; }
         public TimeSpan RequestTimeout { get; set; }
         public TimeSpan RateLimit { get; set; }
