1843f1d46148a190f8070900e472ae06a8fd3184
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-28 19:04:48 +0200

    Fixed: tinyioc failing, since we use quality definitions in the construcctor.

--- src/NzbDrone.Core/Organizer/FileNameSampleService.cs
+++ src/NzbDrone.Core/Organizer/FileNameSampleService.cs
@@ -18,61 +18,73 @@ namespace NzbDrone.Core.Organizer
         private readonly IBuildFileNames _buildFileNames;
         private readonly IQualityDefinitionService _definitionService;
 
+        public MovieFile MovieFile
+        {
+            get
+            {
+                if (_movieFile == null)
+                {
+                    _movieFile = new MovieFile
+                    {
+                        Quality = new QualityModel(_definitionService.Get(Quality.Bluray1080p), new Revision(2)),
+                        RelativePath = "The.Movie.Title.2010.1080p.BluRay.DTS.x264-EVOLVE.mkv",
+                        SceneName = "The.Movie.Title.2010.1080p.BluRay.DTS.x264-EVOLVE",
+                        ReleaseGroup = "EVOLVE",
+                        MediaInfo = new MediaInfoModel()
+                        {
+                            VideoCodec = "AVC",
+                            VideoBitDepth = 8,
+                            AudioFormat = "DTS",
+                            AudioChannels = 6,
+                            AudioChannelPositions = "3/2/0.1",
+                            AudioLanguages = "English",
+                            Subtitles = "English/German"
+                        },
+                        Edition = "Ultimate extended edition",
+                    };
+                }
+
+                return _movieFile;
+            }
+            set => _movieFile = value;
+        }
+
         private static MovieFile _movieFile;
+
+        public Movie Movie
+        {
+            get
+            {
+                if (_movie == null)
+                {
+                    _movie = new Movie
+                    {
+                        Title = "The Movie: Title",
+                        Year = 2010,
+                        ImdbId = "tt0066921",
+                        MovieFile = MovieFile,
+                        MovieFileId = 1,
+                    };
+                }
+
+                return _movie;
+            }
+            set => _movie = value;
+        }
+
         private static Movie _movie;
 
         public FileNameSampleService(IBuildFileNames buildFileNames, IQualityDefinitionService qualityDefinitionService)
         {
             _buildFileNames = buildFileNames;
             _definitionService = qualityDefinitionService;
-
-            var mediaInfo = new MediaInfoModel()
-            {
-                VideoCodec = "AVC",
-                VideoBitDepth = 8,
-                AudioFormat = "DTS",
-                AudioChannels = 6,
-                AudioChannelPositions = "3/2/0.1",
-                AudioLanguages = "English",
-                Subtitles = "English/German"
-            };
-
-            var mediaInfoAnime = new MediaInfoModel()
-            {
-                VideoCodec = "AVC",
-                VideoBitDepth = 8,
-                AudioFormat = "DTS",
-                AudioChannels = 6,
-                AudioChannelPositions = "3/2/0.1",
-                AudioLanguages = "Japanese",
-                Subtitles = "Japanese/English"
-            };
-
-            _movieFile = new MovieFile
-            {
-                Quality = new QualityModel(_definitionService.Get(Quality.Bluray1080p), new Revision(2)),
-                RelativePath = "The.Movie.Title.2010.1080p.BluRay.DTS.x264-EVOLVE.mkv",
-                SceneName = "The.Movie.Title.2010.1080p.BluRay.DTS.x264-EVOLVE",
-                ReleaseGroup = "EVOLVE",
-                MediaInfo = mediaInfo,
-                Edition = "Ultimate extended edition",
-            };
-
-            _movie = new Movie
-            {
-                Title = "The Movie: Title",
-                Year = 2010,
-                ImdbId = "tt0066921",
-                MovieFile = _movieFile,
-                MovieFileId = 1,
-            };
         }
 
         public SampleResult GetMovieSample(NamingConfig nameSpec)
         {
             var result = new SampleResult
             {
-                FileName = BuildSample(_movie, _movieFile, nameSpec),
+                FileName = BuildSample(Movie, MovieFile, nameSpec),
             };
 
             return result;
@@ -80,7 +92,7 @@ namespace NzbDrone.Core.Organizer
 
         public string GetMovieFolderSample(NamingConfig nameSpec)
         {
-            return _buildFileNames.GetMovieFolder(_movie, nameSpec);
+            return _buildFileNames.GetMovieFolder(Movie, nameSpec);
         }
 
         private string BuildSample(Movie movie, MovieFile movieFile, NamingConfig nameSpec)
