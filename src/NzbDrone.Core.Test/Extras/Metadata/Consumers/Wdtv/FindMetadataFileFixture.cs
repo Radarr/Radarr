449a1e826d5f1c3e76c0551b99b1a61a01f04b56
Qstick <qstick@gmail.com>
2018-04-01 21:04:52 -0400

    Fixed: Remove Tests for WDTV Artist and Album Image, Same as XBMC
    Fixes #267

--- src/NzbDrone.Core.Test/Extras/Metadata/Consumers/Wdtv/FindMetadataFileFixture.cs
+++ src/NzbDrone.Core.Test/Extras/Metadata/Consumers/Wdtv/FindMetadataFileFixture.cs
@@ -31,16 +31,6 @@ namespace NzbDrone.Core.Test.Extras.Metadata.Consumers.Wdtv
             Subject.FindMetadataFile(_artist, path).Should().BeNull();
         }
 
-        [TestCase("Specials")]
-        [TestCase("specials")]
-        [TestCase("Season 1")]
-        public void should_return_album_image(string folder)
-        {
-            var path = Path.Combine(_artist.Path, folder, "folder.jpg");
-
-            Subject.FindMetadataFile(_artist, path).Type.Should().Be(MetadataType.AlbumImage);
-        }
-
         [TestCase(".xml", MetadataType.TrackMetadata)]
         public void should_return_metadata_for_track_if_valid_file_for_track(string extension, MetadataType type)
         {
@@ -56,14 +46,6 @@ namespace NzbDrone.Core.Test.Extras.Metadata.Consumers.Wdtv
             var path = Path.Combine(_artist.Path, "the.artist.track" + extension);
 
             Subject.FindMetadataFile(_artist, path).Should().BeNull();
-        }
-
-        [Test]
-        public void should_return_artist_image_for_folder_jpg_in_artist_folder()
-        {
-            var path = Path.Combine(_artist.Path, "folder.jpg");
-
-            Subject.FindMetadataFile(_artist, path).Type.Should().Be(MetadataType.ArtistImage);
         }
     }
 }
