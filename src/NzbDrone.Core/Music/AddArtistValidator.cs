321d99a59719535ed5f4744a17a62c35d63205d4
Qstick <qstick@gmail.com>
2018-04-07 22:43:10 -0400

    Added: Validate Set Profiles Exist when Adding Artist
    Fixes #281

--- src/NzbDrone.Core/Music/AddArtistValidator.cs
+++ src/NzbDrone.Core/Music/AddArtistValidator.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Music
 {
@@ -17,13 +18,22 @@ namespace NzbDrone.Core.Music
     {
         public AddArtistValidator(RootFolderValidator rootFolderValidator,
                                   ArtistPathValidator artistPathValidator,
-                                  ArtistAncestorValidator artistAncestorValidator)
+                                  ArtistAncestorValidator artistAncestorValidator,
+                                  ProfileExistsValidator profileExistsValidator,
+                                  LanguageProfileExistsValidator languageProfileExistsValidator,
+                                  MetadataProfileExistsValidator metadataProfileExistsValidator)
         {
             RuleFor(c => c.Path).Cascade(CascadeMode.StopOnFirstFailure)
                                 .IsValidPath()
                                 .SetValidator(rootFolderValidator)
                                 .SetValidator(artistPathValidator)
                                 .SetValidator(artistAncestorValidator);
+
+            RuleFor(c => c.ProfileId).SetValidator(profileExistsValidator);
+
+            RuleFor(c => c.LanguageProfileId).SetValidator(languageProfileExistsValidator);
+
+            RuleFor(c => c.MetadataProfileId).SetValidator(metadataProfileExistsValidator);
 
         }
     }
