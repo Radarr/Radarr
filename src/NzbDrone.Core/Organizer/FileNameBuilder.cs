2199b63e1fbd9a514e9a17a069bcdf9c6f0c8913
Qstick <qstick@gmail.com>
2018-04-14 22:10:20 -0400

    fixup! Change to Enum, fix quotes

--- src/NzbDrone.Core/Organizer/FileNameBuilder.cs
+++ src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -287,7 +287,26 @@ namespace NzbDrone.Core.Organizer
         public static string CleanFileName(string name, NamingConfig namingConfig)
         {
             bool replace = namingConfig.ReplaceIllegalCharacters;
-            string colonReplacementFormat = namingConfig.ColonReplacementFormat;
+            string colonReplacementFormat;
+
+            switch ((ColonReplacementFormat) namingConfig.ColonReplacementFormat)
+            {
+                case ColonReplacementFormat.Delete:
+                    colonReplacementFormat = "";
+                    break;
+                case ColonReplacementFormat.Dash:
+                    colonReplacementFormat = "-";
+                    break;
+                case ColonReplacementFormat.SpaceDash:
+                    colonReplacementFormat = " -";
+                    break;
+                case ColonReplacementFormat.SpaceDashSpace:
+                    colonReplacementFormat = " - ";
+                    break;
+                default:
+                    colonReplacementFormat = "";
+                    break;
+            }
 
             string result = name;
             string[] badCharacters = { "\\", "/", "<", ">", "?", "*", ":", "|", "\"" };
@@ -662,5 +681,13 @@ namespace NzbDrone.Core.Organizer
         Scene = 3,
         Range = 4,
         PrefixedRange = 5
+    }
+
+    public enum ColonReplacementFormat
+    {
+        Delete = 0,
+        Dash = 1,
+        SpaceDash = 2,
+        SpaceDashSpace = 3
     }
 }
