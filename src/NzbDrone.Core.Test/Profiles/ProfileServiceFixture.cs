831fa0b13731dee6180b65f7da256fb48edd1c52
Qstick <qstick@gmail.com>
2018-05-20 23:07:42 -0400

    Fixed: Profile delete tests

--- src/NzbDrone.Core.Test/Profiles/ProfileServiceFixture.cs
+++ src/NzbDrone.Core.Test/Profiles/ProfileServiceFixture.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using FizzWare.NBuilder;
 using Moq;
 using NUnit.Framework;
@@ -6,6 +6,7 @@ using NzbDrone.Core.Lifecycle;
 using NzbDrone.Core.Profiles;
 using NzbDrone.Core.Test.Framework;
 using NzbDrone.Core.Movies;
+using NzbDrone.Core.NetImport;
 
 namespace NzbDrone.Core.Test.Profiles
 {
@@ -46,8 +47,35 @@ namespace NzbDrone.Core.Test.Profiles
                                             .With(c => c.ProfileId = 2)
                                             .Build().ToList();
 
+            var netImportList = Builder<NetImportDefinition>.CreateListOfSize(3)
+                                                            .All()
+                                                            .With(c => c.ProfileId = 1)
+                                                            .Build().ToList();
 
             Mocker.GetMock<IMovieService>().Setup(c => c.GetAllMovies()).Returns(movieList);
+            Mocker.GetMock<INetImportFactory>().Setup(c => c.All()).Returns(netImportList);
+
+            Assert.Throws<ProfileInUseException>(() => Subject.Delete(2));
+
+            Mocker.GetMock<IProfileRepository>().Verify(c => c.Delete(It.IsAny<int>()), Times.Never());
+
+        }
+
+        [Test]
+        public void should_not_be_able_to_delete_profile_if_assigned_to_list()
+        {
+            var movieList = Builder<Movie>.CreateListOfSize(3)
+                .All()
+                .With(c => c.ProfileId = 1)
+                .Build().ToList();
+
+            var netImportList = Builder<NetImportDefinition>.CreateListOfSize(3)
+                .Random(1)
+                .With(c => c.ProfileId = 2)
+                .Build().ToList();
+
+            Mocker.GetMock<IMovieService>().Setup(c => c.GetAllMovies()).Returns(movieList);
+            Mocker.GetMock<INetImportFactory>().Setup(c => c.All()).Returns(netImportList);
 
             Assert.Throws<ProfileInUseException>(() => Subject.Delete(2));
 
@@ -57,15 +85,20 @@ namespace NzbDrone.Core.Test.Profiles
 
 
         [Test]
-        public void should_delete_profile_if_not_assigned_to_movie()
+        public void should_delete_profile_if_not_assigned_to_movie_or_list()
         {
             var movieList = Builder<Movie>.CreateListOfSize(3)
                                             .All()
                                             .With(c => c.ProfileId = 2)
                                             .Build().ToList();
 
+            var netImportList = Builder<NetImportDefinition>.CreateListOfSize(3)
+                                                            .All()
+                                                            .With(c => c.ProfileId = 2)
+                                                            .Build().ToList();
 
             Mocker.GetMock<IMovieService>().Setup(c => c.GetAllMovies()).Returns(movieList);
+            Mocker.GetMock<INetImportFactory>().Setup(c => c.All()).Returns(netImportList);
 
             Subject.Delete(1);
 
