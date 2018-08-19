a56e2edb74ba97afd4b6d7fc7495951b3b9c83b8
Qstick <qstick@gmail.com>
2018-08-18 21:48:32 -0400

    Fixed: Concurrent manual imports silently failing
    Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

--- src/NzbDrone.Core/MediaFiles/TrackImport/Manual/ManualImportFile.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/Manual/ManualImportFile.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections.Generic;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Languages;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport.Manual
 {
-    public class ManualImportFile
+    public class ManualImportFile : IEquatable<ManualImportFile>
     {
         public string Path { get; set; }
         public string FolderName { get; set; }
@@ -14,5 +16,30 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Manual
         public QualityModel Quality { get; set; }
         public Language Language { get; set; }
         public string DownloadId { get; set; }
+
+        public bool Equals(ManualImportFile other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return Path.PathEquals(other.Path);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Path.PathEquals(((ManualImportFile)obj).Path);
+        }
+        public override int GetHashCode()
+        {
+            return Path != null ? Path.GetHashCode() : 0;
+        }
     }
 }
