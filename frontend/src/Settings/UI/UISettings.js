9611df7e9ee1e6f9153d78fd7ab581816535681f
Qstick <qstick@gmail.com>
2018-09-15 21:49:08 -0400

    Changed: Export Calendar options in UISettings

--- frontend/src/Settings/UI/UISettings.js
+++ frontend/src/Settings/UI/UISettings.js
@@ -11,6 +11,37 @@ import FormGroup from 'Components/Form/FormGroup';
 import FormLabel from 'Components/Form/FormLabel';
 import FormInputGroup from 'Components/Form/FormInputGroup';
 
+export const firstDayOfWeekOptions = [
+  { key: 0, value: 'Sunday' },
+  { key: 1, value: 'Monday' }
+];
+
+export const weekColumnOptions = [
+  { key: 'ddd M/D', value: 'Tue 3/25' },
+  { key: 'ddd MM/DD', value: 'Tue 03/25' },
+  { key: 'ddd D/M', value: 'Tue 25/03' },
+  { key: 'ddd DD/MM', value: 'Tue 25/03' }
+];
+
+const shortDateFormatOptions = [
+  { key: 'MMM D YYYY', value: 'Mar 25 2014' },
+  { key: 'DD MMM YYYY', value: '25 Mar 2014' },
+  { key: 'MM/D/YYYY', value: '03/25/2014' },
+  { key: 'MM/DD/YYYY', value: '03/25/2014' },
+  { key: 'DD/MM/YYYY', value: '25/03/2014' },
+  { key: 'YYYY-MM-DD', value: '2014-03-25' }
+];
+
+const longDateFormatOptions = [
+  { key: 'dddd, MMMM D YYYY', value: 'Tuesday, March 25, 2014' },
+  { key: 'dddd, D MMMM YYYY', value: 'Tuesday, 25 March, 2014' }
+];
+
+export const timeFormatOptions = [
+  { key: 'h(:mm)a', value: '5pm/5:30pm' },
+  { key: 'HH:mm', value: '17:00/17:30' }
+];
+
 class UISettings extends Component {
 
   //
@@ -26,37 +57,6 @@ class UISettings extends Component {
       onSavePress,
       ...otherProps
     } = this.props;
-
-    const firstDayOfWeekOptions = [
-      { key: 0, value: 'Sunday' },
-      { key: 1, value: 'Monday' }
-    ];
-
-    const weekColumnOptions = [
-      { key: 'ddd M/D', value: 'Tue 3/25' },
-      { key: 'ddd MM/DD', value: 'Tue 03/25' },
-      { key: 'ddd D/M', value: 'Tue 25/3' },
-      { key: 'ddd DD/MM', value: 'Tue 25/03' }
-    ];
-
-    const shortDateFormatOptions = [
-      { key: 'MMM D YYYY', value: 'Mar 25 2014' },
-      { key: 'DD MMM YYYY', value: '25 Mar 2014' },
-      { key: 'MM/D/YYYY', value: '03/25/2014' },
-      { key: 'MM/DD/YYYY', value: '03/25/2014' },
-      { key: 'DD/MM/YYYY', value: '25/03/2014' },
-      { key: 'YYYY-MM-DD', value: '2014-03-25' }
-    ];
-
-    const longDateFormatOptions = [
-      { key: 'dddd, MMMM D YYYY', value: 'Tuesday, March 25, 2014' },
-      { key: 'dddd, D MMMM YYYY', value: 'Tuesday, 25 March, 2014' }
-    ];
-
-    const timeFormatOptions = [
-      { key: 'h(:mm)a', value: '5pm/5:30pm' },
-      { key: 'HH:mm', value: '17:00/17:30' }
-    ];
 
     return (
       <PageContent title="UI Settings">
