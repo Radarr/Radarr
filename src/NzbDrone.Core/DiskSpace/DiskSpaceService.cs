097fa34e8b3754bedb8e802545567430fa23d1d2
Ricardo Amaral <master@ricardoamaral.net>
2018-09-24 11:58:33 +0100

    Forgot to import namespace with 'Regex'

--- src/NzbDrone.Core/DiskSpace/DiskSpaceService.cs
+++ src/NzbDrone.Core/DiskSpace/DiskSpaceService.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NLog;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
