07cb844ce6ef126bf9cbfe8988dd0ca98f81478a
Ricardo Amaral <master@ricardoamaral.net>
2018-10-30 10:07:35 +0000

    Changed: Improve model and UI handling for lists. Should finally fix root folder errors. (#3133)
    Fixes #3036 (supposedly)

--- src/UI/Settings/NetImport/Edit/NetImportEditView.js
+++ src/UI/Settings/NetImport/Edit/NetImportEditView.js
@@ -44,11 +44,9 @@ var view = Marionette.ItemView.extend({
 		onRender : function() {
 				var rootFolder = this.model.get("rootFolderPath");
 				if (rootFolder !== "") {
-					//this.ui.rootFolder.val(rootFolder);
                     this.ui.rootFolder.children().filter(function() {
-                        //may want to use $.trim in here
-                        return $(this).text() === rootFolder;
-                    }).attr('selected', true);
+                        return $.trim($(this).text()) === rootFolder;
+                    }).prop('selected', true);
 				} else {
 					var defaultRoot = Config.getValue(Config.Keys.DefaultRootFolderId);
 					if (RootFolders.get(defaultRoot)) {
@@ -58,7 +56,7 @@ var view = Marionette.ItemView.extend({
 		},
 
 		_onBeforeSave : function() {
-			var profile = this.ui.profile.val();
+			var profile = parseInt(this.ui.profile.val(), 10);
 			var minAvail = this.ui.minimumAvailability.val();
 			var rootFolderPath = this.ui.rootFolder.children(':selected').text();
 			this.model.set({
