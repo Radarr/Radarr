d24b2d94e11c5089a3701dc1beb1c9056a4903ef
Ricardo Amaral <master@ricardoamaral.net>
2018-10-31 14:52:05 +0000

    Fix already imported movies with DB update

---
+++ src/NzbDrone.Core/Datastore/Migration/151_update_movie_files_media_info.cs
@@ -0,0 +1,14 @@
+using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(151)]
+    public class update_movie_files_media_info : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.Sql("UPDATE MovieFiles SET MediaInfo = REPLACE(MediaInfo, 'videoCodec', 'videoFormat')");
+        }
+    }
+}
