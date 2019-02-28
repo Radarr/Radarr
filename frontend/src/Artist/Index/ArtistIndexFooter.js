795a445b52e83e4f9941b300f83966066aadc1f1
Qstick <qstick@gmail.com>
2019-02-27 22:15:24 -0500

    Fixed: Rendering Tweaks to UI

--- frontend/src/Artist/Index/ArtistIndexFooter.js
+++ frontend/src/Artist/Index/ArtistIndexFooter.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import formatBytes from 'Utilities/Number/formatBytes';
 import { ColorImpairedConsumer } from 'App/ColorImpairedContext';
@@ -7,141 +7,148 @@ import DescriptionList from 'Components/DescriptionList/DescriptionList';
 import DescriptionListItem from 'Components/DescriptionList/DescriptionListItem';
 import styles from './ArtistIndexFooter.css';
 
-function ArtistIndexFooter({ artist }) {
-  const count = artist.length;
-  let tracks = 0;
-  let trackFiles = 0;
-  let ended = 0;
-  let continuing = 0;
-  let monitored = 0;
-  let totalFileSize = 0;
+class ArtistIndexFooter extends PureComponent {
 
-  artist.forEach((s) => {
-    const { statistics = {} } = s;
+  //
+  // Render
 
-    const {
-      trackCount = 0,
-      trackFileCount = 0,
-      sizeOnDisk = 0
-    } = statistics;
+  render() {
+    const { artist } = this.props;
+    const count = artist.length;
+    let tracks = 0;
+    let trackFiles = 0;
+    let ended = 0;
+    let continuing = 0;
+    let monitored = 0;
+    let totalFileSize = 0;
 
-    tracks += trackCount;
-    trackFiles += trackFileCount;
+    artist.forEach((s) => {
+      const { statistics = {} } = s;
 
-    if (s.status === 'ended') {
-      ended++;
-    } else {
-      continuing++;
-    }
+      const {
+        trackCount = 0,
+        trackFileCount = 0,
+        sizeOnDisk = 0
+      } = statistics;
 
-    if (s.monitored) {
-      monitored++;
-    }
+      tracks += trackCount;
+      trackFiles += trackFileCount;
 
-    totalFileSize += sizeOnDisk;
-  });
+      if (s.status === 'ended') {
+        ended++;
+      } else {
+        continuing++;
+      }
 
-  return (
-    <ColorImpairedConsumer>
-      {(enableColorImpairedMode) => {
-        return (
-          <div className={styles.footer}>
-            <div>
-              <div className={styles.legendItem}>
-                <div
-                  className={classNames(
-                    styles.continuing,
-                    enableColorImpairedMode && 'colorImpaired'
-                  )}
-                />
-                <div>Continuing (All tracks downloaded)</div>
+      if (s.monitored) {
+        monitored++;
+      }
+
+      totalFileSize += sizeOnDisk;
+    });
+
+    return (
+      <ColorImpairedConsumer>
+        {(enableColorImpairedMode) => {
+          return (
+            <div className={styles.footer}>
+              <div>
+                <div className={styles.legendItem}>
+                  <div
+                    className={classNames(
+                      styles.continuing,
+                      enableColorImpairedMode && 'colorImpaired'
+                    )}
+                  />
+                  <div>Continuing (All tracks downloaded)</div>
+                </div>
+
+                <div className={styles.legendItem}>
+                  <div
+                    className={classNames(
+                      styles.ended,
+                      enableColorImpairedMode && 'colorImpaired'
+                    )}
+                  />
+                  <div>Ended (All tracks downloaded)</div>
+                </div>
+
+                <div className={styles.legendItem}>
+                  <div
+                    className={classNames(
+                      styles.missingMonitored,
+                      enableColorImpairedMode && 'colorImpaired'
+                    )}
+                  />
+                  <div>Missing Tracks (Artist monitored)</div>
+                </div>
+
+                <div className={styles.legendItem}>
+                  <div
+                    className={classNames(
+                      styles.missingUnmonitored,
+                      enableColorImpairedMode && 'colorImpaired'
+                    )}
+                  />
+                  <div>Missing Tracks (Artist not monitored)</div>
+                </div>
               </div>
 
-              <div className={styles.legendItem}>
-                <div
-                  className={classNames(
-                    styles.ended,
-                    enableColorImpairedMode && 'colorImpaired'
-                  )}
-                />
-                <div>Ended (All tracks downloaded)</div>
-              </div>
+              <div className={styles.statistics}>
+                <DescriptionList>
+                  <DescriptionListItem
+                    title="Artist"
+                    data={count}
+                  />
 
-              <div className={styles.legendItem}>
-                <div
-                  className={classNames(
-                    styles.missingMonitored,
-                    enableColorImpairedMode && 'colorImpaired'
-                  )}
-                />
-                <div>Missing Tracks (Artist monitored)</div>
-              </div>
+                  <DescriptionListItem
+                    title="Ended"
+                    data={ended}
+                  />
 
-              <div className={styles.legendItem}>
-                <div
-                  className={classNames(
-                    styles.missingUnmonitored,
-                    enableColorImpairedMode && 'colorImpaired'
-                  )}
-                />
-                <div>Missing Tracks (Artist not monitored)</div>
+                  <DescriptionListItem
+                    title="Continuing"
+                    data={continuing}
+                  />
+                </DescriptionList>
+
+                <DescriptionList>
+                  <DescriptionListItem
+                    title="Monitored"
+                    data={monitored}
+                  />
+
+                  <DescriptionListItem
+                    title="Unmonitored"
+                    data={count - monitored}
+                  />
+                </DescriptionList>
+
+                <DescriptionList>
+                  <DescriptionListItem
+                    title="Tracks"
+                    data={tracks}
+                  />
+
+                  <DescriptionListItem
+                    title="Files"
+                    data={trackFiles}
+                  />
+                </DescriptionList>
+
+                <DescriptionList>
+                  <DescriptionListItem
+                    title="Total File Size"
+                    data={formatBytes(totalFileSize)}
+                  />
+                </DescriptionList>
               </div>
             </div>
-
-            <div className={styles.statistics}>
-              <DescriptionList>
-                <DescriptionListItem
-                  title="Artist"
-                  data={count}
-                />
-
-                <DescriptionListItem
-                  title="Ended"
-                  data={ended}
-                />
-
-                <DescriptionListItem
-                  title="Continuing"
-                  data={continuing}
-                />
-              </DescriptionList>
-
-              <DescriptionList>
-                <DescriptionListItem
-                  title="Monitored"
-                  data={monitored}
-                />
-
-                <DescriptionListItem
-                  title="Unmonitored"
-                  data={count - monitored}
-                />
-              </DescriptionList>
-
-              <DescriptionList>
-                <DescriptionListItem
-                  title="Tracks"
-                  data={tracks}
-                />
-
-                <DescriptionListItem
-                  title="Files"
-                  data={trackFiles}
-                />
-              </DescriptionList>
-
-              <DescriptionList>
-                <DescriptionListItem
-                  title="Total File Size"
-                  data={formatBytes(totalFileSize)}
-                />
-              </DescriptionList>
-            </div>
-          </div>
-        );
-      }}
-    </ColorImpairedConsumer>
-  );
+          );
+        }}
+      </ColorImpairedConsumer>
+    );
+  }
 }
 
 ArtistIndexFooter.propTypes = {
