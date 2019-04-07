0dac036fde7ac3c8394cc9530d646e55c80563e8
Stephan Renggli <stephanrenggli@gmail.com>
2019-04-07 18:28:53 +0200

    Added support for Gotify notifications

---
+++ src/NzbDrone.Core/Notifications/Gotify/Gotify.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Movies;
+
+namespace NzbDrone.Core.Notifications.Gotify
+{
+    public class Gotify : NotificationBase<GotifySettings>
+    {
+        private readonly IGotifyProxy _proxy;
+        
+        public Gotify(IGotifyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Link => "https://Gotify.net/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            const string title = "Movie Grabbed";
+
+            _proxy.SendNotification(title, grabMessage.Message, Settings);
+        }
+
+        public override void OnDownload(DownloadMessage message)
+        {
+            const string title = "Movie Downloaded";
+
+            _proxy.SendNotification(title, message.Message, Settings);
+        }
+
+        public override void OnMovieRename(Movie movie)
+        {
+        }
+		
+        public override string Name => "Gotify";
+
+        public override bool SupportsOnRename => false;
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}
