e4f1e5d376d4f193a2e930263f716d5294bdb248
Qstick <qstick@gmail.com>
2019-04-28 00:25:23 -0400

    Fixed: Backup path URL incorrect in some cases

--- frontend/src/System/Backup/BackupRow.js
+++ frontend/src/System/Backup/BackupRow.js
@@ -96,7 +96,7 @@ class BackupRow extends Component {
 
         <TableRowCell>
           <Link
-            to={path}
+            to={`${window.Lidarr.urlBase}${path}`}
             noRouter={true}
           >
             {name}
