d41ae7b172604b6980dbaf564accb3f33ac963ef
Taloth Saldono <Taloth@users.noreply.github.com>
2019-05-29 23:56:37 +0200

    Fixed: Transmission seeding idle time handling

--- src/NzbDrone.Core/Download/Clients/Transmission/TransmissionTorrent.cs
+++ src/NzbDrone.Core/Download/Clients/Transmission/TransmissionTorrent.cs
@@ -3,31 +3,23 @@ namespace NzbDrone.Core.Download.Clients.Transmission
     public class TransmissionTorrent
     {
         public int Id { get; set; }
-
         public string HashString { get; set; }
-
         public string Name { get; set; }
-
         public string DownloadDir { get; set; }
-
         public long TotalSize { get; set; }
-
         public long LeftUntilDone { get; set; }
-
         public bool IsFinished { get; set; }
-
         public int Eta { get; set; }
-
         public TransmissionTorrentStatus Status { get; set; }
-
         public int SecondsDownloading { get; set; }
-
+        public int SecondsSeeding { get; set; }
         public string ErrorString { get; set; }
-
         public long DownloadedEver { get; set; }
-
         public long UploadedEver { get; set; }
-
-        public long SeedRatioLimit { get; set; }
+        public double SeedRatioLimit { get; set; }
+        public int SeedRatioMode { get; set; }
+        public long SeedIdleLimit { get; set; }
+        public int SeedIdleMode { get; set; }
+        public int FileCount { get; set; }
     }
 }
