8f791abbf60eeb3096a99408bdd3954c87bd35a7
Qstick <qstick@gmail.com>
2019-08-01 23:08:25 -0400

    Fixed: Really Don't treat NZBs rejected by SABnzbd as successful

--- src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
+++ src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
@@ -43,7 +43,6 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
 
             if (response == null || response.Ids.Empty())
             {
-                return response.Ids.First();
                 throw new DownloadClientRejectedReleaseException(remoteAlbum.Release, "SABnzbd rejected the NZB for an unknown reason");
             }
 
