65f098027d04073690de931386901058edf89691
Rhys Braunschweig <rbraunschweig@users.noreply.github.com>
2019-09-04 14:53:29 +0200

    Add digits to Deluge's category validator

--- src/NzbDrone.Core/Download/Clients/Deluge/DelugeSettings.cs
+++ src/NzbDrone.Core/Download/Clients/Deluge/DelugeSettings.cs
@@ -12,8 +12,8 @@ namespace NzbDrone.Core.Download.Clients.Deluge
             RuleFor(c => c.Host).ValidHost();
             RuleFor(c => c.Port).InclusiveBetween(1, 65535);
 
-            RuleFor(c => c.MusicCategory).Matches("^[-a-z]*$").WithMessage("Allowed characters a-z and -");
-            RuleFor(c => c.MusicImportedCategory).Matches("^[-a-z]*$").WithMessage("Allowed characters a-z and -");
+            RuleFor(c => c.MusicCategory).Matches("^[-a-z0-9]*$").WithMessage("Allowed characters a-z, 0-9 and -");
+            RuleFor(c => c.MusicImportedCategory).Matches("^[-a-z0-9]*$").WithMessage("Allowed characters a-z, 0-9 and -");
         }
     }
 
