e3ec88fcfddb9859badd05af1622104e216d87fc
Jack Dallas <3620144+JackDallas@users.noreply.github.com>
2019-11-25 15:19:30 +0000

    Add in last and in next filter types

--- frontend/src/Helpers/Props/filterTypePredicates.js
+++ frontend/src/Helpers/Props/filterTypePredicates.js
@@ -1,4 +1,6 @@
 import * as filterTypes from './filterTypes';
+import isAfter from 'Utilities/Date/isAfter';
+import isBefore from 'Utilities/Date/isBefore';
 
 const filterTypePredicates = {
   [filterTypes.CONTAINS]: function(itemValue, filterValue) {
@@ -39,6 +41,20 @@ const filterTypePredicates = {
 
   [filterTypes.NOT_EQUAL]: function(itemValue, filterValue) {
     return itemValue !== filterValue;
+  },
+
+  [filterTypes.IN_LAST]: function(itemValue, filterValue) {
+    return (
+      isAfter(itemValue, { [filterValue.time]: filterValue.value * -1 }) &&
+      isBefore(itemValue)
+    );
+  },
+
+  [filterTypes.IN_NEXT]: function(itemValue, filterValue) {
+    return (
+      isAfter(itemValue) &&
+      isBefore(itemValue, { [filterValue.time]: filterValue.value })
+    );
   }
 };
 
