bbd065940ed420f24821ae6d95665d016bb9a140
Qstick <qstick@gmail.com>
2019-12-07 02:04:38 -0500

    Fixed: Calendar Ledgend not being shown

--- frontend/src/Calendar/CalendarPage.js
+++ frontend/src/Calendar/CalendarPage.js
@@ -156,7 +156,7 @@ class CalendarPage extends Component {
           }
 
           {
-            hasMovie && !! movieError &&
+            hasMovie && !movieError &&
               <LegendConnector />
           }
         </PageContentBodyConnector>
