84faeca0d30d3c77afe5bcc8ade7902d63754082
Taloth Saldono <Taloth@users.noreply.github.com>
2020-01-05 14:37:52 +0100

    Fixed missing interface for the CheckForFinishedDownloadCommand backward compat handling

--- src/NzbDrone.Core/Download/TrackedDownloads/DownloadMonitoringService.cs
+++ src/NzbDrone.Core/Download/TrackedDownloads/DownloadMonitoringService.cs
@@ -12,6 +12,7 @@ using NzbDrone.Core.Messaging.Events;
 namespace NzbDrone.Core.Download.TrackedDownloads
 {
     public class DownloadMonitoringService : IExecute<RefreshMonitoredDownloadsCommand>,
+                                             IExecute<CheckForFinishedDownloadCommand>,
                                              IHandle<BookGrabbedEvent>,
                                              IHandle<TrackImportedEvent>,
                                              IHandle<DownloadsProcessedEvent>,
