c94606dccddbd0bfe188b89f55ed3dc8dcf2a430
Qstick <qstick@gmail.com>
2020-01-05 00:47:38 -0500

    Fixed: MediaCover Test Broken in #3982

--- src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
+++ src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
@@ -48,7 +48,7 @@ namespace NzbDrone.Core.Test.MediaCoverTests
 
             Subject.ConvertToLocalUrls(12, covers);
 
-            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?lastWrite=1234");
         }
 
         [Test]
