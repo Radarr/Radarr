68ea6fbb4fcb0179b7b8353d720b7a38f8df4603
Qstick <qstick@gmail.com>
2020-01-06 22:02:41 -0500

    Fixed: Broken SeedConfigOptions due to System.Text.Json req. for public setters

--- src/NzbDrone.Core/Indexers/ITorrentIndexerSettings.cs
+++ src/NzbDrone.Core/Indexers/ITorrentIndexerSettings.cs
@@ -6,6 +6,8 @@ namespace NzbDrone.Core.Indexers
     {
         int MinimumSeeders { get; set; }
         IEnumerable<int> RequiredFlags { get; set; }
-        SeedCriteriaSettings SeedCriteria { get; }
+
+        // TODO: System.Text.Json requires setter be public for sub-object deserialization in 3.0. https://github.com/dotnet/corefx/issues/42515
+        SeedCriteriaSettings SeedCriteria { get; set; }
     }
 }
