3c1113260d43aca56ac537070fbc6bbf3facae1d
Fossil <Fossil01@users.noreply.github.com>
2020-01-10 19:37:29 +0100

    Fixed: NZB Finder categories (#4054)
    As discussed here https://github.com/Radarr/Radarr/pull/3568

--- src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
+++ src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
@@ -45,7 +45,7 @@ namespace NzbDrone.Core.Indexers.Newznab
                 yield return GetDefinition("Nzb-Tortuga", GetSettings("https://www.nzb-tortuga.com"));
                 yield return GetDefinition("Nzb.su", GetSettings("https://api.nzb.su"));
                 yield return GetDefinition("NZBCat", GetSettings("https://nzb.cat"));
-                yield return GetDefinition("NZBFinder.ws", GetSettings("https://nzbfinder.ws", categories: new[] { 2000, 2010, 2030, 2040, 2045, 2050, 2060, 2070 }));
+                yield return GetDefinition("NZBFinder.ws", GetSettings("https://nzbfinder.ws", categories: new[] { 2030, 2040, 2045, 2050, 2060, 2070, 2080, 2090 }));
                 yield return GetDefinition("NZBgeek", GetSettings("https://api.nzbgeek.info"));
                 yield return GetDefinition("nzbplanet.net", GetSettings("https://api.nzbplanet.net"));
                 yield return GetDefinition("omgwtfnzbs", GetSettings("https://api.omgwtfnzbs.me", categories: new[] { 2000, 2020, 2030, 2040, 2045, 2050, 2070 }));
