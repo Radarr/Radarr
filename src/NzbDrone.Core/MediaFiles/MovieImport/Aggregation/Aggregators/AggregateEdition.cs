68bfd8bc25d84c632bb18118c7071f6a02a8a681
Qstick <qstick@gmail.com>
2020-01-26 01:13:45 -0500

    Fixed: Edition not Saving to DB due to missing Aggregator

---
+++ src/NzbDrone.Core/MediaFiles/MovieImport/Aggregation/Aggregators/AggregateEdition.cs
@@ -0,0 +1,27 @@
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.MediaFiles.MovieImport.Aggregation.Aggregators
+{
+    public class AggregateEdition : IAggregateLocalMovie
+    {
+        public LocalMovie Aggregate(LocalMovie localMovie, bool otherFiles)
+        {
+            var movieEdition = localMovie.DownloadClientMovieInfo?.Edition;
+
+            if (movieEdition.IsNullOrWhiteSpace())
+            {
+                movieEdition = localMovie.FolderMovieInfo?.Edition;
+            }
+
+            if (movieEdition.IsNullOrWhiteSpace())
+            {
+                movieEdition = localMovie.FileMovieInfo?.Edition;
+            }
+
+            localMovie.Edition = movieEdition;
+
+            return localMovie;
+        }
+    }
+}
