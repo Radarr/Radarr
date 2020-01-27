fb73674266b6d5dfc6ded953e3587bdb0d66cb4a
Dyorgio Nascimento <dyorgio@gmail.com>
2020-01-27 11:18:36 -0300

    Fix compiler errors 2

--- src/NzbDrone.Core/Languages/Language.cs
+++ src/NzbDrone.Core/Languages/Language.cs
@@ -96,7 +96,7 @@ namespace NzbDrone.Core.Languages
         public static Language Hebrew => new Language(23, "Hebrew");
         public static Language Lithuanian => new Language(24, "Lithuanian");
         public static Language Czech => new Language(25, "Czech");
-        public static Language Czech => new Language(26, "Brazilian");
+        public static Language Brazilian => new Language(26, "Brazilian");
         public static Language Any => new Language(-1, "Any");
         
         public static List<Language> All
