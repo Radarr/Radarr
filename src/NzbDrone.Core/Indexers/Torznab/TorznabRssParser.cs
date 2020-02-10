d9ee2b9e4106d188d413b2cffc5bd97db2130cb7
Jonas Stendahl <jonas.stendahl@gmail.com>
2020-02-10 13:38:02 +0100

    Use TryParse properly

--- src/NzbDrone.Core/Indexers/Torznab/TorznabRssParser.cs
+++ src/NzbDrone.Core/Indexers/Torznab/TorznabRssParser.cs
@@ -43,7 +43,7 @@ namespace NzbDrone.Core.Indexers.Torznab
 
             if (imdbId != null)
             {
-                torrentInfo.ImdbId = int.TryParse(imdbId);
+                int.TryParse(imdbId, torrentInfo.ImdbId);
             }
 
             torrentInfo.IndexerFlags = GetFlags(item);
