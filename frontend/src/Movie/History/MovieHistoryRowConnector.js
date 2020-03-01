990a65a681031431145359cfd385c6df620d33aa
Qstick <qstick@gmail.com>
2020-03-01 15:38:19 -0500

    Fixed: Change MovieHistoryDetails to Modal

--- frontend/src/Movie/History/MovieHistoryRowConnector.js
+++ frontend/src/Movie/History/MovieHistoryRowConnector.js
@@ -2,14 +2,18 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import { fetchHistory, markAsFailed } from 'Store/Actions/historyActions';
 import createMovieSelector from 'Store/Selectors/createMovieSelector';
+import createUISettingsSelector from 'Store/Selectors/createUISettingsSelector';
 import MovieHistoryRow from './MovieHistoryRow';
 
 function createMapStateToProps() {
   return createSelector(
     createMovieSelector(),
-    (movie) => {
+    createUISettingsSelector(),
+    (movie, uiSettings) => {
       return {
-        movie
+        movie,
+        shortDateFormat: uiSettings.shortDateFormat,
+        timeFormat: uiSettings.timeFormat
       };
     }
   );
