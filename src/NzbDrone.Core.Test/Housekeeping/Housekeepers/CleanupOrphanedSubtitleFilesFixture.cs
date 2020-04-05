407c8084a0e9ee27f52117fbcf76621bcdb788ee
Qstick <qstick@gmail.com>
2020-04-05 11:12:25 -0400

    Fixed: ExtraFiles and PendingReleases not deleted correctly

---
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupOrphanedSubtitleFilesFixture.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Extras.Subtitles;
+using NzbDrone.Core.Housekeeping.Housekeepers;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
+{
+    [TestFixture]
+    public class CleanupOrphanedSubtitleFilesFixture : DbTest<CleanupOrphanedSubtitleFiles, SubtitleFile>
+    {
+        [Test]
+        public void should_delete_subtitle_files_that_dont_have_a_coresponding_movie()
+        {
+            var subtitleFile = Builder<SubtitleFile>.CreateNew()
+                                                    .With(m => m.MovieFileId = 0)
+                                                    .With(m => m.Language = Language.English)
+                                                    .BuildNew();
+
+            Db.Insert(subtitleFile);
+            Subject.Clean();
+            AllStoredModels.Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_not_delete_subtitle_files_that_have_a_coresponding_movie()
+        {
+            var movie = Builder<Movie>.CreateNew()
+                                        .BuildNew();
+
+            Db.Insert(movie);
+
+            var subtitleFile = Builder<SubtitleFile>.CreateNew()
+                                                    .With(m => m.MovieId = movie.Id)
+                                                    .With(m => m.MovieFileId = 0)
+                                                    .With(m => m.Language = Language.English)
+                                                    .BuildNew();
+
+            Db.Insert(subtitleFile);
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void should_delete_subtitle_files_that_dont_have_a_coresponding_movie_file()
+        {
+            var movie = Builder<Movie>.CreateNew()
+                                        .BuildNew();
+
+            Db.Insert(movie);
+
+            var subtitleFile = Builder<SubtitleFile>.CreateNew()
+                                                    .With(m => m.MovieId = movie.Id)
+                                                    .With(m => m.MovieFileId = 10)
+                                                    .With(m => m.Language = Language.English)
+                                                    .BuildNew();
+
+            Db.Insert(subtitleFile);
+            Subject.Clean();
+            AllStoredModels.Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_not_delete_subtitle_files_that_have_a_coresponding_movie_file()
+        {
+            var movie = Builder<Movie>.CreateNew()
+                                        .BuildNew();
+
+            var movieFile = Builder<MovieFile>.CreateNew()
+                                                  .With(h => h.Quality = new QualityModel())
+                                                  .With(h => h.Languages = new List<Language>())
+                                                  .BuildNew();
+
+            Db.Insert(movie);
+            Db.Insert(movieFile);
+
+            var subtitleFile = Builder<SubtitleFile>.CreateNew()
+                                                    .With(m => m.MovieId = movie.Id)
+                                                    .With(m => m.MovieFileId = movieFile.Id)
+                                                    .With(m => m.Language = Language.English)
+                                                    .BuildNew();
+
+            Db.Insert(subtitleFile);
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+        }
+    }
+}
