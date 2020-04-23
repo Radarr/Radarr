2dcea3de4347f9142839f69cc1abcfb70e2dca49
jpogslagman@gmail.com <jpogslagman@gmail.com>
2020-04-23 23:54:37 +0800

    Fixed #3935

--- src/NzbDrone.Core/Extras/Subtitles/SubtitleService.cs
+++ src/NzbDrone.Core/Extras/Subtitles/SubtitleService.cs
@@ -88,11 +88,16 @@ namespace NzbDrone.Core.Extras.Subtitles
             if (SubtitleFileExtensions.Extensions.Contains(Path.GetExtension(path)))
             {
                 var language = LanguageParser.ParseSubtitleLanguage(path);
-                var suffix = GetSuffix(language, 1, false);
+                var subtitleFiles = _subtitleFileService.GetFilesByMovie(movie.Id);
+                var existingSrtSubs = subtitleFiles.Where(m => m.MovieFileId == movieFile.Id)
+                    .Where(m => m.Language == language)
+                    .Where(m => m.Extension == extension);
+
+                var suffix = GetSuffix(language, existingSrtSubs.Count() + 1, extension.EqualsIgnoreCase(".srt"));                
                 var subtitleFile = ImportFile(movie, movieFile, path, readOnly, extension, suffix);
                 subtitleFile.Language = language;
 
-                _subtitleFileService.Upsert(subtitleFile);
+                _subtitleFileService.Upsert(subtitleFile);                                            
 
                 return subtitleFile;
             }
