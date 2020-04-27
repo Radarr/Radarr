7afea3df90617a92a52835a8239361413ffff1f1
Taloth Saldono <Taloth@users.noreply.github.com>
2020-04-27 23:58:35 +0200

    Log Real IP on Authentication failure in case of a reverse proxy
    closes #3711

--- src/NzbDrone.Common/Extensions/IpAddressExtensions.cs
+++ src/NzbDrone.Common/Extensions/IpAddressExtensions.cs
@@ -1,29 +1,40 @@
-using System.Net;
+﻿using System.Net;
+﻿using System.Net.Sockets;
 
-namespace NzbDrone.Common.Extensions
+﻿namespace NzbDrone.Common.Extensions
 {
     public static class IPAddressExtensions
     {
         public static bool IsLocalAddress(this IPAddress ipAddress)
         {
-            if (ipAddress.ToString() == "::1")
+            if (ipAddress.IsIPv6LinkLocal)
             {
                 return true;
             }
 
-            byte[] bytes = ipAddress.GetAddressBytes();
-            switch (bytes[0])
+            if (IPAddress.IsLoopback(ipAddress))
             {
-                case 10:
-                case 127:
-                    return true;
-                case 172:
-                    return bytes[1] < 32 && bytes[1] >= 16;
-                case 192:
-                    return bytes[1] == 168;
-                default:
-                    return false;
+                return true;
             }
+
+            if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+            {
+                byte[] bytes = ipAddress.GetAddressBytes();
+                switch (bytes[0])
+                {
+                    case 10:
+                    case 127:
+                        return true;
+                    case 172:
+                        return bytes[1] < 32 && bytes[1] >= 16;
+                    case 192:
+                        return bytes[1] == 168;
+                    default:
+                        return false;
+                }
+            }
+
+            return false;
         }
     }
 }
