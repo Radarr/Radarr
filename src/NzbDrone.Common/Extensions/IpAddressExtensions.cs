73eb83c9dcba416a62f369b540e047987e277e96
Qstick <qstick@gmail.com>
2020-04-29 20:55:57 -0400

    Fix Indent Issue from cherry pick

--- src/NzbDrone.Common/Extensions/IpAddressExtensions.cs
+++ src/NzbDrone.Common/Extensions/IpAddressExtensions.cs
@@ -1,7 +1,7 @@
-﻿using System.Net;
-﻿using System.Net.Sockets;
+using System.Net;
+using System.Net.Sockets;
 
-﻿namespace NzbDrone.Common.Extensions
+namespace NzbDrone.Common.Extensions
 {
     public static class IPAddressExtensions
     {
