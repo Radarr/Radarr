aefa4aa20b3d24340173b0613df2cde1b0d745c8
Qstick <qstick@gmail.com>
2020-05-30 15:04:26 -0400

    Fixed: Not removing seeded download if it was manual imported in some cases
    Fixes #4474
    
    Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

--- src/NzbDrone.Core/Download/DownloadProcessingService.cs
+++ src/NzbDrone.Core/Download/DownloadProcessingService.cs
@@ -40,7 +40,7 @@ namespace NzbDrone.Core.Download
 
             foreach (var trackedDownload in trackedDownloads)
             {
-                _eventAggregator.PublishEvent(new DownloadCompletedEvent(trackedDownload, trackedDownload.RemoteMovie.Movie.Id));
+                _eventAggregator.PublishEvent(new DownloadCanBeRemovedEvent(trackedDownload));
             }
         }
 
