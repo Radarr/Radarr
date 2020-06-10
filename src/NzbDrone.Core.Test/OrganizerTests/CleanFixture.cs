759fcd0de44e195b233f2a964986aa74086f61e3
Filippo Fuochi <ffuochi@hotmail.com>
2020-06-10 16:22:50 +0200

    remove initial dot in filename

--- src/NzbDrone.Core.Test/OrganizerTests/CleanFixture.cs
+++ src/NzbDrone.Core.Test/OrganizerTests/CleanFixture.cs
@@ -10,6 +10,8 @@ namespace NzbDrone.Core.Test.OrganizerTests
     {
         [TestCase("Mission: Impossible - no [HDTV-720p]",
             "Mission Impossible - no [HDTV-720p]")]
+        [TestCase(".45 (2006)", "45 (2006)")]
+        [TestCase(" The Movie Title ", "The Movie Title")]
         public void CleanFileName(string name, string expectedName)
         {
             FileNameBuilder.CleanFileName(name).Should().Be(expectedName);
