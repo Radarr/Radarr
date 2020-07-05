b82d636a8c8c35adae329b331bbf0ac7990eca0f
Qstick <qstick@gmail.com>
2020-07-04 22:31:13 -0400

    Fixed: Don't make Editor call on initial page load

--- frontend/src/Movie/Index/MovieIndex.js
+++ frontend/src/Movie/Index/MovieIndex.js
@@ -255,10 +255,14 @@ class MovieIndex extends Component {
   }
 
   onSaveSelected = (changes) => {
-    this.props.onSaveSelected({
-      movieIds: this.getSelectedIds(),
-      ...changes
-    });
+    const selectedMovieIds = this.getSelectedIds();
+
+    if (selectedMovieIds.length > 0) {
+      this.props.onSaveSelected({
+        movieIds: this.getSelectedIds(),
+        ...changes
+      });
+    }
   }
 
   onOrganizeMoviePress = () => {
