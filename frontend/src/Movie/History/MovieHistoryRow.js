9562c40ff0e8f37ea3f238e0082ab1903664e3d2
Mark McDowall <mark@mcdowall.ca>
2020-08-02 13:07:12 -0700

    Fixed: Long paths overflowing in movie history

--- frontend/src/Movie/History/MovieHistoryRow.js
+++ frontend/src/Movie/History/MovieHistoryRow.js
@@ -81,7 +81,7 @@ class MovieHistoryRow extends Component {
           data={data}
         />
 
-        <TableRowCell>
+        <TableRowCell className={styles.sourceTitle}>
           {sourceTitle}
         </TableRowCell>
 
