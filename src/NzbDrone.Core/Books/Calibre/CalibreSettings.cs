655017e1ed5b3d708e54e797be82c7e43a0614aa
ta264 <ta264@users.noreply.github.com>
2020-08-06 22:06:40 +0100

    Simplify calibre settings
    The IProviderConfig stuff wasn't used

--- src/NzbDrone.Core/Books/Calibre/CalibreSettings.cs
+++ src/NzbDrone.Core/Books/Calibre/CalibreSettings.cs
@@ -2,8 +2,7 @@ using System;
 using System.Linq;
 using FluentValidation;
 using NzbDrone.Common.Extensions;
-using NzbDrone.Core.Annotations;
-using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Books.Calibre
@@ -23,7 +22,7 @@ namespace NzbDrone.Core.Books.Calibre
         }
     }
 
-    public class CalibreSettings : IProviderConfig
+    public class CalibreSettings : IEmbeddedDocument
     {
         private static readonly CalibreSettingsValidator Validator = new CalibreSettingsValidator();
 
@@ -32,28 +31,13 @@ namespace NzbDrone.Core.Books.Calibre
             Port = 8080;
         }
 
-        [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
         public string Host { get; set; }
-
-        [FieldDefinition(1, Label = "Port", Type = FieldType.Textbox)]
-        public int Port { get; set; }
-
-        [FieldDefinition(2, Label = "Url Base", Type = FieldType.Textbox, Advanced = true, HelpText = "Adds a prefix to the calibre url, e.g. http://[host]:[port]/[urlBase]")]
+        public int Port { get; set; } = 8080;
         public string UrlBase { get; set; }
-
-        [FieldDefinition(3, Label = "Username", Type = FieldType.Textbox)]
         public string Username { get; set; }
-
-        [FieldDefinition(4, Label = "Password", Type = FieldType.Password)]
         public string Password { get; set; }
-
-        [FieldDefinition(5, Label = "Convert to Format", Type = FieldType.Textbox, HelpText = "Optionally ask calibre to convert to other formats on import. Comma separated list.")]
         public string OutputFormat { get; set; }
-
-        [FieldDefinition(6, Label = "Conversion Profile", Type = FieldType.Select, SelectOptions = typeof(CalibreProfile), HelpText = "The output profile to use for conversion")]
         public int OutputProfile { get; set; }
-
-        [FieldDefinition(9, Label = "Use SSL", Type = FieldType.Checkbox)]
         public bool UseSsl { get; set; }
 
         public NzbDroneValidationResult Validate()
