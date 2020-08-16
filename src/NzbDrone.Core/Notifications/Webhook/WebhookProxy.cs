robertdigital/Readarr

Support for digest auth with HttpRequests
[common]

src/NzbDrone.Core/Notifications/Webhook/WebhookProxy.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
@@ -33,7 +34,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 
                 if (settings.Username.IsNotNullOrWhiteSpace() || settings.Password.IsNotNullOrWhiteSpace())
                 {
-                    request.AddBasicAuthentication(settings.Username, settings.Password);
+                    request.Credentials = new NetworkCredential(settings.Username, settings.Password);
                 }
 
                 _httpClient.Execute(request);

Radarr/Radarr

src/NzbDrone.Core/Notifications/Webhook/WebhookProxy.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
@@ -33,7 +34,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 
                 if (settings.Username.IsNotNullOrWhiteSpace() || settings.Password.IsNotNullOrWhiteSpace())
                 {
-                    request.AddBasicAuthentication(settings.Username, settings.Password);
+                    request.Credentials = new BasicNetworkCredential(settings.Username, settings.Password);
                 }
 
                 _httpClient.Execute(request);