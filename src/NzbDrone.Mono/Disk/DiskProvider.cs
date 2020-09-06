SimplyRyan/Sonarr

Remove stacktrace if hardlink resulted in EXDEV.

src/NzbDrone.Mono/Disk/DiskProvider.cs
@@ -410,9 +410,21 @@ namespace NzbDrone.Mono.Disk
                 fileInfo.CreateLink(destination);
                 return true;
             }
+            catch (UnixIOException ex)
+            {
+                if (ex.ErrorCode == Errno.EXDEV)
+                {
+                    Logger.Trace("Hardlink '{0}' to '{1}' failed due to cross-device access.", source, destination);
+                }
+                else
+                {
+                    Logger.Debug(ex, "Hardlink '{0}' to '{1}' failed.", source, destination);
+                }
+                return false;
+            }
             catch (Exception ex)
             {
-                Logger.Debug(ex, string.Format("Hardlink '{0}' to '{1}' failed.", source, destination));
+                Logger.Debug(ex, "Hardlink '{0}' to '{1}' failed.", source, destination);
                 return false;
             }
         }

Radarr/Radarr

src/NzbDrone.Mono/Disk/DiskProvider.cs
@@ -418,9 +418,22 @@ namespace NzbDrone.Mono.Disk
                 fileInfo.CreateLink(destination);
                 return true;
             }
+            catch (UnixIOException ex)
+            {
+                if (ex.ErrorCode == Errno.EXDEV)
+                {
+                    _logger.Trace("Hardlink '{0}' to '{1}' failed due to cross-device access.", source, destination);
+                }
+                else
+                {
+                    _logger.Debug(ex, "Hardlink '{0}' to '{1}' failed.", source, destination);
+                }
+
+                return false;
+            }
             catch (Exception ex)
             {
-                _logger.Debug(ex, string.Format("Hardlink '{0}' to '{1}' failed.", source, destination));
+                _logger.Debug(ex, "Hardlink '{0}' to '{1}' failed.", source, destination);
                 return false;
             }
         }