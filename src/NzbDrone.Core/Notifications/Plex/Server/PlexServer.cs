CorentinB/Sonarr

Fixed: Refreshing Plex Server series in high volume systems

src/NzbDrone.Core/Notifications/Plex/Server/PlexServer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation.Results;
+using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Exceptions;
 using NzbDrone.Core.Notifications.Plex.PlexTv;
@@ -14,10 +16,20 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         private readonly IPlexServerService _plexServerService;
         private readonly IPlexTvService _plexTvService;
 
-        public PlexServer(IPlexServerService plexServerService, IPlexTvService plexTvService)
+        class PlexUpdateQueue
+        {
+            public Dictionary<int, Series> Pending { get; } = new Dictionary<int, Series>();
+            public bool Refreshing { get; set; }
+        }
+
+        private readonly ICached<PlexUpdateQueue> _pendingSeriesCache;
+
+        public PlexServer(IPlexServerService plexServerService, IPlexTvService plexTvService, ICacheManager cacheManager)
         {
             _plexServerService = plexServerService;
             _plexTvService = plexTvService;
+
+            _pendingSeriesCache = cacheManager.GetCache<PlexUpdateQueue>(GetType(), "pendingSeries");
         }
 
         public override string Link => "https://www.plex.tv/";
@@ -37,7 +49,60 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             if (Settings.UpdateLibrary)
             {
-                _plexServerService.UpdateLibrary(series, Settings);
+                lock (_pendingSeriesCache)
+                {
+                    var queue = _pendingSeriesCache.Get(Settings.Host, () => new PlexUpdateQueue(), TimeSpan.FromDays(1));
+                    queue.Pending[series.Id] = series;
+                }
+            }
+        }
+
+        public override void ProcessQueue()
+        {
+            PlexUpdateQueue queue = null;
+            var refreshing = false;
+            try
+            {
+                lock (_pendingSeriesCache)
+                {
+                    queue = _pendingSeriesCache.Find(Settings.Host);
+                    if (queue == null || queue.Refreshing)
+                    {
+                        return;
+                    }
+                    queue.Refreshing = refreshing = true;
+                    _pendingSeriesCache.Set(Settings.Host, queue, TimeSpan.FromDays(1));
+                }
+
+                while (true)
+                {
+                    List<Series> refreshingSeries;
+                    lock (_pendingSeriesCache)
+                    {
+                        if (queue.Pending.Empty())
+                        {
+                            return;
+                        }
+
+                        refreshingSeries = queue.Pending.Values.ToList();
+                        queue.Pending.Clear();
+                    }
+
+                    if (Settings.UpdateLibrary)
+                    {
+                        _plexServerService.UpdateLibrary(refreshingSeries, Settings);
+                    }
+                }
+            }
+            finally
+            {
+                lock (_pendingSeriesCache)
+                {
+                    if (refreshing && queue != null)
+                    {
+                        queue.Refreshing = false;
+                    }
+                }
             }
         }
 

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation.Results;
@@ -13,6 +14,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     public interface IPlexServerService
     {
         void UpdateLibrary(Series series, PlexServerSettings settings);
+        void UpdateLibrary(IEnumerable<Series> series, PlexServerSettings settings);
         ValidationFailure Test(PlexServerSettings settings);
     }
 
@@ -33,9 +35,15 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public void UpdateLibrary(Series series, PlexServerSettings settings)
         {
+            UpdateLibrary(new[] { series }, settings);
+        }
+
+        public void UpdateLibrary(IEnumerable<Series> multipleSeries, PlexServerSettings settings)
+        {
             try
             {
                 _logger.Debug("Sending Update Request to Plex Server");
+                var watch = Stopwatch.StartNew();
 
                 var version = _versionCache.Get(settings.Host, () => GetVersion(settings), TimeSpan.FromHours(2));
                 ValidateVersion(version);
@@ -45,16 +53,34 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
                 if (partialUpdates)
                 {
-                    UpdatePartialSection(series, sections, settings);
-                }
+                    var partiallyUpdated = true;
 
+                    foreach (var series in multipleSeries)
+                    {
+                        partiallyUpdated &= UpdatePartialSection(series, sections, settings);
+
+                        if (!partiallyUpdated)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Only update complete sections if all partial updates failed
+                    if (!partiallyUpdated)
+                    {
+                        _logger.Debug("Unable to update partial section, updating all TV sections");
+                        sections.ForEach(s => UpdateSection(s.Id, settings));
+                    }
+                }
                 else
                 {
                     sections.ForEach(s => UpdateSection(s.Id, settings));
                 }
+
+                _logger.Debug("Finished sending Update Request to Plex Server (took {0} ms)", watch.ElapsedMilliseconds);
             }
 
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.Warn(ex, "Failed to Update Plex host: " + settings.Host);
                 throw;
@@ -125,7 +151,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             _plexServerProxy.Update(sectionId, settings);
         }
 
-        private void UpdatePartialSection(Series series, List<PlexSection> sections, PlexServerSettings settings)
+        private bool UpdatePartialSection(Series series, List<PlexSection> sections, PlexServerSettings settings)
         {
             var partiallyUpdated = false;
 
@@ -142,12 +168,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
                 }
             }
 
-            // Only update complete sections if all partial updates failed
-            if (!partiallyUpdated)
-            {
-                _logger.Debug("Unable to update partial section, updating all TV sections");
-                sections.ForEach(s => UpdateSection(s.Id, settings));
-            }
+            return partiallyUpdated;
         }
 
         private int? GetMetadataId(int sectionId, Series series, string language, PlexServerSettings settings)
@@ -161,6 +182,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             try
             {
+                _versionCache.Remove(settings.Host);
+                _partialUpdateCache.Remove(settings.Host);
                 var sections = GetSections(settings);
 
                 if (sections.Empty())

Radarr/Radarr

src/NzbDrone.Core/Notifications/Plex/Server/PlexServer.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation.Results;
+using NLog;
+using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Exceptions;
 using NzbDrone.Core.Movies;
@@ -13,11 +16,23 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     {
         private readonly IPlexServerService _plexServerService;
         private readonly IPlexTvService _plexTvService;
+        private readonly Logger _logger;
 
-        public PlexServer(IPlexServerService plexServerService, IPlexTvService plexTvService)
+        private class PlexUpdateQueue
+        {
+            public Dictionary<int, Movie> Pending { get; } = new Dictionary<int, Movie>();
+            public bool Refreshing { get; set; }
+        }
+
+        private readonly ICached<PlexUpdateQueue> _pendingMoviesCache;
+
+        public PlexServer(IPlexServerService plexServerService, IPlexTvService plexTvService, ICacheManager cacheManager, Logger logger)
         {
             _plexServerService = plexServerService;
             _plexTvService = plexTvService;
+            _logger = logger;
+
+            _pendingMoviesCache = cacheManager.GetRollingCache<PlexUpdateQueue>(GetType(), "pendingSeries", TimeSpan.FromDays(1));
         }
 
         public override string Link => "https://www.plex.tv/";
@@ -37,7 +52,65 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             if (Settings.UpdateLibrary)
             {
-                _plexServerService.UpdateLibrary(movie, Settings);
+                _logger.Debug("Scheduling library update for movie {0} {1}", movie.Id, movie.Title);
+                var queue = _pendingMoviesCache.Get(Settings.Host, () => new PlexUpdateQueue());
+                lock (queue)
+                {
+                    queue.Pending[movie.Id] = movie;
+                }
+            }
+        }
+
+        public override void ProcessQueue()
+        {
+            PlexUpdateQueue queue = _pendingMoviesCache.Find(Settings.Host);
+            if (queue == null)
+            {
+                return;
+            }
+
+            lock (queue)
+            {
+                if (queue.Refreshing)
+                {
+                    return;
+                }
+
+                queue.Refreshing = true;
+            }
+
+            try
+            {
+                while (true)
+                {
+                    List<Movie> refreshingMovies;
+                    lock (queue)
+                    {
+                        if (queue.Pending.Empty())
+                        {
+                            queue.Refreshing = false;
+                            return;
+                        }
+
+                        refreshingMovies = queue.Pending.Values.ToList();
+                        queue.Pending.Clear();
+                    }
+
+                    if (Settings.UpdateLibrary)
+                    {
+                        _logger.Debug("Performing library update for {0} movies", refreshingMovies.Count);
+                        _plexServerService.UpdateLibrary(refreshingMovies, Settings);
+                    }
+                }
+            }
+            catch
+            {
+                lock (queue)
+                {
+                    queue.Refreshing = false;
+                }
+
+                throw;
             }
         }
 

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation.Results;
@@ -13,6 +14,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     public interface IPlexServerService
     {
         void UpdateLibrary(Movie movie, PlexServerSettings settings);
+        void UpdateLibrary(IEnumerable<Movie> movie, PlexServerSettings settings);
         ValidationFailure Test(PlexServerSettings settings);
     }
 
@@ -33,9 +35,15 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public void UpdateLibrary(Movie movie, PlexServerSettings settings)
         {
+            UpdateLibrary(new[] { movie }, settings);
+        }
+
+        public void UpdateLibrary(IEnumerable<Movie> multipleMovies, PlexServerSettings settings)
+        {
             try
             {
                 _logger.Debug("Sending Update Request to Plex Server");
+                var watch = Stopwatch.StartNew();
 
                 var version = _versionCache.Get(settings.Host, () => GetVersion(settings), TimeSpan.FromHours(2));
                 ValidateVersion(version);
@@ -45,12 +53,31 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
                 if (partialUpdates)
                 {
-                    UpdatePartialSection(movie, sections, settings);
+                    var partiallyUpdated = true;
+
+                    foreach (var movie in multipleMovies)
+                    {
+                        partiallyUpdated &= UpdatePartialSection(movie, sections, settings);
+
+                        if (!partiallyUpdated)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Only update complete sections if all partial updates failed
+                    if (!partiallyUpdated)
+                    {
+                        _logger.Debug("Unable to update partial section, updating all Movie sections");
+                        sections.ForEach(s => UpdateSection(s.Id, settings));
+                    }
                 }
                 else
                 {
                     sections.ForEach(s => UpdateSection(s.Id, settings));
                 }
+
+                _logger.Debug("Finished sending Update Request to Plex Server (took {0} ms)", watch.ElapsedMilliseconds);
             }
             catch (Exception ex)
             {
@@ -123,7 +150,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             _plexServerProxy.Update(sectionId, settings);
         }
 
-        private void UpdatePartialSection(Movie movie, List<PlexSection> sections, PlexServerSettings settings)
+        private bool UpdatePartialSection(Movie movie, List<PlexSection> sections, PlexServerSettings settings)
         {
             var partiallyUpdated = false;
 
@@ -140,12 +167,7 @@ namespace NzbDrone.Core.Notifications.Plex.Server
                 }
             }
 
-            // Only update complete sections if all partial updates failed
-            if (!partiallyUpdated)
-            {
-                _logger.Debug("Unable to update partial section, updating all Movie sections");
-                sections.ForEach(s => UpdateSection(s.Id, settings));
-            }
+            return partiallyUpdated;
         }
 
         private int? GetMetadataId(int sectionId, Movie movie, string language, PlexServerSettings settings)
@@ -159,6 +181,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             try
             {
+                _versionCache.Remove(settings.Host);
+                _partialUpdateCache.Remove(settings.Host);
                 var sections = GetSections(settings);
 
                 if (sections.Empty())