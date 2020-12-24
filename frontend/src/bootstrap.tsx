ced624c2ff87702685d24890847a4379a0b0341b
Taloth Saldono <Taloth@users.noreply.github.com>
2020-12-24 23:11:03 +0100

    Small helper in UI to access Radarr API more easily
    (cherry picked from commit 090cdc364ef335fbfea8cf540696af813f6ecea4)

--- frontend/src/bootstrap.tsx
+++ frontend/src/bootstrap.tsx
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 import createAppStore from 'Store/createAppStore';
 import App from './App/App';
 
+import 'Diag/ConsoleApi';
+
 export async function bootstrap() {
   const history = createBrowserHistory();
   const store = createAppStore(history);
