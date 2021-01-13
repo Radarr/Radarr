c8d0a6a4eacc63e17c292422681d5843c18b66b3
nitsua <austin.w.best@gmail.com>
2021-01-13 14:56:04 -0500

    Correct an instance of sonarr in one of the validation error output examples

--- src/NzbDrone.Core/Validation/RuleBuilderExtensions.cs
+++ src/NzbDrone.Core/Validation/RuleBuilderExtensions.cs
@@ -33,7 +33,7 @@ namespace NzbDrone.Core.Validation
             return ruleBuilder.SetValidator(new RegularExpressionValidator("^https?://[-_a-z0-9.]+", RegexOptions.IgnoreCase)).WithMessage("must be valid URL that starts with http(s)://");
         }
 
-        public static IRuleBuilderOptions<T, string> ValidUrlBase<T>(this IRuleBuilder<T, string> ruleBuilder, string example = "/sonarr")
+        public static IRuleBuilderOptions<T, string> ValidUrlBase<T>(this IRuleBuilder<T, string> ruleBuilder, string example = "/radarr")
         {
             return ruleBuilder.SetValidator(new RegularExpressionValidator(@"^(?!\/?https?://[-_a-z0-9.]+)", RegexOptions.IgnoreCase)).WithMessage($"Must be a valid URL path (ie: '{example}')");
         }
