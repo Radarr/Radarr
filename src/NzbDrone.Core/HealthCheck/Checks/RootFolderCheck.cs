Lidarr/Lidarr

Improve root folder health check
Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

src/NzbDrone.Core/HealthCheck/Checks/RootFolderCheck.cs
@@ -4,6 +4,7 @@ using NzbDrone.Core.ImportLists;
 using NzbDrone.Core.MediaFiles.Events;
 using NzbDrone.Core.Music;
 using NzbDrone.Core.Music.Events;
+using NzbDrone.Core.RootFolders;
 
 namespace NzbDrone.Core.HealthCheck.Checks
 {
@@ -16,21 +17,24 @@ namespace NzbDrone.Core.HealthCheck.Checks
         private readonly IArtistService _artistService;
         private readonly IImportListFactory _importListFactory;
         private readonly IDiskProvider _diskProvider;
+        private readonly IRootFolderService _rootFolderService;
 
-        public RootFolderCheck(IArtistService artistService, IImportListFactory importListFactory, IDiskProvider diskProvider)
+        public RootFolderCheck(IArtistService artistService, IImportListFactory importListFactory, IDiskProvider diskProvider, IRootFolderService rootFolderService)
         {
             _artistService = artistService;
             _importListFactory = importListFactory;
             _diskProvider = diskProvider;
+            _rootFolderService = rootFolderService;
         }
 
         public override HealthCheck Check()
         {
-            var missingRootFolders = _artistService.GetAllArtists()
-                                                   .Select(s => _diskProvider.GetParentFolder(s.Path))
-                                                   .Distinct()
-                                                   .Where(s => !_diskProvider.FolderExists(s))
-                                                   .ToList();
+            var rootFolders = _artistService.GetAllArtists()
+                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Path))
+                                                           .Distinct();
+
+            var missingRootFolders = rootFolders.Where(s => !_diskProvider.FolderExists(s))
+                                                          .ToList();
 
             missingRootFolders.AddRange(_importListFactory.All()
                 .Select(s => s.RootFolderPath)

Radarr/Radarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core/HealthCheck/Checks/RootFolderCheck.cs
@@ -16,21 +16,24 @@ namespace NzbDrone.Core.HealthCheck.Checks
         private readonly IAuthorService _authorService;
         private readonly IImportListFactory _importListFactory;
         private readonly IDiskProvider _diskProvider;
+        private readonly IRootFolderService _rootFolderService;
 
-        public RootFolderCheck(IAuthorService authorService, IImportListFactory importListFactory, IDiskProvider diskProvider)
+        public RootFolderCheck(IArtistService authorService, IImportListFactory importListFactory, IDiskProvider diskProvider, IRootFolderService rootFolderService)
         {
             _authorService = authorService;
             _importListFactory = importListFactory;
             _diskProvider = diskProvider;
+            _rootFolderService = rootFolderService;
         }
 
         public override HealthCheck Check()
         {
-            var missingRootFolders = _authorService.GetAllAuthors()
-                                                   .Select(s => _diskProvider.GetParentFolder(s.Path))
-                                                   .Distinct()
-                                                   .Where(s => !_diskProvider.FolderExists(s))
-                                                   .ToList();
+            var rootFolders = _artistService.GetAllAuthors()
+                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Path))
+                                                           .Distinct();
+
+            var missingRootFolders = rootFolders.Where(s => !_diskProvider.FolderExists(s))
+                                                          .ToList();
 
             missingRootFolders.AddRange(_importListFactory.All()
                 .Select(s => s.RootFolderPath)