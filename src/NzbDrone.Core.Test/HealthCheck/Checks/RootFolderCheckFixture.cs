Lidarr/Lidarr

Improve use of All() for Path related queries

src/NzbDrone.Core.Test/HealthCheck/Checks/RootFolderCheckFixture.cs
@@ -25,8 +25,8 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
                 .ToList();
 
             Mocker.GetMock<IArtistService>()
-                  .Setup(s => s.GetAllArtists())
-                  .Returns(artist);
+                  .Setup(s => s.AllArtistPaths())
+                  .Returns(artist.ToDictionary(x => x.Id, x => x.Path));
 
             Mocker.GetMock<IImportListFactory>()
                 .Setup(s => s.All())
@@ -45,8 +45,8 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
         public void should_not_return_error_when_no_artist()
         {
             Mocker.GetMock<IArtistService>()
-                  .Setup(s => s.GetAllArtists())
-                  .Returns(new List<Artist>());
+                  .Setup(s => s.AllArtistPaths())
+                  .Returns(new Dictionary<int, string>());
 
             Mocker.GetMock<IImportListFactory>()
                 .Setup(s => s.All())

src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -19,8 +19,8 @@ namespace NzbDrone.Core.HealthCheck.Checks
         public override HealthCheck Check()
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
-            var mounts = _artistService.GetAllArtists()
-                                       .Select(artist => _diskProvider.GetMount(artist.Path))
+            var mounts = _artistService.AllArtistPaths()
+                                       .Select(path => _diskProvider.GetMount(path.Value))
                                        .Where(m => m != null && m.MountOptions != null && m.MountOptions.IsReadOnly)
                                        .DistinctBy(m => m.RootDirectory)
                                        .ToList();

src/NzbDrone.Core/HealthCheck/Checks/RootFolderCheck.cs
@@ -29,8 +29,8 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
         public override HealthCheck Check()
         {
-            var rootFolders = _artistService.GetAllArtists()
-                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Path))
+            var rootFolders = _artistService.AllArtistPaths()
+                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Value))
                                                            .Distinct();
 
             var missingRootFolders = rootFolders.Where(s => !_diskProvider.FolderExists(s))

src/NzbDrone.Core/Housekeeping/Housekeepers/DeleteBadMediaCovers.cs
@@ -38,19 +38,19 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
                 return;
             }
 
-            var artists = _artistService.GetAllArtists();
+            var artists = _artistService.AllArtistPaths();
             var imageExtensions = new List<string> { ".jpg", ".png", ".gif" };
 
             foreach (var artist in artists)
             {
-                var images = _metaFileService.GetFilesByArtist(artist.Id)
+                var images = _metaFileService.GetFilesByArtist(artist.Key)
                     .Where(c => c.LastUpdated > new DateTime(2014, 12, 27) && imageExtensions.Any(x => c.RelativePath.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)));
 
                 foreach (var image in images)
                 {
                     try
                     {
-                        var path = Path.Combine(artist.Path, image.RelativePath);
+                        var path = Path.Combine(artist.Value, image.RelativePath);
                         if (!IsValid(path))
                         {
                             _logger.Debug("Deleting invalid image file " + path);

src/NzbDrone.Core/MediaFiles/MediaFileDeletionService.cs
@@ -104,22 +104,22 @@ namespace NzbDrone.Core.MediaFiles
             if (message.DeleteFiles)
             {
                 var artist = message.Artist;
-                var allArtists = _artistService.GetAllArtists();
+                var allArtists = _artistService.AllArtistPaths();
 
                 foreach (var s in allArtists)
                 {
-                    if (s.Id == artist.Id)
+                    if (s.Key == artist.Id)
                     {
                         continue;
                     }
 
-                    if (artist.Path.IsParentPath(s.Path))
+                    if (artist.Path.IsParentPath(s.Value))
                     {
                         _logger.Error("Artist path: '{0}' is a parent of another artist, not deleting files.", artist.Path);
                         return;
                     }
 
-                    if (artist.Path.PathEquals(s.Path))
+                    if (artist.Path.PathEquals(s.Value))
                     {
                         _logger.Error("Artist path: '{0}' is the same as another artist, not deleting files.", artist.Path);
                         return;

Radarr/Radarr

Signed-off-by: Robin Dadswell <robin@dadswell.email>

src/NzbDrone.Core.Test/HealthCheck/Checks/RootFolderCheckFixture.cs
@@ -25,8 +25,8 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
                 .ToList();
 
             Mocker.GetMock<IAuthorService>()
-                  .Setup(s => s.GetAllAuthors())
-                  .Returns(artist);
+                  .Setup(s => s.AllAuthorPaths())
+                  .Returns(artist.ToDictionary(x => x.Id, x => x.Path));
 
             Mocker.GetMock<IImportListFactory>()
                 .Setup(s => s.All())
@@ -45,8 +45,8 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
         public void should_not_return_error_when_no_artist()
         {
             Mocker.GetMock<IAuthorService>()
-                  .Setup(s => s.GetAllAuthors())
-                  .Returns(new List<Author>());
+                  .Setup(s => s.AllAuthorPaths())
+                  .Returns(new Dictionary<int, string>());
 
             Mocker.GetMock<IImportListFactory>()
                 .Setup(s => s.All())

src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -19,8 +19,8 @@ namespace NzbDrone.Core.HealthCheck.Checks
         public override HealthCheck Check()
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
-            var mounts = _authorService.GetAllAuthors()
-                                       .Select(author => _diskProvider.GetMount(author.Path))
+            var mounts = _authorService.AllAuthorPaths()
+                                       .Select(path => _diskProvider.GetMount(path.Value))
                                        .Where(m => m != null && m.MountOptions != null && m.MountOptions.IsReadOnly)
                                        .DistinctBy(m => m.RootDirectory)
                                        .ToList();

src/NzbDrone.Core/HealthCheck/Checks/RootFolderCheck.cs
@@ -28,8 +28,8 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
         public override HealthCheck Check()
         {
-            var rootFolders = _artistService.GetAllAuthors()
-                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Path))
+            var rootFolders = _authorService.AllAuthorPaths()
+                                                           .Select(s => _rootFolderService.GetBestRootFolderPath(s.Value))
                                                            .Distinct();
 
             var missingRootFolders = rootFolders.Where(s => !_diskProvider.FolderExists(s))

src/NzbDrone.Core/Housekeeping/Housekeepers/DeleteBadMediaCovers.cs
@@ -38,19 +38,19 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
                 return;
             }
 
-            var artists = _authorService.GetAllAuthors();
+            var artists = _authorService.AllAuthorPaths();
             var imageExtensions = new List<string> { ".jpg", ".png", ".gif" };
 
             foreach (var author in artists)
             {
-                var images = _metaFileService.GetFilesByAuthor(author.Id)
+                var images = _metaFileService.GetFilesByAuthor(author.Key)
                     .Where(c => c.LastUpdated > new DateTime(2014, 12, 27) && imageExtensions.Any(x => c.RelativePath.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)));
 
                 foreach (var image in images)
                 {
                     try
                     {
-                        var path = Path.Combine(author.Path, image.RelativePath);
+                        var path = Path.Combine(author.Value, image.RelativePath);
                         if (!IsValid(path))
                         {
                             _logger.Debug("Deleting invalid image file " + path);

src/NzbDrone.Core/MediaFiles/MediaFileDeletionService.cs
@@ -104,22 +104,22 @@ namespace NzbDrone.Core.MediaFiles
             if (message.DeleteFiles)
             {
                 var author = message.Author;
-                var allArtists = _authorService.GetAllAuthors();
+                var allArtists = _authorService.AllAuthorPaths();
 
                 foreach (var s in allArtists)
                 {
-                    if (s.Id == author.Id)
+                    if (s.Key == author.Id)
                     {
                         continue;
                     }
 
-                    if (author.Path.IsParentPath(s.Path))
+                    if (author.Path.IsParentPath(s.Value))
                     {
                         _logger.Error("Author path: '{0}' is a parent of another author, not deleting files.", author.Path);
                         return;
                     }
 
-                    if (author.Path.PathEquals(s.Path))
+                    if (author.Path.PathEquals(s.Value))
                     {
                         _logger.Error("Author path: '{0}' is the same as another author, not deleting files.", author.Path);
                         return;