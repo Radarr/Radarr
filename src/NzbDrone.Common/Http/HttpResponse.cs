24de3fdee61f7e8720d98ccbb60aaa1107ba316f
Qstick <qstick@gmail.com>
2021-03-04 13:29:15 -0500

    Remove trailing whitespace in HttpResponce

--- src/NzbDrone.Common/Http/HttpResponse.cs
+++ src/NzbDrone.Common/Http/HttpResponse.cs
@@ -54,7 +54,7 @@ namespace NzbDrone.Common.Http
                                        StatusCode == HttpStatusCode.MovedPermanently ||
                                        StatusCode == HttpStatusCode.Found ||
                                        StatusCode == HttpStatusCode.TemporaryRedirect ||
-                                       StatusCode == HttpStatusCode.RedirectMethod ||          
+                                       StatusCode == HttpStatusCode.RedirectMethod ||
                                        StatusCode == HttpStatusCode.SeeOther;
 
         public string[] GetCookieHeaders()
