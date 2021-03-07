Sonarr/Sonarr

Generalized RateLimit logic to all indexers based on indexer id

src/NzbDrone.Core/Download/TorrentClientBase.cs
@@ -128,6 +128,7 @@ namespace NzbDrone.Core.Download
             try
             {
                 var request = new HttpRequest(torrentUrl);
+                request.RateLimitKey = remoteEpisode?.Release?.IndexerId.ToString();
                 request.Headers.Accept = "application/x-bittorrent";
                 request.AllowAutoRedirect = false;
 

src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -43,7 +43,9 @@ namespace NzbDrone.Core.Download
 
             try
             {
-                nzbData = _httpClient.Get(new HttpRequest(url)).ResponseData;
+                var request = new HttpRequest(url);
+                request.RateLimitKey = remoteEpisode?.Release?.IndexerId.ToString();
+                nzbData = _httpClient.Get(request).ResponseData;
 
                 _logger.Debug("Downloaded nzb for episode '{0}' finished ({1} bytes from {2})", remoteEpisode.Release.Title, nzbData.Length, url);
             }

Radarr/Radarr

src/NzbDrone.Core/Download/TorrentClientBase.cs
@@ -129,6 +129,7 @@ namespace NzbDrone.Core.Download
             try
             {
                 var request = new HttpRequest(torrentUrl);
+                request.RateLimitKey = remoteMovie?.Release?.IndexerId.ToString();
                 request.Headers.Accept = "application/x-bittorrent";
                 request.AllowAutoRedirect = false;
 

src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -44,7 +44,9 @@ namespace NzbDrone.Core.Download
 
             try
             {
-                nzbData = _httpClient.Get(new HttpRequest(url)).ResponseData;
+                var request = new HttpRequest(url);
+                request.RateLimitKey = remoteMovie?.Release?.IndexerId.ToString();
+                nzbData = _httpClient.Get(request).ResponseData;
 
                 _logger.Debug("Downloaded nzb for movie '{0}' finished ({1} bytes from {2})", remoteMovie.Release.Title, nzbData.Length, url);
             }