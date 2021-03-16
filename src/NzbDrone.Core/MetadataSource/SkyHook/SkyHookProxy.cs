Sonarr/Sonarr

Log Skyhook connection failures with more info.

src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -109,14 +109,20 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
 
                 return httpResponse.Resource.SelectList(MapSearchResult);
             }
-            catch (HttpException)
+            catch (HttpException ex)
             {
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with SkyHook.", title);
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with SkyHook.", ex, title);
+            }
+            catch (WebException ex)
+            {
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with SkyHook.", ex, title, ex.Message);
             }
             catch (Exception ex)
             {
-                _logger.Warn(ex, ex.Message);
-                throw new SkyHookException("Search for '{0}' failed. Invalid response received from SkyHook.", title);
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Invalid response received from SkyHook.", ex, title);
             }
         }
 

Radarr/Radarr

(cherry picked from commit 6672650b6b5e152e82fb3ad38a0a158d66c0b83d)

src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -421,14 +421,20 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
 
                 return httpResponse.Resource.SelectList(MapSearchResult);
             }
-            catch (HttpException)
+            catch (HttpException ex)
             {
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with TMDb.", title);
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with TMDb.", ex, title);
+            }
+            catch (WebException ex)
+            {
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with TMDb.", ex, title, ex.Message);
             }
             catch (Exception ex)
             {
-                _logger.Warn(ex, ex.Message);
-                throw new SkyHookException("Search for '{0}' failed. Invalid response received from TMDb.", title);
+                _logger.Warn(ex);
+                throw new SkyHookException("Search for '{0}' failed. Invalid response received from TMDb.", ex, title);
             }
         }
 