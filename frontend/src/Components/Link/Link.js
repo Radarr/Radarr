Lidarr/Lidarr

Fixed: restoring scroll position when going back to index page

frontend/src/Components/Link/Link.js
@@ -38,7 +38,7 @@ class Link extends Component {
     const linkProps = { target };
     let el = component;
 
-    if (to) {
+    if (to && typeof to === 'string') {
       if ((/\w+?:\/\//).test(to)) {
         el = 'a';
         linkProps.href = to;
@@ -55,6 +55,18 @@ class Link extends Component {
         el = RouterLink;
         linkProps.to = `${window.Lidarr.urlBase}/${to.replace(/^\//, '')}`;
         linkProps.target = target;
+      }
+    } else if (to && typeof to === 'object') {
+      el = RouterLink;
+      linkProps.target = target;
+      if (to.pathname.startsWith(`${window.Lidarr.urlBase}/`)) {
+        linkProps.to = to;
+      } else {
+        const pathname = `${window.Lidarr.urlBase}/${to.pathname.replace(/^\//, '')}`;
+        linkProps.to = {
+          ...to,
+          pathname
+        };
       }
     }
 
@@ -86,7 +98,7 @@ class Link extends Component {
 Link.propTypes = {
   className: PropTypes.string,
   component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-  to: PropTypes.string,
+  to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   target: PropTypes.string,
   isDisabled: PropTypes.bool,
   noRouter: PropTypes.bool,

Radarr/Radarr

(cherry picked from commit 1bc52d0138c7bcb94ffce31ec05f675387612a62)

frontend/src/Components/Link/Link.js
@@ -38,7 +38,7 @@ class Link extends Component {
     const linkProps = { target };
     let el = component;
 
-    if (to) {
+    if (to && typeof to === 'string') {
       if ((/\w+?:\/\//).test(to)) {
         el = 'a';
         linkProps.href = to;
@@ -52,6 +52,18 @@ class Link extends Component {
         el = RouterLink;
         linkProps.to = `${window.Radarr.urlBase}/${to.replace(/^\//, '')}`;
         linkProps.target = target;
+      }
+    } else if (to && typeof to === 'object') {
+      el = RouterLink;
+      linkProps.target = target;
+      if (to.pathname.startsWith(`${window.Radarr.urlBase}/`)) {
+        linkProps.to = to;
+      } else {
+        const pathname = `${window.Radarr.urlBase}/${to.pathname.replace(/^\//, '')}`;
+        linkProps.to = {
+          ...to,
+          pathname
+        };
       }
     }
 
@@ -83,7 +95,7 @@ class Link extends Component {
 Link.propTypes = {
   className: PropTypes.string,
   component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-  to: PropTypes.string,
+  to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   target: PropTypes.string,
   isDisabled: PropTypes.bool,
   noRouter: PropTypes.bool,