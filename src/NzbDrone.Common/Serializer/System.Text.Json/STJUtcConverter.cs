7d7b2c9e2b597de2febd2b93ac2e499be987385a
ta264 <ta264@users.noreply.github.com>
2021-06-09 22:03:08 -0700

    Fixed: RSS sync paging back too far for some indexers

--- src/NzbDrone.Common/Serializer/System.Text.Json/STJUtcConverter.cs
+++ src/NzbDrone.Common/Serializer/System.Text.Json/STJUtcConverter.cs
@@ -8,7 +8,7 @@ namespace NzbDrone.Common.Serializer
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.Parse(reader.GetString());
+            return DateTime.Parse(reader.GetString()).ToUniversalTime();
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
