8eedae1af080fbac50cda4a6340a2b50478bae0f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-06-28 22:33:25 -0500

    Fixed: Migrate Legacy MediaInfo HDR token to VideoDynamicRange
    Closes #4132

---
+++ src/NzbDrone.Core/Datastore/Migration/196_legacy_mediainfo_hdr.cs
@@ -0,0 +1,14 @@
+using FluentMigrator;
+using NzbDrone.Core.Datastore.Migration.Framework;
+
+namespace NzbDrone.Core.Datastore.Migration
+{
+    [Migration(196)]
+    public class legacy_mediainfo_hdr : NzbDroneMigrationBase
+    {
+        protected override void MainDbUpgrade()
+        {
+            Execute.Sql("UPDATE NamingConfig SET StandardMovieFormat = Replace(StandardMovieFormat, '{MediaInfo HDR}', '{MediaInfo VideoDynamicRange}');");
+        }
+    }
+}
