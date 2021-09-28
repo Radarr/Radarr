88d516a6d4d9857059b750357cb18befd22d9d07
Qstick <qstick@gmail.com>
2021-09-27 22:58:04 -0500

    Cleanup Unused Tags Housekeeper
    Fixes #6535

--- src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupUnusedTags.cs
+++ src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupUnusedTags.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Dapper;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Datastore;
 
 namespace NzbDrone.Core.Housekeeping.Housekeepers
@@ -22,9 +23,9 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
                 var usedTags = new[] { "Movies", "Notifications", "DelayProfiles", "Restrictions", "ImportLists" }
                     .SelectMany(v => GetUsedTags(v, mapper))
                     .Distinct()
-                    .ToArray();
+                    .ToList();
 
-                var usedTagsList = string.Join(",", usedTags.Select(d => d.ToString()).ToArray());
+                var usedTagsList = usedTags.Select(d => d.ToString()).Join(",");
 
                 mapper.Execute($"DELETE FROM Tags WHERE NOT Id IN ({usedTagsList})");
             }
@@ -32,7 +33,7 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
 
         private int[] GetUsedTags(string table, IDbConnection mapper)
         {
-            return mapper.Query<List<int>>($"SELECT DISTINCT Tags FROM {table} WHERE NOT Tags = '[]'")
+            return mapper.Query<List<int>>($"SELECT DISTINCT Tags FROM {table} WHERE NOT Tags = '[]' AND NOT Tags IS NULL")
                 .SelectMany(x => x)
                 .Distinct()
                 .ToArray();
