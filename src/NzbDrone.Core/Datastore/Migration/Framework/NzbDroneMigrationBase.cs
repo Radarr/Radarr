4f8b0dd5cc680c9d6ce2bdd167eb3b91697593cd
Mark McDowall <mark@mcdowall.ca>
2021-10-02 19:00:25 -0700

    New: Log which DB is being migrated
    (cherry picked from commit 07c95f06d3b9b32aaeb923d4c678f10a2bf1141a)

--- src/NzbDrone.Core/Datastore/Migration/Framework/NzbDroneMigrationBase.cs
+++ src/NzbDrone.Core/Datastore/Migration/Framework/NzbDroneMigrationBase.cs
@@ -41,15 +41,18 @@ namespace NzbDrone.Core.Datastore.Migration.Framework
             switch (MigrationContext.Current.MigrationType)
             {
                 case MigrationType.Main:
-                    _logger.Info("Starting migration to " + Version);
+                    LogMigrationMessage(MigrationType.Main);
                     MainDbUpgrade();
                     return;
                 case MigrationType.Log:
-                    _logger.Info("Starting migration to " + Version);
+                    LogMigrationMessage(MigrationType.Log);
                     LogDbUpgrade();
                     return;
                 default:
+                    LogMigrationMessage(MigrationType.Log);
                     LogDbUpgrade();
+
+                    LogMigrationMessage(MigrationType.Main);
                     MainDbUpgrade();
                     return;
             }
@@ -58,6 +61,11 @@ namespace NzbDrone.Core.Datastore.Migration.Framework
         public override void Down()
         {
             throw new NotImplementedException();
+        }
+
+        private void LogMigrationMessage(MigrationType type)
+        {
+            _logger.Info("Starting migration of {0} DB to {1}", type.ToString(), Version);
         }
     }
 }
