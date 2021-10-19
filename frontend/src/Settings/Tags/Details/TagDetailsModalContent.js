Sonarr/Sonarr

New: Add tag support to indexers
Closes #487

frontend/src/Settings/Tags/Details/TagDetailsModalContent.js
@@ -21,6 +21,7 @@ function TagDetailsModalContent(props) {
     importLists,
     notifications,
     releaseProfiles,
+    indexers,
     onModalClose,
     onDeleteTagPress
   } = props;
@@ -38,7 +39,7 @@ function TagDetailsModalContent(props) {
         }
 
         {
-          !!series.length &&
+          series.length ?
             <FieldSet legend="Series">
               {
                 series.map((item) => {
@@ -49,11 +50,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!delayProfiles.length &&
+          delayProfiles.length ?
             <FieldSet legend="Delay Profile">
               {
                 delayProfiles.map((item) => {
@@ -78,11 +80,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!notifications.length &&
+          notifications.length ?
             <FieldSet legend="Connections">
               {
                 notifications.map((item) => {
@@ -93,11 +96,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!importLists.length &&
+          importLists.length ?
             <FieldSet legend="Import Lists">
               {
                 importLists.map((item) => {
@@ -108,11 +112,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!releaseProfiles.length &&
+          releaseProfiles.length ?
             <FieldSet legend="Release Profiles">
               {
                 releaseProfiles.map((item) => {
@@ -154,7 +159,24 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
+        }
+
+        {
+          indexers.length ?
+            <FieldSet legend="Indexers">
+              {
+                indexers.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      {item.name}
+                    </div>
+                  );
+                })
+              }
+            </FieldSet> :
+            null
         }
       </ModalBody>
 
@@ -189,6 +211,7 @@ TagDetailsModalContent.propTypes = {
   importLists: PropTypes.arrayOf(PropTypes.object).isRequired,
   notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
   releaseProfiles: PropTypes.arrayOf(PropTypes.object).isRequired,
+  indexers: PropTypes.arrayOf(PropTypes.object).isRequired,
   onModalClose: PropTypes.func.isRequired,
   onDeleteTagPress: PropTypes.func.isRequired
 };

frontend/src/Settings/Tags/TagsConnector.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import { fetchTagDetails } from 'Store/Actions/tagActions';
-import { fetchDelayProfiles, fetchNotifications, fetchReleaseProfiles, fetchImportLists } from 'Store/Actions/settingsActions';
+import { fetchDelayProfiles, fetchNotifications, fetchReleaseProfiles, fetchImportLists, fetchIndexers } from 'Store/Actions/settingsActions';
 import Tags from './Tags';
 
 function createMapStateToProps() {
@@ -29,7 +29,8 @@ const mapDispatchToProps = {
   dispatchFetchDelayProfiles: fetchDelayProfiles,
   dispatchFetchImportLists: fetchImportLists,
   dispatchFetchNotifications: fetchNotifications,
-  dispatchFetchReleaseProfiles: fetchReleaseProfiles
+  dispatchFetchReleaseProfiles: fetchReleaseProfiles,
+  dispatchFetchIndexers: fetchIndexers
 };
 
 class MetadatasConnector extends Component {
@@ -43,7 +44,8 @@ class MetadatasConnector extends Component {
       dispatchFetchDelayProfiles,
       dispatchFetchImportLists,
       dispatchFetchNotifications,
-      dispatchFetchReleaseProfiles
+      dispatchFetchReleaseProfiles,
+      dispatchFetchIndexers
     } = this.props;
 
     dispatchFetchTagDetails();
@@ -51,6 +53,7 @@ class MetadatasConnector extends Component {
     dispatchFetchImportLists();
     dispatchFetchNotifications();
     dispatchFetchReleaseProfiles();
+    dispatchFetchIndexers();
   }
 
   //
@@ -70,7 +73,8 @@ MetadatasConnector.propTypes = {
   dispatchFetchDelayProfiles: PropTypes.func.isRequired,
   dispatchFetchImportLists: PropTypes.func.isRequired,
   dispatchFetchNotifications: PropTypes.func.isRequired,
-  dispatchFetchReleaseProfiles: PropTypes.func.isRequired
+  dispatchFetchReleaseProfiles: PropTypes.func.isRequired,
+  dispatchFetchIndexers: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(MetadatasConnector);

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/IndexerTagSpecification.cs
@@ -1,0 +1,39 @@
+using System.Linq;
+using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
+{
+    public class IndexerTagSpecification : IDecisionEngineSpecification
+    {
+        private readonly Logger _logger;
+        private readonly IIndexerRepository _indexerRepository;
+
+        public IndexerTagSpecification(Logger logger, IIndexerRepository indexerRepository)
+        {
+            _logger = logger;
+            _indexerRepository = indexerRepository;
+        }
+
+        public SpecificationPriority Priority => SpecificationPriority.Default;
+        public RejectionType Type => RejectionType.Permanent;
+
+        public virtual Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
+        {
+            // If indexer has tags, check that at least one of them is present on the series
+            var indexerTags = _indexerRepository.Get(subject.Release.IndexerId).Tags;
+
+            if (indexerTags.Any() && indexerTags.Intersect(subject.Series.Tags).Empty())
+            {
+                _logger.Debug("Indexer {0} has tags. None of these are present on series {1}. Rejecting", subject.Release.Indexer, subject.Series);
+
+                return Decision.Reject("Series tags do not match any of the indexer tags");
+            }
+
+            return Decision.Accept();
+        }
+    }
+}

Radarr/Radarr

(cherry picked from commit c3d54b312ef18b837d54605ea78f1a263fd6900b)

frontend/src/Settings/Tags/Details/TagDetailsModalContent.js
@@ -22,6 +22,7 @@ function TagDetailsModalContent(props) {
     notifications,
     restrictions,
     importLists,
+    indexers,
     onModalClose,
     onDeleteTagPress
   } = props;
@@ -41,7 +42,7 @@ function TagDetailsModalContent(props) {
         }
 
         {
-          !!movies.length &&
+          movies.length ?
             <FieldSet legend={translate('Movies')}>
               {
                 movies.map((item) => {
@@ -52,11 +53,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!delayProfiles.length &&
+          delayProfiles.length ?
             <FieldSet legend={translate('DelayProfile')}>
               {
                 delayProfiles.map((item) => {
@@ -81,11 +83,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!notifications.length &&
+          notifications.length ?
             <FieldSet legend={translate('Connections')}>
               {
                 notifications.map((item) => {
@@ -96,11 +99,12 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
         }
 
         {
-          !!restrictions.length &&
+          restrictions.length ?
             <FieldSet legend={translate('Restrictions')}>
               {
                 restrictions.map((item) => {
@@ -142,7 +146,24 @@ function TagDetailsModalContent(props) {
                   );
                 })
               }
-            </FieldSet>
+            </FieldSet> :
+            null
+        }
+
+        {
+          indexers.length ?
+            <FieldSet legend="Indexers">
+              {
+                indexers.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      {item.name}
+                    </div>
+                  );
+                })
+              }
+            </FieldSet> :
+            null
         }
 
         {
@@ -192,6 +213,7 @@ TagDetailsModalContent.propTypes = {
   notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
   restrictions: PropTypes.arrayOf(PropTypes.object).isRequired,
   importLists: PropTypes.arrayOf(PropTypes.object).isRequired,
+  indexers: PropTypes.arrayOf(PropTypes.object).isRequired,
   onModalClose: PropTypes.func.isRequired,
   onDeleteTagPress: PropTypes.func.isRequired
 };

frontend/src/Settings/Tags/TagsConnector.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { fetchDelayProfiles, fetchImportLists, fetchNotifications, fetchRestrictions } from 'Store/Actions/settingsActions';
+import { fetchDelayProfiles, fetchImportLists, fetchIndexers, fetchNotifications, fetchRestrictions } from 'Store/Actions/settingsActions';
 import { fetchTagDetails } from 'Store/Actions/tagActions';
 import Tags from './Tags';
 
@@ -29,7 +29,8 @@ const mapDispatchToProps = {
   dispatchFetchDelayProfiles: fetchDelayProfiles,
   dispatchFetchNotifications: fetchNotifications,
   dispatchFetchRestrictions: fetchRestrictions,
-  dispatchFetchImportLists: fetchImportLists
+  dispatchFetchImportLists: fetchImportLists,
+  dispatchFetchIndexers: fetchIndexers
 };
 
 class MetadatasConnector extends Component {
@@ -43,7 +44,8 @@ class MetadatasConnector extends Component {
       dispatchFetchDelayProfiles,
       dispatchFetchNotifications,
       dispatchFetchRestrictions,
-      dispatchFetchImportLists
+      dispatchFetchImportLists,
+      dispatchFetchIndexers
     } = this.props;
 
     dispatchFetchTagDetails();
@@ -51,6 +53,7 @@ class MetadatasConnector extends Component {
     dispatchFetchNotifications();
     dispatchFetchRestrictions();
     dispatchFetchImportLists();
+    dispatchFetchIndexers();
   }
 
   //
@@ -70,7 +73,8 @@ MetadatasConnector.propTypes = {
   dispatchFetchDelayProfiles: PropTypes.func.isRequired,
   dispatchFetchNotifications: PropTypes.func.isRequired,
   dispatchFetchRestrictions: PropTypes.func.isRequired,
-  dispatchFetchImportLists: PropTypes.func.isRequired
+  dispatchFetchImportLists: PropTypes.func.isRequired,
+  dispatchFetchIndexers: PropTypes.func.isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(MetadatasConnector);

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/IndexerTagSpecification.cs
@@ -1,0 +1,39 @@
+using System.Linq;
+using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.IndexerSearch.Definitions;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
+{
+    public class IndexerTagSpecification : IDecisionEngineSpecification
+    {
+        private readonly Logger _logger;
+        private readonly IIndexerRepository _indexerRepository;
+
+        public IndexerTagSpecification(Logger logger, IIndexerRepository indexerRepository)
+        {
+            _logger = logger;
+            _indexerRepository = indexerRepository;
+        }
+
+        public SpecificationPriority Priority => SpecificationPriority.Default;
+        public RejectionType Type => RejectionType.Permanent;
+
+        public virtual Decision IsSatisfiedBy(RemoteMovie subject, SearchCriteriaBase searchCriteria)
+        {
+            // If indexer has tags, check that at least one of them is present on the series
+            var indexerTags = _indexerRepository.Get(subject.Release.IndexerId).Tags;
+
+            if (indexerTags.Any() && indexerTags.Intersect(subject.Movie.Tags).Empty())
+            {
+                _logger.Debug("Indexer {0} has tags. None of these are present on movie {1}. Rejecting", subject.Release.Indexer, subject.Movie);
+
+                return Decision.Reject("Movie tags do not match any of the indexer tags");
+            }
+
+            return Decision.Accept();
+        }
+    }
+}