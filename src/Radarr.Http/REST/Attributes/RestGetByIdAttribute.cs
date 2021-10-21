8cad9600cc0fb4cc0f7a1489814ae492bc803bb5
ta264 <ta264@users.noreply.github.com>
2021-10-21 21:04:26 +0100

    Tidy conversion to aspnetcore
    (cherry picked from commit 490f6e2e6aa3f220cc98f257a3ca3b2bea48fb80)
    (cherry picked from commit 8f3f90d4078d9d072d8ad4ccc3be35963b7435d6)
    (cherry picked from commit 2b0da546c9dae40fbc1b2654387be80a17c1848f)

--- src/Radarr.Http/REST/Attributes/RestGetByIdAttribute.cs
+++ src/Radarr.Http/REST/Attributes/RestGetByIdAttribute.cs
@@ -1,21 +1,15 @@
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Radarr.Http.REST.Attributes
 {
     [AttributeUsage(AttributeTargets.Method)]
-    public class RestGetByIdAttribute : ActionFilterAttribute, IActionHttpMethodProvider, IRouteTemplateProvider
+    public class RestGetByIdAttribute : Attribute, IActionHttpMethodProvider, IRouteTemplateProvider
     {
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            Console.WriteLine($"OnExecuting {context.Controller.GetType()} {context.ActionDescriptor.DisplayName}");
-        }
-
         public IEnumerable<string> HttpMethods => new[] { "GET" };
         public string Template => "{id:int}";
-        public new int? Order => 0;
+        public int? Order => 0;
         public string Name { get; }
     }
 }
