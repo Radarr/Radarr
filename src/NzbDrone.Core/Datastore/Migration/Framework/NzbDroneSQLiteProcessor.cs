f844cbecaf3940cb3efacd16d58bb5d70687076b
Qstick <qstick@gmail.com>
2021-10-31 20:58:38 -0500

    Bump FluentMigrator to 3.3.1
    [common]

--- src/NzbDrone.Core/Datastore/Migration/Framework/NzbDroneSQLiteProcessor.cs
+++ src/NzbDrone.Core/Datastore/Migration/Framework/NzbDroneSQLiteProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -20,8 +20,9 @@ namespace NzbDrone.Core.Datastore.Migration.Framework
                                        ILogger<NzbDroneSQLiteProcessor> logger,
                                        IOptionsSnapshot<ProcessorOptions> options,
                                        IConnectionStringAccessor connectionStringAccessor,
-                                       IServiceProvider serviceProvider)
-        : base(factory, generator, logger, options, connectionStringAccessor, serviceProvider)
+                                       IServiceProvider serviceProvider,
+                                       SQLiteQuoter quoter)
+        : base(factory, generator, logger, options, connectionStringAccessor, serviceProvider, quoter)
         {
         }
 
