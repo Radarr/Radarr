57a141ebf8f5385fdf242c453c499157057867d5
Mark McDowall <mark@mcdowall.ca>
2021-11-21 11:39:22 -0800

    Fixed: Get full path for download station instead of shared folder
    Closes #6751
    Fixes #6818
    
    (cherry picked from commit b184e62fa7dd7ecd089619f176e6388c1c3be25d)

--- src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
+++ src/NzbDrone.Core/Download/Clients/DownloadStation/UsenetDownloadStation.cs
@@ -142,14 +142,17 @@ namespace NzbDrone.Core.Download.Clients.DownloadStation
             try
             {
                 var serialNumber = _serialNumberProvider.GetSerialNumber(Settings);
-                var sharedFolder = GetDownloadDirectory() ?? GetDefaultDir();
-                var outputPath = new OsPath($"/{sharedFolder.TrimStart('/')}");
-                var path = _sharedFolderResolver.RemapToFullPath(outputPath, Settings, serialNumber);
+
+                // Download station returns the path without the leading `/`, but the leading
+                // slash is required to get the full path back from download station.
+                var path = new OsPath($"/{GetDownloadDirectory()}");
+
+                var fullPath = _sharedFolderResolver.RemapToFullPath(path, Settings, serialNumber);
 
                 return new DownloadClientInfo
                 {
                     IsLocalhost = Settings.Host == "127.0.0.1" || Settings.Host == "localhost",
-                    OutputRootFolders = new List<OsPath> { _remotePathMappingService.RemapRemoteToLocal(Settings.Host, path) }
+                    OutputRootFolders = new List<OsPath> { _remotePathMappingService.RemapRemoteToLocal(Settings.Host, fullPath) }
                 };
             }
             catch (DownloadClientException e)
