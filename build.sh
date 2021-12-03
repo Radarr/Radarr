Readarr/Readarr

New: Add osx-arm64 and linux-musl-arm builds

build.sh
@@ -140,12 +140,13 @@ PackageLinux()
 PackageMacOS()
 {
     local framework="$1"
+    local runtime="$2"
     
-    ProgressStart "Creating MacOS Package for $framework"
+    ProgressStart "Creating MacOS Package for $framework $runtime"
 
-    local folder=$artifactsFolder/macos/$framework/Readarr
+    local folder=$artifactsFolder/$runtime/$framework/Readarr
 
-    PackageFiles "$folder" "$framework" "osx-x64"
+    PackageFiles "$folder" "$framework" "$runtime"
 
     echo "Removing Service helpers"
     rm -f $folder/ServiceUninstall.*
@@ -167,10 +168,11 @@ PackageMacOS()
 PackageMacOSApp()
 {
     local framework="$1"
+    local runtime="$2"
     
-    ProgressStart "Creating macOS App Package for $framework"
+    ProgressStart "Creating macOS App Package for $framework $runtime"
 
-    local folder=$artifactsFolder/macos-app/$framework
+    local folder="$artifactsFolder/$runtime-app/$framework"
 
     rm -rf $folder
     mkdir -p $folder
@@ -178,7 +180,7 @@ PackageMacOSApp()
     mkdir -p $folder/Readarr.app/Contents/MacOS
 
     echo "Copying Binaries"
-    cp -r $artifactsFolder/macos/$framework/Readarr/* $folder/Readarr.app/Contents/MacOS
+    cp -r $artifactsFolder/$runtime/$framework/Readarr/* $folder/Readarr.app/Contents/MacOS
 
     echo "Removing Update Folder"
     rm -r $folder/Readarr.app/Contents/MacOS/Readarr.Update
@@ -225,8 +227,8 @@ Package()
             PackageWindows "$framework" "$runtime"
             ;;
         osx)
-            PackageMacOS "$framework"
-            PackageMacOSApp "$framework"
+            PackageMacOS "$framework" "$runtime"
+            PackageMacOSApp "$framework" "$runtime"
             ;;
     esac
 }
@@ -369,7 +371,9 @@ then
         Package "net6.0" "linux-arm64"
         Package "net6.0" "linux-musl-arm64"
         Package "net6.0" "linux-arm"
+        Package "net6.0" "linux-musl-arm"
         Package "net6.0" "osx-x64"
+        Package "net6.0" "osx-arm64"
         if [ "$ENABLE_BSD" = "YES" ];
         then
             Package "net6.0" "freebsd-x64"

Radarr/Radarr

(cherry picked from commit 4ce405728a3ae32c8d0282e8e2b758084e8e5864)

build.sh
@@ -140,12 +140,13 @@ PackageLinux()
 PackageMacOS()
 {
     local framework="$1"
+    local runtime="$2"
     
-    ProgressStart "Creating MacOS Package for $framework"
+    ProgressStart "Creating MacOS Package for $framework $runtime"
 
-    local folder=$artifactsFolder/macos/$framework/Radarr
+    local folder=$artifactsFolder/$runtime/$framework/Radarr
 
-    PackageFiles "$folder" "$framework" "osx-x64"
+    PackageFiles "$folder" "$framework" "$runtime"
 
     echo "Removing Service helpers"
     rm -f $folder/ServiceUninstall.*
@@ -167,10 +168,11 @@ PackageMacOS()
 PackageMacOSApp()
 {
     local framework="$1"
+    local runtime="$2"
     
-    ProgressStart "Creating macOS App Package for $framework"
+    ProgressStart "Creating macOS App Package for $framework $runtime"
 
-    local folder=$artifactsFolder/macos-app/$framework
+    local folder="$artifactsFolder/$runtime-app/$framework"
 
     rm -rf $folder
     mkdir -p $folder
@@ -178,7 +180,7 @@ PackageMacOSApp()
     mkdir -p $folder/Radarr.app/Contents/MacOS
 
     echo "Copying Binaries"
-    cp -r $artifactsFolder/macos/$framework/Radarr/* $folder/Radarr.app/Contents/MacOS
+    cp -r $artifactsFolder/$runtime/$framework/Radarr/* $folder/Radarr.app/Contents/MacOS
 
     echo "Removing Update Folder"
     rm -r $folder/Radarr.app/Contents/MacOS/Radarr.Update
@@ -225,8 +227,8 @@ Package()
             PackageWindows "$framework" "$runtime"
             ;;
         osx)
-            PackageMacOS "$framework"
-            PackageMacOSApp "$framework"
+            PackageMacOS "$framework" "$runtime"
+            PackageMacOSApp "$framework" "$runtime"
             ;;
     esac
 }
@@ -369,7 +371,9 @@ then
         Package "net6.0" "linux-arm64"
         Package "net6.0" "linux-musl-arm64"
         Package "net6.0" "linux-arm"
+        Package "net6.0" "linux-musl-arm"
         Package "net6.0" "osx-x64"
+        Package "net6.0" "osx-arm64"
         if [ "$ENABLE_BSD" = "YES" ];
         then
             Package "net6.0" "freebsd-x64"