c2e5686bcf51c19c3c144b0380e29db3d0a98a0b
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-01-23 14:10:44 -0600

    fixed sonarr ref in UTorrentSettings

--- src/NzbDrone.Core/Download/Clients/uTorrent/UTorrentSettings.cs
+++ src/NzbDrone.Core/Download/Clients/uTorrent/UTorrentSettings.cs
@@ -49,7 +49,7 @@ namespace NzbDrone.Core.Download.Clients.UTorrent
         [FieldDefinition(6, Label = "Category", Type = FieldType.Textbox, HelpText = "Adding a category specific to Radarr avoids conflicts with unrelated downloads, but it's optional")]
         public string MovieCategory { get; set; }
 
-        [FieldDefinition(7, Label = "Post-Import Category", Type = FieldType.Textbox, Advanced = true, HelpText = "Category for Radarr to set after it has imported the download. Sonarr will not remove the torrent if seeding has finished. Leave blank to keep same category.")]
+        [FieldDefinition(7, Label = "Post-Import Category", Type = FieldType.Textbox, Advanced = true, HelpText = "Category for Radarr to set after it has imported the download. Radarr will not remove the torrent if seeding has finished. Leave blank to keep same category.")]
         public string MovieImportedCategory { get; set; }
 
         [FieldDefinition(8, Label = "Recent Priority", Type = FieldType.Select, SelectOptions = typeof(UTorrentPriority), HelpText = "Priority to use when grabbing movies that aired within the last 21 days")]
