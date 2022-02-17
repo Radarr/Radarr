d0bfdce9c5ceb13daa20e9dcca8086531a88dc4e
Mark McDowall <mark@mcdowall.ca>
2022-02-16 18:15:26 -0800

    Fixed: Sorting of some titles with acronyms or common words at the start
    (cherry picked from commit 79436149eb6869033d2263cd9558dbe75b1d3a68)

--- src/NzbDrone.Core/Movies/MovieTitleNormalizer.cs
+++ src/NzbDrone.Core/Movies/MovieTitleNormalizer.cs
@@ -4,16 +4,19 @@ namespace NzbDrone.Core.Movies
 {
     public static class MovieTitleNormalizer
     {
-        private static readonly Dictionary<int, string> PreComputedTitles = new Dictionary<int, string>
-                                                                     {
-                                                                         { 999999999, "a to z" },
-                                                                     };
-
-        public static string Normalize(string title, int tmdbid)
+        private static readonly Dictionary<int, string> PreComputedTitles = new ()
         {
-            if (PreComputedTitles.ContainsKey(tmdbid))
+            { 387354, "a to z" },
+            { 1212922, "a to z" },
+            { 888700, "a to z the first alphabet" },
+            { 101273, "a to zeppelin the story of led zeppelin" },
+        };
+
+        public static string Normalize(string title, int tmdbId)
+        {
+            if (PreComputedTitles.TryGetValue(tmdbId, out var value))
             {
-                return PreComputedTitles[tmdbid];
+                return value;
             }
 
             return Parser.Parser.NormalizeTitle(title).ToLower();
