3d2c37c855b9a208074b7750cfda49c718a830fa
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-02-21 14:15:32 +0000

    Fixed: Remove pre-DB from frontend storage

--- frontend/src/Store/Migrators/migrate.js
+++ frontend/src/Store/Migrators/migrate.js
@@ -1,5 +1,7 @@
 import migrateBlacklistToBlocklist from './migrateBlacklistToBlocklist';
+import migratePreDbToReleased from './migratePreDbToReleased';
 
 export default function migrate(persistedState) {
   migrateBlacklistToBlocklist(persistedState);
+  migratePreDbToReleased(persistedState);
 }
