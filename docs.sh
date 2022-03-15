76a3ca37edeee4e92e3a101bf3c21be1a006f6d8
Qstick <qstick@gmail.com>
2022-03-14 22:25:17 -0500

    Bump Swashbuckle to 6.3.0

--- docs.sh
+++ docs.sh
@@ -27,7 +27,7 @@ dotnet clean $slnFile -c Release
 dotnet msbuild -restore $slnFile -p:Configuration=Debug -p:Platform=$platform -p:RuntimeIdentifiers=$RUNTIME -t:PublishAllRids
 
 dotnet new tool-manifest
-dotnet tool install --version 6.2.3 Swashbuckle.AspNetCore.Cli
+dotnet tool install --version 6.3.0 Swashbuckle.AspNetCore.Cli
 
 dotnet tool run swagger tofile --output ./src/Radarr.Api.V3/openapi.json "$outputFolder/net6.0/$RUNTIME/radarr.console.dll" v3 &
 
