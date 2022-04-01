Sonarr/Sonarr

Fixed: Scrolling in Firefox in small window (requires refresh)
Closes #4403

frontend/src/Components/Page/PageContentBody.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { isMobile as isMobileUtil } from 'Utilities/mobile';
+import { isMobile, isFirefox } from 'Utilities/browser';
 import { isLocked } from 'Utilities/scrollLock';
 import { scrollDirections } from 'Helpers/Props';
 import OverlayScroller from 'Components/Scroller/OverlayScroller';
@@ -15,7 +15,8 @@ class PageContentBody extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this._isMobile = isMobileUtil();
+    this._isMobile = isMobile();
+    this._isSmallScreenFirefox = isFirefox && window.innerWidth < 768;
   }
 
   //
@@ -41,7 +42,9 @@ class PageContentBody extends Component {
       ...otherProps
     } = this.props;
 
-    const ScrollerComponent = this._isMobile ? Scroller : OverlayScroller;
+    const ScrollerComponent = this._isMobile || this._isSmallScreenFirefox ?
+      Scroller :
+      OverlayScroller;
 
     return (
       <ScrollerComponent

Radarr/Radarr

Closes #7142
(cherry picked from commit e3aa92d09a247f696a003b87f6e9511c33340a1d)

frontend/src/Components/Page/PageContentBody.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import OverlayScroller from 'Components/Scroller/OverlayScroller';
 import Scroller from 'Components/Scroller/Scroller';
 import { scrollDirections } from 'Helpers/Props';
-import { isMobile as isMobileUtil } from 'Utilities/mobile';
+import { isFirefox, isMobile } from 'Utilities/browser';
 import { isLocked } from 'Utilities/scrollLock';
 import styles from './PageContentBody.css';
 
@@ -15,7 +15,8 @@ class PageContentBody extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this._isMobile = isMobileUtil();
+    this._isMobile = isMobile();
+    this._isSmallScreenFirefox = isFirefox && window.innerWidth < 768;
   }
 
   //
@@ -41,7 +42,9 @@ class PageContentBody extends Component {
       ...otherProps
     } = this.props;
 
-    const ScrollerComponent = this._isMobile ? Scroller : OverlayScroller;
+    const ScrollerComponent = this._isMobile || this._isSmallScreenFirefox ?
+      Scroller :
+      OverlayScroller;
 
     return (
       <ScrollerComponent