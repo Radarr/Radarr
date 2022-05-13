b7e10e6c5a10f91a9e6494de19a271cb4623d8b6
Qstick <qstick@gmail.com>
2022-05-12 19:06:14 -0500

    New: Instance name for Page Title

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -8,7 +8,7 @@ import AppRoutes from './AppRoutes';
 
 function App({ store, history }) {
   return (
-    <DocumentTitle title="Radarr">
+    <DocumentTitle title={window.Radarr.instanceName}>
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <PageConnector>
