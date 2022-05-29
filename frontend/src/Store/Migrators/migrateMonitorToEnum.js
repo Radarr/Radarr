63dde3eb89b7808f8e881c2a9f5022a30f5bab5a
ta264 <ta264@users.noreply.github.com>
2022-05-29 22:49:03 +0100

    Fix frontend monitor migration

--- frontend/src/Store/Migrators/migrateMonitorToEnum.js
+++ frontend/src/Store/Migrators/migrateMonitorToEnum.js
@@ -4,22 +4,22 @@ export default function migrateMonitorToEnum(persistedState) {
   const addMovie = get(persistedState, 'addMovie.defaults.monitor');
   const discoverMovie = get(persistedState, 'discoverMovie.defaults.monitor');
 
-  if (addMovie != null) {
-    if (addMovie === true) {
+  if (addMovie) {
+    if (addMovie === 'true') {
       persistedState.addMovie.defaults.monitor = 'movieOnly';
     }
 
-    if (addMovie === false) {
+    if (addMovie === 'false') {
       persistedState.addMovie.defaults.monitor = 'none';
     }
   }
 
-  if (discoverMovie != null) {
-    if (discoverMovie === true) {
+  if (discoverMovie) {
+    if (discoverMovie === 'true') {
       persistedState.discoverMovie.defaults.monitor = 'movieOnly';
     }
 
-    if (discoverMovie === false) {
+    if (discoverMovie === 'false') {
       persistedState.discoverMovie.defaults.monitor = 'none';
     }
   }
