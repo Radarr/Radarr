aa0104b6bc9ceb84485db30622e60bc2d0caa42a
Qstick <qstick@gmail.com>
2022-07-02 13:47:59 -0500

    Fixed: Housekeeper doesn't remove collections that have MovieMeta from lists

--- src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupOrphanedCollectionsFixture.cs
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupOrphanedCollectionsFixture.cs
@@ -26,7 +26,7 @@ namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
         }
 
         [Test]
-        public void should_not_delete_unorphaned_collection_items()
+        public void should_delete_orphaned_collection_with_meta_but_no_movie_items()
         {
             var collection = Builder<MovieCollection>.CreateNew()
                                               .With(h => h.Id = 3)
@@ -38,6 +38,27 @@ namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
 
             var movie = Builder<MovieMetadata>.CreateNew().With(m => m.CollectionTmdbId = collection.TmdbId).BuildNew();
 
+            Db.Insert(movie);
+
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(0);
+        }
+
+        [Test]
+        public void should_not_delete_unorphaned_collection()
+        {
+            var collection = Builder<MovieCollection>.CreateNew()
+                                              .With(h => h.Id = 3)
+                                              .With(h => h.TmdbId = 123456)
+                                              .With(h => h.Title = "Some Credit")
+                                              .BuildNew();
+
+            Db.Insert(collection);
+
+            var movieMeta = Builder<MovieMetadata>.CreateNew().With(m => m.CollectionTmdbId = collection.TmdbId).BuildNew();
+            Db.Insert(movieMeta);
+
+            var movie = Builder<Movie>.CreateNew().With(m => m.MovieMetadataId = movieMeta.Id).BuildNew();
             Db.Insert(movie);
 
             Subject.Clean();
