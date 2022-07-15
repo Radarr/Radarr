e89b98d0f6846efaca7ffe399895cbb52de8af25
Qstick <qstick@gmail.com>
2022-07-14 22:08:11 -0500

    Fixed: Avoid NullRef in MapMovieToTmdbMovie

--- src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
+++ src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -337,7 +337,7 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
                         yearStr = $" {movie.Year}";
                     }
 
-                    newMovie = SearchForNewMovie(movie.Title + yearStr).FirstOrDefault().MovieMetadata;
+                    newMovie = SearchForNewMovie(movie.Title + yearStr).FirstOrDefault()?.MovieMetadata ?? null;
                 }
 
                 if (newMovie == null)
