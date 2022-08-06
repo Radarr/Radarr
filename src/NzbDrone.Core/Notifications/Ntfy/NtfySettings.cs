2031863e9fb743a72828e5607d731bcc900e8a3b
psylenced <1967906+psylenced@users.noreply.github.com>
2022-08-06 17:47:36 +1000

    Changed: Clean up of validation and fields for ntfy

--- src/NzbDrone.Core/Notifications/Ntfy/NtfySettings.cs
+++ src/NzbDrone.Core/Notifications/Ntfy/NtfySettings.cs
@@ -17,6 +17,8 @@ namespace NzbDrone.Core.Notifications.Ntfy
             RuleFor(c => c.Title).NotEmpty();
             RuleFor(c => c.ServerUrl).IsValidUrl().When(c => !c.ServerUrl.IsNullOrWhiteSpace());
             RuleFor(c => c.ClickUrl).IsValidUrl().When(c => !c.ClickUrl.IsNullOrWhiteSpace());
+            RuleFor(c => c.UserName).NotEmpty().When(c => !c.Password.IsNullOrWhiteSpace());
+            RuleFor(c => c.Password).NotEmpty().When(c => !c.UserName.IsNullOrWhiteSpace());
             RuleForEach(c => c.Topics).NotEmpty().Matches("[a-zA-Z0-9_-]+").Must(c => !InvalidTopics.Contains(c)).WithMessage("Invalid topic");
         }
 
@@ -33,7 +35,7 @@ namespace NzbDrone.Core.Notifications.Ntfy
             Priority = 3;
         }
 
-        [FieldDefinition(0, Label = "Server Url", Type = FieldType.Url, HelpLink = "https://ntfy.sh", HelpText = "Leave blank for https://ntfy.sh")]
+        [FieldDefinition(0, Label = "Server Url", Type = FieldType.Url, HelpLink = "https://ntfy.sh/docs/install/", HelpText = "Leave blank to use public server (https://ntfy.sh)", Placeholder = "https://ntfy.sh")]
         public string ServerUrl { get; set; }
 
         [FieldDefinition(1, Label = "User Name", HelpText = "Optional Authorisation", Privacy = PrivacyLevel.UserName)]
@@ -42,7 +44,7 @@ namespace NzbDrone.Core.Notifications.Ntfy
         [FieldDefinition(2, Label = "Password", Type = FieldType.Password, HelpText = "Optional Password", Privacy = PrivacyLevel.Password)]
         public string Password { get; set; }
 
-        [FieldDefinition(4, Label = "Message Title", Placeholder = "Radarr alert")]
+        [FieldDefinition(4, Label = "Message Title", Placeholder = "Radarr Notification")]
         public string Title { get; set; }
 
         [FieldDefinition(5, Label = "Priority", Type = FieldType.Select, SelectOptions = typeof(NtfyPriority))]
@@ -51,10 +53,10 @@ namespace NzbDrone.Core.Notifications.Ntfy
         [FieldDefinition(6, Label = "Topics", HelpText = "List of Topics to send notifications to", Type = FieldType.Tag, Placeholder = "Topic1234,Topic4321")]
         public IEnumerable<string> Topics { get; set; }
 
-        [FieldDefinition(7, Label = "Tags", Type = FieldType.Tag, HelpText = "Optional list of tags to use", Placeholder = "warning,skull", HelpLink = "https://ntfy.sh/docs/emojis/")]
+        [FieldDefinition(7, Label = "Ntfy Tags and Emojis", Type = FieldType.Tag, HelpText = "Optional list of tags or emojis to use", Placeholder = "warning,skull", HelpLink = "https://ntfy.sh/docs/emojis/")]
         public IEnumerable<string> Tags { get; set; }
 
-        [FieldDefinition(8, Label = "Click Url", Type = FieldType.Url, HelpText = "Optional url to direct user to on alert", Placeholder = "https://radarr.io")]
+        [FieldDefinition(8, Label = "Click Url", Type = FieldType.Url, HelpText = "Optional link when user clicks notification", Placeholder = "https://myserver.example.com/radarr")]
         public string ClickUrl { get; set; }
 
         public NzbDroneValidationResult Validate()
