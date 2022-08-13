5d8775ac96d4b7de1b4381c463e71cf035a3f942
Qstick <qstick@gmail.com>
2022-08-12 22:42:38 -0500

    Remove non-functional filters for Trakt Lists
    Fixes #7464

--- src/NzbDrone.Core/ImportLists/Trakt/TraktSettingsBase.cs
+++ src/NzbDrone.Core/ImportLists/Trakt/TraktSettingsBase.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text.RegularExpressions;
 using FluentValidation;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Annotations;
@@ -29,24 +28,6 @@ namespace NzbDrone.Core.ImportLists.Trakt
                                    .WithMessage("Must authenticate with Trakt")
                                    .When(c => c.AccessToken.IsNotNullOrWhiteSpace() && c.RefreshToken.IsNotNullOrWhiteSpace());
 
-            // Loose validation @TODO
-            RuleFor(c => c.Rating)
-                .Matches(@"^\d+\-\d+$", RegexOptions.IgnoreCase)
-                .When(c => c.Rating.IsNotNullOrWhiteSpace())
-                .WithMessage("Not a valid rating");
-
-            // Any valid certification
-            RuleFor(c => c.Certification)
-                .Matches(@"^\bNR\b|\bG\b|\bPG\b|\bPG\-13\b|\bR\b|\bNC\-17\b$", RegexOptions.IgnoreCase)
-                .When(c => c.Certification.IsNotNullOrWhiteSpace())
-                .WithMessage("Not a valid cerification");
-
-            // Loose validation @TODO
-            RuleFor(c => c.Years)
-                .Matches(@"^\d+(\-\d+)?$", RegexOptions.IgnoreCase)
-                .When(c => c.Years.IsNotNullOrWhiteSpace())
-                .WithMessage("Not a valid year or range of years");
-
             // Limit not smaller than 1 and not larger than 100
             RuleFor(c => c.Limit)
                 .GreaterThan(0)
@@ -62,10 +43,6 @@ namespace NzbDrone.Core.ImportLists.Trakt
         public TraktSettingsBase()
         {
             SignIn = "startOAuth";
-            Rating = "0-100";
-            Certification = "NR,G,PG,PG-13,R,NC-17";
-            Genres = "";
-            Years = "";
             Limit = 100;
         }
 
@@ -83,18 +60,6 @@ namespace NzbDrone.Core.ImportLists.Trakt
 
         [FieldDefinition(0, Label = "Auth User", Type = FieldType.Textbox, Hidden = HiddenType.Hidden)]
         public string AuthUser { get; set; }
-
-        [FieldDefinition(1, Label = "Rating", HelpText = "Filter movies by rating range (0-100)")]
-        public string Rating { get; set; }
-
-        [FieldDefinition(2, Label = "Certification", HelpText = "Filter movies by a certification (NR,G,PG,PG-13,R,NC-17), (Comma Separated)")]
-        public string Certification { get; set; }
-
-        [FieldDefinition(3, Label = "Genres", HelpText = "Filter movies by Trakt Genre Slug (Comma Separated) Only for Popular Lists")]
-        public string Genres { get; set; }
-
-        [FieldDefinition(4, Label = "Years", HelpText = "Filter movies by year or year range")]
-        public string Years { get; set; }
 
         [FieldDefinition(5, Label = "Limit", HelpText = "Limit the number of movies to get")]
         public int Limit { get; set; }
