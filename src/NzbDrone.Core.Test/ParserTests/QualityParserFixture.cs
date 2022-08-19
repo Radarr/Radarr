bfd38490f499645d787cc5043207d85c19d2840e
ItsME6969 <89707006+ItsME6969@users.noreply.github.com>
2022-08-18 18:27:08 -0700

    Update QualityParserFixture.cs

--- src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
@@ -270,7 +270,6 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Movie.Name.2011.1080p.UHD.BluRay.DD5.1.HDR.x265-CtrlHD.mkv", false)]
         [TestCase("Movie.Name.2016.German.DTS.DL.1080p.UHDBD.x265-TDO.mkv", false)]
         [TestCase("Movie.Name.2021.1080p.BDLight.x265-AVCDVD", false)]
-        [TestCase("Random.Title.2010.1080p.HD.DVD.AVC.DDP.5.1-GRouP", false)]
         [TestCase("Movie.Title.2012.German.DL.1080p.UHD2BD.x264-QfG", false)]
         public void should_parse_bluray1080p_quality(string title, bool proper)
         {
@@ -350,8 +349,8 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Movie.Title.2008.BONUS.GERMAN.SUBBED.COMPLETE.BLURAY")]
         [TestCase("The German 2021 Bluray AVC")]
         [TestCase("German.Only.Movie.2021.French.1080p.BluRay.AVC-UNTAVC")]
-        [TestCase("Movie.Title.2008.5xBlu-ray 1080p AVC DTS-HD MA 5.1")]
         [TestCase("Movie.Title.2008.US.Directors.Cut.UHD.BD66.Blu-ray")]
+        [TestCase("Movie Title 2010 720p Blu-ray AVC DTS-HD MA 5.1")]
         public void should_parse_brdisk_1080p_quality(string title)
         {
             ParseAndVerifyQuality(title, Source.BLURAY, false, Resolution.R1080p, Modifier.BRDISK);
