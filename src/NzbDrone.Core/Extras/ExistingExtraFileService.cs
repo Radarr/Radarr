Sonarr/Sonarr

Fixed: Skip extras in 'Extras' subfolder
Closes #4980

src/NzbDrone.Core/Extras/ExistingExtraFileService.cs
@@ -31,7 +31,6 @@ namespace NzbDrone.Core.Extras
         public void Handle(SeriesScannedEvent message)
         {
             var series = message.Series;
-            var extraFiles = new List<ExtraFile>();
 
             if (!_diskProvider.FolderExists(series.Path))
             {
@@ -41,19 +40,18 @@ namespace NzbDrone.Core.Extras
             _logger.Debug("Looking for existing extra files in {0}", series.Path);
 
             var filesOnDisk = _diskScanService.GetNonVideoFiles(series.Path);
-            var possibleExtraFiles = _diskScanService.FilterPaths(series.Path, filesOnDisk, false);
+            var possibleExtraFiles = _diskScanService.FilterPaths(series.Path, filesOnDisk);
 
-            var filteredFiles = possibleExtraFiles;
             var importedFiles = new List<string>();
 
             foreach (var existingExtraFileImporter in _existingExtraFileImporters)
             {
-                var imported = existingExtraFileImporter.ProcessFiles(series, filteredFiles, importedFiles);
+                var imported = existingExtraFileImporter.ProcessFiles(series, possibleExtraFiles, importedFiles);
 
                 importedFiles.AddRange(imported.Select(f => Path.Combine(series.Path, f.RelativePath)));
             }
 
-            _logger.Info("Found {0} extra files", extraFiles.Count);
+            _logger.Info("Found {0} possible extra files, imported {1} files.", possibleExtraFiles.Count, importedFiles.Count);
         }
     }
 }

Radarr/Radarr

src/NzbDrone.Core/Extras/ExistingExtraFileService.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using NLog;
 using NzbDrone.Common.Disk;
-using NzbDrone.Core.Extras.Files;
 using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.MediaFiles.Events;
 using NzbDrone.Core.Messaging.Events;
@@ -31,7 +30,6 @@ namespace NzbDrone.Core.Extras
         public void Handle(MovieScannedEvent message)
         {
             var movie = message.Movie;
-            var extraFiles = new List<ExtraFile>();
 
             if (!_diskProvider.FolderExists(movie.Path))
             {
@@ -43,17 +41,16 @@ namespace NzbDrone.Core.Extras
             var filesOnDisk = _diskScanService.GetNonVideoFiles(movie.Path);
             var possibleExtraFiles = _diskScanService.FilterPaths(movie.Path, filesOnDisk, false);
 
-            var filteredFiles = possibleExtraFiles;
             var importedFiles = new List<string>();
 
             foreach (var existingExtraFileImporter in _existingExtraFileImporters)
             {
-                var imported = existingExtraFileImporter.ProcessFiles(movie, filteredFiles, importedFiles);
+                var imported = existingExtraFileImporter.ProcessFiles(movie, possibleExtraFiles, importedFiles);
 
                 importedFiles.AddRange(imported.Select(f => Path.Combine(movie.Path, f.RelativePath)));
             }
 
-            _logger.Info("Found {0} extra files", extraFiles.Count);
+            _logger.Info("Found {0} possible extra files, imported {1} files.", possibleExtraFiles.Count, importedFiles.Count);
         }
     }
 }