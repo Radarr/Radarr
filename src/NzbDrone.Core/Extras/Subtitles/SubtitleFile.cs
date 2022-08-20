5d9d3a84ccb25ff24f4f3dcc6ef567c82b38f112
Qstick <qstick@gmail.com>
2022-08-20 17:24:42 -0500

    Fixed: Fix NullRef in Subtitle Service due to #4924
    (cherry picked from commit 89ee7d4452ae3b476e24aede89b668a3df2344fc)

--- src/NzbDrone.Core/Extras/Subtitles/SubtitleFile.cs
+++ src/NzbDrone.Core/Extras/Subtitles/SubtitleFile.cs
@@ -5,6 +5,11 @@ namespace NzbDrone.Core.Extras.Subtitles
 {
     public class SubtitleFile : ExtraFile
     {
+        public SubtitleFile()
+        {
+            LanguageTags = new List<string>();
+        }
+
         public Language Language { get; set; }
     }
 }
