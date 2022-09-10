850bfdcf827135b26806cd89ba5c178d6df41d2e
Qstick <qstick@gmail.com>
2022-09-10 16:29:38 -0500

    New: Native Theme Engine
    Co-Authored-By: Zak Saunders <1936903+thezak48@users.noreply.github.com>

---
+++ frontend/src/Styles/Themes/light.js
@@ -0,0 +1,232 @@
+const radarrYellow = '#ffc230';
+const radarrAlternateYellow = '#2193b5';
+const darkGray = '#888';
+const mediumGray = '#999';
+const gray = '#adadad';
+const black = '#000';
+const white = '#fff';
+const offWhite = '#f5f7fa';
+const purple = '#7a43b6';
+const pink = '#ff69b4';
+const lightGray = '#ddd';
+const defaultColor = '#333';
+
+module.exports = {
+  textColor: '#515253',
+  defaultColor,
+  disabledColor: '#999',
+  dimColor: '#555',
+  black,
+  white,
+  offWhite,
+  primaryColor: '#5d9cec',
+  selectedColor: '#f9be03',
+  successColor: '#27c24c',
+  dangerColor: '#f05050',
+  warningColor: '#ffa500',
+  infoColor: '#5d9cec',
+  queueColor: '#7a43b6',
+  purple,
+  pink,
+  radarrYellow,
+  helpTextColor: '#909293',
+  darkGray,
+  gray,
+  lightGray,
+  mediumGray,
+
+  // Theme Colors
+
+  themeBlue: radarrYellow,
+  themeAlternateYellow: radarrAlternateYellow,
+  themeRed: '#c4273c',
+  themeDarkColor: '#595959',
+  themeLightColor: '#707070',
+  pageBackground: '#f5f7fa',
+  pageFooterBackgroud: '#f1f1f1',
+
+  torrentColor: '#00853d',
+  usenetColor: '#17b1d9',
+
+  // Labels
+  inverseLabelColor: '#ddd',
+  inverseLabelTextColor: defaultColor,
+  disabledLabelColor: '#999',
+  infoTextColor: white,
+
+  // Links
+  defaultLinkHoverColor: '#fff',
+  linkColor: '#5d9cec',
+  linkHoverColor: '#1b72e2',
+
+  // Header
+  pageHeaderBackgroundColor: '#464b51',
+
+  // Sidebar
+
+  sidebarColor: '#e1e2e3',
+  sidebarBackgroundColor: '#595959',
+  sidebarActiveBackgroundColor: '#333333',
+
+  // Toolbar
+  toolbarColor: '#e1e2e3',
+  toolbarBackgroundColor: '#707070',
+  toolbarMenuItemBackgroundColor: '#606060',
+  toolbarMenuItemHoverBackgroundColor: '#515151',
+  toolbarLabelColor: '#e1e2e3',
+
+  // Accents
+  borderColor: '#e5e5e5',
+  inputBorderColor: '#dde6e9',
+  inputBoxShadowColor: 'rgba(0, 0, 0, 0.075)',
+  inputFocusBorderColor: '#66afe9',
+  inputFocusBoxShadowColor: 'rgba(102, 175, 233, 0.6)',
+  inputErrorBorderColor: '#f05050',
+  inputErrorBoxShadowColor: 'rgba(240, 80, 80, 0.6)',
+  inputWarningBorderColor: '#ffa500',
+  inputWarningBoxShadowColor: 'rgba(255, 165, 0, 0.6)',
+  colorImpairedGradient: '#ffffff',
+  colorImpairedGradientDark: '#f4f5f6',
+
+  //
+  // Buttons
+
+  defaultButtonTextColor: '#eee',
+  defaultBackgroundColor: '#fff',
+  defaultBorderColor: '#eaeaea',
+  defaultHoverBackgroundColor: '#f5f5f5',
+  defaultHoverBorderColor: '#d6d6d6',
+
+  primaryBackgroundColor: '#5d9cec',
+  primaryBorderColor: '#5899eb',
+  primaryHoverBackgroundColor: '#4b91ea',
+  primaryHoverBorderColor: '#3483e7',
+
+  successBackgroundColor: '#27c24c',
+  successBorderColor: '#26be4a',
+  successHoverBackgroundColor: '#24b145',
+  successHoverBorderColor: '#1f9c3d',
+
+  warningBackgroundColor: '#ff902b',
+  warningBorderColor: '#ff8d26',
+  warningHoverBackgroundColor: '#ff8517',
+  warningHoverBorderColor: '#fc7800',
+
+  dangerBackgroundColor: '#f05050',
+  dangerBorderColor: '#f04b4b',
+  dangerHoverBackgroundColor: '#ee3d3d',
+  dangerHoverBorderColor: '#ec2626',
+
+  iconButtonDisabledColor: '#7a7a7a',
+  iconButtonHoverColor: '#666',
+  iconButtonHoverLightColor: '#ccc',
+
+  //
+  // Modal
+
+  modalBackdropBackgroundColor: 'rgba(0, 0, 0, 0.6)',
+  modalBackgroundColor: '#fff',
+  modalCloseButtonHoverColor: '#888',
+
+  //
+  // Menu
+  menuItemColor: '#e1e2e3',
+  menuItemHoverColor: '#fbfcfc',
+  menuItemHoverBackgroundColor: '#f5f7fa',
+
+  //
+  // Toolbar
+
+  toobarButtonHoverColor: '#ffc230',
+  toobarButtonSelectedColor: '#ffc230',
+
+  //
+  // Scroller
+
+  scrollbarBackgroundColor: '#9ea4b9',
+  scrollbarHoverBackgroundColor: '#656d8c',
+
+  //
+  // Card
+
+  cardBackgroundColor: '#fff',
+  cardShadowColor: '#e1e1e1',
+  cardAlternateBackgroundColor: '#f5f5f5',
+  cardCenterBackgroundColor: '#fff',
+
+  //
+  // Alert
+
+  alertDangerBorderColor: '#ebccd1',
+  alertDangerBackgroundColor: '#f2dede',
+  alertDangerColor: '#a94442',
+
+  alertInfoBorderColor: '#bce8f1',
+  alertInfoBackgroundColor: '#d9edf7',
+  alertInfoColor: '#31708f',
+
+  alertSuccessBorderColor: '#d6e9c6',
+  alertSuccessBackgroundColor: '#dff0d8',
+  alertSuccessColor: '#3c763d',
+
+  alertWarningBorderColor: '#faebcc',
+  alertWarningBackgroundColor: '#fcf8e3',
+  alertWarningColor: '#8a6d3b',
+
+  //
+  // Slider
+
+  sliderAccentColor: '#5d9cec',
+
+  //
+  // Form
+
+  inputBackgroundColor: '#fff',
+  inputReadOnlyBackgroundColor: '#eee',
+  inputHoverBackgroundColor: '#f8f8f8',
+  inputSelectedBackgroundColor: '#e2e2e2',
+  advancedFormLabelColor: '#ff902b',
+  disabledCheckInputColor: '#ddd',
+  disabledInputColor: '#808080',
+
+  //
+  // Popover
+
+  popoverTitleBackgroundColor: '#f7f7f7',
+  popoverTitleBorderColor: '#ebebeb',
+  popoverBodyBackgroundColor: '#e9e9e9',
+  popoverShadowColor: 'rgba(0, 0, 0, 0.2)',
+  popoverArrowBorderColor: '#fff',
+
+  popoverTitleBackgroundInverseColor: '#595959',
+  popoverTitleBorderInverseColor: '#707070',
+  popoverShadowInverseColor: 'rgba(0, 0, 0, 0.2)',
+  popoverArrowBorderInverseColor: 'rgba(58, 63, 81, 0.75)',
+
+  //
+  // Calendar
+
+  calendarTodayBackgroundColor: '#c5c5c5',
+  calendarBackgroudColor: '#e4eaec',
+  calendarBorderColor: '#cecece',
+  calendarTextDim: '#666',
+  calendarTextDimAlternate: '#000',
+
+  //
+  // Table
+
+  tableRowHoverBackgroundColor: '#fafbfc',
+
+  //
+  // Movie
+
+  addMovieBackgroundColor: '#ededed',
+  movieBackgroundColor: '#fafbfc',
+  searchIconContainerBackgroundColor: offWhite,
+
+  //
+  // Misc
+
+  progressBarBackgroundColor: '#fff',
+  logEventsBackgroundColor: '#fff'
+};
