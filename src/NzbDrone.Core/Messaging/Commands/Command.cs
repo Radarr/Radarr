41f10d098e2bc56fb99341d17fbd3dc420cbb8f3
Qstick <qstick@gmail.com>
2022-11-29 21:44:03 -0600

    Don't block task queue for queued update task when there are longer running tasks
    Fixes #7538
    
    Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

--- src/NzbDrone.Core/Messaging/Commands/Command.cs
+++ src/NzbDrone.Core/Messaging/Commands/Command.cs
@@ -27,6 +27,7 @@ namespace NzbDrone.Core.Messaging.Commands
         public virtual bool RequiresDiskAccess => false;
         public virtual bool IsExclusive => false;
         public virtual bool IsTypeExclusive => false;
+        public virtual bool IsLongRunning => false;
 
         public string Name { get; private set; }
         public DateTime? LastExecutionTime { get; set; }
