Qstick/Sonarr

Add OIDC and Plex authentication methods

frontend/src/Components/Page/Header/PageHeaderActionsMenu.js
@@ -11,7 +11,7 @@ import styles from './PageHeaderActionsMenu.css';
 
 function PageHeaderActionsMenu(props) {
   const {
-    formsAuth,
+    cookieAuth,
     onKeyboardShortcutsPress,
     onRestartPress,
     onShutdownPress
@@ -55,22 +55,20 @@ function PageHeaderActionsMenu(props) {
           </MenuItem>
 
           {
-            formsAuth &&
-              <div className={styles.separator} />
-          }
-
-          {
-            formsAuth &&
-              <MenuItem
-                to={`${window.Sonarr.urlBase}/logout`}
-                noRouter={true}
-              >
-                <Icon
-                  className={styles.itemIcon}
-                  name={icons.LOGOUT}
-                />
-                Logout
-              </MenuItem>
+            cookieAuth &&
+              <>
+                <div className={styles.separator} />
+                <MenuItem
+                  to={`${window.Sonarr.urlBase}/logout?ReturnUrl=/`}
+                  noRouter={true}
+                >
+                  <Icon
+                    className={styles.itemIcon}
+                    name={icons.LOGOUT}
+                  />
+                  Logout
+                </MenuItem>
+              </>
           }
         </MenuContent>
       </Menu>
@@ -79,7 +77,7 @@ function PageHeaderActionsMenu(props) {
 }
 
 PageHeaderActionsMenu.propTypes = {
-  formsAuth: PropTypes.bool.isRequired,
+  cookieAuth: PropTypes.bool.isRequired,
   onKeyboardShortcutsPress: PropTypes.func.isRequired,
   onRestartPress: PropTypes.func.isRequired,
   onShutdownPress: PropTypes.func.isRequired

src/Sonarr.Http/Authentication/AuthenticationBuilderExtensions.cs
@@ -1,7 +1,9 @@
 using System;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
+using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Core.Authentication;
+using Sonarr.Http.Authentication.Plex;
 
 namespace Sonarr.Http.Authentication
 {
@@ -22,25 +24,50 @@ namespace Sonarr.Http.Authentication
             return authenticationBuilder.AddScheme<AuthenticationSchemeOptions, NoAuthenticationHandler>(name, options => { });
         }
 
-        public static AuthenticationBuilder AddExternal(this AuthenticationBuilder authenticationBuilder, string name)
+        public static string GetChallengeScheme(this AuthenticationType scheme)
         {
-            return authenticationBuilder.AddScheme<AuthenticationSchemeOptions, NoAuthenticationHandler>(name, options => { });
+            return scheme.ToString() + "Remote";
         }
 
         public static AuthenticationBuilder AddAppAuthentication(this IServiceCollection services)
         {
-            return services.AddAuthentication()
+            var builder = services.AddAuthentication()
                 .AddNone(AuthenticationType.None.ToString())
-                .AddExternal(AuthenticationType.External.ToString())
+                .AddNone(AuthenticationType.External.ToString())
                 .AddBasic(AuthenticationType.Basic.ToString())
                 .AddCookie(AuthenticationType.Forms.ToString(), options =>
                 {
-                    options.Cookie.Name = "SonarrAuth";
-                    options.AccessDeniedPath = "/login?loginFailed=true";
+                    options.Cookie.Name = BuildInfo.AppName + "Auth";
                     options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/login/failed";
+                    options.LogoutPath = "/logout";
                     options.ExpireTimeSpan = TimeSpan.FromDays(7);
                     options.SlidingExpiration = true;
                 })
+                .AddCookie(AuthenticationType.Plex.ToString(), options =>
+                {
+                    options.Cookie.Name = BuildInfo.AppName + "PlexAuth";
+                    options.LoginPath = "/login/sso";
+                    options.AccessDeniedPath = "/login/sso/failed";
+                    options.LogoutPath = "/logout";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.SlidingExpiration = true;
+                })
+                .AddPlex(AuthenticationType.Plex.GetChallengeScheme(), options =>
+                {
+                    options.SignInScheme = AuthenticationType.Plex.ToString();
+                    options.CorrelationCookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+                })
+                .AddCookie(AuthenticationType.Oidc.ToString(), options =>
+                {
+                    options.Cookie.Name = BuildInfo.AppName + "OidcAuth";
+                    options.LoginPath = "/login/sso";
+                    options.AccessDeniedPath = "/login/sso/failed";
+                    options.LogoutPath = "/logout";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.SlidingExpiration = true;
+                })
+                .AddOpenIdConnect(AuthenticationType.Oidc.GetChallengeScheme(), _ => { } /* See ConfigureOidcOptions.cs */)
                 .AddApiKey("API", options =>
                 {
                     options.HeaderName = "X-Api-Key";
@@ -51,6 +78,8 @@ namespace Sonarr.Http.Authentication
                     options.HeaderName = "X-Api-Key";
                     options.QueryName = "access_token";
                 });
+
+            return builder;
         }
     }
 }

src/Sonarr.Http/Authentication/UiAuthorizationHandler.cs
@@ -9,7 +9,7 @@ using NzbDrone.Core.Configuration.Events;
 using NzbDrone.Core.Messaging.Events;
 using Sonarr.Http.Extensions;
 
-namespace NzbDrone.Http.Authentication
+namespace Sonarr.Http.Authentication
 {
     public class UiAuthorizationHandler : AuthorizationHandler<BypassableDenyAnonymousAuthorizationRequirement>, IAuthorizationRequirement, IHandle<ConfigSavedEvent>
     {

Radarr/Radarr

(cherry picked from commit 3ff3de6b90704fba266833115cd9d03ace99aae9)

frontend/src/Components/Page/Header/PageHeaderActionsMenu.js
@@ -12,7 +12,7 @@ import styles from './PageHeaderActionsMenu.css';
 
 function PageHeaderActionsMenu(props) {
   const {
-    formsAuth,
+    cookieAuth,
     onKeyboardShortcutsPress,
     onRestartPress,
     onShutdownPress
@@ -56,22 +56,20 @@ function PageHeaderActionsMenu(props) {
           </MenuItem>
 
           {
-            formsAuth &&
-              <div className={styles.separator} />
-          }
-
-          {
-            formsAuth &&
-              <MenuItem
-                to={`${window.Radarr.urlBase}/logout`}
-                noRouter={true}
-              >
-                <Icon
-                  className={styles.itemIcon}
-                  name={icons.LOGOUT}
-                />
-                Logout
-              </MenuItem>
+            cookieAuth &&
+              <>
+                <div className={styles.separator} />
+                <MenuItem
+                  to={`${window.Radarr.urlBase}/logout?ReturnUrl=/`}
+                  noRouter={true}
+                >
+                  <Icon
+                    className={styles.itemIcon}
+                    name={icons.LOGOUT}
+                  />
+                  Logout
+                </MenuItem>
+              </>
           }
         </MenuContent>
       </Menu>
@@ -80,7 +78,7 @@ function PageHeaderActionsMenu(props) {
 }
 
 PageHeaderActionsMenu.propTypes = {
-  formsAuth: PropTypes.bool.isRequired,
+  cookieAuth: PropTypes.bool.isRequired,
   onKeyboardShortcutsPress: PropTypes.func.isRequired,
   onRestartPress: PropTypes.func.isRequired,
   onShutdownPress: PropTypes.func.isRequired

src/Radarr.Http/Authentication/AuthenticationBuilderExtensions.cs
@@ -1,7 +1,9 @@
 using System;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
+using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Core.Authentication;
+using Radarr.Http.Authentication.Plex;
 
 namespace Radarr.Http.Authentication
 {
@@ -22,25 +24,50 @@ namespace Radarr.Http.Authentication
             return authenticationBuilder.AddScheme<AuthenticationSchemeOptions, NoAuthenticationHandler>(name, options => { });
         }
 
-        public static AuthenticationBuilder AddExternal(this AuthenticationBuilder authenticationBuilder, string name)
+        public static string GetChallengeScheme(this AuthenticationType scheme)
         {
-            return authenticationBuilder.AddScheme<AuthenticationSchemeOptions, NoAuthenticationHandler>(name, options => { });
+            return scheme.ToString() + "Remote";
         }
 
         public static AuthenticationBuilder AddAppAuthentication(this IServiceCollection services)
         {
-            return services.AddAuthentication()
+            var builder = services.AddAuthentication()
                 .AddNone(AuthenticationType.None.ToString())
-                .AddExternal(AuthenticationType.External.ToString())
+                .AddNone(AuthenticationType.External.ToString())
                 .AddBasic(AuthenticationType.Basic.ToString())
                 .AddCookie(AuthenticationType.Forms.ToString(), options =>
                 {
-                    options.Cookie.Name = "RadarrAuth";
-                    options.AccessDeniedPath = "/login?loginFailed=true";
+                    options.Cookie.Name = BuildInfo.AppName + "Auth";
                     options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/login/failed";
+                    options.LogoutPath = "/logout";
                     options.ExpireTimeSpan = TimeSpan.FromDays(7);
                     options.SlidingExpiration = true;
                 })
+                .AddCookie(AuthenticationType.Plex.ToString(), options =>
+                {
+                    options.Cookie.Name = BuildInfo.AppName + "PlexAuth";
+                    options.LoginPath = "/login/sso";
+                    options.AccessDeniedPath = "/login/sso/failed";
+                    options.LogoutPath = "/logout";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.SlidingExpiration = true;
+                })
+                .AddPlex(AuthenticationType.Plex.GetChallengeScheme(), options =>
+                {
+                    options.SignInScheme = AuthenticationType.Plex.ToString();
+                    options.CorrelationCookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+                })
+                .AddCookie(AuthenticationType.Oidc.ToString(), options =>
+                {
+                    options.Cookie.Name = BuildInfo.AppName + "OidcAuth";
+                    options.LoginPath = "/login/sso";
+                    options.AccessDeniedPath = "/login/sso/failed";
+                    options.LogoutPath = "/logout";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.SlidingExpiration = true;
+                })
+                .AddOpenIdConnect(AuthenticationType.Oidc.GetChallengeScheme(), _ => { } /* See ConfigureOidcOptions.cs */)
                 .AddApiKey("API", options =>
                 {
                     options.HeaderName = "X-Api-Key";
@@ -51,6 +78,8 @@ namespace Radarr.Http.Authentication
                     options.HeaderName = "X-Api-Key";
                     options.QueryName = "access_token";
                 });
+
+            return builder;
         }
     }
 }

src/Radarr.Http/Authentication/UiAuthorizationHandler.cs
@@ -9,7 +9,7 @@ using NzbDrone.Core.Configuration.Events;
 using NzbDrone.Core.Messaging.Events;
 using Radarr.Http.Extensions;
 
-namespace NzbDrone.Http.Authentication
+namespace Radarr.Http.Authentication
 {
     public class UiAuthorizationHandler : AuthorizationHandler<BypassableDenyAnonymousAuthorizationRequirement>, IAuthorizationRequirement, IHandle<ConfigSavedEvent>
     {