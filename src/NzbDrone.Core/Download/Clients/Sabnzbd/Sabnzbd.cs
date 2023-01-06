Sonarr/Sonarr

New: Added health check warning if SABnzbd sorting is enabled
Closes #4985

src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
@@ -260,6 +260,19 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
 
             if (category != null)
             {
+                if (config.Misc.enable_tv_sorting && ContainsCategory(config.Misc.tv_categories, Settings.TvCategory))
+                {
+                    status.SortingMode = "TV";
+                }
+                else if (config.Misc.enable_movie_sorting && ContainsCategory(config.Misc.movie_categories, Settings.TvCategory))
+                {
+                    status.SortingMode = "Movie";
+                }
+                else if (config.Misc.enable_date_sorting && ContainsCategory(config.Misc.date_categories, Settings.TvCategory))
+                {
+                    status.SortingMode = "Date";
+                }
+
                 status.OutputRootFolders = new List<OsPath> { _remotePathMappingService.RemapRemoteToLocal(Settings.Host, category.FullPath) };
             }
 

Radarr/Radarr

(cherry picked from commit 61fa1e5e3f00072f0d5f59cc883fac74fe12ee9d)

src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
@@ -262,6 +262,19 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
 
             if (category != null)
             {
+                if (config.Misc.enable_tv_sorting && ContainsCategory(config.Misc.tv_categories, Settings.MovieCategory))
+                {
+                    status.SortingMode = "TV";
+                }
+                else if (config.Misc.enable_movie_sorting && ContainsCategory(config.Misc.movie_categories, Settings.MovieCategory))
+                {
+                    status.SortingMode = "Movie";
+                }
+                else if (config.Misc.enable_date_sorting && ContainsCategory(config.Misc.date_categories, Settings.MovieCategory))
+                {
+                    status.SortingMode = "Date";
+                }
+
                 status.OutputRootFolders = new List<OsPath> { _remotePathMappingService.RemapRemoteToLocal(Settings.Host, category.FullPath) };
             }
 