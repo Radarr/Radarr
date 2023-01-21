04185d6839943f32b2bad72250645b143319917f
Qstick <qstick@gmail.com>
2023-01-21 17:16:54 -0600

    Filter useless PG Errors from coming to Sentry

--- src/NzbDrone.Common/Instrumentation/Sentry/SentryTarget.cs
+++ src/NzbDrone.Common/Instrumentation/Sentry/SentryTarget.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using NLog;
 using NLog.Common;
 using NLog.Targets;
+using Npgsql;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using Sentry;
@@ -32,6 +33,14 @@ namespace NzbDrone.Common.Instrumentation.Sentry
             SQLiteErrorCode.Full,
             SQLiteErrorCode.CantOpen,
             SQLiteErrorCode.Auth
+        };
+
+        private static readonly HashSet<string> FilteredPostgresErrorCodes = new HashSet<string>
+        {
+            PostgresErrorCodes.OutOfMemory,
+            PostgresErrorCodes.TooManyConnections,
+            PostgresErrorCodes.DiskFull,
+            PostgresErrorCodes.ProgramLimitExceeded
         };
 
         // use string and not Type so we don't need a reference to the project
@@ -248,6 +257,19 @@ namespace NzbDrone.Common.Instrumentation.Sentry
                         if (sqlEx != null && FilteredSQLiteErrors.Contains(sqlEx.ResultCode))
                         {
                             isSentry = false;
+                        }
+
+                        var pgEx = logEvent.Exception as PostgresException;
+                        if (pgEx != null && FilteredPostgresErrorCodes.Contains(pgEx.SqlState))
+                        {
+                            return false;
+                        }
+
+                        // We don't care about transient network and timeout errors
+                        var npgEx = logEvent.Exception as NpgsqlException;
+                        if (npgEx != null && npgEx.IsTransient)
+                        {
+                            return false;
                         }
 
                         if (FilteredExceptionTypeNames.Contains(ex.GetType().Name))
