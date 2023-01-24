Sonarr/Sonarr

Fixed: UTC time sent to UI for already imported message
Closes #5366

src/NzbDrone.Core/MediaFiles/EpisodeImport/Specifications/AlreadyImportedSpecification.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
                 if (lastImported.DownloadId == downloadClientItem.DownloadId)
                 {
                     _logger.Debug("Episode file previously imported at {0}", lastImported.Date);
-                    return Decision.Reject("Episode file already imported at {0}", lastImported.Date);
+                    return Decision.Reject("Episode file already imported at {0}", lastImported.Date.ToLocalTime());
                 }
             }
 

Radarr/Radarr

(cherry picked from commit 3f598ffa6fbec90ecdbb266de4b0fe7558fbbc30)

src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/AlreadyImportedSpecification.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
             if (lastImported.DownloadId == downloadClientItem.DownloadId)
             {
                 _logger.Debug("Movie file previously imported at {0}", lastImported.Date);
-                return Decision.Reject("Movie file already imported at {0}", lastImported.Date);
+                return Decision.Reject("Movie file already imported at {0}", lastImported.Date.ToLocalTime());
             }
 
             return Decision.Accept();