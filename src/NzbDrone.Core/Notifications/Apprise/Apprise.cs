ac51a943fbb7c391acce804d718ae325bd0df235
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-04-23 05:42:41 +0300

    New: (Notifications) Add Apprise

---
+++ src/NzbDrone.Core/Notifications/Apprise/Apprise.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Movies;
+
+namespace NzbDrone.Core.Notifications.Apprise
+{
+    public class Apprise : NotificationBase<AppriseSettings>
+    {
+        public override string Name => "Apprise";
+
+        public override string Link => "https://github.com/caronc/apprise";
+
+        private readonly IAppriseProxy _proxy;
+
+        public Apprise(IAppriseProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(MOVIE_GRABBED_TITLE, grabMessage.Message, Settings);
+        }
+
+        public override void OnDownload(DownloadMessage message)
+        {
+            _proxy.SendNotification(MOVIE_DOWNLOADED_TITLE, message.Message, Settings);
+        }
+
+        public override void OnMovieAdded(Movie movie)
+        {
+            _proxy.SendNotification(MOVIE_ADDED_TITLE, $"{movie.Title} added to library", Settings);
+        }
+
+        public override void OnMovieFileDelete(MovieFileDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(MOVIE_FILE_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnMovieDelete(MovieDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(MOVIE_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, Settings);
+        }
+
+        public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
+        {
+            _proxy.SendNotification(APPLICATION_UPDATE_TITLE, updateMessage.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}
