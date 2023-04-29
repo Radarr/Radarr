146693b6cb16209c7bfa83ee785e4ede533e7a40
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-04-29 21:43:12 +0300

    Bump Swashbuckle to 6.5.0

--- docs.sh
+++ docs.sh
@@ -3,9 +3,9 @@ PLATFORM=$1
 if [ "$PLATFORM" = "Windows" ]; then
   RUNTIME="win-x64"
 elif [ "$PLATFORM" = "Linux" ]; then
-  WHERE="linux-x64"
+  RUNTIME="linux-x64"
 elif [ "$PLATFORM" = "Mac" ]; then
-  WHERE="osx-x64"
+  RUNTIME="osx-x64"
 else
   echo "Platform must be provided as first arguement: Windows, Linux or Mac"
   exit 1
@@ -27,7 +27,7 @@ dotnet clean $slnFile -c Release
 dotnet msbuild -restore $slnFile -p:Configuration=Debug -p:Platform=$platform -p:RuntimeIdentifiers=$RUNTIME -t:PublishAllRids
 
 dotnet new tool-manifest
-dotnet tool install --version 6.3.0 Swashbuckle.AspNetCore.Cli
+dotnet tool install --version 6.5.0 Swashbuckle.AspNetCore.Cli
 
 dotnet tool run swagger tofile --output ./src/Radarr.Api.V3/openapi.json "$outputFolder/net6.0/$RUNTIME/radarr.console.dll" v3 &
 
