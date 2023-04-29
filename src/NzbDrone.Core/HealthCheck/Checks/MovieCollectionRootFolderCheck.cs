e2445a61b6d27c82b6e62ef37676788eaf03d08a
Bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-04-29 11:59:24 -0500

    Misc HealthCheck Cleanup and Sonarr Alignment

--- src/NzbDrone.Core/HealthCheck/Checks/MovieCollectionRootFolderCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/MovieCollectionRootFolderCheck.cs
@@ -5,7 +5,6 @@ using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Datastore.Events;
 using NzbDrone.Core.Localization;
 using NzbDrone.Core.Movies.Collections;
-using NzbDrone.Core.Movies.Events;
 using NzbDrone.Core.RootFolders;
 
 namespace NzbDrone.Core.HealthCheck.Checks
