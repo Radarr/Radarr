e0389ca08c9b24c97173e60ffcb592d4b77ab056
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-05-08 06:56:26 +0300

    API key improvements
    Fixed: Special characters in API key
    New: Add heathcheck for API Key
    
    (cherry picked from commit 9325140b90f8ac625ae5b26075748c22f6f06158)

---
+++ src/NzbDrone.Core/HealthCheck/Checks/ApiKeyValidationCheck.cs
@@ -0,0 +1,35 @@
+﻿using NLog;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Configuration.Events;
+using NzbDrone.Core.Lifecycle;
+using NzbDrone.Core.Localization;
+
+namespace NzbDrone.Core.HealthCheck.Checks
+{
+    [CheckOn(typeof(ApplicationStartedEvent))]
+    [CheckOn(typeof(ConfigSavedEvent))]
+    public class ApiKeyValidationCheck : HealthCheckBase
+    {
+        private readonly IConfigFileProvider _configFileProvider;
+        private readonly Logger _logger;
+
+        public ApiKeyValidationCheck(IConfigFileProvider configFileProvider, ILocalizationService localizationService, Logger logger)
+            : base(localizationService)
+        {
+            _configFileProvider = configFileProvider;
+            _logger = logger;
+        }
+
+        public override HealthCheck Check()
+        {
+            if (_configFileProvider.ApiKey.Length < 20)
+            {
+                _logger.Warn("Please update your API key to be at least 20 characters long. You can do this via settings or the config file");
+
+                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("ApiKeyValidationHealthCheckMessage"), "#invalid-api-key");
+            }
+
+            return new HealthCheck(GetType());
+        }
+    }
+}
