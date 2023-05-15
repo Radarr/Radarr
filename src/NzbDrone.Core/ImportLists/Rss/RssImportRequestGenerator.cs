Sonarr/Sonarr

New: Plex Watchlist RSS support

src/NzbDrone.Core/ImportLists/Rss/RssImportRequestGenerator.cs
@@ -1,0 +1,26 @@
+using System.Collections.Generic;
+using NzbDrone.Common.Http;
+
+namespace NzbDrone.Core.ImportLists.Rss
+{
+    public class RssImportRequestGenerator : IImportListRequestGenerator
+    {
+        public RssImportBaseSettings Settings { get; set; }
+
+        public virtual ImportListPageableRequestChain GetListItems()
+        {
+            var pageableRequests = new ImportListPageableRequestChain();
+
+            pageableRequests.Add(GetSeriesRequest());
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<ImportListRequest> GetSeriesRequest()
+        {
+            var request = new ImportListRequest(Settings.Url, HttpAccept.Rss);
+
+            yield return request;
+        }
+    }
+}

Radarr/Radarr

(cherry picked from commit 6d88a98282d1441f903d567470a9f1ce6ba0b52f)

src/NzbDrone.Core/ImportLists/Rss/RssImportRequestGenerator.cs
@@ -1,0 +1,26 @@
+using System.Collections.Generic;
+using NzbDrone.Common.Http;
+
+namespace NzbDrone.Core.ImportLists.Rss
+{
+    public class RssImportRequestGenerator : IImportListRequestGenerator
+    {
+        public RssImportBaseSettings Settings { get; set; }
+
+        public virtual ImportListPageableRequestChain GetMovies()
+        {
+            var pageableRequests = new ImportListPageableRequestChain();
+
+            pageableRequests.Add(GetMoviesRequest());
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<ImportListRequest> GetMoviesRequest()
+        {
+            var request = new ImportListRequest(Settings.Url, HttpAccept.Rss);
+
+            yield return request;
+        }
+    }
+}