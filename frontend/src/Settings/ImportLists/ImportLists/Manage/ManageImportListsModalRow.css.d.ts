Sonarr/Sonarr

New: Bulk Manage Import Lists, Indexers, Clients

frontend/src/Settings/ImportLists/ImportLists/Manage/ManageImportListsModalRow.css.d.ts
@@ -1,0 +1,12 @@
+// This file is automatically generated.
+// Please do not change this file!
+interface CssExports {
+  'enableAutomaticAdd': string;
+  'implementation': string;
+  'name': string;
+  'qualityProfileId': string;
+  'rootFolderPath': string;
+  'tags': string;
+}
+export const cssExports: CssExports;
+export default cssExports;

frontend/src/Store/Actions/Settings/importLists.js
@@ -1,4 +1,6 @@
 import { createAction } from 'redux-actions';
+import createBulkEditItemHandler from 'Store/Actions/Creators/createBulkEditItemHandler';
+import createBulkRemoveItemHandler from 'Store/Actions/Creators/createBulkRemoveItemHandler';
 import createFetchHandler from 'Store/Actions/Creators/createFetchHandler';
 import createFetchSchemaHandler from 'Store/Actions/Creators/createFetchSchemaHandler';
 import createRemoveItemHandler from 'Store/Actions/Creators/createRemoveItemHandler';
@@ -30,6 +32,9 @@ export const TEST_IMPORT_LIST = 'settings/importlists/testImportList';
 export const CANCEL_TEST_IMPORT_LIST = 'settings/importlists/cancelTestImportList';
 export const TEST_ALL_IMPORT_LISTS = 'settings/importlists/testAllImportLists';
 
+export const BULK_DELETE_IMPORT_LISTS = 'settings/importlists/bulkDeleteImportLists';
+export const BULK_EDIT_IMPORT_LISTS = 'settings/importlists/bulkEditImportLists';
+
 //
 // Action Creators
 
@@ -43,6 +48,9 @@ export const deleteImportList = createThunk(DELETE_IMPORT_LIST);
 export const testImportList = createThunk(TEST_IMPORT_LIST);
 export const cancelTestImportList = createThunk(CANCEL_TEST_IMPORT_LIST);
 export const testAllImportLists = createThunk(TEST_ALL_IMPORT_LISTS);
+
+export const bulkDeleteImportLists = createThunk(BULK_DELETE_IMPORT_LISTS);
+export const bulkEditImportLists = createThunk(BULK_EDIT_IMPORT_LISTS);
 
 export const setImportListValue = createAction(SET_IMPORT_LIST_VALUE, (payload) => {
   return {
@@ -94,7 +102,9 @@ export default {
     [DELETE_IMPORT_LIST]: createRemoveItemHandler(section, '/importlist'),
     [TEST_IMPORT_LIST]: createTestProviderHandler(section, '/importlist'),
     [CANCEL_TEST_IMPORT_LIST]: createCancelTestProviderHandler(section),
-    [TEST_ALL_IMPORT_LISTS]: createTestAllProvidersHandler(section, '/importlist')
+    [TEST_ALL_IMPORT_LISTS]: createTestAllProvidersHandler(section, '/importlist'),
+    [BULK_DELETE_IMPORT_LISTS]: createBulkRemoveItemHandler(section, '/importlist/bulk'),
+    [BULK_EDIT_IMPORT_LISTS]: createBulkEditItemHandler(section, '/importlist/bulk')
   },
 
   //

src/Sonarr.Api.V3/ImportLists/ImportListBulkResource.cs
@@ -1,0 +1,32 @@
+using System.Collections.Generic;
+using NzbDrone.Core.ImportLists;
+
+namespace Sonarr.Api.V3.ImportLists
+{
+    public class ImportListBulkResource : ProviderBulkResource<ImportListBulkResource>
+    {
+        public bool? EnableAutomaticAdd { get; set; }
+        public string RootFolderPath { get; set; }
+        public int? QualityProfileId { get; set; }
+    }
+
+    public class ImportListBulkResourceMapper : ProviderBulkResourceMapper<ImportListBulkResource, ImportListDefinition>
+    {
+        public override List<ImportListDefinition> UpdateModel(ImportListBulkResource resource, List<ImportListDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<ImportListDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.EnableAutomaticAdd = resource.EnableAutomaticAdd ?? existing.EnableAutomaticAdd;
+                existing.RootFolderPath = resource.RootFolderPath ?? existing.RootFolderPath;
+                existing.QualityProfileId = resource.QualityProfileId ?? existing.QualityProfileId;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Sonarr.Api.V3/Metadata/MetadataBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Extras.Metadata;
+
+namespace Sonarr.Api.V3.Metadata
+{
+    public class MetadataBulkResource : ProviderBulkResource<MetadataBulkResource>
+    {
+    }
+
+    public class MetadataBulkResourceMapper : ProviderBulkResourceMapper<MetadataBulkResource, MetadataDefinition>
+    {
+    }
+}

src/Sonarr.Api.V3/Notifications/NotificationBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Notifications;
+
+namespace Sonarr.Api.V3.Notifications
+{
+    public class NotificationBulkResource : ProviderBulkResource<NotificationBulkResource>
+    {
+    }
+
+    public class NotificationBulkResourceMapper : ProviderBulkResourceMapper<NotificationBulkResource, NotificationDefinition>
+    {
+    }
+}

Radarr/Radarr

(cherry picked from commit 73ccab53d5194282de4b983354c9afa5a6d678fb)

frontend/src/Settings/ImportLists/ImportLists/Manage/ManageImportListsModalRow.css.d.ts
@@ -1,0 +1,12 @@
+// This file is automatically generated.
+// Please do not change this file!
+interface CssExports {
+  'enableAuto': string;
+  'implementation': string;
+  'name': string;
+  'qualityProfileId': string;
+  'rootFolderPath': string;
+  'tags': string;
+}
+export const cssExports: CssExports;
+export default cssExports;

frontend/src/Store/Actions/Settings/importLists.js
@@ -1,4 +1,6 @@
 import { createAction } from 'redux-actions';
+import createBulkEditItemHandler from 'Store/Actions/Creators/createBulkEditItemHandler';
+import createBulkRemoveItemHandler from 'Store/Actions/Creators/createBulkRemoveItemHandler';
 import createFetchHandler from 'Store/Actions/Creators/createFetchHandler';
 import createFetchSchemaHandler from 'Store/Actions/Creators/createFetchSchemaHandler';
 import createRemoveItemHandler from 'Store/Actions/Creators/createRemoveItemHandler';
@@ -30,6 +32,9 @@ export const TEST_IMPORT_LIST = 'settings/importLists/testImportList';
 export const CANCEL_TEST_IMPORT_LIST = 'settings/importLists/cancelTestImportList';
 export const TEST_ALL_IMPORT_LIST = 'settings/importLists/testAllImportList';
 
+export const BULK_DELETE_IMPORT_LISTS = 'settings/importlists/bulkDeleteImportLists';
+export const BULK_EDIT_IMPORT_LISTS = 'settings/importlists/bulkEditImportLists';
+
 //
 // Action Creators
 
@@ -43,6 +48,9 @@ export const deleteImportList = createThunk(DELETE_IMPORT_LIST);
 export const testImportList = createThunk(TEST_IMPORT_LIST);
 export const cancelTestImportList = createThunk(CANCEL_TEST_IMPORT_LIST);
 export const testAllImportList = createThunk(TEST_ALL_IMPORT_LIST);
+
+export const bulkDeleteImportLists = createThunk(BULK_DELETE_IMPORT_LISTS);
+export const bulkEditImportLists = createThunk(BULK_EDIT_IMPORT_LISTS);
 
 export const setImportListValue = createAction(SET_IMPORT_LIST_VALUE, (payload) => {
   return {
@@ -95,7 +103,10 @@ export default {
     [DELETE_IMPORT_LIST]: createRemoveItemHandler(section, '/importlist'),
     [TEST_IMPORT_LIST]: createTestProviderHandler(section, '/importlist'),
     [CANCEL_TEST_IMPORT_LIST]: createCancelTestProviderHandler(section),
-    [TEST_ALL_IMPORT_LIST]: createTestAllProvidersHandler(section, '/importlist')
+    [TEST_ALL_IMPORT_LIST]: createTestAllProvidersHandler(section, '/importlist'),
+
+    [BULK_DELETE_IMPORT_LISTS]: createBulkRemoveItemHandler(section, '/importlist/bulk'),
+    [BULK_EDIT_IMPORT_LISTS]: createBulkEditItemHandler(section, '/importlist/bulk')
   },
 
   //

src/Radarr.Api.V3/ImportLists/ImportListBulkResource.cs
@@ -1,0 +1,32 @@
+using System.Collections.Generic;
+using NzbDrone.Core.ImportLists;
+
+namespace Radarr.Api.V3.ImportLists
+{
+    public class ImportListBulkResource : ProviderBulkResource<ImportListBulkResource>
+    {
+        public bool? EnableAuto { get; set; }
+        public string RootFolderPath { get; set; }
+        public int? ProfileId { get; set; }
+    }
+
+    public class ImportListBulkResourceMapper : ProviderBulkResourceMapper<ImportListBulkResource, ImportListDefinition>
+    {
+        public override List<ImportListDefinition> UpdateModel(ImportListBulkResource resource, List<ImportListDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<ImportListDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.EnableAuto = resource.EnableAuto ?? existing.EnableAuto;
+                existing.RootFolderPath = resource.RootFolderPath ?? existing.RootFolderPath;
+                existing.ProfileId = resource.ProfileId ?? existing.ProfileId;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Radarr.Api.V3/Metadata/MetadataBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Extras.Metadata;
+
+namespace Radarr.Api.V3.Metadata
+{
+    public class MetadataBulkResource : ProviderBulkResource<MetadataBulkResource>
+    {
+    }
+
+    public class MetadataBulkResourceMapper : ProviderBulkResourceMapper<MetadataBulkResource, MetadataDefinition>
+    {
+    }
+}

src/Radarr.Api.V3/Notifications/NotificationBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Notifications;
+
+namespace Radarr.Api.V3.Notifications
+{
+    public class NotificationBulkResource : ProviderBulkResource<NotificationBulkResource>
+    {
+    }
+
+    public class NotificationBulkResourceMapper : ProviderBulkResourceMapper<NotificationBulkResource, NotificationDefinition>
+    {
+    }
+}