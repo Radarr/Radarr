Sonarr/Sonarr

Inline 'out' variable declarations

src/NzbDrone.Core/Indexers/Newznab/NewznabRssParser.cs
@@ -129,10 +129,8 @@ namespace NzbDrone.Core.Indexers.Newznab
 
         protected override long GetSize(XElement item)
         {
-            long size;
-
             var sizeString = TryGetNewznabAttribute(item, "size");
-            if (!sizeString.IsNullOrWhiteSpace() && long.TryParse(sizeString, out size))
+            if (!sizeString.IsNullOrWhiteSpace() && long.TryParse(sizeString, out var size))
             {
                 return size;
             }
@@ -156,9 +154,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         protected virtual int GetTvdbId(XElement item)
         {
             var tvdbIdString = TryGetNewznabAttribute(item, "tvdbid");
-            int tvdbId;
 
-            if (!tvdbIdString.IsNullOrWhiteSpace() && int.TryParse(tvdbIdString, out tvdbId))
+            if (!tvdbIdString.IsNullOrWhiteSpace() && int.TryParse(tvdbIdString, out var tvdbId))
             {
                 return tvdbId;
             }
@@ -169,9 +166,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         protected virtual int GetTvRageId(XElement item)
         {
             var tvRageIdString = TryGetNewznabAttribute(item, "rageid");
-            int tvRageId;
 
-            if (!tvRageIdString.IsNullOrWhiteSpace() && int.TryParse(tvRageIdString, out tvRageId))
+            if (!tvRageIdString.IsNullOrWhiteSpace() && int.TryParse(tvRageIdString, out var tvRageId))
             {
                 return tvRageId;
             }

src/NzbDrone.Test.Common/LoggingTest.cs
@@ -23,8 +23,7 @@ namespace NzbDrone.Test.Common
             {
                 LogManager.Configuration = new LoggingConfiguration();
 
-                var logOutput = TestLogOutput.Console;
-                Enum.TryParse<TestLogOutput>(Environment.GetEnvironmentVariable("SONARR_TESTS_LOG_OUTPUT"), out logOutput);
+                Enum.TryParse<TestLogOutput>(Environment.GetEnvironmentVariable("SONARR_TESTS_LOG_OUTPUT"), out var logOutput);
 
                 RegisterSentryLogger();
 

Radarr/Radarr

(cherry picked from commit 281add47de1d3940990156c841362125dea9cc7d)

src/NzbDrone.Core/Indexers/Newznab/NewznabRssParser.cs
@@ -131,10 +131,8 @@ namespace NzbDrone.Core.Indexers.Newznab
 
         protected override long GetSize(XElement item)
         {
-            long size;
-
             var sizeString = TryGetNewznabAttribute(item, "size");
-            if (!sizeString.IsNullOrWhiteSpace() && long.TryParse(sizeString, out size))
+            if (!sizeString.IsNullOrWhiteSpace() && long.TryParse(sizeString, out var size))
             {
                 return size;
             }
@@ -170,9 +168,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         protected virtual int GetImdbId(XElement item)
         {
             var imdbIdString = TryGetNewznabAttribute(item, "imdb");
-            int imdbId;
 
-            if (!imdbIdString.IsNullOrWhiteSpace() && int.TryParse(imdbIdString, out imdbId))
+            if (!imdbIdString.IsNullOrWhiteSpace() && int.TryParse(imdbIdString, out var imdbId))
             {
                 return imdbId;
             }
@@ -196,9 +193,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         protected virtual int GetImdbYear(XElement item)
         {
             var imdbYearString = TryGetNewznabAttribute(item, "imdbyear");
-            int imdbYear;
 
-            if (!imdbYearString.IsNullOrWhiteSpace() && int.TryParse(imdbYearString, out imdbYear))
+            if (!imdbYearString.IsNullOrWhiteSpace() && int.TryParse(imdbYearString, out var imdbYear))
             {
                 return imdbYear;
             }

src/NzbDrone.Test.Common/LoggingTest.cs
@@ -23,8 +23,7 @@ namespace NzbDrone.Test.Common
             {
                 LogManager.Configuration = new LoggingConfiguration();
 
-                var logOutput = TestLogOutput.Console;
-                Enum.TryParse<TestLogOutput>(Environment.GetEnvironmentVariable("RADARR_TESTS_LOG_OUTPUT"), out logOutput);
+                Enum.TryParse<TestLogOutput>(Environment.GetEnvironmentVariable("RADARR_TESTS_LOG_OUTPUT"), out var logOutput);
 
                 RegisterSentryLogger();
 