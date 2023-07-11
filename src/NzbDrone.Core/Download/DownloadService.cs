Sonarr/Sonarr

New: Download Client Tags
Closes #402

src/NzbDrone.Core/Download/DownloadService.cs
@@ -53,9 +53,11 @@ namespace NzbDrone.Core.Download
         {
             var filterBlockedClients = remoteEpisode.Release.PendingReleaseReason == PendingReleaseReason.DownloadClientUnavailable;
 
+            var tags = remoteEpisode.Series?.Tags;
+
             var downloadClient = downloadClientId.HasValue
                 ? _downloadClientProvider.Get(downloadClientId.Value)
-                : _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol, remoteEpisode.Release.IndexerId, filterBlockedClients);
+                : _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol, remoteEpisode.Release.IndexerId, filterBlockedClients, tags);
 
             DownloadReport(remoteEpisode, downloadClient);
         }

src/NzbDrone.Core/Extras/Metadata/Consumers/Xbmc/XbmcMetadata.cs
@@ -24,19 +24,19 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
     {
         private readonly Logger _logger;
         private readonly IMapCoversToLocal _mediaCoverService;
-        private readonly ITagService _tagService;
+        private readonly ITagRepository _tagRepo;
         private readonly IDetectXbmcNfo _detectNfo;
         private readonly IDiskProvider _diskProvider;
 
         public XbmcMetadata(IDetectXbmcNfo detectNfo,
                             IDiskProvider diskProvider,
                             IMapCoversToLocal mediaCoverService,
-                            ITagService tagService,
+                            ITagRepository tagRepo,
                             Logger logger)
         {
             _logger = logger;
             _mediaCoverService = mediaCoverService;
-            _tagService = tagService;
+            _tagRepo = tagRepo;
             _diskProvider = diskProvider;
             _detectNfo = detectNfo;
         }
@@ -183,7 +183,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
 
                     if (series.Tags.Any())
                     {
-                        var tags = _tagService.GetTags(series.Tags);
+                        var tags = _tagRepo.Get(series.Tags);
 
                         foreach (var tag in tags)
                         {

src/NzbDrone.Core/Tags/TagService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Core.AutoTagging;
 using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.ImportLists;
 using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Messaging.Events;
@@ -36,6 +37,7 @@ namespace NzbDrone.Core.Tags
         private readonly ISeriesService _seriesService;
         private readonly IIndexerFactory _indexerService;
         private readonly IAutoTaggingService _autoTaggingService;
+        private readonly IDownloadClientFactory _downloadClientFactory;
 
         public TagService(ITagRepository repo,
                           IEventAggregator eventAggregator,
@@ -45,7 +47,8 @@ namespace NzbDrone.Core.Tags
                           IReleaseProfileService releaseProfileService,
                           ISeriesService seriesService,
                           IIndexerFactory indexerService,
-                          IAutoTaggingService autoTaggingService)
+                          IAutoTaggingService autoTaggingService,
+                          IDownloadClientFactory downloadClientFactory)
         {
             _repo = repo;
             _eventAggregator = eventAggregator;
@@ -56,6 +59,7 @@ namespace NzbDrone.Core.Tags
             _seriesService = seriesService;
             _indexerService = indexerService;
             _autoTaggingService = autoTaggingService;
+            _downloadClientFactory = downloadClientFactory;
         }
 
         public Tag GetTag(int tagId)
@@ -90,6 +94,7 @@ namespace NzbDrone.Core.Tags
             var series = _seriesService.AllForTag(tagId);
             var indexers = _indexerService.AllForTag(tagId);
             var autoTags = _autoTaggingService.AllForTag(tagId);
+            var downloadClients = _downloadClientFactory.AllForTag(tagId);
 
             return new TagDetails
             {
@@ -101,7 +106,8 @@ namespace NzbDrone.Core.Tags
                 RestrictionIds = restrictions.Select(c => c.Id).ToList(),
                 SeriesIds = series.Select(c => c.Id).ToList(),
                 IndexerIds = indexers.Select(c => c.Id).ToList(),
-                AutoTagIds = autoTags.Select(c => c.Id).ToList()
+                AutoTagIds = autoTags.Select(c => c.Id).ToList(),
+                DownloadClientIds = downloadClients.Select(c => c.Id).ToList()
             };
         }
 
@@ -115,6 +121,7 @@ namespace NzbDrone.Core.Tags
             var series = _seriesService.GetAllSeriesTags();
             var indexers = _indexerService.All();
             var autotags = _autoTaggingService.All();
+            var downloadClients = _downloadClientFactory.All();
 
             var details = new List<TagDetails>();
 
@@ -131,6 +138,7 @@ namespace NzbDrone.Core.Tags
                         SeriesIds = series.Where(c => c.Value.Contains(tag.Id)).Select(c => c.Key).ToList(),
                         IndexerIds = indexers.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
                         AutoTagIds = autotags.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
+                        DownloadClientIds = downloadClients.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
                     });
             }
 

Radarr/Radarr

(cherry picked from commit f6ae9fd6c5173cbf1540341fa99d2f120be1d28e)

src/NzbDrone.Core/Download/DownloadService.cs
@@ -55,7 +55,8 @@ namespace NzbDrone.Core.Download
 
             var downloadTitle = remoteMovie.Release.Title;
             var filterBlockedClients = remoteMovie.Release.PendingReleaseReason == PendingReleaseReason.DownloadClientUnavailable;
-            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteMovie.Release.DownloadProtocol, remoteMovie.Release.IndexerId, filterBlockedClients);
+            var tags = remoteMovie.Movie?.Tags;
+            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteMovie.Release.DownloadProtocol, remoteMovie.Release.IndexerId, filterBlockedClients, tags);
 
             if (downloadClient == null)
             {

src/NzbDrone.Core/Extras/Metadata/Consumers/Xbmc/XbmcMetadata.cs
@@ -28,14 +28,14 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
         private readonly IDetectXbmcNfo _detectNfo;
         private readonly IDiskProvider _diskProvider;
         private readonly ICreditService _creditService;
-        private readonly ITagService _tagService;
+        private readonly ITagRepository _tagRepository;
         private readonly IMovieTranslationService _movieTranslationsService;
 
         public XbmcMetadata(IDetectXbmcNfo detectNfo,
                             IDiskProvider diskProvider,
                             IMapCoversToLocal mediaCoverService,
                             ICreditService creditService,
-                            ITagService tagService,
+                            ITagRepository tagRepository,
                             IMovieTranslationService movieTranslationsService,
                             Logger logger)
         {
@@ -44,7 +44,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
             _diskProvider = diskProvider;
             _detectNfo = detectNfo;
             _creditService = creditService;
-            _tagService = tagService;
+            _tagRepository = tagRepository;
             _movieTranslationsService = movieTranslationsService;
         }
 
@@ -273,7 +273,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
                         details.Add(setElement);
                     }
 
-                    var tags = _tagService.GetTags(movie.Tags);
+                    var tags = _tagRepository.Get(movie.Tags);
 
                     foreach (var tag in tags)
                     {

src/NzbDrone.Core/Tags/TagService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.ImportLists;
 using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Messaging.Events;
@@ -34,6 +35,7 @@ namespace NzbDrone.Core.Tags
         private readonly IRestrictionService _restrictionService;
         private readonly IMovieService _movieService;
         private readonly IIndexerFactory _indexerService;
+        private readonly IDownloadClientFactory _downloadClientFactory;
 
         public TagService(ITagRepository repo,
                           IEventAggregator eventAggregator,
@@ -42,7 +44,8 @@ namespace NzbDrone.Core.Tags
                           INotificationFactory notificationFactory,
                           IRestrictionService restrictionService,
                           IMovieService movieService,
-                          IIndexerFactory indexerService)
+                          IIndexerFactory indexerService,
+                          IDownloadClientFactory downloadClientFactory)
         {
             _repo = repo;
             _eventAggregator = eventAggregator;
@@ -52,6 +55,7 @@ namespace NzbDrone.Core.Tags
             _restrictionService = restrictionService;
             _movieService = movieService;
             _indexerService = indexerService;
+            _downloadClientFactory = downloadClientFactory;
         }
 
         public Tag GetTag(int tagId)
@@ -85,6 +89,7 @@ namespace NzbDrone.Core.Tags
             var restrictions = _restrictionService.AllForTag(tagId);
             var movies = _movieService.AllMovieTags().Where(x => x.Value.Contains(tagId)).Select(x => x.Key).ToList();
             var indexers = _indexerService.AllForTag(tagId);
+            var downloadClients = _downloadClientFactory.AllForTag(tagId);
 
             return new TagDetails
             {
@@ -95,7 +100,8 @@ namespace NzbDrone.Core.Tags
                 NotificationIds = notifications.Select(c => c.Id).ToList(),
                 RestrictionIds = restrictions.Select(c => c.Id).ToList(),
                 MovieIds = movies,
-                IndexerIds = indexers.Select(c => c.Id).ToList()
+                IndexerIds = indexers.Select(c => c.Id).ToList(),
+                DownloadClientIds = downloadClients.Select(c => c.Id).ToList()
             };
         }
 
@@ -108,6 +114,7 @@ namespace NzbDrone.Core.Tags
             var restrictions = _restrictionService.All();
             var movies = _movieService.AllMovieTags();
             var indexers = _indexerService.All();
+            var downloadClients = _downloadClientFactory.All();
 
             var details = new List<TagDetails>();
 
@@ -122,7 +129,8 @@ namespace NzbDrone.Core.Tags
                     NotificationIds = notifications.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
                     RestrictionIds = restrictions.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
                     MovieIds = movies.Where(c => c.Value.Contains(tag.Id)).Select(c => c.Key).ToList(),
-                    IndexerIds = indexers.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList()
+                    IndexerIds = indexers.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
+                    DownloadClientIds = downloadClients.Where(c => c.Tags.Contains(tag.Id)).Select(c => c.Id).ToList(),
                 });
             }
 