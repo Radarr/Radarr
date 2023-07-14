31a16ab5713aff8905c83d10d47c2ed68067f4c8
Mark McDowall <mark@mcdowall.ca>
2023-07-14 16:55:25 -0700

    Fixed: Ensure translations are fetched before loading app
    (cherry picked from commit ad2721dc55f3233e4c299babe5744418bc530418)

--- frontend/src/Utilities/String/translate.js
+++ frontend/src/Utilities/String/translate.js
@@ -1,22 +1,27 @@
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 
 function getTranslations() {
-  let localization = null;
-  const ajaxOptions = {
-    async: false,
+  return createAjaxRequest({
+    global: false,
     dataType: 'json',
-    url: '/localization',
-    success: function(data) {
-      localization = data.Strings;
-    }
-  };
-
-  createAjaxRequest(ajaxOptions);
-
-  return localization;
+    url: '/localization'
+  }).request;
 }
 
-const translations = getTranslations();
+let translations = {};
+
+export function fetchTranslations() {
+  return new Promise(async(resolve) => {
+    try {
+      const data = await getTranslations();
+      translations = data.Strings;
+
+      resolve(true);
+    } catch (error) {
+      resolve(false);
+    }
+  });
+}
 
 export default function translate(key, args) {
   const translation = translations[key] || key;
