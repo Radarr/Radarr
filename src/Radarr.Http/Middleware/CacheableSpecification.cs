e69125341943accb9fdbfbaf30d2da417cc5bcc9
Mark McDowall <mark@mcdowall.ca>
2023-07-17 20:37:02 -0700

    UI loading improvements
    Fixed: Caching for dynamically loaded JS files
    Fixed: Incorrect caching of initialize.js
    (cherry picked from commit f0cb5b81f140c67fa84162e094cc4e0f3476f5da)

--- src/Radarr.Http/Middleware/CacheableSpecification.cs
+++ src/Radarr.Http/Middleware/CacheableSpecification.cs
@@ -46,7 +46,7 @@ namespace Radarr.Http.Middleware
                 return false;
             }
 
-            if (path.EndsWith("/initialize.js"))
+            if (path.EndsWith("/initialize.json"))
             {
                 return false;
             }
