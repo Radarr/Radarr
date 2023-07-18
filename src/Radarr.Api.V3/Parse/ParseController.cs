nicksen/Sonarr

New: Added UI for parsing release names
Closes #5263

src/Sonarr.Api.V3/Parse/ParseController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.Parser;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Api.V3.Series;
 using Sonarr.Http;
@@ -13,12 +15,15 @@ namespace Sonarr.Api.V3.Parse
     {
         private readonly IParsingService _parsingService;
         private readonly IRemoteEpisodeAggregationService _aggregationService;
+        private readonly ICustomFormatCalculationService _formatCalculator;
 
         public ParseController(IParsingService parsingService,
-                               IRemoteEpisodeAggregationService aggregationService)
+                               IRemoteEpisodeAggregationService aggregationService,
+                               ICustomFormatCalculationService formatCalculator)
         {
             _parsingService = parsingService;
             _aggregationService = aggregationService;
+            _formatCalculator = formatCalculator;
         }
 
         [HttpGet]
@@ -42,16 +47,22 @@ namespace Sonarr.Api.V3.Parse
 
             var remoteEpisode = _parsingService.Map(parsedEpisodeInfo, 0, 0);
 
-            _aggregationService.Augment(remoteEpisode);
-
             if (remoteEpisode != null)
             {
+                _aggregationService.Augment(remoteEpisode);
+
+                remoteEpisode.CustomFormats = _formatCalculator.ParseCustomFormat(remoteEpisode, 0);
+                remoteEpisode.CustomFormatScore = remoteEpisode?.Series?.QualityProfile?.Value.CalculateCustomFormatScore(remoteEpisode.CustomFormats) ?? 0;
+
                 return new ParseResource
                 {
                     Title = title,
                     ParsedEpisodeInfo = remoteEpisode.ParsedEpisodeInfo,
                     Series = remoteEpisode.Series.ToResource(),
-                    Episodes = remoteEpisode.Episodes.ToResource()
+                    Episodes = remoteEpisode.Episodes.ToResource(),
+                    Languages = remoteEpisode.Languages,
+                    CustomFormats = remoteEpisode.CustomFormats?.ToResource(false),
+                    CustomFormatScore = remoteEpisode.CustomFormatScore
                 };
             }
             else

src/Sonarr.Api.V3/Parse/ParseResource.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Parser.Model;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Api.V3.Series;
 using Sonarr.Http.REST;
@@ -12,5 +14,8 @@ namespace Sonarr.Api.V3.Parse
         public ParsedEpisodeInfo ParsedEpisodeInfo { get; set; }
         public SeriesResource Series { get; set; }
         public List<EpisodeResource> Episodes { get; set; }
+        public List<Language> Languages { get; set; }
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
     }
 }

Radarr/Radarr

(cherry picked from commit 85e285598106346099ceae676599c5cb4b789c92)

src/Radarr.Api.V3/Parse/ParseController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.Parser;
+using Radarr.Api.V3.CustomFormats;
 using Radarr.Api.V3.Movies;
 using Radarr.Http;
 
@@ -14,14 +16,17 @@ namespace Radarr.Api.V3.Parse
         private readonly IParsingService _parsingService;
         private readonly IConfigService _configService;
         private readonly IRemoteMovieAggregationService _aggregationService;
+        private readonly ICustomFormatCalculationService _formatCalculator;
 
         public ParseController(IParsingService parsingService,
                                IConfigService configService,
-                               IRemoteMovieAggregationService aggregationService)
+                               IRemoteMovieAggregationService aggregationService,
+                               ICustomFormatCalculationService formatCalculator)
         {
             _parsingService = parsingService;
             _configService = configService;
             _aggregationService = aggregationService;
+            _formatCalculator = formatCalculator;
         }
 
         [HttpGet]
@@ -44,15 +49,21 @@ namespace Radarr.Api.V3.Parse
 
             var remoteMovie = _parsingService.Map(parsedMovieInfo, "", 0);
 
-            _aggregationService.Augment(remoteMovie);
-
             if (remoteMovie != null)
             {
+                _aggregationService.Augment(remoteMovie);
+
+                remoteMovie.CustomFormats = _formatCalculator.ParseCustomFormat(remoteMovie, 0);
+                remoteMovie.CustomFormatScore = remoteMovie.Movie?.Profile?.CalculateCustomFormatScore(remoteMovie.CustomFormats) ?? 0;
+
                 return new ParseResource
                 {
                     Title = title,
                     ParsedMovieInfo = remoteMovie.ParsedMovieInfo,
-                    Movie = remoteMovie.Movie.ToResource(_configService.AvailabilityDelay)
+                    Movie = remoteMovie.Movie.ToResource(_configService.AvailabilityDelay),
+                    Languages = remoteMovie.Languages,
+                    CustomFormats = remoteMovie.CustomFormats?.ToResource(false),
+                    CustomFormatScore = remoteMovie.CustomFormatScore
                 };
             }
             else

src/Radarr.Api.V3/Parse/ParseResource.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Parser.Model;
+using Radarr.Api.V3.CustomFormats;
 using Radarr.Api.V3.Movies;
 using Radarr.Http.REST;
 
@@ -9,5 +12,8 @@ namespace Radarr.Api.V3.Parse
         public string Title { get; set; }
         public ParsedMovieInfo ParsedMovieInfo { get; set; }
         public MovieResource Movie { get; set; }
+        public List<Language> Languages { get; set; }
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
     }
 }