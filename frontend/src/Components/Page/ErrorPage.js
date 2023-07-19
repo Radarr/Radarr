3815df7adab17ce9c9e48538d27d98c2adfd073b
Mark McDowall <mark@mcdowall.ca>
2023-07-18 20:20:27 -0700

    Fixed: Improve translation loading
    (cherry picked from commit 73c5ec1da4dd00301e1b0dddbcea37590a99b045)

--- frontend/src/Components/Page/ErrorPage.js
+++ frontend/src/Components/Page/ErrorPage.js
@@ -7,7 +7,7 @@ function ErrorPage(props) {
   const {
     version,
     isLocalStorageSupported,
-    hasTranslationsError,
+    translationsError,
     moviesError,
     customFiltersError,
     tagsError,
@@ -21,8 +21,8 @@ function ErrorPage(props) {
 
   if (!isLocalStorageSupported) {
     errorMessage = 'Local Storage is not supported or disabled. A plugin or private browsing may have disabled it.';
-  } else if (hasTranslationsError) {
-    errorMessage = 'Failed to load translations from API';
+  } else if (translationsError) {
+    errorMessage = getErrorMessage(translationsError, 'Failed to load translations from API');
   } else if (moviesError) {
     errorMessage = getErrorMessage(moviesError, 'Failed to load movie from API');
   } else if (customFiltersError) {
@@ -55,7 +55,7 @@ function ErrorPage(props) {
 ErrorPage.propTypes = {
   version: PropTypes.string.isRequired,
   isLocalStorageSupported: PropTypes.bool.isRequired,
-  hasTranslationsError: PropTypes.bool.isRequired,
+  translationsError: PropTypes.object,
   moviesError: PropTypes.object,
   customFiltersError: PropTypes.object,
   tagsError: PropTypes.object,
--- frontend/src/bootstrap.tsx
+++ frontend/src/bootstrap.tsx
@@ -2,20 +2,14 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import { render } from 'react-dom';
 import createAppStore from 'Store/createAppStore';
-import { fetchTranslations } from 'Utilities/String/translate';
 import App from './App/App';
 
 export async function bootstrap() {
   const history = createBrowserHistory();
   const store = createAppStore(history);
-  const hasTranslationsError = !(await fetchTranslations());
 
   render(
-    <App
-      store={store}
-      history={history}
-      hasTranslationsError={hasTranslationsError}
-    />,
+    <App store={store} history={history} />,
     document.getElementById('root')
   );
 }
