6e23153e3cd0843968aa117fee091a29180cf3b3
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-21 00:49:58 +0300

    Add support for deprecated values in field select options
    (cherry picked from commit d9786887f3fe30ef60ad9c50b3272bf60dfef309)

--- src/Radarr.Http/ClientSchema/SchemaBuilder.cs
+++ src/Radarr.Http/ClientSchema/SchemaBuilder.cs
@@ -108,7 +108,7 @@ namespace Radarr.Http.ClientSchema
                         Placeholder = fieldAttribute.Placeholder
                     };
 
-                    if (fieldAttribute.Type == FieldType.Select || fieldAttribute.Type == FieldType.TagSelect)
+                    if (fieldAttribute.Type is FieldType.Select or FieldType.TagSelect)
                     {
                         if (fieldAttribute.SelectOptionsProviderAction.IsNotNullOrWhiteSpace())
                         {
@@ -157,31 +157,33 @@ namespace Radarr.Http.ClientSchema
         {
             if (selectOptions.IsEnum)
             {
-                var options = selectOptions.GetFields().Where(v => v.IsStatic).Select(v =>
-                {
-                    var name = v.Name.Replace('_', ' ');
-                    var value = Convert.ToInt32(v.GetRawConstantValue());
-                    var attrib = v.GetCustomAttribute<FieldOptionAttribute>();
-                    if (attrib != null)
+                var options = selectOptions
+                    .GetFields()
+                    .Where(v => v.IsStatic && !v.GetCustomAttributes(false).OfType<ObsoleteAttribute>().Any())
+                    .Select(v =>
                     {
-                        return new SelectOption
+                        var name = v.Name.Replace('_', ' ');
+                        var value = Convert.ToInt32(v.GetRawConstantValue());
+                        var attrib = v.GetCustomAttribute<FieldOptionAttribute>();
+
+                        if (attrib != null)
                         {
-                            Value = value,
-                            Name = attrib.Label ?? name,
-                            Order = attrib.Order,
-                            Hint = attrib.Hint ?? $"({value})"
-                        };
-                    }
-                    else
-                    {
+                            return new SelectOption
+                            {
+                                Value = value,
+                                Name = attrib.Label ?? name,
+                                Order = attrib.Order,
+                                Hint = attrib.Hint ?? $"({value})"
+                            };
+                        }
+
                         return new SelectOption
                         {
                             Value = value,
                             Name = name,
                             Order = value
                         };
-                    }
-                });
+                    });
 
                 return options.OrderBy(o => o.Order).ToList();
             }
