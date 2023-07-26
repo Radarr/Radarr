938b69b240f7ceecec90bbb55402567a805f3349
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-26 11:36:59 +0300

    Fixed: Add dedupe releases rule based on indexer priority

--- src/NzbDrone.Core/IndexerSearch/ReleaseSearchService.cs
+++ src/NzbDrone.Core/IndexerSearch/ReleaseSearchService.cs
@@ -143,8 +143,10 @@ namespace NzbDrone.Core.IndexerSearch
 
         private List<DownloadDecision> DeDupeDecisions(List<DownloadDecision> decisions)
         {
-            // De-dupe reports by guid so duplicate results aren't returned. Pick the one with the least rejections.
-            return decisions.GroupBy(d => d.RemoteMovie.Release.Guid).Select(d => d.OrderBy(v => v.Rejections.Count()).First()).ToList();
+            // De-dupe reports by guid so duplicate results aren't returned. Pick the one with the least rejections and higher indexer priority.
+            return decisions.GroupBy(d => d.RemoteMovie.Release.Guid)
+                .Select(d => d.OrderBy(v => v.Rejections.Count()).ThenBy(v => v.RemoteMovie?.Release?.IndexerPriority ?? 100).First())
+                .ToList();
         }
     }
 }
