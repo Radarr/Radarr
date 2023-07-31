Sonarr/Sonarr

Convert store selectors to Typescript

frontend/src/App/State/TagsAppState.ts
@@ -1,12 +1,32 @@
 import ModelBase from 'App/ModelBase';
 import AppSectionState, {
   AppSectionDeleteState,
+  AppSectionSaveState,
 } from 'App/State/AppSectionState';
 
 export interface Tag extends ModelBase {
   label: string;
 }
 
-interface TagsAppState extends AppSectionState<Tag>, AppSectionDeleteState {}
+export interface TagDetail extends ModelBase {
+  label: string;
+  autoTagIds: number[];
+  delayProfileIds: number[];
+  downloadClientIds: [];
+  importListIds: number[];
+  indexerIds: number[];
+  notificationIds: number[];
+  restrictionIds: number[];
+  seriesIds: number[];
+}
+
+export interface TagDetailAppState
+  extends AppSectionState<TagDetail>,
+    AppSectionDeleteState,
+    AppSectionSaveState {}
+
+interface TagsAppState extends AppSectionState<Tag>, AppSectionDeleteState {
+  details: TagDetailAppState;
+}
 
 export default TagsAppState;

frontend/src/Store/Selectors/createQueueItemSelector.ts
@@ -1,17 +1,16 @@
 import { createSelector } from 'reselect';
+import AppState from 'App/State/AppState';
 
 function createQueueItemSelector() {
   return createSelector(
-    (state, { episodeId }) => episodeId,
-    (state) => state.queue.details.items,
+    (_: AppState, { episodeId }: { episodeId: number }) => episodeId,
+    (state: AppState) => state.queue.details.items,
     (episodeId, details) => {
       if (!episodeId || !details) {
         return null;
       }
 
-      return details.find((item) => {
-        return item.episodeId === episodeId;
-      });
+      return details.find((item) => item.episodeId === episodeId);
     }
   );
 }

Radarr/Radarr

(cherry picked from commit a57b35a1966266b49d1241474fe3b69523f70474)

frontend/src/App/State/TagsAppState.ts
@@ -1,12 +1,32 @@
 import ModelBase from 'App/ModelBase';
 import AppSectionState, {
   AppSectionDeleteState,
+  AppSectionSaveState,
 } from 'App/State/AppSectionState';
 
 export interface Tag extends ModelBase {
   label: string;
 }
 
-interface TagsAppState extends AppSectionState<Tag>, AppSectionDeleteState {}
+export interface TagDetail extends ModelBase {
+  label: string;
+  autoTagIds: number[];
+  delayProfileIds: number[];
+  downloadClientIds: number[];
+  importListIds: number[];
+  indexerIds: number[];
+  movieIds: number[];
+  notificationIds: number[];
+  restrictionIds: number[];
+}
+
+export interface TagDetailAppState
+  extends AppSectionState<TagDetail>,
+    AppSectionDeleteState,
+    AppSectionSaveState {}
+
+interface TagsAppState extends AppSectionState<Tag>, AppSectionDeleteState {
+  details: TagDetailAppState;
+}
 
 export default TagsAppState;

frontend/src/Store/Selectors/createQueueItemSelector.ts
@@ -1,17 +1,16 @@
 import { createSelector } from 'reselect';
+import AppState from 'App/State/AppState';
 
 function createQueueItemSelector() {
   return createSelector(
-    (state, { movieId }) => movieId,
-    (state) => state.queue.details.items,
+    (_: AppState, { movieId }: { movieId: number }) => movieId,
+    (state: AppState) => state.queue.details.items,
     (movieId, details) => {
       if (!movieId || !details) {
         return null;
       }
 
-      return details.find((item) => {
-        return item.movieId === movieId;
-      });
+      return details.find((item) => item.movieId === movieId);
     }
   );
 }