c96f57ecccf28e6f016a4a0018a973f67868030b
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-08-02 03:02:25 +0300

    Fixed: Clear movie search results when navigating to another page
    (cherry picked from commit 67dc8987970aa2a9eade48c02ae72be1851fa196)

--- frontend/src/Movie/Search/MovieInteractiveSearchModalConnector.js
+++ frontend/src/Movie/Search/MovieInteractiveSearchModalConnector.js
@@ -1,9 +1,19 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { cancelFetchReleases, clearReleases } from 'Store/Actions/releaseActions';
 import MovieInteractiveSearchModal from './MovieInteractiveSearchModal';
 
 function createMapDispatchToProps(dispatch, props) {
   return {
+    dispatchCancelFetchReleases() {
+      dispatch(cancelFetchReleases());
+    },
+
+    dispatchClearReleases() {
+      dispatch(clearReleases());
+    },
+
     onModalClose() {
       dispatch(cancelFetchReleases());
       dispatch(clearReleases());
@@ -12,4 +22,38 @@ function createMapDispatchToProps(dispatch, props) {
   };
 }
 
-export default connect(null, createMapDispatchToProps)(MovieInteractiveSearchModal);
+class MovieInteractiveSearchModalConnector extends Component {
+
+  //
+  // Lifecycle
+
+  componentWillUnmount() {
+    this.props.dispatchCancelFetchReleases();
+    this.props.dispatchClearReleases();
+  }
+
+  //
+  // Render
+
+  render() {
+    const {
+      dispatchCancelFetchReleases,
+      dispatchClearReleases,
+      ...otherProps
+    } = this.props;
+
+    return (
+      <MovieInteractiveSearchModal
+        {...otherProps}
+      />
+    );
+  }
+}
+
+MovieInteractiveSearchModalConnector.propTypes = {
+  ...MovieInteractiveSearchModal.propTypes,
+  dispatchCancelFetchReleases: PropTypes.func.isRequired,
+  dispatchClearReleases: PropTypes.func.isRequired
+};
+
+export default connect(null, createMapDispatchToProps)(MovieInteractiveSearchModalConnector);
