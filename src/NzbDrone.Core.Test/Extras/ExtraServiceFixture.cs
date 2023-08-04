Sonarr/Sonarr

New: Ignore inaccessible files with getting files
Closes #5818

src/NzbDrone.Core.Test/Extras/ExtraServiceFixture.cs
@@ -121,7 +121,7 @@ namespace NzbDrone.Core.Test.Extras
                 WithExistingFile(file);
             }
 
-            Mocker.GetMock<IDiskProvider>().Setup(s => s.GetFiles(_episodeFolder, It.IsAny<SearchOption>()))
+            Mocker.GetMock<IDiskProvider>().Setup(s => s.GetFiles(_episodeFolder, It.IsAny<bool>()))
                   .Returns(files.ToArray());
         }
 
@@ -224,8 +224,8 @@ namespace NzbDrone.Core.Test.Extras
 
             Subject.ImportEpisode(_localEpisode, _episodeFile, true);
 
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, SearchOption.AllDirectories), Times.Once);
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, SearchOption.TopDirectoryOnly), Times.Never);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, true), Times.Once);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, false), Times.Never);
         }
 
         [Test]
@@ -245,8 +245,8 @@ namespace NzbDrone.Core.Test.Extras
 
             Subject.ImportEpisode(_localEpisode, _episodeFile, true);
 
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, SearchOption.AllDirectories), Times.Never);
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, SearchOption.TopDirectoryOnly), Times.Once);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, true), Times.Never);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_episodeFolder, false), Times.Once);
         }
     }
 }

src/NzbDrone.Core.Test/MediaFiles/DiskScanServiceTests/ScanFixture.cs
@@ -81,7 +81,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
         private void GivenFiles(IEnumerable<string> files)
         {
             Mocker.GetMock<IDiskProvider>()
-                  .Setup(s => s.GetFiles(It.IsAny<string>(), SearchOption.AllDirectories))
+                  .Setup(s => s.GetFiles(It.IsAny<string>(), true))
                   .Returns(files.ToArray());
         }
 
@@ -93,7 +93,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
             ExceptionVerification.ExpectedWarns(1);
 
             Mocker.GetMock<IDiskProvider>()
-                  .Verify(v => v.GetFiles(_series.Path, SearchOption.AllDirectories), Times.Never());
+                  .Verify(v => v.GetFiles(_series.Path, true), Times.Never());
 
             Mocker.GetMock<IDiskProvider>()
                   .Verify(v => v.CreateFolder(_series.Path), Times.Never());
@@ -112,7 +112,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
             ExceptionVerification.ExpectedWarns(1);
 
             Mocker.GetMock<IDiskProvider>()
-                  .Verify(v => v.GetFiles(_series.Path, SearchOption.AllDirectories), Times.Never());
+                  .Verify(v => v.GetFiles(_series.Path, true), Times.Never());
 
             Mocker.GetMock<IDiskProvider>()
                   .Verify(v => v.CreateFolder(_series.Path), Times.Never());
@@ -267,7 +267,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
             Subject.Scan(_series);
 
             Mocker.GetMock<IDiskProvider>()
-                  .Verify(v => v.GetFiles(It.IsAny<string>(), It.IsAny<SearchOption>()), Times.Once());
+                  .Verify(v => v.GetFiles(It.IsAny<string>(), It.IsAny<bool>()), Times.Once());
 
             Mocker.GetMock<IMakeImportDecision>()
                   .Verify(v => v.GetImportDecisions(It.Is<List<string>>(l => l.Count == 1), _series, false), Times.Once());

Radarr/Radarr

(cherry picked from commit e5aa8584100d96a2077c57f74ae5b2ceab63de19)

src/NzbDrone.Core.Test/Extras/ExtraServiceFixture.cs
@@ -113,7 +113,7 @@ namespace NzbDrone.Core.Test.Extras
                 WithExistingFile(file);
             }
 
-            Mocker.GetMock<IDiskProvider>().Setup(s => s.GetFiles(_releaseFolder, It.IsAny<SearchOption>()))
+            Mocker.GetMock<IDiskProvider>().Setup(s => s.GetFiles(_releaseFolder, It.IsAny<bool>()))
                   .Returns(files.ToArray());
         }
 
@@ -216,8 +216,8 @@ namespace NzbDrone.Core.Test.Extras
 
             Subject.ImportMovie(_localMovie, _movieFile, true);
 
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, SearchOption.AllDirectories), Times.Once);
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, SearchOption.TopDirectoryOnly), Times.Never);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, true), Times.Once);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, false), Times.Never);
         }
 
         [Test]
@@ -237,8 +237,8 @@ namespace NzbDrone.Core.Test.Extras
 
             Subject.ImportMovie(_localMovie, _movieFile, true);
 
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, SearchOption.AllDirectories), Times.Never);
-            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, SearchOption.TopDirectoryOnly), Times.Once);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, true), Times.Never);
+            Mocker.GetMock<IDiskProvider>().Verify(v => v.GetFiles(_releaseFolder, false), Times.Once);
         }
     }
 }

src/NzbDrone.Core.Test/MediaFiles/DiskScanServiceTests/ScanFixture.cs
@@ -81,7 +81,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
         private void GivenFiles(IEnumerable<string> files)
         {
             Mocker.GetMock<IDiskProvider>()
-                  .Setup(s => s.GetFiles(It.IsAny<string>(), SearchOption.AllDirectories))
+                  .Setup(s => s.GetFiles(It.IsAny<string>(), true))
                   .Returns(files.ToArray());
         }
 
@@ -93,7 +93,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
             ExceptionVerification.ExpectedWarns(1);
 
             Mocker.GetMock<IDiskProvider>()
-                  .Verify(v => v.GetFiles(_movie.Path, SearchOption.AllDirectories), Times.Never());
+                  .Verify(v => v.GetFiles(_movie.Path, true), Times.Never());
 
             Mocker.GetMock<IDiskProvider>()
                   .Verify(v => v.CreateFolder(_movie.Path), Times.Never());
@@ -112,7 +112,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
             ExceptionVerification.ExpectedWarns(1);
 
             Mocker.GetMock<IDiskProvider>()
-                  .Verify(v => v.GetFiles(_movie.Path, SearchOption.AllDirectories), Times.Never());
+                  .Verify(v => v.GetFiles(_movie.Path, true), Times.Never());
 
             Mocker.GetMock<IDiskProvider>()
                   .Verify(v => v.CreateFolder(_movie.Path), Times.Never());
@@ -154,6 +154,9 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
                        });
 
             Subject.Scan(_movie);
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.GetFiles(It.IsAny<string>(), It.IsAny<bool>()), Times.Once());
 
             Mocker.GetMock<IMakeImportDecision>()
                   .Verify(v => v.GetImportDecisions(It.Is<List<string>>(l => l.Count == 1), _movie, false), Times.Once());