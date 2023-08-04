Sonarr/Sonarr

Fix GetBestRootFolderPath tests

src/NzbDrone.Core.Test/RootFolderTests/GetBestRootFolderPathFixture.cs
@@ -1,8 +1,6 @@
-﻿using System.Linq;
+using System.Linq;
 using FluentAssertions;
-using Moq;
 using NUnit.Framework;
-using NzbDrone.Common.Disk;
 using NzbDrone.Core.RootFolders;
 using NzbDrone.Core.Test.Framework;
 using NzbDrone.Test.Common;
@@ -34,15 +32,34 @@ namespace NzbDrone.Core.Test.RootFolderTests
         }
 
         [Test]
-        public void should_get_parent_path_from_diskProvider_if_matching_root_folder_is_not_found()
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found()
         {
             var seriesPath = @"T:\Test\TV\Series Title".AsOsAgnostic();
 
             GivenRootFolders(@"C:\Test\TV".AsOsAgnostic(), @"D:\Test\TV".AsOsAgnostic());
-            Subject.GetBestRootFolderPath(seriesPath);
+            Subject.GetBestRootFolderPath(seriesPath).Should().Be(@"T:\Test\TV".AsOsAgnostic());
+        }
 
-            Mocker.GetMock<IDiskProvider>()
-                .Verify(v => v.GetParentFolder(seriesPath), Times.Once);
+        [Test]
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found_for_posix_path()
+        {
+            WindowsOnly();
+
+            var seriesPath = "/mnt/tv/Series Title";
+
+            GivenRootFolders(@"C:\Test\TV".AsOsAgnostic(), @"D:\Test\TV".AsOsAgnostic());
+            Subject.GetBestRootFolderPath(seriesPath).Should().Be(@"/mnt/tv".AsOsAgnostic());
+        }
+
+        [Test]
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found_for_windows_path()
+        {
+            PosixOnly();
+
+            var seriesPath = @"T:\Test\TV\Series Title";
+
+            GivenRootFolders(@"C:\Test\TV".AsOsAgnostic(), @"D:\Test\TV".AsOsAgnostic());
+            Subject.GetBestRootFolderPath(seriesPath).Should().Be(@"T:\Test\TV".AsOsAgnostic());
         }
     }
 }

Radarr/Radarr

(cherry picked from commit 63a911a9a549749b5460c2b9fea48a25e78c52a4)

src/NzbDrone.Core.Test/RootFolderTests/GetBestRootFolderPathFixture.cs
@@ -1,8 +1,6 @@
 using System.Linq;
 using FluentAssertions;
-using Moq;
 using NUnit.Framework;
-using NzbDrone.Common.Disk;
 using NzbDrone.Core.RootFolders;
 using NzbDrone.Core.Test.Framework;
 using NzbDrone.Test.Common;
@@ -34,15 +32,34 @@ namespace NzbDrone.Core.Test.RootFolderTests
         }
 
         [Test]
-        public void should_get_parent_path_from_diskProvider_if_matching_root_folder_is_not_found()
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found()
         {
             var moviePath = @"T:\Test\Movies\Movie Title".AsOsAgnostic();
 
             GivenRootFolders(@"C:\Test\Movies".AsOsAgnostic(), @"D:\Test\Movies".AsOsAgnostic());
-            Subject.GetBestRootFolderPath(moviePath);
+            Subject.GetBestRootFolderPath(moviePath).Should().Be(@"T:\Test\Movies".AsOsAgnostic());
+        }
 
-            Mocker.GetMock<IDiskProvider>()
-                .Verify(v => v.GetParentFolder(moviePath), Times.Once);
+        [Test]
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found_for_posix_path()
+        {
+            WindowsOnly();
+
+            var moviePath = "/mnt/movies/Movie Title";
+
+            GivenRootFolders(@"C:\Test\Movies".AsOsAgnostic(), @"D:\Test\Movies".AsOsAgnostic());
+            Subject.GetBestRootFolderPath(moviePath).Should().Be(@"/mnt/movies");
+        }
+
+        [Test]
+        public void should_get_parent_path_from_os_path_if_matching_root_folder_is_not_found_for_windows_path()
+        {
+            PosixOnly();
+
+            var moviePath = @"T:\Test\Movies\Movie Title";
+
+            GivenRootFolders(@"C:\Test\Movies".AsOsAgnostic(), @"D:\Test\Movies".AsOsAgnostic());
+            Subject.GetBestRootFolderPath(moviePath).Should().Be(@"T:\Test\Movies");
         }
     }
 }