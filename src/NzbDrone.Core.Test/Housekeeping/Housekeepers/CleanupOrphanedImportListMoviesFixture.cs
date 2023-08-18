337d01e4ed89ed8e11d5d4ac4482691518be3910
Qstick <qstick@gmail.com>
2023-08-17 22:03:50 -0500

    Add housekeeper for orphaned list movies

---
+++ src/NzbDrone.Core.Test/Housekeeping/Housekeepers/CleanupOrphanedImportListMoviesFixture.cs
@@ -0,0 +1,55 @@
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Housekeeping.Housekeepers;
+using NzbDrone.Core.ImportLists;
+using NzbDrone.Core.ImportLists.ImportListMovies;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Housekeeping.Housekeepers
+{
+    [TestFixture]
+    public class CleanupOrphanedImportListMoviesFixture : DbTest<CleanupOrphanedImportListMovies, ImportListMovie>
+    {
+        private ImportListDefinition _importList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _importList = Builder<ImportListDefinition>.CreateNew()
+                                                       .BuildNew();
+        }
+
+        private void GivenImportList()
+        {
+            Db.Insert(_importList);
+        }
+
+        [Test]
+        public void should_delete_orphaned_importlistmovies()
+        {
+            var status = Builder<ImportListMovie>.CreateNew()
+                                                 .With(h => h.ListId = _importList.Id)
+                                                 .BuildNew();
+            Db.Insert(status);
+
+            Subject.Clean();
+            AllStoredModels.Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_not_delete_unorphaned_importlistmovies()
+        {
+            GivenImportList();
+
+            var status = Builder<ImportListMovie>.CreateNew()
+                                                 .With(h => h.ListId = _importList.Id)
+                                                 .BuildNew();
+            Db.Insert(status);
+
+            Subject.Clean();
+            AllStoredModels.Should().HaveCount(1);
+            AllStoredModels.Should().Contain(h => h.ListId == _importList.Id);
+        }
+    }
+}
