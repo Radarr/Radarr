Sonarr/Sonarr

New: Calendar filtering by tags
Closes #5476

frontend/src/Store/Actions/calendarActions.js
@@ -4,9 +4,10 @@ import { createAction } from 'redux-actions';
 import { batchActions } from 'redux-batched-actions';
 import * as calendarViews from 'Calendar/calendarViews';
 import * as commandNames from 'Commands/commandNames';
-import { filterTypes } from 'Helpers/Props';
+import { filterBuilderTypes, filterBuilderValueTypes, filterTypes } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
+import findSelectedFilters from 'Utilities/Filter/findSelectedFilters';
 import { set, update } from './baseActions';
 import { executeCommandHelper } from './commandActions';
 import createHandleActions from './Creators/createHandleActions';
@@ -50,14 +51,16 @@ export const defaultState = {
 
   selectedFilterKey: 'monitored',
 
+  customFilters: [],
+
   filters: [
     {
       key: 'all',
       label: 'All',
       filters: [
         {
-          key: 'monitored',
-          value: false,
+          key: 'unmonitored',
+          value: [true],
           type: filterTypes.EQUAL
         }
       ]
@@ -67,20 +70,35 @@ export const defaultState = {
       label: 'Monitored Only',
       filters: [
         {
-          key: 'monitored',
-          value: true,
+          key: 'unmonitored',
+          value: [false],
           type: filterTypes.EQUAL
         }
       ]
     }
+  ],
 
+  filterBuilderProps: [
+    {
+      name: 'unmonitored',
+      label: 'Include Unmonitored',
+      type: filterBuilderTypes.EQUAL,
+      valueType: filterBuilderValueTypes.BOOL
+    },
+    {
+      name: 'tags',
+      label: 'Tags',
+      type: filterBuilderTypes.CONTAINS,
+      valueType: filterBuilderValueTypes.TAG
+    }
   ]
 };
 
 export const persistState = [
   'calendar.view',
   'calendar.selectedFilterKey',
-  'calendar.options'
+  'calendar.options',
+  'seriesIndex.customFilters'
 ];
 
 //
@@ -192,6 +210,10 @@ function isRangePopulated(start, end, state) {
   return false;
 }
 
+function getCustomFilters(state, type) {
+  return state.customFilters.items.filter((customFilter) => customFilter.type === type);
+}
+
 //
 // Action Creators
 
@@ -213,7 +235,8 @@ export const actionHandlers = handleThunks({
   [FETCH_CALENDAR]: function(getState, payload, dispatch) {
     const state = getState();
     const calendar = state.calendar;
-    const unmonitored = calendar.selectedFilterKey === 'all';
+    const customFilters = getCustomFilters(state, section);
+    const selectedFilters = findSelectedFilters(calendar.selectedFilterKey, calendar.filters, customFilters);
 
     const {
       time = calendar.time,
@@ -240,13 +263,26 @@ export const actionHandlers = handleThunks({
 
     dispatch(set(attrs));
 
+    const requestParams = {
+      start,
+      end
+    };
+
+    selectedFilters.forEach((selectedFilter) => {
+      if (selectedFilter.key === 'unmonitored') {
+        requestParams.unmonitored = selectedFilter.value.includes(true);
+      }
+
+      if (selectedFilter.key === 'tags') {
+        requestParams.tags = selectedFilter.value.join(',');
+      }
+    });
+
+    requestParams.unmonitored = requestParams.unmonitored ?? false;
+
     const promise = createAjaxRequest({
       url: '/calendar',
-      data: {
-        unmonitored,
-        start,
-        end
-      }
+      data: requestParams
     }).request;
 
     promise.done((data) => {

src/Sonarr.Api.V3/Calendar/CalendarController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.DecisionEngine.Specifications;
+using NzbDrone.Core.Tags;
 using NzbDrone.Core.Tv;
 using NzbDrone.SignalR;
 using Sonarr.Api.V3.Episodes;
@@ -14,23 +16,53 @@ namespace Sonarr.Api.V3.Calendar
     [V3ApiController]
     public class CalendarController : EpisodeControllerWithSignalR
     {
+        private readonly ITagService _tagService;
+
         public CalendarController(IBroadcastSignalRMessage signalR,
                             IEpisodeService episodeService,
                             ISeriesService seriesService,
                             IUpgradableSpecification qualityUpgradableSpecification,
+                            ITagService tagService,
                             ICustomFormatCalculationService formatCalculator)
             : base(episodeService, seriesService, qualityUpgradableSpecification, formatCalculator, signalR)
         {
+            _tagService = tagService;
         }
 
         [HttpGet]
         [Produces("application/json")]
-        public List<EpisodeResource> GetCalendar(DateTime? start, DateTime? end, bool unmonitored = false, bool includeSeries = false, bool includeEpisodeFile = false, bool includeEpisodeImages = false)
+        public List<EpisodeResource> GetCalendar(DateTime? start, DateTime? end, bool unmonitored = false, bool includeSeries = false, bool includeEpisodeFile = false, bool includeEpisodeImages = false, string tags = "")
         {
             var startUse = start ?? DateTime.Today;
             var endUse = end ?? DateTime.Today.AddDays(2);
+            var episodes = _episodeService.EpisodesBetweenDates(startUse, endUse, unmonitored);
+            var allSeries = _seriesService.GetAllSeries();
+            var parsedTags = new List<int>();
+            var result = new List<Episode>();
 
-            var resources = MapToResource(_episodeService.EpisodesBetweenDates(startUse, endUse, unmonitored), includeSeries, includeEpisodeFile, includeEpisodeImages);
+            if (tags.IsNotNullOrWhiteSpace())
+            {
+                parsedTags.AddRange(tags.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
+            }
+
+            foreach (var episode in episodes)
+            {
+                var series = allSeries.SingleOrDefault(s => s.Id == episode.SeriesId);
+
+                if (series == null)
+                {
+                    continue;
+                }
+
+                if (parsedTags.Any() && parsedTags.None(series.Tags.Contains))
+                {
+                    continue;
+                }
+
+                result.Add(episode);
+            }
+
+            var resources = MapToResource(result, includeSeries, includeEpisodeFile, includeEpisodeImages);
 
             return resources.OrderBy(e => e.AirDateUtc).ToList();
         }

Radarr/Radarr

Closes #8502
(cherry picked from commit 62b948b24c4b9c572db225cb19985444d3d80c0f)

frontend/src/Store/Actions/calendarActions.js
@@ -4,9 +4,10 @@ import { createAction } from 'redux-actions';
 import { batchActions } from 'redux-batched-actions';
 import * as calendarViews from 'Calendar/calendarViews';
 import * as commandNames from 'Commands/commandNames';
-import { filterTypes } from 'Helpers/Props';
+import { filterBuilderTypes, filterBuilderValueTypes, filterTypes } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
+import findSelectedFilters from 'Utilities/Filter/findSelectedFilters';
 import translate from 'Utilities/String/translate';
 import { set, update } from './baseActions';
 import { executeCommandHelper } from './commandActions';
@@ -47,14 +48,16 @@ export const defaultState = {
 
   selectedFilterKey: 'monitored',
 
+  customFilters: [],
+
   filters: [
     {
       key: 'all',
       label: () => translate('All'),
       filters: [
         {
-          key: 'monitored',
-          value: false,
+          key: 'unmonitored',
+          value: [true],
           type: filterTypes.EQUAL
         }
       ]
@@ -64,20 +67,35 @@ export const defaultState = {
       label: () => translate('MonitoredOnly'),
       filters: [
         {
-          key: 'monitored',
-          value: true,
+          key: 'unmonitored',
+          value: [false],
           type: filterTypes.EQUAL
         }
       ]
     }
+  ],
 
+  filterBuilderProps: [
+    {
+      name: 'unmonitored',
+      label: () => translate('IncludeUnmonitored'),
+      type: filterBuilderTypes.EQUAL,
+      valueType: filterBuilderValueTypes.BOOL
+    },
+    {
+      name: 'tags',
+      label: () => translate('Tags'),
+      type: filterBuilderTypes.CONTAINS,
+      valueType: filterBuilderValueTypes.TAG
+    }
   ]
 };
 
 export const persistState = [
   'calendar.view',
   'calendar.selectedFilterKey',
-  'calendar.options'
+  'calendar.options',
+  'movieIndex.customFilters'
 ];
 
 //
@@ -189,6 +207,10 @@ function isRangePopulated(start, end, state) {
   return false;
 }
 
+function getCustomFilters(state, type) {
+  return state.customFilters.items.filter((customFilter) => customFilter.type === type);
+}
+
 //
 // Action Creators
 
@@ -210,7 +232,8 @@ export const actionHandlers = handleThunks({
   [FETCH_CALENDAR]: function(getState, payload, dispatch) {
     const state = getState();
     const calendar = state.calendar;
-    const unmonitored = calendar.selectedFilterKey === 'all';
+    const customFilters = getCustomFilters(state, section);
+    const selectedFilters = findSelectedFilters(calendar.selectedFilterKey, calendar.filters, customFilters);
 
     const {
       time = calendar.time,
@@ -237,13 +260,26 @@ export const actionHandlers = handleThunks({
 
     dispatch(set(attrs));
 
+    const requestParams = {
+      start,
+      end
+    };
+
+    selectedFilters.forEach((selectedFilter) => {
+      if (selectedFilter.key === 'unmonitored') {
+        requestParams.unmonitored = selectedFilter.value.includes(true);
+      }
+
+      if (selectedFilter.key === 'tags') {
+        requestParams.tags = selectedFilter.value.join(',');
+      }
+    });
+
+    requestParams.unmonitored = requestParams.unmonitored ?? false;
+
     const promise = createAjaxRequest({
       url: '/calendar',
-      data: {
-        unmonitored,
-        start,
-        end
-      }
+      data: requestParams
     }).request;
 
     promise.done((data) => {

src/Radarr.Api.V3/Calendar/CalendarController.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.Movies;
 using NzbDrone.Core.Movies.Translations;
+using NzbDrone.Core.Tags;
 using NzbDrone.SignalR;
 using Radarr.Api.V3.Movies;
 using Radarr.Http;
@@ -20,18 +22,21 @@ namespace Radarr.Api.V3.Calendar
         private readonly IMovieService _moviesService;
         private readonly IMovieTranslationService _movieTranslationService;
         private readonly IUpgradableSpecification _qualityUpgradableSpecification;
+        private readonly ITagService _tagService;
         private readonly IConfigService _configService;
 
         public CalendarController(IBroadcastSignalRMessage signalR,
                             IMovieService moviesService,
                             IMovieTranslationService movieTranslationService,
                             IUpgradableSpecification qualityUpgradableSpecification,
+                            ITagService tagService,
                             IConfigService configService)
             : base(signalR)
         {
             _moviesService = moviesService;
             _movieTranslationService = movieTranslationService;
             _qualityUpgradableSpecification = qualityUpgradableSpecification;
+            _tagService = tagService;
             _configService = configService;
         }
 
@@ -42,12 +47,36 @@ namespace Radarr.Api.V3.Calendar
         }
 
         [HttpGet]
-        public List<MovieResource> GetCalendar(DateTime? start, DateTime? end, bool unmonitored = false)
+        [Produces("application/json")]
+        public List<MovieResource> GetCalendar(DateTime? start, DateTime? end, bool unmonitored = false, string tags = "")
         {
             var startUse = start ?? DateTime.Today;
             var endUse = end ?? DateTime.Today.AddDays(2);
+            var movies = _moviesService.GetMoviesBetweenDates(startUse, endUse, unmonitored);
+            var parsedTags = new List<int>();
+            var result = new List<Movie>();
 
-            var resources = _moviesService.GetMoviesBetweenDates(startUse, endUse, unmonitored).Select(MapToResource);
+            if (tags.IsNotNullOrWhiteSpace())
+            {
+                parsedTags.AddRange(tags.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
+            }
+
+            foreach (var movie in movies)
+            {
+                if (movie == null)
+                {
+                    continue;
+                }
+
+                if (parsedTags.Any() && parsedTags.None(movie.Tags.Contains))
+                {
+                    continue;
+                }
+
+                result.Add(movie);
+            }
+
+            var resources = result.Select(MapToResource);
 
             return resources.OrderBy(e => e.InCinemas).ToList();
         }