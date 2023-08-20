Sonarr/Sonarr

Improve CF calculation for files without scene name
Fixed: Use original filename instead of complete path when calculating CF for existing file without scene name
Closes #5365

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -155,13 +155,14 @@ namespace NzbDrone.Core.CustomFormats
         private static List<CustomFormat> ParseCustomFormat(EpisodeFile episodeFile, Series series, List<CustomFormat> allCustomFormats)
         {
             var sceneName = string.Empty;
+
             if (episodeFile.SceneName.IsNotNullOrWhiteSpace())
             {
                 sceneName = episodeFile.SceneName;
             }
             else if (episodeFile.OriginalFilePath.IsNotNullOrWhiteSpace())
             {
-                sceneName = episodeFile.OriginalFilePath;
+                sceneName = Path.GetFileName(episodeFile.OriginalFilePath);
             }
             else if (episodeFile.RelativePath.IsNotNullOrWhiteSpace())
             {

Radarr/Radarr

(cherry picked from commit 997aabbc3cfc3c9c5c220786d1d08cfceec5e2f2)
Closes #8115

src/NzbDrone.Core/CustomFormats/CustomFormatCalculationService.cs
@@ -168,13 +168,14 @@ namespace NzbDrone.Core.CustomFormats
         private static List<CustomFormat> ParseCustomFormat(MovieFile movieFile, Movie movie, List<CustomFormat> allCustomFormats)
         {
             var releaseTitle = string.Empty;
+
             if (movieFile.SceneName.IsNotNullOrWhiteSpace())
             {
                 releaseTitle = movieFile.SceneName;
             }
             else if (movieFile.OriginalFilePath.IsNotNullOrWhiteSpace())
             {
-                releaseTitle = movieFile.OriginalFilePath;
+                releaseTitle = Path.GetFileName(movieFile.OriginalFilePath);
             }
             else if (movieFile.RelativePath.IsNotNullOrWhiteSpace())
             {