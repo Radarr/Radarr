0aebd90ac9338428ca0efed5f7b19d28073feec5
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2023-08-31 22:05:46 +0100

    Fixed: Another attempted fix on ODIC correlation error

--- src/NzbDrone.Host/Startup.cs
+++ src/NzbDrone.Host/Startup.cs
@@ -61,6 +61,11 @@ namespace NzbDrone.Host
                 options.KnownProxies.Clear();
             });
 
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.Secure = CookieSecurePolicy.Always;
+            });
+
             services.AddRouting(options => options.LowercaseUrls = true);
 
             services.AddResponseCompression(options => options.EnableForHttps = true);
@@ -252,11 +257,6 @@ namespace NzbDrone.Host
             }
 
             app.UseForwardedHeaders();
-            app.UseCookiePolicy(new CookiePolicyOptions
-            {
-                Secure = CookieSecurePolicy.Always,
-                MinimumSameSitePolicy = SameSiteMode.None
-            });
             app.UseMiddleware<LoggingMiddleware>();
             app.UsePathBase(new PathString(configFileProvider.UrlBase));
             app.UseExceptionHandler(new ExceptionHandlerOptions
@@ -265,6 +265,7 @@ namespace NzbDrone.Host
                 ExceptionHandler = errorHandler.HandleException
             });
 
+            app.UseCookiePolicy();
             app.UseRouting();
             app.UseCors();
             app.UseAuthentication();
