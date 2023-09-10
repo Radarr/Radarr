b48b970f25c56dd443bfb0920e7889a6afee5ae9
Mark McDowall <mark@mcdowall.ca>
2023-09-10 13:05:41 -0700

    Map Clearlogo images to cover type
    (cherry picked from commit 809788eb2ed7e448f7f23101ae535533de57cd02)

--- src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
+++ src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -661,6 +661,8 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
                     return MediaCoverTypes.Headshot;
                 case "fanart":
                     return MediaCoverTypes.Fanart;
+                case "clearlogo":
+                    return MediaCoverTypes.Clearlogo;
                 default:
                     return MediaCoverTypes.Unknown;
             }
