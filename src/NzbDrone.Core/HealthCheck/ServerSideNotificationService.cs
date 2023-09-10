f55c6d1ae6be73eaa12d122a756af79f23c752c5
Mark McDowall <mark@mcdowall.ca>
2023-09-10 13:07:57 -0700

    Fixed: Duplicate notifications for failed health checks
    (cherry picked from commit c0e54773e213f526a5046fa46aa7b57532471128)

--- src/NzbDrone.Core/HealthCheck/ServerSideNotificationService.cs
+++ src/NzbDrone.Core/HealthCheck/ServerSideNotificationService.cs
@@ -9,63 +9,67 @@ using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Localization;
 
 namespace NzbDrone.Core.HealthCheck
 {
-    public interface IServerSideNotificationService
-    {
-        public List<HealthCheck> GetServerChecks();
-    }
-
-    public class ServerSideNotificationService : IServerSideNotificationService
+    public class ServerSideNotificationService : HealthCheckBase
     {
         private readonly IHttpClient _client;
+        private readonly IRadarrCloudRequestBuilder _cloudRequestBuilder;
         private readonly IConfigFileProvider _configFileProvider;
-        private readonly IHttpRequestBuilderFactory _cloudRequestBuilder;
         private readonly Logger _logger;
 
-        private readonly ICached<List<HealthCheck>> _cache;
+        private readonly ICached<HealthCheck> _cache;
 
         public ServerSideNotificationService(IHttpClient client,
-                                             IConfigFileProvider configFileProvider,
                                              IRadarrCloudRequestBuilder cloudRequestBuilder,
+                                             IConfigFileProvider configFileProvider,
+                                             ILocalizationService localizationService,
                                              ICacheManager cacheManager,
                                              Logger logger)
+            : base(localizationService)
         {
             _client = client;
+            _cloudRequestBuilder = cloudRequestBuilder;
             _configFileProvider = configFileProvider;
-            _cloudRequestBuilder = cloudRequestBuilder.Services;
             _logger = logger;
 
-            _cache = cacheManager.GetCache<List<HealthCheck>>(GetType());
+            _cache = cacheManager.GetCache<HealthCheck>(GetType());
         }
 
-        public List<HealthCheck> GetServerChecks()
+        public override HealthCheck Check()
         {
-            return _cache.Get("ServerChecks", () => RetrieveServerChecks(), TimeSpan.FromHours(2));
+            return _cache.Get("ServerChecks", RetrieveServerChecks, TimeSpan.FromHours(2));
         }
 
-        private List<HealthCheck> RetrieveServerChecks()
+        private HealthCheck RetrieveServerChecks()
         {
-            var request = _cloudRequestBuilder.Create()
-                                      .Resource("/notification")
-                                      .AddQueryParam("version", BuildInfo.Version)
-                                      .AddQueryParam("os", OsInfo.Os.ToString().ToLowerInvariant())
-                                      .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
-                                      .AddQueryParam("runtime", "netcore")
-                                      .AddQueryParam("branch", _configFileProvider.Branch)
-                                      .Build();
+            var request = _cloudRequestBuilder.Services.Create()
+                .Resource("/notification")
+                .AddQueryParam("version", BuildInfo.Version)
+                .AddQueryParam("os", OsInfo.Os.ToString().ToLowerInvariant())
+                .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
+                .AddQueryParam("branch", _configFileProvider.Branch)
+                .Build();
+
             try
             {
-                _logger.Trace("Getting server side health notifications");
+                _logger.Trace("Getting notifications");
+
                 var response = _client.Execute(request);
                 var result = Json.Deserialize<List<ServerNotificationResponse>>(response.Content);
-                return result.Select(x => new HealthCheck(GetType(), x.Type, x.Message, x.WikiUrl)).ToList();
+
+                var checks = result.Select(x => new HealthCheck(GetType(), x.Type, x.Message, x.WikiUrl)).ToList();
+
+                // Only one health check is supported, services returns an ordered list, so use the first one
+                return checks.FirstOrDefault() ?? new HealthCheck(GetType());
             }
             catch (Exception ex)
             {
-                _logger.Error(ex, "Failed to retrieve server notifications");
-                return new List<HealthCheck>();
+                _logger.Error(ex, "Failed to retrieve notifications");
+
+                return new HealthCheck(GetType());
             }
         }
     }
