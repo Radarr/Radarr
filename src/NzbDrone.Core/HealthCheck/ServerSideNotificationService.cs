572c410f54831c24fe20eb56acdd15b4f9b7899c
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-10-02 18:16:43 +0300

    Add runtime param to ServerSideNotificationService

--- src/NzbDrone.Core/HealthCheck/ServerSideNotificationService.cs
+++ src/NzbDrone.Core/HealthCheck/ServerSideNotificationService.cs
@@ -50,6 +50,7 @@ namespace NzbDrone.Core.HealthCheck
                 .AddQueryParam("version", BuildInfo.Version)
                 .AddQueryParam("os", OsInfo.Os.ToString().ToLowerInvariant())
                 .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
+                .AddQueryParam("runtime", "netcore")
                 .AddQueryParam("branch", _configFileProvider.Branch)
                 .Build();
 
