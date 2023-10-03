Sonarr/Sonarr

New: Calendar month view will scroll to today on load and press
Closes #5574

frontend/src/Calendar/Events/CalendarEvent.js
@@ -236,6 +236,7 @@ class CalendarEvent extends Component {
 
 CalendarEvent.propTypes = {
   id: PropTypes.number.isRequired,
+  episodeId: PropTypes.number.isRequired,
   series: PropTypes.object.isRequired,
   episodeFile: PropTypes.object,
   title: PropTypes.string.isRequired,
@@ -248,14 +249,15 @@ CalendarEvent.propTypes = {
   hasFile: PropTypes.bool.isRequired,
   grabbed: PropTypes.bool,
   queueItem: PropTypes.object,
-  showEpisodeInformation: PropTypes.bool.isRequired,
-  showFinaleIcon: PropTypes.bool.isRequired,
-  showSpecialIcon: PropTypes.bool.isRequired,
-  showCutoffUnmetIcon: PropTypes.bool.isRequired,
-  fullColorEvents: PropTypes.bool.isRequired,
-  timeFormat: PropTypes.string.isRequired,
-  colorImpairedMode: PropTypes.bool.isRequired,
-  onEventModalOpenToggle: PropTypes.func.isRequired
+  // These props come from the connector, not marked as required to apease TS for now.
+  showEpisodeInformation: PropTypes.bool,
+  showFinaleIcon: PropTypes.bool,
+  showSpecialIcon: PropTypes.bool,
+  showCutoffUnmetIcon: PropTypes.bool,
+  fullColorEvents: PropTypes.bool,
+  timeFormat: PropTypes.string,
+  colorImpairedMode: PropTypes.bool,
+  onEventModalOpenToggle: PropTypes.func
 };
 
 export default CalendarEvent;

Radarr/Radarr

(cherry picked from commit 7c0d3444376caa8a116b5c2084821c326beb01a1)
Closes #8501

frontend/src/Calendar/Events/CalendarEvent.js
@@ -25,6 +25,7 @@ class CalendarEvent extends Component {
       title,
       titleSlug,
       genres,
+      date,
       monitored,
       certification,
       hasFile,
@@ -33,8 +34,7 @@ class CalendarEvent extends Component {
       showMovieInformation,
       showCutoffUnmetIcon,
       fullColorEvents,
-      colorImpairedMode,
-      date
+      colorImpairedMode
     } = this.props;
 
     const isDownloading = !!(queueItem || grabbed);
@@ -148,17 +148,18 @@ CalendarEvent.propTypes = {
   inCinemas: PropTypes.string,
   physicalRelease: PropTypes.string,
   digitalRelease: PropTypes.string,
+  date: PropTypes.string.isRequired,
   monitored: PropTypes.bool.isRequired,
   certification: PropTypes.string,
   hasFile: PropTypes.bool.isRequired,
   grabbed: PropTypes.bool,
   queueItem: PropTypes.object,
-  showMovieInformation: PropTypes.bool.isRequired,
-  showCutoffUnmetIcon: PropTypes.bool.isRequired,
-  fullColorEvents: PropTypes.bool.isRequired,
-  timeFormat: PropTypes.string.isRequired,
-  colorImpairedMode: PropTypes.bool.isRequired,
-  date: PropTypes.string.isRequired
+  // These props come from the connector, not marked as required to appease TS for now.
+  showMovieInformation: PropTypes.bool,
+  showCutoffUnmetIcon: PropTypes.bool,
+  fullColorEvents: PropTypes.bool,
+  timeFormat: PropTypes.string,
+  colorImpairedMode: PropTypes.bool
 };
 
 CalendarEvent.defaultProps = {