Sonarr/Sonarr

Cleanup Calendar custom filters

frontend/src/App/State/CalendarAppState.ts
@@ -1,9 +1,10 @@
-import AppSectionState from 'App/State/AppSectionState';
+import AppSectionState, {
+  AppSectionFilterState,
+} from 'App/State/AppSectionState';
 import Episode from 'Episode/Episode';
-import { FilterBuilderProp } from './AppState';
 
-interface CalendarAppState extends AppSectionState<Episode> {
-  filterBuilderProps: FilterBuilderProp<Episode>[];
-}
+interface CalendarAppState
+  extends AppSectionState<Episode>,
+    AppSectionFilterState<Episode> {}
 
 export default CalendarAppState;

Radarr/Radarr

(cherry picked from commit fd789343b587da252461d84bafba2d72651a11df)
Closes #9296

frontend/src/App/State/CalendarAppState.ts
@@ -1,9 +1,10 @@
-import AppSectionState from 'App/State/AppSectionState';
+import AppSectionState, {
+  AppSectionFilterState,
+} from 'App/State/AppSectionState';
 import Movie from 'Movie/Movie';
-import { FilterBuilderProp } from './AppState';
 
-interface CalendarAppState extends AppSectionState<Movie> {
-  filterBuilderProps: FilterBuilderProp<Movie>[];
-}
+interface CalendarAppState
+  extends AppSectionState<Movie>,
+    AppSectionFilterState<Movie> {}
 
 export default CalendarAppState;