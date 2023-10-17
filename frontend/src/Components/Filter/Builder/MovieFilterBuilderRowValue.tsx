9a50fcb82a333621c795b3a609fc28f42496b5f1
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-10-17 18:59:34 +0300

    Sort movies by name in filter builder

--- frontend/src/Components/Filter/Builder/MovieFilterBuilderRowValue.tsx
+++ frontend/src/Components/Filter/Builder/MovieFilterBuilderRowValue.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Movie from 'Movie/Movie';
 import createAllMoviesSelector from 'Store/Selectors/createAllMoviesSelector';
+import sortByName from 'Utilities/Array/sortByName';
 import FilterBuilderRowValue from './FilterBuilderRowValue';
 import FilterBuilderRowValueProps from './FilterBuilderRowValueProps';
 
 function MovieFilterBuilderRowValue(props: FilterBuilderRowValueProps) {
   const allMovies: Movie[] = useSelector(createAllMoviesSelector());
 
-  const tagList = allMovies.map((movie) => {
-    return {
-      id: movie.id,
-      name: movie.title,
-    };
-  });
+  const tagList = allMovies
+    .map((movie) => ({ id: movie.id, name: movie.title }))
+    .sort(sortByName);
 
   return <FilterBuilderRowValue {...props} tagList={tagList} />;
 }
