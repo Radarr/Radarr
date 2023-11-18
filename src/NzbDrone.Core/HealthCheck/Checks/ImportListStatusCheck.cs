Sonarr/Sonarr

Use named tokens for backend translations
Closes #6051

src/NzbDrone.Core/HealthCheck/Checks/ImportListStatusCheck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.ImportLists;
@@ -45,7 +46,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             return new HealthCheck(GetType(),
                 HealthCheckResult.Warning,
-                string.Format(_localizationService.GetLocalizedString("ImportListStatusUnavailableHealthCheckMessage"), string.Join(", ", backOffProviders.Select(v => v.ImportList.Definition.Name))),
+                _localizationService.GetLocalizedString("ImportListStatusUnavailableHealthCheckMessage", new Dictionary<string, object>
+                {
+                    { "importListNames", string.Join(", ", backOffProviders.Select(v => v.ImportList.Definition.Name)) }
+                }),
                 "#import-lists-are-unavailable-due-to-failures");
         }
     }

src/NzbDrone.Core/HealthCheck/Checks/IndexerLongTermStatusCheck.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Indexers;
@@ -48,7 +49,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             return new HealthCheck(GetType(),
                 HealthCheckResult.Warning,
-                string.Format(_localizationService.GetLocalizedString("IndexerLongTermStatusUnavailableHealthCheckMessage"), string.Join(", ", backOffProviders.Select(v => v.Provider.Definition.Name))),
+                _localizationService.GetLocalizedString("IndexerLongTermStatusUnavailableHealthCheckMessage", new Dictionary<string, object>
+                {
+                    { "indexerNames", string.Join(", ", backOffProviders.Select(v => v.Provider.Definition.Name)) }
+                }),
                 "#indexers-are-unavailable-due-to-failures");
         }
     }

src/NzbDrone.Core/HealthCheck/Checks/RecyclingBinCheck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
@@ -33,7 +34,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
             {
                 return new HealthCheck(GetType(),
                     HealthCheckResult.Error,
-                    string.Format(_localizationService.GetLocalizedString("RecycleBinUnableToWriteHealthCheckMessage"), recycleBin),
+                    _localizationService.GetLocalizedString("RecycleBinUnableToWriteHealthCheckMessage", new Dictionary<string, object>
+                    {
+                        { "path", recycleBin }
+                    }),
                     "#cannot-write-recycle-bin");
             }
 

src/NzbDrone.Core/Localization/LocalizationService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NLog;
 using NzbDrone.Common.Cache;
@@ -18,14 +19,17 @@ namespace NzbDrone.Core.Localization
     public interface ILocalizationService
     {
         Dictionary<string, string> GetLocalizationDictionary();
+
         string GetLocalizedString(string phrase);
-        string GetLocalizedString(string phrase, string language);
+        string GetLocalizedString(string phrase, Dictionary<string, object> tokens);
         string GetLanguageIdentifier();
     }
 
     public class LocalizationService : ILocalizationService, IHandleAsync<ConfigSavedEvent>
     {
         private const string DefaultCulture = "en";
+        private static readonly Regex TokenRegex = new Regex(@"(?:\{)(?<token>[a-z0-9]+)(?:\})",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 
         private readonly ICached<Dictionary<string, string>> _cache;
 
@@ -53,21 +57,16 @@ namespace NzbDrone.Core.Localization
 
         public string GetLocalizedString(string phrase)
         {
-            var language = GetLanguageFileName();
-
-            return GetLocalizedString(phrase, language);
+            return GetLocalizedString(phrase, new Dictionary<string, object>());
         }
 
-        public string GetLocalizedString(string phrase, string language)
+        public string GetLocalizedString(string phrase, Dictionary<string, object> tokens)
         {
+            var language = GetLanguageFileName();
+
             if (string.IsNullOrEmpty(phrase))
             {
                 throw new ArgumentNullException(nameof(phrase));
-            }
-
-            if (language.IsNullOrWhiteSpace())
-            {
-                language = GetLanguageFileName();
             }
 
             if (language == null)
@@ -79,7 +78,7 @@ namespace NzbDrone.Core.Localization
 
             if (dictionary.TryGetValue(phrase, out var value))
             {
-                return value;
+                return ReplaceTokens(value, tokens);
             }
 
             return phrase;
@@ -96,6 +95,20 @@ namespace NzbDrone.Core.Localization
             }
 
             return language;
+        }
+
+        private string ReplaceTokens(string input, Dictionary<string, object> tokens)
+        {
+            tokens.TryAdd("appName", "Sonarr");
+
+            return TokenRegex.Replace(input, (match) =>
+            {
+                var tokenName = match.Groups["token"].Value;
+
+                tokens.TryGetValue(tokenName, out var token);
+
+                return token?.ToString() ?? $"{{{tokenName}}}";
+            });
         }
 
         private string GetLanguageFileName()

Radarr/Radarr

(cherry picked from commit 11f96c31048c2d1aafca0c91736d439f7f9a95a8)

src/NzbDrone.Core/HealthCheck/Checks/ImportListStatusCheck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.ImportLists;
@@ -40,7 +41,13 @@ namespace NzbDrone.Core.HealthCheck.Checks
                 return new HealthCheck(GetType(), HealthCheckResult.Error, _localizationService.GetLocalizedString("ImportListStatusCheckAllClientMessage"), "#lists-are-unavailable-due-to-failures");
             }
 
-            return new HealthCheck(GetType(), HealthCheckResult.Warning, string.Format(_localizationService.GetLocalizedString("ImportListStatusCheckSingleClientMessage"), string.Join(", ", backOffProviders.Select(v => v.ImportList.Definition.Name))), "#lists-are-unavailable-due-to-failures");
+            return new HealthCheck(GetType(),
+                HealthCheckResult.Warning,
+                _localizationService.GetLocalizedString("ImportListStatusCheckSingleClientMessage", new Dictionary<string, object>
+                {
+                    { "importListNames", string.Join(", ", backOffProviders.Select(v => v.ImportList.Definition.Name)) }
+                }),
+                "#import-lists-are-unavailable-due-to-failures");
         }
     }
 }

src/NzbDrone.Core/HealthCheck/Checks/IndexerLongTermStatusCheck.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Indexers;
@@ -50,8 +51,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             return new HealthCheck(GetType(),
                 HealthCheckResult.Warning,
-                string.Format(_localizationService.GetLocalizedString("IndexerLongTermStatusCheckSingleClientMessage"),
-                    string.Join(", ", backOffProviders.Select(v => v.Provider.Definition.Name))),
+                _localizationService.GetLocalizedString("IndexerLongTermStatusCheckSingleClientMessage", new Dictionary<string, object>
+                {
+                    { "indexerNames", string.Join(", ", backOffProviders.Select(v => v.Provider.Definition.Name)) }
+                }),
                 "#indexers-are-unavailable-due-to-failures");
         }
     }

src/NzbDrone.Core/HealthCheck/Checks/RecyclingBinCheck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
@@ -31,7 +32,13 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             if (!_diskProvider.FolderWritable(recycleBin))
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Error, string.Format(_localizationService.GetLocalizedString("RecycleBinUnableToWriteHealthCheck"), recycleBin), "#cannot-write-recycle-bin");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Error,
+                    _localizationService.GetLocalizedString("RecycleBinUnableToWriteHealthCheck", new Dictionary<string, object>
+                    {
+                        { "path", recycleBin }
+                    }),
+                    "#cannot-write-recycle-bin");
             }
 
             return new HealthCheck(GetType());

src/NzbDrone.Core/Localization/LocalizationService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NLog;
 using NzbDrone.Common.Cache;
@@ -18,14 +19,17 @@ namespace NzbDrone.Core.Localization
     public interface ILocalizationService
     {
         Dictionary<string, string> GetLocalizationDictionary();
+
         string GetLocalizedString(string phrase);
-        string GetLocalizedString(string phrase, string language);
+        string GetLocalizedString(string phrase, Dictionary<string, object> tokens);
         string GetLanguageIdentifier();
     }
 
     public class LocalizationService : ILocalizationService, IHandleAsync<ConfigSavedEvent>
     {
         private const string DefaultCulture = "en";
+        private static readonly Regex TokenRegex = new Regex(@"(?:\{)(?<token>[a-z0-9]+)(?:\})",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 
         private readonly ICached<Dictionary<string, string>> _cache;
 
@@ -53,21 +57,16 @@ namespace NzbDrone.Core.Localization
 
         public string GetLocalizedString(string phrase)
         {
-            var language = GetLanguageFileName();
-
-            return GetLocalizedString(phrase, language);
+            return GetLocalizedString(phrase, new Dictionary<string, object>());
         }
 
-        public string GetLocalizedString(string phrase, string language)
+        public string GetLocalizedString(string phrase, Dictionary<string, object> tokens)
         {
+            var language = GetLanguageFileName();
+
             if (string.IsNullOrEmpty(phrase))
             {
                 throw new ArgumentNullException(nameof(phrase));
-            }
-
-            if (language.IsNullOrWhiteSpace())
-            {
-                language = GetLanguageFileName();
             }
 
             if (language == null)
@@ -79,7 +78,7 @@ namespace NzbDrone.Core.Localization
 
             if (dictionary.TryGetValue(phrase, out var value))
             {
-                return value;
+                return ReplaceTokens(value, tokens);
             }
 
             return phrase;
@@ -96,6 +95,20 @@ namespace NzbDrone.Core.Localization
             }
 
             return language;
+        }
+
+        private string ReplaceTokens(string input, Dictionary<string, object> tokens)
+        {
+            tokens.TryAdd("appName", "Radarr");
+
+            return TokenRegex.Replace(input, (match) =>
+            {
+                var tokenName = match.Groups["token"].Value;
+
+                tokens.TryGetValue(tokenName, out var token);
+
+                return token?.ToString() ?? $"{{{tokenName}}}";
+            });
         }
 
         private string GetLanguageFileName()