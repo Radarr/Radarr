Sonarr/Sonarr

New: Show Proper or Repack tag in interactive search

frontend/src/InteractiveSearch/InteractiveSearchRow.tsx
@@ -244,7 +244,7 @@ function InteractiveSearchRow(props: InteractiveSearchRowProps) {
       </TableRowCell>
 
       <TableRowCell className={styles.quality}>
-        <EpisodeQuality quality={quality} />
+        <EpisodeQuality quality={quality} showRevision={true} />
       </TableRowCell>
 
       <TableRowCell className={styles.customFormatScore}>

Radarr/Radarr

(cherry picked from commit efb000529b5dff42829df3ef151e4750a7b15cf6)
Closes #9523

frontend/src/InteractiveSearch/InteractiveSearchRow.tsx
@@ -266,7 +266,7 @@ function InteractiveSearchRow(props: InteractiveSearchRowProps) {
       </TableRowCell>
 
       <TableRowCell className={styles.quality}>
-        <MovieQuality quality={quality} />
+        <MovieQuality quality={quality} showRevision={true} />
       </TableRowCell>
 
       <TableRowCell className={styles.customFormatScore}>