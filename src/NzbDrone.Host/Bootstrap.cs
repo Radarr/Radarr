Prowlarr/Prowlarr

Translate fields on the backend
(cherry picked from commit 48b12f5b00429a7cd218d23f0544641b0da62a06)

src/NzbDrone.Host/Bootstrap.cs
@@ -24,6 +24,7 @@ using NzbDrone.Common.Instrumentation;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Datastore.Extensions;
+using Prowlarr.Http.ClientSchema;
 using PostgresOptions = NzbDrone.Core.Datastore.PostgresOptions;
 
 namespace NzbDrone.Host
@@ -147,6 +148,8 @@ namespace NzbDrone.Host
                         .AddNzbDroneLogger()
                         .AddDatabase()
                         .AddStartupContext(context);
+
+                    SchemaBuilder.Initialize(c);
                 })
                 .ConfigureServices(services =>
                 {

src/NzbDrone.Integration.Test/IntegrationTest.cs
@@ -1,13 +1,14 @@
+using System;
+using System.Linq;
 using System.Threading;
 using NLog;
 using NUnit.Framework;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Datastore.Migration.Framework;
-using NzbDrone.Core.Indexers.Definitions.FileList;
+using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Test.Common;
 using NzbDrone.Test.Common.Datastore;
-using Prowlarr.Http.ClientSchema;
 
 namespace NzbDrone.Integration.Test
 {
@@ -49,16 +50,19 @@ namespace NzbDrone.Integration.Test
         {
             WaitForCompletion(() => Tasks.All().SelectList(x => x.TaskName).Contains("CheckHealth"), 20000);
 
-            Indexers.Post(new Prowlarr.Api.V1.Indexers.IndexerResource
+            var indexer = Indexers.Schema().FirstOrDefault(i => i.Implementation == nameof(Newznab));
+
+            if (indexer == null)
             {
-                Enable = false,
-                ConfigContract = nameof(FileListSettings),
-                Implementation = nameof(FileList),
-                Name = "NewznabTest",
-                Protocol = Core.Indexers.DownloadProtocol.Usenet,
-                AppProfileId = 1,
-                Fields = SchemaBuilder.ToSchema(new FileListSettings())
-            });
+                throw new NullReferenceException("Expected valid indexer schema, found null");
+            }
+
+            indexer.Enable = false;
+            indexer.ConfigContract = nameof(NewznabSettings);
+            indexer.Implementation = nameof(Newznab);
+            indexer.Name = "NewznabTest";
+            indexer.Protocol = Core.Indexers.DownloadProtocol.Usenet;
+            indexer.AppProfileId = 1;
 
             // Change Console Log Level to Debug so we get more details.
             var config = HostConfig.Get(1);

Radarr/Radarr

src/NzbDrone.Host/Bootstrap.cs
@@ -23,6 +23,7 @@ using NzbDrone.Common.Instrumentation;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Datastore.Extensions;
+using Radarr.Http.ClientSchema;
 using PostgresOptions = NzbDrone.Core.Datastore.PostgresOptions;
 
 namespace NzbDrone.Host
@@ -146,6 +147,8 @@ namespace NzbDrone.Host
                         .AddNzbDroneLogger()
                         .AddDatabase()
                         .AddStartupContext(context);
+
+                    SchemaBuilder.Initialize(c);
                 })
                 .ConfigureServices(services =>
                 {

src/NzbDrone.Integration.Test/IntegrationTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading;
 using NLog;
 using NUnit.Framework;
@@ -7,7 +9,6 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Test.Common;
 using NzbDrone.Test.Common.Datastore;
-using Radarr.Http.ClientSchema;
 
 namespace NzbDrone.Integration.Test
 {
@@ -48,19 +49,22 @@ namespace NzbDrone.Integration.Test
         protected override void InitializeTestTarget()
         {
             // Make sure tasks have been initialized so the config put below doesn't cause errors
-            WaitForCompletion(() => Tasks.All().SelectList(x => x.TaskName).Contains("RssSync"), 20000);
+            WaitForCompletion(() => Tasks.All().SelectList(x => x.TaskName).Contains("RssSync"), 30000);
 
-            Indexers.Post(new Radarr.Api.V3.Indexers.IndexerResource
+            var indexer = Indexers.Schema().FirstOrDefault(i => i.Implementation == nameof(Newznab));
+
+            if (indexer == null)
             {
-                EnableRss = false,
-                EnableInteractiveSearch = false,
-                EnableAutomaticSearch = false,
-                ConfigContract = nameof(NewznabSettings),
-                Implementation = nameof(Newznab),
-                Name = "NewznabTest",
-                Protocol = Core.Indexers.DownloadProtocol.Usenet,
-                Fields = SchemaBuilder.ToSchema(new NewznabSettings())
-            });
+                throw new NullReferenceException("Expected valid indexer schema, found null");
+            }
+
+            indexer.EnableRss = false;
+            indexer.EnableInteractiveSearch = false;
+            indexer.EnableAutomaticSearch = false;
+            indexer.ConfigContract = nameof(NewznabSettings);
+            indexer.Implementation = nameof(Newznab);
+            indexer.Name = "NewznabTest";
+            indexer.Protocol = Core.Indexers.DownloadProtocol.Usenet;
 
             // Change Console Log Level to Debug so we get more details.
             var config = HostConfig.Get(1);