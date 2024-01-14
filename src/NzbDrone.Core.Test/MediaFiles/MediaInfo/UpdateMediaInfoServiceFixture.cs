Sonarr/Sonarr

New: Custom import scripts can communicate information back

src/NzbDrone.Core.Test/MediaFiles/MediaInfo/UpdateMediaInfoServiceFixture.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using FizzWare.NBuilder;
 using FluentAssertions;
@@ -71,7 +72,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_series.Path, "media.mkv")), Times.Exactly(2));
@@ -97,7 +98,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_series.Path, "media.mkv")), Times.Exactly(2));
@@ -123,7 +124,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_series.Path, "media.mkv")), Times.Exactly(3));
@@ -146,7 +147,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
 
             GivenSuccessfulScan();
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo("media.mkv"), Times.Never());
@@ -173,7 +174,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenSuccessfulScan();
             GivenFailedScan(Path.Combine(_series.Path, "media2.mkv"));
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_series.Path, "media.mkv")), Times.Exactly(1));
@@ -203,7 +204,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new SeriesScannedEvent(_series));
+            Subject.Handle(new SeriesScannedEvent(_series, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                 .Verify(v => v.GetMediaInfo(It.IsAny<string>()), Times.Never());

src/NzbDrone.Core/Extras/ExistingExtraFileService.cs
@@ -2,44 +2,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NLog;
-using NzbDrone.Common.Disk;
-using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.MediaFiles.Events;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Extras
 {
-    public class ExistingExtraFileService : IHandle<SeriesScannedEvent>
+    public interface IExistingExtraFiles
     {
-        private readonly IDiskProvider _diskProvider;
-        private readonly IDiskScanService _diskScanService;
+        List<string> ImportExtraFiles(Series series, List<string> possibleExtraFiles);
+    }
+
+    public class ExistingExtraFileService : IExistingExtraFiles, IHandle<SeriesScannedEvent>
+    {
         private readonly List<IImportExistingExtraFiles> _existingExtraFileImporters;
         private readonly Logger _logger;
 
-        public ExistingExtraFileService(IDiskProvider diskProvider,
-                                        IDiskScanService diskScanService,
-                                        IEnumerable<IImportExistingExtraFiles> existingExtraFileImporters,
+        public ExistingExtraFileService(IEnumerable<IImportExistingExtraFiles> existingExtraFileImporters,
                                         Logger logger)
         {
-            _diskProvider = diskProvider;
-            _diskScanService = diskScanService;
             _existingExtraFileImporters = existingExtraFileImporters.OrderBy(e => e.Order).ToList();
             _logger = logger;
         }
 
-        public void Handle(SeriesScannedEvent message)
+        public List<string> ImportExtraFiles(Series series, List<string> possibleExtraFiles)
         {
-            var series = message.Series;
-
-            if (!_diskProvider.FolderExists(series.Path))
-            {
-                return;
-            }
-
             _logger.Debug("Looking for existing extra files in {0}", series.Path);
-
-            var filesOnDisk = _diskScanService.GetNonVideoFiles(series.Path);
-            var possibleExtraFiles = _diskScanService.FilterPaths(series.Path, filesOnDisk);
 
             var importedFiles = new List<string>();
 
@@ -49,6 +37,15 @@ namespace NzbDrone.Core.Extras
 
                 importedFiles.AddRange(imported.Select(f => Path.Combine(series.Path, f.RelativePath)));
             }
+
+            return importedFiles;
+        }
+
+        public void Handle(SeriesScannedEvent message)
+        {
+            var series = message.Series;
+            var possibleExtraFiles = message.PossibleExtraFiles;
+            var importedFiles = ImportExtraFiles(series, possibleExtraFiles);
 
             _logger.Info("Found {0} possible extra files, imported {1} files.", possibleExtraFiles.Count, importedFiles.Count);
         }

src/NzbDrone.Core/Extras/ExtraService.cs
@@ -17,6 +17,7 @@ namespace NzbDrone.Core.Extras
 {
     public interface IExtraService
     {
+        void MoveFilesAfterRename(Series series, EpisodeFile episodeFile);
         void ImportEpisode(LocalEpisode localEpisode, EpisodeFile episodeFile, bool isReadOnly);
     }
 
@@ -136,6 +137,16 @@ namespace NzbDrone.Core.Extras
             foreach (var extraFileManager in _extraFileManagers)
             {
                 extraFileManager.CreateAfterEpisodeFolder(series, message.SeriesFolder, message.SeasonFolder);
+            }
+        }
+
+        public void MoveFilesAfterRename(Series series, EpisodeFile episodeFile)
+        {
+            var episodeFiles = new List<EpisodeFile> { episodeFile };
+
+            foreach (var extraFileManager in _extraFileManagers)
+            {
+                extraFileManager.MoveFilesAfterRename(series, episodeFiles);
             }
         }
 

src/NzbDrone.Core/MediaFiles/DiskScanService.cs
@@ -121,7 +121,7 @@ namespace NzbDrone.Core.MediaFiles
                 }
 
                 CleanMediaFiles(series, new List<string>());
-                CompletedScanning(series);
+                CompletedScanning(series, new List<string>());
 
                 return;
             }
@@ -174,8 +174,11 @@ namespace NzbDrone.Core.MediaFiles
             fileInfoStopwatch.Stop();
             _logger.Trace("Reprocessing existing files complete for: {0} [{1}]", series, decisionsStopwatch.Elapsed);
 
+            var filesOnDisk = GetNonVideoFiles(series.Path);
+            var possibleExtraFiles = FilterPaths(series.Path, filesOnDisk);
+
             RemoveEmptySeriesFolder(series.Path);
-            CompletedScanning(series);
+            CompletedScanning(series, possibleExtraFiles);
         }
 
         private void CleanMediaFiles(Series series, List<string> mediaFileList)
@@ -184,10 +187,10 @@ namespace NzbDrone.Core.MediaFiles
             _mediaFileTableCleanupService.Clean(series, mediaFileList);
         }
 
-        private void CompletedScanning(Series series)
+        private void CompletedScanning(Series series, List<string> possibleExtraFiles)
         {
             _logger.Info("Completed scanning disk for {0}", series.Title);
-            _eventAggregator.PublishEvent(new SeriesScannedEvent(series));
+            _eventAggregator.PublishEvent(new SeriesScannedEvent(series, possibleExtraFiles));
         }
 
         public string[] GetVideoFiles(string path, bool allDirectories = true)

Radarr/Radarr

(cherry picked from commit b4ac495983d61819d9ab84f49c880957ba57418b)

src/NzbDrone.Core.Test/MediaFiles/MediaInfo/UpdateMediaInfoServiceFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+using System.Collections.Generic;
+using System.IO;
 using FizzWare.NBuilder;
 using FluentAssertions;
 using Moq;
@@ -71,7 +72,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_movie.Path, "media.mkv")), Times.Exactly(2));
@@ -97,7 +98,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_movie.Path, "media.mkv")), Times.Exactly(2));
@@ -123,7 +124,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_movie.Path, "media.mkv")), Times.Exactly(3));
@@ -146,7 +147,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
 
             GivenSuccessfulScan();
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo("media.mkv"), Times.Never());
@@ -173,7 +174,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenSuccessfulScan();
             GivenFailedScan(Path.Combine(_movie.Path, "media2.mkv"));
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                   .Verify(v => v.GetMediaInfo(Path.Combine(_movie.Path, "media.mkv")), Times.Exactly(1));
@@ -203,7 +204,7 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo
             GivenFileExists();
             GivenSuccessfulScan();
 
-            Subject.Handle(new MovieScannedEvent(_movie));
+            Subject.Handle(new MovieScannedEvent(_movie, new List<string>()));
 
             Mocker.GetMock<IVideoFileInfoReader>()
                 .Verify(v => v.GetMediaInfo(It.IsAny<string>()), Times.Never());

src/NzbDrone.Core/Extras/ExistingExtraFileService.cs
@@ -2,44 +2,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NLog;
-using NzbDrone.Common.Disk;
-using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.MediaFiles.Events;
 using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Movies;
 
 namespace NzbDrone.Core.Extras
 {
-    public class ExistingExtraFileService : IHandle<MovieScannedEvent>
+    public interface IExistingExtraFiles
     {
-        private readonly IDiskProvider _diskProvider;
-        private readonly IDiskScanService _diskScanService;
+        List<string> ImportExtraFiles(Movie movie, List<string> possibleExtraFiles);
+    }
+
+    public class ExistingExtraFileService : IExistingExtraFiles, IHandle<MovieScannedEvent>
+    {
         private readonly List<IImportExistingExtraFiles> _existingExtraFileImporters;
         private readonly Logger _logger;
 
-        public ExistingExtraFileService(IDiskProvider diskProvider,
-                                        IDiskScanService diskScanService,
-                                        IEnumerable<IImportExistingExtraFiles> existingExtraFileImporters,
+        public ExistingExtraFileService(IEnumerable<IImportExistingExtraFiles> existingExtraFileImporters,
                                         Logger logger)
         {
-            _diskProvider = diskProvider;
-            _diskScanService = diskScanService;
             _existingExtraFileImporters = existingExtraFileImporters.OrderBy(e => e.Order).ToList();
             _logger = logger;
         }
 
-        public void Handle(MovieScannedEvent message)
+        public List<string> ImportExtraFiles(Movie movie, List<string> possibleExtraFiles)
         {
-            var movie = message.Movie;
-
-            if (!_diskProvider.FolderExists(movie.Path))
-            {
-                return;
-            }
-
             _logger.Debug("Looking for existing extra files in {0}", movie.Path);
-
-            var filesOnDisk = _diskScanService.GetNonVideoFiles(movie.Path);
-            var possibleExtraFiles = _diskScanService.FilterPaths(movie.Path, filesOnDisk, false);
 
             var importedFiles = new List<string>();
 
@@ -49,6 +37,15 @@ namespace NzbDrone.Core.Extras
 
                 importedFiles.AddRange(imported.Select(f => Path.Combine(movie.Path, f.RelativePath)));
             }
+
+            return importedFiles;
+        }
+
+        public void Handle(MovieScannedEvent message)
+        {
+            var movie = message.Movie;
+            var possibleExtraFiles = message.PossibleExtraFiles;
+            var importedFiles = ImportExtraFiles(movie, possibleExtraFiles);
 
             _logger.Info("Found {0} possible extra files, imported {1} files.", possibleExtraFiles.Count, importedFiles.Count);
         }

src/NzbDrone.Core/Extras/ExtraService.cs
@@ -17,6 +17,7 @@ namespace NzbDrone.Core.Extras
 {
     public interface IExtraService
     {
+        void MoveFilesAfterRename(Movie movie, MovieFile movieFile);
         void ImportMovie(LocalMovie localMovie, MovieFile movieFile, bool isReadOnly);
     }
 
@@ -136,6 +137,16 @@ namespace NzbDrone.Core.Extras
             foreach (var extraFileManager in _extraFileManagers)
             {
                 extraFileManager.CreateAfterMovieFolder(movie, message.MovieFolder);
+            }
+        }
+
+        public void MoveFilesAfterRename(Movie movie, MovieFile movieFile)
+        {
+            var movieFiles = new List<MovieFile> { movieFile };
+
+            foreach (var extraFileManager in _extraFileManagers)
+            {
+                extraFileManager.MoveFilesAfterRename(movie, movieFiles);
             }
         }
 

src/NzbDrone.Core/MediaFiles/DiskScanService.cs
@@ -121,7 +121,7 @@ namespace NzbDrone.Core.MediaFiles
                 }
 
                 CleanMediaFiles(movie, new List<string>());
-                CompletedScanning(movie);
+                CompletedScanning(movie, new List<string>());
 
                 return;
             }
@@ -173,8 +173,11 @@ namespace NzbDrone.Core.MediaFiles
             fileInfoStopwatch.Stop();
             _logger.Trace("Reprocessing existing files complete for: {0} [{1}]", movie, decisionsStopwatch.Elapsed);
 
+            var filesOnDisk = GetNonVideoFiles(movie.Path);
+            var possibleExtraFiles = FilterPaths(movie.Path, filesOnDisk);
+
             RemoveEmptyMovieFolder(movie.Path);
-            CompletedScanning(movie);
+            CompletedScanning(movie, possibleExtraFiles);
         }
 
         private void CleanMediaFiles(Movie movie, List<string> mediaFileList)
@@ -183,10 +186,10 @@ namespace NzbDrone.Core.MediaFiles
             _mediaFileTableCleanupService.Clean(movie, mediaFileList);
         }
 
-        private void CompletedScanning(Movie movie)
+        private void CompletedScanning(Movie movie, List<string> possibleExtraFiles)
         {
             _logger.Info("Completed scanning disk for {0}", movie.Title);
-            _eventAggregator.PublishEvent(new MovieScannedEvent(movie));
+            _eventAggregator.PublishEvent(new MovieScannedEvent(movie, possibleExtraFiles));
         }
 
         public string[] GetVideoFiles(string path, bool allDirectories = true)