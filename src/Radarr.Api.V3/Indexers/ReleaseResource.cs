b6b809f4735fdd79f3dc8b66edaf0ecabbc02613
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-02-08 12:10:41 +0200

    Fixed: Reprocessing custom formats for file in Manual Import

--- src/Radarr.Api.V3/Indexers/ReleaseResource.cs
+++ src/Radarr.Api.V3/Indexers/ReleaseResource.cs
@@ -52,7 +52,7 @@ namespace Radarr.Api.V3.Indexers
         public int? Seeders { get; set; }
         public int? Leechers { get; set; }
         public DownloadProtocol Protocol { get; set; }
-        public int IndexerFlags { get; set; }
+        public IEnumerable<string> IndexerFlags { get; set; }
 
         // Sent when queuing an unknown release
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
@@ -76,7 +76,7 @@ namespace Radarr.Api.V3.Indexers
             var parsedMovieInfo = model.RemoteMovie.ParsedMovieInfo;
             var remoteMovie = model.RemoteMovie;
             var torrentInfo = (model.RemoteMovie.Release as TorrentInfo) ?? new TorrentInfo();
-            var indexerFlags = torrentInfo.IndexerFlags;
+            var indexerFlags = torrentInfo.IndexerFlags.ToString().Split(new[] { ", " }, StringSplitOptions.None).Where(x => x != "0");
 
             // TODO: Clean this mess up. don't mix data from multiple classes, use sub-resources instead? (Got a huge Deja Vu, didn't we talk about this already once?)
             return new ReleaseResource
@@ -118,7 +118,7 @@ namespace Radarr.Api.V3.Indexers
                 Seeders = torrentInfo.Seeders,
                 Leechers = (torrentInfo.Peers.HasValue && torrentInfo.Seeders.HasValue) ? (torrentInfo.Peers.Value - torrentInfo.Seeders.Value) : (int?)null,
                 Protocol = releaseInfo.DownloadProtocol,
-                IndexerFlags = (int)indexerFlags
+                IndexerFlags = indexerFlags
             };
         }
 
