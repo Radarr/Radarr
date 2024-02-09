2b1838517c2458118333b3c0e1731f155d46c641
flb <f.lb@gmx.com>
2024-02-09 19:20:17 +0100

    fix 5f03093b3869030ce07ba9f7e0b927d7afd51381, add missing namespace

--- src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
+++ src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using NLog;
 using NzbDrone.Common.Cloud;
