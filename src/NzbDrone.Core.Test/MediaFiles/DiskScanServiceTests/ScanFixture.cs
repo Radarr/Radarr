Sonarr/Sonarr

New: Ignore 'Other' subfolder when scanning disk
Closes #6437

src/NzbDrone.Core.Test/MediaFiles/DiskScanServiceTests/ScanFixture.cs
@@ -186,6 +186,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
                            Path.Combine(_series.Path, "Scenes", "file6.mkv").AsOsAgnostic(),
                            Path.Combine(_series.Path, "Shorts", "file7.mkv").AsOsAgnostic(),
                            Path.Combine(_series.Path, "Trailers", "file8.mkv").AsOsAgnostic(),
+                           Path.Combine(_series.Path, "Other", "file9.mkv").AsOsAgnostic(),
                            Path.Combine(_series.Path, "Series Title S01E01 (1080p BluRay x265 10bit Tigole).mkv").AsOsAgnostic(),
                        });
 

Radarr/Radarr

Closes #9718
(cherry picked from commit e1c6722aad1e35a1e2371bc47f399b57e4f96f27)

src/NzbDrone.Core.Test/MediaFiles/DiskScanServiceTests/ScanFixture.cs
@@ -178,6 +178,7 @@ namespace NzbDrone.Core.Test.MediaFiles.DiskScanServiceTests
                            Path.Combine(_movie.Path, "Scenes", "file7.mkv").AsOsAgnostic(),
                            Path.Combine(_movie.Path, "Shorts", "file8.mkv").AsOsAgnostic(),
                            Path.Combine(_movie.Path, "Trailers", "file9.mkv").AsOsAgnostic(),
+                           Path.Combine(_movie.Path, "Other", "file9.mkv").AsOsAgnostic(),
                            Path.Combine(_movie.Path, "The Count of Monte Cristo (2002) (1080p BluRay x265 10bit Tigole).mkv").AsOsAgnostic(),
                        });
 