Sonarr/Sonarr

Improve Custom Format rejection messaging

src/NzbDrone.Core/MediaFiles/EpisodeImport/Specifications/UpgradeSpecification.cs
@@ -72,7 +72,11 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
                         currentFormats != null ? currentFormats.ConcatToString() : "",
                         currentFormatScore);
 
-                    return Decision.Reject("Not a Custom Format upgrade for existing episode file(s)");
+                    return Decision.Reject("Not a Custom Format upgrade for existing episode file(s). New: [{0}] ({1}) do not improve on Existing: [{2}] ({3})",
+                        newFormats != null ? newFormats.ConcatToString() : "",
+                        newFormatScore,
+                        currentFormats != null ? currentFormats.ConcatToString() : "",
+                        currentFormatScore);
                 }
 
                 _logger.Debug("New item's custom formats [{0}] ({1}) improve on [{2}] ({3}), accepting",

Radarr/Radarr

(cherry picked from commit cac97c057faa44c1656e02681cb9ba668faca488)
Closes #9747

src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/UpgradeSpecification.cs
@@ -74,7 +74,11 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
                         currentCustomFormats != null ? currentCustomFormats.ConcatToString() : "",
                         currentFormatScore);
 
-                    return Decision.Reject("Not a Custom Format upgrade for existing movie file(s)");
+                    return Decision.Reject("Not a Custom Format upgrade for existing movie file(s). New: [{0}] ({1}) do not improve on Existing: [{2}] ({3})",
+                        newCustomFormats != null ? newCustomFormats.ConcatToString() : "",
+                        newFormatScore,
+                        currentCustomFormats != null ? currentCustomFormats.ConcatToString() : "",
+                        currentFormatScore);
                 }
 
                 _logger.Debug("New item's custom formats [{0}] ({1}) do improve on [{2}] ({3}), accepting",