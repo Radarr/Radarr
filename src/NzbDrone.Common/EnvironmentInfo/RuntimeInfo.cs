c2ec7915844cf44c4be64790a88a773047e834df
Uruk <uruknarb20@gmail.com>
2024-03-06 18:30:53 +0100

    Update to .NET 8  - Update .NET to 8.0  - Update .NETFramework to 4.8.1  - Update packages versions  - Update Ubuntu to 20.04  - Update MacOS to 12 since earlier version or not supported : https://github.com/dotnet/core/blob/main/release-notes/8.0/supported-os.md  - OS-release update to Ubuntu 20.04  - Fix task: NodeTool@0 to UseNode@1  - Fix task: reportgenerator@4 to reportgenerator@5
    Update Ubuntu to 20.04
    
    Remove clock and change add to append
    
    Fix miss spelling net8.0

--- src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
+++ src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
@@ -21,7 +21,7 @@ namespace NzbDrone.Common.EnvironmentInfo
             IsWindowsService = hostLifetime is WindowsServiceLifetime;
             IsStarting = true;
 
-            // net6.0 will return Radarr.dll for entry assembly, we need the actual
+            // net8.0 will return Radarr.dll for entry assembly, we need the actual
             // executable name (Radarr on linux).  On mono this will return the location of
             // the mono executable itself, which is not what we want.
             var entry = Process.GetCurrentProcess().MainModule;
