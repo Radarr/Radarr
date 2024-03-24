a75619c8efd1967623d7ed9047bdb3d6f4ba56f8
Mark McDowall <mark@mcdowall.ca>
2024-03-23 21:42:54 -0700

    Fixed: Task with removed movie causing error
    (cherry picked from commit fc6494c569324c839debdb1d08dde23b8f1b8d76)
    
    Closes #9866

--- frontend/src/Store/Selectors/createMultiMoviesSelector.ts
+++ frontend/src/Store/Selectors/createMultiMoviesSelector.ts
@@ -1,12 +1,21 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
+import Movie from 'Movie/Movie';
 
 function createMultiMoviesSelector(movieIds: number[]) {
   return createSelector(
     (state: AppState) => state.movies.itemMap,
     (state: AppState) => state.movies.items,
     (itemMap, allMovies) => {
-      return movieIds.map((movieId) => allMovies[itemMap[movieId]]);
+      return movieIds.reduce((acc: Movie[], movieId) => {
+        const movie = allMovies[itemMap[movieId]];
+
+        if (movie) {
+          acc.push(movie);
+        }
+
+        return acc;
+      }, []);
     }
   );
 }
