Sonarr/Sonarr

New: Blocklist Custom Filters
Closes #6763

src/Sonarr.Api.V3/Blocklist/BlocklistController.cs
@@ -1,7 +1,9 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Indexers;
 using Sonarr.Http;
 using Sonarr.Http.Extensions;
 using Sonarr.Http.REST.Attributes;
@@ -23,12 +25,22 @@ namespace Sonarr.Api.V3.Blocklist
 
         [HttpGet]
         [Produces("application/json")]
-        public PagingResource<BlocklistResource> GetBlocklist([FromQuery] PagingRequestResource paging)
+        public PagingResource<BlocklistResource> GetBlocklist([FromQuery] PagingRequestResource paging, [FromQuery] int[] seriesIds = null, [FromQuery] DownloadProtocol[] protocols = null)
         {
             var pagingResource = new PagingResource<BlocklistResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<BlocklistResource, NzbDrone.Core.Blocklisting.Blocklist>("date", SortDirection.Descending);
 
-            return pagingSpec.ApplyToPage(_blocklistService.Paged, model => BlocklistResourceMapper.MapToResource(model, _formatCalculator));
+            if (seriesIds?.Any() == true)
+            {
+                pagingSpec.FilterExpressions.Add(b => seriesIds.Contains(b.SeriesId));
+            }
+
+            if (protocols?.Any() == true)
+            {
+                pagingSpec.FilterExpressions.Add(b => protocols.Contains(b.Protocol));
+            }
+
+            return pagingSpec.ApplyToPage(b => _blocklistService.Paged(pagingSpec), b => BlocklistResourceMapper.MapToResource(b, _formatCalculator));
         }
 
         [RestDeleteById]

Radarr/Radarr

(cherry picked from commit f81bb3ec1945d343dd0695a2826dac8833cb6346)
Closes #9997

src/Radarr.Api.V3/Blocklist/BlocklistController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Indexers;
 using Radarr.Http;
 using Radarr.Http.Extensions;
 using Radarr.Http.REST.Attributes;
@@ -25,12 +26,22 @@ namespace Radarr.Api.V3.Blocklist
 
         [HttpGet]
         [Produces("application/json")]
-        public PagingResource<BlocklistResource> GetBlocklist([FromQuery] PagingRequestResource paging)
+        public PagingResource<BlocklistResource> GetBlocklist([FromQuery] PagingRequestResource paging, [FromQuery] int[] movieIds = null, [FromQuery] DownloadProtocol[] protocols = null)
         {
             var pagingResource = new PagingResource<BlocklistResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<BlocklistResource, NzbDrone.Core.Blocklisting.Blocklist>("date", SortDirection.Descending);
 
-            return pagingSpec.ApplyToPage(_blocklistService.Paged, model => BlocklistResourceMapper.MapToResource(model, _formatCalculator));
+            if (movieIds?.Any() == true)
+            {
+                pagingSpec.FilterExpressions.Add(b => movieIds.Contains(b.MovieId));
+            }
+
+            if (protocols?.Any() == true)
+            {
+                pagingSpec.FilterExpressions.Add(b => protocols.Contains(b.Protocol));
+            }
+
+            return pagingSpec.ApplyToPage(b => _blocklistService.Paged(pagingSpec), b => BlocklistResourceMapper.MapToResource(b, _formatCalculator));
         }
 
         [HttpGet("movie")]