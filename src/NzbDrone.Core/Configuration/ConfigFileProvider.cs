3f01ba026ab82e36238004ee63130b7ab3a2a733
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-18 19:05:24 +0300

    Fixed: Trimming slashes from UrlBase when using environment variable
    (cherry picked from commit d7ceb11a64c3926f35aabf67c935680cf031bd0e)

--- src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
+++ src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
@@ -241,7 +241,7 @@ namespace NzbDrone.Core.Configuration
         {
             get
             {
-                var urlBase = _serverOptions.UrlBase ?? GetValue("UrlBase", "").Trim('/');
+                var urlBase = (_serverOptions.UrlBase ?? GetValue("UrlBase", "")).Trim('/');
 
                 if (urlBase.IsNullOrWhiteSpace())
                 {
