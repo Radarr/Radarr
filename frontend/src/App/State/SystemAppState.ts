f900d623dc46b18a4d33c98fecf24a980ac45b39
Mark McDowall <mark@mcdowall.ca>
2024-07-14 16:42:35 -0700

    New: Allow major version updates to be installed
    (cherry picked from commit 0e95ba2021b23cc65bce0a0620dd48e355250dab)

--- frontend/src/App/State/SystemAppState.ts
+++ frontend/src/App/State/SystemAppState.ts
@@ -2,18 +2,21 @@ import DiskSpace from 'typings/DiskSpace';
 import Health from 'typings/Health';
 import SystemStatus from 'typings/SystemStatus';
 import Task from 'typings/Task';
+import Update from 'typings/Update';
 import AppSectionState, { AppSectionItemState } from './AppSectionState';
 
 export type DiskSpaceAppState = AppSectionState<DiskSpace>;
 export type HealthAppState = AppSectionState<Health>;
 export type SystemStatusAppState = AppSectionItemState<SystemStatus>;
 export type TaskAppState = AppSectionState<Task>;
+export type UpdateAppState = AppSectionState<Update>;
 
 interface SystemAppState {
   diskSpace: DiskSpaceAppState;
   health: HealthAppState;
   status: SystemStatusAppState;
   tasks: TaskAppState;
+  updates: UpdateAppState;
 }
 
 export default SystemAppState;
