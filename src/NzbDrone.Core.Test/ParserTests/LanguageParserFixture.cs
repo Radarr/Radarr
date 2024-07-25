ae89ae175f1cb44ba5e97e50e88935aacf7930f1
Mark McDowall <mark@mcdowall.ca>
2024-07-24 17:07:30 -0700

    Fixed: Don't treat SubFrench as French audio language
    (cherry picked from commit 5ad3d2efcce7d983bd783b551f32666529086901)
    
    Closes #10209

--- src/NzbDrone.Core.Test/ParserTests/LanguageParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/LanguageParserFixture.cs
@@ -21,6 +21,7 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("The Danish Movie 2015")]
         [TestCase("Movie.Title.2018.2160p.WEBRip.x265.10bit.HDR.DD5.1-GASMASK")]
         [TestCase("Movie.Title.2010.720p.BluRay.x264.-[YTS.LT]")]
+        [TestCase("Movie.Title.2010.SUBFRENCH.1080p.WEB.x264-GROUP")]
         public void should_parse_language_unknown(string postTitle)
         {
             var result = Parser.Parser.ParseMovieTitle(postTitle, true);
