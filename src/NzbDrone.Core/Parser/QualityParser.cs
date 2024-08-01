b7510af4af5594e9683170ce5cff58833be142f9
bokkoman <bokkoman@gmail.com>
2024-08-01 19:32:10 +0200

    Update QualityParser.cs
    Added the word `newcam` for CAM quality.
    
    Movie.Namw.2024.NEWCAM.1080p.HEVC.AC3.English-RypS

--- src/NzbDrone.Core/Parser/QualityParser.cs
+++ src/NzbDrone.Core/Parser/QualityParser.cs
@@ -28,7 +28,7 @@ namespace NzbDrone.Core.Parser
                                                             (?<scr>SCR|SCREENER|DVDSCR|DVDSCREENER)|
                                                             (?<ts>TS[-_. ]|TELESYNC|HD-TS|HDTS|PDVD|TSRip|HDTSRip)|
                                                             (?<tc>TC|TELECINE|HD-TC|HDTC)|
-                                                            (?<cam>CAMRIP|CAM|HD-?CAM(?:Rip)?|HQCAM)|
+                                                            (?<cam>CAMRIP|CAM|HD-?CAM(?:Rip)?|HQCAM|newcam)|
                                                             (?<wp>WORKPRINT|WP)|
                                                             (?<pdtv>PDTV)|
                                                             (?<sdtv>SDTV)|
