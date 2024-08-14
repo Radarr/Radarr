Sonarr/Sonarr

Update React Lint rules for TSX

frontend/src/Components/Error/ErrorBoundaryError.tsx
@@ -64,7 +64,7 @@ function ErrorBoundaryError(props: ErrorBoundaryErrorProps) {
           <div>{info.componentStack}</div>
         )}
 
-        {<div className={styles.version}>Version: {window.Sonarr.version}</div>}
+        <div className={styles.version}>Version: {window.Sonarr.version}</div>
       </details>
     </div>
   );

Radarr/Radarr

(cherry picked from commit 1299a97579bec52ee3d16ab8d05c9e22edd80330)
Closes #10248

frontend/src/Components/Error/ErrorBoundaryError.tsx
@@ -63,7 +63,7 @@ function ErrorBoundaryError(props: ErrorBoundaryErrorProps) {
           <div>{info.componentStack}</div>
         )}
 
-        {<div className={styles.version}>Version: {window.Radarr.version}</div>}
+        <div className={styles.version}>Version: {window.Radarr.version}</div>
       </details>
     </div>
   );