Sonarr/Sonarr

New: Optionally include Custom Format Score for Discord On File Import notifications

src/NzbDrone.Core/Notifications/Discord/Discord.cs
@@ -224,6 +224,14 @@ namespace NzbDrone.Core.Notifications.Discord
                         discordField.Name = "Links";
                         discordField.Value = GetLinksString(series);
                         break;
+                    case DiscordImportFieldType.CustomFormats:
+                        discordField.Name = "Custom Formats";
+                        discordField.Value = string.Join("|", message.EpisodeInfo.CustomFormats);
+                        break;
+                    case DiscordImportFieldType.CustomFormatScore:
+                        discordField.Name = "Custom Format Score";
+                        discordField.Value = message.EpisodeInfo.CustomFormatScore.ToString();
+                        break;
                 }
 
                 if (discordField.Name.IsNotNullOrWhiteSpace() && discordField.Value.IsNotNullOrWhiteSpace())

Radarr/Radarr

(cherry picked from commit e16ace54a8120cd98007a09fe1e6136be3e699fc)
Closes #10313

src/NzbDrone.Core/Notifications/Discord/Discord.cs
@@ -229,6 +229,14 @@ namespace NzbDrone.Core.Notifications.Discord
                         discordField.Name = "Tags";
                         discordField.Value = GetTagLabels(message.Movie)?.Join(", ") ?? string.Empty;
                         break;
+                    case DiscordImportFieldType.CustomFormats:
+                        discordField.Name = "Custom Formats";
+                        discordField.Value = string.Join("|", message.MovieInfo.CustomFormats);
+                        break;
+                    case DiscordImportFieldType.CustomFormatScore:
+                        discordField.Name = "Custom Format Score";
+                        discordField.Value = message.MovieInfo.CustomFormatScore.ToString();
+                        break;
                 }
 
                 if (discordField.Name.IsNotNullOrWhiteSpace() && discordField.Value.IsNotNullOrWhiteSpace())