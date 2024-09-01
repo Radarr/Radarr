Sonarr/Sonarr

Reimport files imported prematurely during script import

src/NzbDrone.Core/Extras/ImportExistingExtraFilesBase.cs
@@ -23,12 +23,16 @@ namespace NzbDrone.Core.Extras
 
         public virtual ImportExistingExtraFileFilterResult<TExtraFile> FilterAndClean(Series series, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
+            var seriesFiles = _extraFileService.GetFilesBySeries(series.Id);
+
             if (keepExistingEntries)
             {
+                var incompleteImports = seriesFiles.IntersectBy(f => Path.Combine(series.Path, f.RelativePath), filesOnDisk, i => i, PathEqualityComparer.Instance).Select(f => f.Id);
+
+                _extraFileService.DeleteMany(incompleteImports);
+
                 return Filter(series, filesOnDisk, importedFiles, new List<TExtraFile>());
             }
-
-            var seriesFiles = _extraFileService.GetFilesBySeries(series.Id);
 
             Clean(series, filesOnDisk, importedFiles, seriesFiles);
 

Radarr/Radarr

(cherry picked from commit 7776ec995571a6bc3ff1a35bbede02c05b943063)

src/NzbDrone.Core/Extras/ImportExistingExtraFilesBase.cs
@@ -23,12 +23,16 @@ namespace NzbDrone.Core.Extras
 
         public virtual ImportExistingExtraFileFilterResult<TExtraFile> FilterAndClean(Movie movie, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
+            var movieFiles = _extraFileService.GetFilesByMovie(movie.Id);
+
             if (keepExistingEntries)
             {
+                var incompleteImports = movieFiles.IntersectBy(f => Path.Combine(movie.Path, f.RelativePath), filesOnDisk, i => i, PathEqualityComparer.Instance).Select(f => f.Id);
+
+                _extraFileService.DeleteMany(incompleteImports);
+
                 return Filter(movie, filesOnDisk, importedFiles, new List<TExtraFile>());
             }
-
-            var movieFiles = _extraFileService.GetFilesByMovie(movie.Id);
 
             Clean(movie, filesOnDisk, importedFiles, movieFiles);
 