Sonarr/Sonarr

Fixed: Respect Quality cutoff if Custom Format cutoff isn't met
Closes #7132

src/NzbDrone.Core/DecisionEngine/Specifications/QueueSpecification.cs
@@ -70,13 +70,28 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                 _logger.Debug("Checking if release is higher quality than queued release. Queued: {0}", remoteEpisode.ParsedEpisodeInfo.Quality);
 
-                if (!_upgradableSpecification.IsUpgradable(qualityProfile,
-                                                           remoteEpisode.ParsedEpisodeInfo.Quality,
-                                                           queuedItemCustomFormats,
-                                                           subject.ParsedEpisodeInfo.Quality,
-                                                           subject.CustomFormats))
+                var upgradeableRejectReason = _upgradableSpecification.IsUpgradable(qualityProfile,
+                    remoteEpisode.ParsedEpisodeInfo.Quality,
+                    queuedItemCustomFormats,
+                    subject.ParsedEpisodeInfo.Quality,
+                    subject.CustomFormats);
+
+                switch (upgradeableRejectReason)
                 {
-                    return Decision.Reject("Release in queue is of equal or higher preference: {0}", remoteEpisode.ParsedEpisodeInfo.Quality);
+                    case UpgradeableRejectReason.BetterQuality:
+                        return Decision.Reject("Release in queue on disk is of equal or higher preference: {0}", remoteEpisode.ParsedEpisodeInfo.Quality);
+
+                    case UpgradeableRejectReason.BetterRevision:
+                        return Decision.Reject("Release in queue on disk is of equal or higher revision: {0}", remoteEpisode.ParsedEpisodeInfo.Quality.Revision);
+
+                    case UpgradeableRejectReason.QualityCutoff:
+                        return Decision.Reject("Release in queue on disk meets quality cutoff: {0}", qualityProfile.Items[qualityProfile.GetIndex(qualityProfile.Cutoff).Index]);
+
+                    case UpgradeableRejectReason.CustomFormatCutoff:
+                        return Decision.Reject("Release in queue on disk meets Custom Format cutoff: {0}", qualityProfile.CutoffFormatScore);
+
+                    case UpgradeableRejectReason.CustomFormatScore:
+                        return Decision.Reject("Release in queue on disk has an equal or higher custom format score: {0}", qualityProfile.CalculateCustomFormatScore(queuedItemCustomFormats));
                 }
 
                 _logger.Debug("Checking if profiles allow upgrading. Queued: {0}", remoteEpisode.ParsedEpisodeInfo.Quality);

src/NzbDrone.Core/DecisionEngine/Specifications/UpgradeDiskSpecification.cs
@@ -26,6 +26,8 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
         public virtual Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
         {
+            var qualityProfile = subject.Series.QualityProfile.Value;
+
             foreach (var file in subject.Episodes.Where(c => c.EpisodeFileId != 0).Select(c => c.EpisodeFile.Value))
             {
                 if (file == null)
@@ -36,15 +38,45 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                 var customFormats = _formatService.ParseCustomFormat(file);
 
-                _logger.Debug("Comparing file quality with report. Existing file is {0}", file.Quality);
+                _logger.Debug("Comparing file quality with report. Existing file is {0}.", file.Quality);
 
-                if (!_upgradableSpecification.IsUpgradable(subject.Series.QualityProfile,
-                                                           file.Quality,
-                                                           customFormats,
-                                                           subject.ParsedEpisodeInfo.Quality,
-                                                           subject.CustomFormats))
+                if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
+                        file.Quality,
+                        _formatService.ParseCustomFormat(file),
+                        subject.ParsedEpisodeInfo.Quality))
                 {
-                    return Decision.Reject("Existing file on disk is of equal or higher preference: {0}", file.Quality);
+                    _logger.Debug("Cutoff already met, rejecting.");
+
+                    var qualityCutoffIndex = qualityProfile.GetIndex(qualityProfile.Cutoff);
+                    var qualityCutoff = qualityProfile.Items[qualityCutoffIndex.Index];
+
+                    return Decision.Reject("Existing file meets cutoff: {0}", qualityCutoff);
+                }
+
+                var upgradeableRejectReason = _upgradableSpecification.IsUpgradable(qualityProfile,
+                    file.Quality,
+                    customFormats,
+                    subject.ParsedEpisodeInfo.Quality,
+                    subject.CustomFormats);
+
+                switch (upgradeableRejectReason)
+                {
+                    case UpgradeableRejectReason.None:
+                        continue;
+                    case UpgradeableRejectReason.BetterQuality:
+                        return Decision.Reject("Existing file on disk is of equal or higher preference: {0}", file.Quality);
+
+                    case UpgradeableRejectReason.BetterRevision:
+                        return Decision.Reject("Existing file on disk is of equal or higher revision: {0}", file.Quality.Revision);
+
+                    case UpgradeableRejectReason.QualityCutoff:
+                        return Decision.Reject("Existing file on disk meets quality cutoff: {0}", qualityProfile.Items[qualityProfile.GetIndex(qualityProfile.Cutoff).Index]);
+
+                    case UpgradeableRejectReason.CustomFormatCutoff:
+                        return Decision.Reject("Existing file on disk meets Custom Format cutoff: {0}", qualityProfile.CutoffFormatScore);
+
+                    case UpgradeableRejectReason.CustomFormatScore:
+                        return Decision.Reject("Existing file on disk has a equal or higher custom format score: {0}", qualityProfile.CalculateCustomFormatScore(customFormats));
                 }
             }
 

Radarr/Radarr

(cherry picked from commit 6f51e72d0073444b441bee5508322cc9e52e98e4)

src/NzbDrone.Core/DecisionEngine/Specifications/QueueSpecification.cs
@@ -55,29 +55,44 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
                     continue;
                 }
 
-                var customFormats = _formatService.ParseCustomFormat(remoteMovie, (long)queueItem.Size);
+                var queuedItemCustomFormats = _formatService.ParseCustomFormat(remoteMovie, (long)queueItem.Size);
 
                 _logger.Debug("Checking if existing release in queue meets cutoff. Queued quality is: {0} - {1}",
                               remoteMovie.ParsedMovieInfo.Quality,
-                              customFormats.ConcatToString());
+                              queuedItemCustomFormats.ConcatToString());
 
                 if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
-                                                           remoteMovie.ParsedMovieInfo.Quality,
-                                                           customFormats,
-                                                           subject.ParsedMovieInfo.Quality))
+                    remoteMovie.ParsedMovieInfo.Quality,
+                    queuedItemCustomFormats,
+                    subject.ParsedMovieInfo.Quality))
                 {
                     return Decision.Reject("Quality for release in queue already meets cutoff: {0}", remoteMovie.ParsedMovieInfo.Quality);
                 }
 
                 _logger.Debug("Checking if release is higher quality than queued release. Queued quality is: {0}", remoteMovie.ParsedMovieInfo.Quality);
 
-                if (!_upgradableSpecification.IsUpgradable(qualityProfile,
-                                                           remoteMovie.ParsedMovieInfo.Quality,
-                                                           remoteMovie.CustomFormats,
-                                                           subject.ParsedMovieInfo.Quality,
-                                                           subject.CustomFormats))
+                var upgradeableRejectReason = _upgradableSpecification.IsUpgradable(qualityProfile,
+                    remoteMovie.ParsedMovieInfo.Quality,
+                    queuedItemCustomFormats,
+                    subject.ParsedMovieInfo.Quality,
+                    subject.CustomFormats);
+
+                switch (upgradeableRejectReason)
                 {
-                    return Decision.Reject("Quality for release in queue is of equal or higher preference: {0}", remoteMovie.ParsedMovieInfo.Quality);
+                    case UpgradeableRejectReason.BetterQuality:
+                        return Decision.Reject("Release in queue on disk is of equal or higher preference: {0}", remoteMovie.ParsedMovieInfo.Quality);
+
+                    case UpgradeableRejectReason.BetterRevision:
+                        return Decision.Reject("Release in queue on disk is of equal or higher revision: {0}", remoteMovie.ParsedMovieInfo.Quality.Revision);
+
+                    case UpgradeableRejectReason.QualityCutoff:
+                        return Decision.Reject("Release in queue on disk meets quality cutoff: {0}", qualityProfile.Items[qualityProfile.GetIndex(qualityProfile.Cutoff).Index]);
+
+                    case UpgradeableRejectReason.CustomFormatCutoff:
+                        return Decision.Reject("Release in queue on disk meets Custom Format cutoff: {0}", qualityProfile.CutoffFormatScore);
+
+                    case UpgradeableRejectReason.CustomFormatScore:
+                        return Decision.Reject("Release in queue on disk has an equal or higher custom format score: {0}", qualityProfile.CalculateCustomFormatScore(queuedItemCustomFormats));
                 }
 
                 _logger.Debug("Checking if profiles allow upgrading. Queued: {0}", remoteMovie.ParsedMovieInfo.Quality);

src/NzbDrone.Core/DecisionEngine/Specifications/UpgradeDiskSpecification.cs
@@ -8,15 +8,15 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 {
     public class UpgradeDiskSpecification : IDecisionEngineSpecification
     {
-        private readonly UpgradableSpecification _qualityUpgradableSpecification;
+        private readonly UpgradableSpecification _upgradableSpecification;
         private readonly ICustomFormatCalculationService _formatService;
         private readonly Logger _logger;
 
-        public UpgradeDiskSpecification(UpgradableSpecification qualityUpgradableSpecification,
+        public UpgradeDiskSpecification(UpgradableSpecification upgradableSpecification,
                                         ICustomFormatCalculationService formatService,
                                         Logger logger)
         {
-            _qualityUpgradableSpecification = qualityUpgradableSpecification;
+            _upgradableSpecification = upgradableSpecification;
             _formatService = formatService;
             _logger = logger;
         }
@@ -26,24 +26,58 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
         public virtual Decision IsSatisfiedBy(RemoteMovie subject, SearchCriteriaBase searchCriteria)
         {
-            if (subject.Movie.MovieFile == null)
+            var qualityProfile = subject.Movie.QualityProfile;
+
+            var file = subject.Movie.MovieFile;
+
+            if (file == null)
             {
+                _logger.Debug("File is no longer available, skipping this file.");
                 return Decision.Accept();
             }
 
-            var profile = subject.Movie.QualityProfile;
-            var file = subject.Movie.MovieFile;
             file.Movie = subject.Movie;
             var customFormats = _formatService.ParseCustomFormat(file);
-            _logger.Debug("Comparing file quality with report. Existing file is {0} [{1}]", file.Quality, customFormats.ConcatToString());
 
-            if (!_qualityUpgradableSpecification.IsUpgradable(profile,
-                                                              file.Quality,
-                                                              customFormats,
-                                                              subject.ParsedMovieInfo.Quality,
-                                                              subject.CustomFormats))
+            _logger.Debug("Comparing file quality with report. Existing file is {0} [{1}].", file.Quality, customFormats.ConcatToString());
+
+            if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
+                    file.Quality,
+                    _formatService.ParseCustomFormat(file),
+                    subject.ParsedMovieInfo.Quality))
             {
-                return Decision.Reject("Quality for existing file on disk is of equal or higher preference: {0} [{1}]", file.Quality, customFormats.ConcatToString());
+                _logger.Debug("Cutoff already met, rejecting.");
+
+                var qualityCutoffIndex = qualityProfile.GetIndex(qualityProfile.Cutoff);
+                var qualityCutoff = qualityProfile.Items[qualityCutoffIndex.Index];
+
+                return Decision.Reject("Existing file meets cutoff: {0} [{1}]", qualityCutoff, customFormats.ConcatToString());
+            }
+
+            var upgradeableRejectReason = _upgradableSpecification.IsUpgradable(qualityProfile,
+                file.Quality,
+                customFormats,
+                subject.ParsedMovieInfo.Quality,
+                subject.CustomFormats);
+
+            switch (upgradeableRejectReason)
+            {
+                case UpgradeableRejectReason.None:
+                    return Decision.Accept();
+                case UpgradeableRejectReason.BetterQuality:
+                    return Decision.Reject("Existing file on disk is of equal or higher preference: {0}", file.Quality);
+
+                case UpgradeableRejectReason.BetterRevision:
+                    return Decision.Reject("Existing file on disk is of equal or higher revision: {0}", file.Quality.Revision);
+
+                case UpgradeableRejectReason.QualityCutoff:
+                    return Decision.Reject("Existing file on disk meets quality cutoff: {0}", qualityProfile.Items[qualityProfile.GetIndex(qualityProfile.Cutoff).Index]);
+
+                case UpgradeableRejectReason.CustomFormatCutoff:
+                    return Decision.Reject("Existing file on disk meets Custom Format cutoff: {0}", qualityProfile.CutoffFormatScore);
+
+                case UpgradeableRejectReason.CustomFormatScore:
+                    return Decision.Reject("Existing file on disk has a equal or higher custom format score: {0}", qualityProfile.CalculateCustomFormatScore(customFormats));
             }
 
             return Decision.Accept();