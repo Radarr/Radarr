Sonarr/Sonarr

Fixed: Replace illegal characters even when renaming is disabled
Closes #7183

src/NzbDrone.Core.Test/OrganizerTests/FileNameBuilderTests/FileNameBuilderFixture.cs
@@ -274,7 +274,7 @@ namespace NzbDrone.Core.Test.OrganizerTests.FileNameBuilderTests
         {
             _namingConfig.RenameEpisodes = false;
             _episodeFile.RelativePath = null;
-            _episodeFile.Path = @"C:\Test\Unsorted\Series - S01E01 - Test";
+            _episodeFile.Path = @"C:\Test\Unsorted\Series - S01E01 - Test".AsOsAgnostic();
 
             Subject.BuildFileName(new List<Episode> { _episode1 }, _series, _episodeFile)
                    .Should().Be(Path.GetFileNameWithoutExtension(_episodeFile.Path));
@@ -289,6 +289,20 @@ namespace NzbDrone.Core.Test.OrganizerTests.FileNameBuilderTests
 
             Subject.BuildFileName(new List<Episode> { _episode1 }, _series, _episodeFile)
                    .Should().Be("30.Rock.S01E01.xvid-LOL");
+        }
+
+        [Test]
+        public void should_replace_illegal_characters_when_renaming_is_disabled()
+        {
+            _namingConfig.RenameEpisodes = false;
+            _namingConfig.ReplaceIllegalCharacters = true;
+            _namingConfig.ColonReplacementFormat = ColonReplacementFormat.Smart;
+
+            _episodeFile.SceneName = "30.Rock.S01E01.xvid:LOL";
+            _episodeFile.RelativePath = "30 Rock - S01E01 - Test";
+
+            Subject.BuildFileName(new List<Episode> { _episode1 }, _series, _episodeFile)
+                .Should().Be("30.Rock.S01E01.xvid-LOL");
         }
 
         [Test]

src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -1116,10 +1116,10 @@ namespace NzbDrone.Core.Organizer
         {
             if (episodeFile.SceneName.IsNullOrWhiteSpace())
             {
-                return GetOriginalFileName(episodeFile, useCurrentFilenameAsFallback);
+                return CleanFileName(GetOriginalFileName(episodeFile, useCurrentFilenameAsFallback));
             }
 
-            return episodeFile.SceneName;
+            return CleanFileName(episodeFile.SceneName);
         }
 
         private string GetOriginalFileName(EpisodeFile episodeFile, bool useCurrentFilenameAsFallback)

Radarr/Radarr

(cherry picked from commit 4d8a4436810828494e99f0854cf6de3269668fe4)

src/NzbDrone.Core.Test/OrganizerTests/FileNameBuilderTests/FileNameBuilderFixture.cs
@@ -16,6 +16,7 @@ using NzbDrone.Core.Movies.Translations;
 using NzbDrone.Core.Organizer;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
+using NzbDrone.Test.Common;
 
 namespace NzbDrone.Core.Test.OrganizerTests.FileNameBuilderTests
 {
@@ -292,7 +293,7 @@ namespace NzbDrone.Core.Test.OrganizerTests.FileNameBuilderTests
         {
             _namingConfig.RenameMovies = false;
             _movieFile.RelativePath = null;
-            _movieFile.Path = @"C:\Test\Unsorted\Movie - S01E01 - Test";
+            _movieFile.Path = @"C:\Test\Unsorted\Movie - S01E01 - Test".AsOsAgnostic();
 
             Subject.BuildFileName(_movie, _movieFile)
                    .Should().Be(Path.GetFileNameWithoutExtension(_movieFile.Path));
@@ -307,6 +308,20 @@ namespace NzbDrone.Core.Test.OrganizerTests.FileNameBuilderTests
 
             Subject.BuildFileName(_movie, _movieFile)
                    .Should().Be("30.Rock.S01E01.xvid-LOL");
+        }
+
+        [Test]
+        public void should_replace_illegal_characters_when_renaming_is_disabled()
+        {
+            _namingConfig.RenameMovies = false;
+            _namingConfig.ReplaceIllegalCharacters = true;
+            _namingConfig.ColonReplacementFormat = ColonReplacementFormat.Smart;
+
+            _movieFile.SceneName = "30.Rock.S01E01.xvid:LOL";
+            _movieFile.RelativePath = "30 Rock - S01E01 - Test";
+
+            Subject.BuildFileName(_movie, _movieFile)
+                .Should().Be("30.Rock.S01E01.xvid-LOL");
         }
 
         [Test]

src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -621,10 +621,10 @@ namespace NzbDrone.Core.Organizer
         {
             if (movieFile.SceneName.IsNullOrWhiteSpace())
             {
-                return GetOriginalFileName(movieFile, multipleTokens);
+                return CleanFileName(GetOriginalFileName(movieFile, multipleTokens));
             }
 
-            return movieFile.SceneName;
+            return CleanFileName(movieFile.SceneName);
         }
 
         private string GetOriginalFileName(MovieFile movieFile, bool multipleTokens)