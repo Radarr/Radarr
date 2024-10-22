Sonarr/Sonarr

Convert Components to TypeScript

frontend/src/Components/FileBrowser/FileBrowserModalContent.tsx
@@ -1,0 +1,237 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Alert from 'Components/Alert';
+import PathInput from 'Components/Form/PathInput';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import Scroller from 'Components/Scroller/Scroller';
+import Column from 'Components/Table/Column';
+import Table from 'Components/Table/Table';
+import TableBody from 'Components/Table/TableBody';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { kinds, scrollDirections } from 'Helpers/Props';
+import { clearPaths, fetchPaths } from 'Store/Actions/pathActions';
+import createSystemStatusSelector from 'Store/Selectors/createSystemStatusSelector';
+import { InputChanged } from 'typings/inputs';
+import translate from 'Utilities/String/translate';
+import createPathsSelector from './createPathsSelector';
+import FileBrowserRow from './FileBrowserRow';
+import styles from './FileBrowserModalContent.css';
+
+const columns: Column[] = [
+  {
+    name: 'type',
+    label: () => translate('Type'),
+    isVisible: true,
+  },
+  {
+    name: 'name',
+    label: () => translate('Name'),
+    isVisible: true,
+  },
+];
+
+const handleClearPaths = () => {};
+
+export interface FileBrowserModalContentProps {
+  name: string;
+  value: string;
+  includeFiles?: boolean;
+  onChange: (args: InputChanged<string>) => unknown;
+  onModalClose: () => void;
+}
+
+function FileBrowserModalContent(props: FileBrowserModalContentProps) {
+  const { name, value, includeFiles = true, onChange, onModalClose } = props;
+
+  const dispatch = useDispatch();
+
+  const { isWindows, mode } = useSelector(createSystemStatusSelector());
+  const { isFetching, isPopulated, error, parent, directories, files, paths } =
+    useSelector(createPathsSelector());
+
+  const [currentPath, setCurrentPath] = useState(value);
+  const scrollerRef = useRef(null);
+  const previousValue = usePrevious(value);
+
+  const emptyParent = parent === '';
+  const isWindowsService = isWindows && mode === 'service';
+
+  const handlePathInputChange = useCallback(
+    ({ value }: InputChanged<string>) => {
+      setCurrentPath(value);
+    },
+    []
+  );
+
+  const handleRowPress = useCallback(
+    (path: string) => {
+      setCurrentPath(path);
+
+      dispatch(
+        fetchPaths({
+          path,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+    },
+    [includeFiles, dispatch, setCurrentPath]
+  );
+
+  const handleOkPress = useCallback(() => {
+    onChange({
+      name,
+      value: currentPath,
+    });
+
+    dispatch(clearPaths());
+    onModalClose();
+  }, [name, currentPath, dispatch, onChange, onModalClose]);
+
+  const handleFetchPaths = useCallback(
+    (path: string) => {
+      dispatch(
+        fetchPaths({
+          path,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+    },
+    [includeFiles, dispatch]
+  );
+
+  useEffect(() => {
+    if (value !== previousValue && value !== currentPath) {
+      setCurrentPath(value);
+    }
+  }, [value, previousValue, currentPath, setCurrentPath]);
+
+  useEffect(
+    () => {
+      dispatch(
+        fetchPaths({
+          path: currentPath,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+
+      return () => {
+        dispatch(clearPaths());
+      };
+    },
+    // This should only run once when the component mounts,
+    // so we don't need to include the other dependencies.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dispatch]
+  );
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('FileBrowser')}</ModalHeader>
+
+      <ModalBody
+        className={styles.modalBody}
+        scrollDirection={scrollDirections.NONE}
+      >
+        {isWindowsService ? (
+          <Alert className={styles.mappedDrivesWarning} kind={kinds.WARNING}>
+            <InlineMarkdown
+              data={translate('MappedNetworkDrivesWindowsService', {
+                url: 'https://wiki.servarr.com/sonarr/faq#why-cant-sonarr-see-my-files-on-a-remote-server',
+              })}
+            />
+          </Alert>
+        ) : null}
+
+        <PathInput
+          className={styles.pathInput}
+          placeholder={translate('FileBrowserPlaceholderText')}
+          hasFileBrowser={false}
+          includeFiles={includeFiles}
+          paths={paths}
+          name={name}
+          value={currentPath}
+          onChange={handlePathInputChange}
+          onFetchPaths={handleFetchPaths}
+          onClearPaths={handleClearPaths}
+        />
+
+        <Scroller
+          ref={scrollerRef}
+          className={styles.scroller}
+          scrollDirection="both"
+        >
+          {error ? <div>{translate('ErrorLoadingContents')}</div> : null}
+
+          {isPopulated && !error ? (
+            <Table horizontalScroll={false} columns={columns}>
+              <TableBody>
+                {emptyParent ? (
+                  <FileBrowserRow
+                    type="computer"
+                    name={translate('MyComputer')}
+                    path={parent}
+                    onPress={handleRowPress}
+                  />
+                ) : null}
+
+                {!emptyParent && parent ? (
+                  <FileBrowserRow
+                    type="parent"
+                    name="..."
+                    path={parent}
+                    onPress={handleRowPress}
+                  />
+                ) : null}
+
+                {directories.map((directory) => {
+                  return (
+                    <FileBrowserRow
+                      key={directory.path}
+                      type={directory.type}
+                      name={directory.name}
+                      path={directory.path}
+                      onPress={handleRowPress}
+                    />
+                  );
+                })}
+
+                {files.map((file) => {
+                  return (
+                    <FileBrowserRow
+                      key={file.path}
+                      type={file.type}
+                      name={file.name}
+                      path={file.path}
+                      onPress={handleRowPress}
+                    />
+                  );
+                })}
+              </TableBody>
+            </Table>
+          ) : null}
+        </Scroller>
+      </ModalBody>
+
+      <ModalFooter>
+        {isFetching ? (
+          <LoadingIndicator className={styles.loading} size={20} />
+        ) : null}
+
+        <Button onPress={onModalClose}>{translate('Cancel')}</Button>
+
+        <Button onPress={handleOkPress}>{translate('Ok')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default FileBrowserModalContent;

frontend/src/Components/Tooltip/Tooltip.tsx
@@ -1,0 +1,216 @@
+import classNames from 'classnames';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { Manager, Popper, Reference } from 'react-popper';
+import Portal from 'Components/Portal';
+import { kinds, tooltipPositions } from 'Helpers/Props';
+import { Kind } from 'Helpers/Props/kinds';
+import dimensions from 'Styles/Variables/dimensions';
+import { isMobile as isMobileUtil } from 'Utilities/browser';
+import styles from './Tooltip.css';
+
+export interface TooltipProps {
+  className?: string;
+  bodyClassName?: string;
+  anchor: React.ReactNode;
+  tooltip: string | React.ReactNode;
+  kind?: Extract<Kind, keyof typeof styles>;
+  position?: (typeof tooltipPositions.all)[number];
+  canFlip?: boolean;
+}
+function Tooltip(props: TooltipProps) {
+  const {
+    className,
+    bodyClassName = styles.body,
+    anchor,
+    tooltip,
+    kind = kinds.DEFAULT,
+    position = tooltipPositions.TOP,
+    canFlip = false,
+  } = props;
+
+  const closeTimeout = useRef(0);
+  const updater = useRef<(() => void) | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    if (!isMobileUtil()) {
+      return;
+    }
+
+    setIsOpen((isOpen) => {
+      return !isOpen;
+    });
+  }, [setIsOpen]);
+
+  const handleMouseEnter = useCallback(() => {
+    // Mobile will fire mouse enter and click events rapidly,
+    // this causes the tooltip not to open on the first press.
+    // Ignore the mouse enter event on mobile.
+    if (isMobileUtil()) {
+      return;
+    }
+
+    if (closeTimeout.current) {
+      window.clearTimeout(closeTimeout.current);
+    }
+
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const handleMouseLeave = useCallback(() => {
+    // Still listen for mouse leave on mobile to allow clicks outside to close the tooltip.
+
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 100);
+  }, [setIsOpen]);
+
+  const maxWidth = useMemo(() => {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth >= parseInt(dimensions.breakpointLarge)) {
+      return 800;
+    } else if (windowWidth >= parseInt(dimensions.breakpointMedium)) {
+      return 650;
+    } else if (windowWidth >= parseInt(dimensions.breakpointSmall)) {
+      return 500;
+    }
+
+    return 450;
+  }, []);
+
+  const computeMaxSize = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (data: any) => {
+      const { top, right, bottom, left } = data.offsets.reference;
+
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      if (/^top/.test(data.placement)) {
+        data.styles.maxHeight = top - 20;
+      } else if (/^bottom/.test(data.placement)) {
+        data.styles.maxHeight = windowHeight - bottom - 20;
+      } else if (/^right/.test(data.placement)) {
+        data.styles.maxWidth = Math.min(maxWidth, windowWidth - right - 20);
+        data.styles.maxHeight = top - 20;
+      } else {
+        data.styles.maxWidth = Math.min(maxWidth, left - 20);
+        data.styles.maxHeight = top - 20;
+      }
+
+      return data;
+    },
+    [maxWidth]
+  );
+
+  useEffect(() => {
+    const currentTimeout = closeTimeout.current;
+
+    if (updater.current && isOpen) {
+      updater.current();
+    }
+
+    return () => {
+      if (currentTimeout) {
+        window.clearTimeout(currentTimeout);
+      }
+    };
+  });
+
+  return (
+    <Manager>
+      <Reference>
+        {({ ref }) => (
+          <span
+            ref={ref}
+            className={className}
+            onClick={handleClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {anchor}
+          </span>
+        )}
+      </Reference>
+
+      <Portal>
+        <Popper
+          // @ts-expect-error - PopperJS types are not in sync with our position types.
+          placement={position}
+          // Disable events to improve performance when many tooltips
+          // are shown (Quality Definitions for example).
+          eventsEnabled={false}
+          modifiers={{
+            computeMaxHeight: {
+              order: 851,
+              enabled: true,
+              fn: computeMaxSize,
+            },
+            preventOverflow: {
+              // Fixes positioning for tooltips in the queue
+              // and likely others.
+              escapeWithReference: false,
+            },
+            flip: {
+              enabled: canFlip,
+            },
+          }}
+        >
+          {({ ref, style, placement, arrowProps, scheduleUpdate }) => {
+            updater.current = scheduleUpdate;
+
+            const popperPlacement = placement
+              ? placement.split('-')[0]
+              : position;
+            const vertical =
+              popperPlacement === 'top' || popperPlacement === 'bottom';
+
+            return (
+              <div
+                ref={ref}
+                className={classNames(
+                  styles.tooltipContainer,
+                  vertical
+                    ? styles.verticalContainer
+                    : styles.horizontalContainer
+                )}
+                style={style}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <div
+                  ref={arrowProps.ref}
+                  className={
+                    isOpen
+                      ? classNames(
+                          styles.arrow,
+                          styles[kind],
+                          // @ts-expect-error - is a string that may not exist in styles
+                          styles[popperPlacement]
+                        )
+                      : styles.arrowDisabled
+                  }
+                  style={arrowProps.style}
+                />
+                {isOpen ? (
+                  <div className={classNames(styles.tooltip, styles[kind])}>
+                    <div className={bodyClassName}>{tooltip}</div>
+                  </div>
+                ) : null}
+              </div>
+            );
+          }}
+        </Popper>
+      </Portal>
+    </Manager>
+  );
+}
+
+export default Tooltip;

Radarr/Radarr

(cherry picked from commit e1cbc4a78249881de96160739a50c0a399ea4313)
Closes #10378
Fixed: Links tooltip closing too quickly
(cherry picked from commit 0b9a212f33381d07ff67e2453753aaab64cc8041)
Closes #10400
Fixed: Movie links not opening on iOS
(cherry picked from commit f20ac9dc348e1f5ded635f12ab925d982b1b8957)
Closes #10425

frontend/src/Components/FileBrowser/FileBrowserModalContent.tsx
@@ -1,0 +1,237 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Alert from 'Components/Alert';
+import PathInput from 'Components/Form/PathInput';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import Scroller from 'Components/Scroller/Scroller';
+import Column from 'Components/Table/Column';
+import Table from 'Components/Table/Table';
+import TableBody from 'Components/Table/TableBody';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { kinds, scrollDirections } from 'Helpers/Props';
+import { clearPaths, fetchPaths } from 'Store/Actions/pathActions';
+import createSystemStatusSelector from 'Store/Selectors/createSystemStatusSelector';
+import { InputChanged } from 'typings/inputs';
+import translate from 'Utilities/String/translate';
+import createPathsSelector from './createPathsSelector';
+import FileBrowserRow from './FileBrowserRow';
+import styles from './FileBrowserModalContent.css';
+
+const columns: Column[] = [
+  {
+    name: 'type',
+    label: () => translate('Type'),
+    isVisible: true,
+  },
+  {
+    name: 'name',
+    label: () => translate('Name'),
+    isVisible: true,
+  },
+];
+
+const handleClearPaths = () => {};
+
+export interface FileBrowserModalContentProps {
+  name: string;
+  value: string;
+  includeFiles?: boolean;
+  onChange: (args: InputChanged<string>) => unknown;
+  onModalClose: () => void;
+}
+
+function FileBrowserModalContent(props: FileBrowserModalContentProps) {
+  const { name, value, includeFiles = true, onChange, onModalClose } = props;
+
+  const dispatch = useDispatch();
+
+  const { isWindows, mode } = useSelector(createSystemStatusSelector());
+  const { isFetching, isPopulated, error, parent, directories, files, paths } =
+    useSelector(createPathsSelector());
+
+  const [currentPath, setCurrentPath] = useState(value);
+  const scrollerRef = useRef(null);
+  const previousValue = usePrevious(value);
+
+  const emptyParent = parent === '';
+  const isWindowsService = isWindows && mode === 'service';
+
+  const handlePathInputChange = useCallback(
+    ({ value }: InputChanged<string>) => {
+      setCurrentPath(value);
+    },
+    []
+  );
+
+  const handleRowPress = useCallback(
+    (path: string) => {
+      setCurrentPath(path);
+
+      dispatch(
+        fetchPaths({
+          path,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+    },
+    [includeFiles, dispatch, setCurrentPath]
+  );
+
+  const handleOkPress = useCallback(() => {
+    onChange({
+      name,
+      value: currentPath,
+    });
+
+    dispatch(clearPaths());
+    onModalClose();
+  }, [name, currentPath, dispatch, onChange, onModalClose]);
+
+  const handleFetchPaths = useCallback(
+    (path: string) => {
+      dispatch(
+        fetchPaths({
+          path,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+    },
+    [includeFiles, dispatch]
+  );
+
+  useEffect(() => {
+    if (value !== previousValue && value !== currentPath) {
+      setCurrentPath(value);
+    }
+  }, [value, previousValue, currentPath, setCurrentPath]);
+
+  useEffect(
+    () => {
+      dispatch(
+        fetchPaths({
+          path: currentPath,
+          allowFoldersWithoutTrailingSlashes: true,
+          includeFiles,
+        })
+      );
+
+      return () => {
+        dispatch(clearPaths());
+      };
+    },
+    // This should only run once when the component mounts,
+    // so we don't need to include the other dependencies.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dispatch]
+  );
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('FileBrowser')}</ModalHeader>
+
+      <ModalBody
+        className={styles.modalBody}
+        scrollDirection={scrollDirections.NONE}
+      >
+        {isWindowsService ? (
+          <Alert className={styles.mappedDrivesWarning} kind={kinds.WARNING}>
+            <InlineMarkdown
+              data={translate('MappedNetworkDrivesWindowsService', {
+                url: 'https://wiki.servarr.com/radarr/faq#why-cant-radarr-see-my-files-on-a-remote-server',
+              })}
+            />
+          </Alert>
+        ) : null}
+
+        <PathInput
+          className={styles.pathInput}
+          placeholder={translate('FileBrowserPlaceholderText')}
+          hasFileBrowser={false}
+          includeFiles={includeFiles}
+          paths={paths}
+          name={name}
+          value={currentPath}
+          onChange={handlePathInputChange}
+          onFetchPaths={handleFetchPaths}
+          onClearPaths={handleClearPaths}
+        />
+
+        <Scroller
+          ref={scrollerRef}
+          className={styles.scroller}
+          scrollDirection="both"
+        >
+          {error ? <div>{translate('ErrorLoadingContents')}</div> : null}
+
+          {isPopulated && !error ? (
+            <Table horizontalScroll={false} columns={columns}>
+              <TableBody>
+                {emptyParent ? (
+                  <FileBrowserRow
+                    type="computer"
+                    name={translate('MyComputer')}
+                    path={parent}
+                    onPress={handleRowPress}
+                  />
+                ) : null}
+
+                {!emptyParent && parent ? (
+                  <FileBrowserRow
+                    type="parent"
+                    name="..."
+                    path={parent}
+                    onPress={handleRowPress}
+                  />
+                ) : null}
+
+                {directories.map((directory) => {
+                  return (
+                    <FileBrowserRow
+                      key={directory.path}
+                      type={directory.type}
+                      name={directory.name}
+                      path={directory.path}
+                      onPress={handleRowPress}
+                    />
+                  );
+                })}
+
+                {files.map((file) => {
+                  return (
+                    <FileBrowserRow
+                      key={file.path}
+                      type={file.type}
+                      name={file.name}
+                      path={file.path}
+                      onPress={handleRowPress}
+                    />
+                  );
+                })}
+              </TableBody>
+            </Table>
+          ) : null}
+        </Scroller>
+      </ModalBody>
+
+      <ModalFooter>
+        {isFetching ? (
+          <LoadingIndicator className={styles.loading} size={20} />
+        ) : null}
+
+        <Button onPress={onModalClose}>{translate('Cancel')}</Button>
+
+        <Button onPress={handleOkPress}>{translate('Ok')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default FileBrowserModalContent;

frontend/src/Components/Tooltip/Tooltip.tsx
@@ -1,0 +1,226 @@
+import classNames from 'classnames';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { Manager, Popper, Reference } from 'react-popper';
+import Portal from 'Components/Portal';
+import { kinds, tooltipPositions } from 'Helpers/Props';
+import { Kind } from 'Helpers/Props/kinds';
+import dimensions from 'Styles/Variables/dimensions';
+import { isMobile as isMobileUtil } from 'Utilities/browser';
+import styles from './Tooltip.css';
+
+export interface TooltipProps {
+  className?: string;
+  bodyClassName?: string;
+  anchor: React.ReactNode;
+  tooltip: string | React.ReactNode;
+  kind?: Extract<Kind, keyof typeof styles>;
+  position?: (typeof tooltipPositions.all)[number];
+  canFlip?: boolean;
+}
+function Tooltip(props: TooltipProps) {
+  const {
+    className,
+    bodyClassName = styles.body,
+    anchor,
+    tooltip,
+    kind = kinds.DEFAULT,
+    position = tooltipPositions.TOP,
+    canFlip = false,
+  } = props;
+
+  const closeTimeout = useRef<ReturnType<typeof setTimeout>>();
+  const updater = useRef<(() => void) | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = useCallback(() => {
+    if (!isMobileUtil()) {
+      return;
+    }
+
+    setIsOpen((isOpen) => {
+      return !isOpen;
+    });
+  }, [setIsOpen]);
+
+  const handleMouseEnterAnchor = useCallback(() => {
+    // Mobile will fire mouse enter and click events rapidly,
+    // this causes the tooltip not to open on the first press.
+    // Ignore the mouse enter event on mobile.
+
+    if (isMobileUtil()) {
+      return;
+    }
+
+    if (closeTimeout.current) {
+      clearTimeout(closeTimeout.current);
+    }
+
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const handleMouseEnterTooltip = useCallback(() => {
+    if (closeTimeout.current) {
+      clearTimeout(closeTimeout.current);
+    }
+
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const handleMouseLeave = useCallback(() => {
+    // Still listen for mouse leave on mobile to allow clicks outside to close the tooltip.
+
+    clearTimeout(closeTimeout.current);
+    closeTimeout.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 100);
+  }, [setIsOpen]);
+
+  const maxWidth = useMemo(() => {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth >= parseInt(dimensions.breakpointLarge)) {
+      return 800;
+    } else if (windowWidth >= parseInt(dimensions.breakpointMedium)) {
+      return 650;
+    } else if (windowWidth >= parseInt(dimensions.breakpointSmall)) {
+      return 500;
+    }
+
+    return 450;
+  }, []);
+
+  const computeMaxSize = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (data: any) => {
+      const { top, right, bottom, left } = data.offsets.reference;
+
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      if (/^top/.test(data.placement)) {
+        data.styles.maxHeight = top - 20;
+      } else if (/^bottom/.test(data.placement)) {
+        data.styles.maxHeight = windowHeight - bottom - 20;
+      } else if (/^right/.test(data.placement)) {
+        data.styles.maxWidth = Math.min(maxWidth, windowWidth - right - 20);
+        data.styles.maxHeight = top - 20;
+      } else {
+        data.styles.maxWidth = Math.min(maxWidth, left - 20);
+        data.styles.maxHeight = top - 20;
+      }
+
+      return data;
+    },
+    [maxWidth]
+  );
+
+  useEffect(() => {
+    if (updater.current && isOpen) {
+      updater.current();
+    }
+  });
+
+  useEffect(() => {
+    return () => {
+      if (closeTimeout.current) {
+        clearTimeout(closeTimeout.current);
+      }
+    };
+  }, []);
+
+  return (
+    <Manager>
+      <Reference>
+        {({ ref }) => (
+          <span
+            ref={ref}
+            className={className}
+            onClick={handleClick}
+            onMouseEnter={handleMouseEnterAnchor}
+            onMouseLeave={handleMouseLeave}
+          >
+            {anchor}
+          </span>
+        )}
+      </Reference>
+
+      <Portal>
+        <Popper
+          // @ts-expect-error - PopperJS types are not in sync with our position types.
+          placement={position}
+          // Disable events to improve performance when many tooltips
+          // are shown (Quality Definitions for example).
+          eventsEnabled={false}
+          modifiers={{
+            computeMaxHeight: {
+              order: 851,
+              enabled: true,
+              fn: computeMaxSize,
+            },
+            preventOverflow: {
+              // Fixes positioning for tooltips in the queue
+              // and likely others.
+              escapeWithReference: false,
+            },
+            flip: {
+              enabled: canFlip,
+            },
+          }}
+        >
+          {({ ref, style, placement, arrowProps, scheduleUpdate }) => {
+            updater.current = scheduleUpdate;
+
+            const popperPlacement = placement
+              ? placement.split('-')[0]
+              : position;
+            const vertical =
+              popperPlacement === 'top' || popperPlacement === 'bottom';
+
+            return (
+              <div
+                ref={ref}
+                className={classNames(
+                  styles.tooltipContainer,
+                  vertical
+                    ? styles.verticalContainer
+                    : styles.horizontalContainer
+                )}
+                style={style}
+                onMouseEnter={handleMouseEnterTooltip}
+                onMouseLeave={handleMouseLeave}
+              >
+                <div
+                  ref={arrowProps.ref}
+                  className={
+                    isOpen
+                      ? classNames(
+                          styles.arrow,
+                          styles[kind],
+                          // @ts-expect-error - is a string that may not exist in styles
+                          styles[popperPlacement]
+                        )
+                      : styles.arrowDisabled
+                  }
+                  style={arrowProps.style}
+                />
+                {isOpen ? (
+                  <div className={classNames(styles.tooltip, styles[kind])}>
+                    <div className={bodyClassName}>{tooltip}</div>
+                  </div>
+                ) : null}
+              </div>
+            );
+          }}
+        </Popper>
+      </Portal>
+    </Manager>
+  );
+}
+
+export default Tooltip;