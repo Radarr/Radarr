b4eff4d4f9f20a9e4b3b46c9dbd31914c0a238ed
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-11-04 12:31:05 +0200

    Show a movie path as example in Mount Health Check
    Closes #10649

--- src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using NzbDrone.Common.Disk;
 using NzbDrone.Core.Localization;
@@ -21,14 +22,17 @@ namespace NzbDrone.Core.HealthCheck.Checks
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
             var mounts = _movieService.AllMoviePaths()
-                .Select(p => _diskProvider.GetMount(p.Value))
-                .Where(m => m is { MountOptions.IsReadOnly: true })
-                .DistinctBy(m => m.RootDirectory)
+                .Select(p => new Tuple<IMount, string>(_diskProvider.GetMount(p.Value), p.Value))
+                .Where(m => m.Item1 is { MountOptions.IsReadOnly: true })
+                .DistinctBy(m => m.Item1.RootDirectory)
                 .ToList();
 
             if (mounts.Any())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Error, _localizationService.GetLocalizedString("MountCheckMessage") + string.Join(", ", mounts.Select(m => m.Name)), "#movie-mount-ro");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Error,
+                    $"{_localizationService.GetLocalizedString("MountCheckMessage")}{string.Join(", ", mounts.Select(m => $"{m.Item1.Name} ({m.Item2})"))}",
+                    "#movie-mount-ro");
             }
 
             return new HealthCheck(GetType());
