ea86d14ca7d2ca896f2487c22bce5d026ad63602
Mark McDowall <mark@mcdowall.ca>
2024-11-03 16:22:32 -0800

    Fixed: Normalize unicode characters when comparing paths for equality
    (cherry picked from commit ceeec091f85d0094e07537b7f62f18292655a710)

--- src/NzbDrone.Common/Extensions/PathExtensions.cs
+++ src/NzbDrone.Common/Extensions/PathExtensions.cs
@@ -55,6 +55,10 @@ namespace NzbDrone.Common.Extensions
 
         public static bool PathEquals(this string firstPath, string secondPath, StringComparison? comparison = null)
         {
+            // Normalize paths to ensure unicode characters are represented the same way
+            firstPath = firstPath.Normalize();
+            secondPath = secondPath?.Normalize();
+
             if (!comparison.HasValue)
             {
                 comparison = DiskProviderBase.PathStringComparison;
