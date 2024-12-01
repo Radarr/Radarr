Sonarr/Sonarr

New: Labels support for Transmission 4.0
Closes #7300

src/NzbDrone.Core/Download/Clients/Transmission/Transmission.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Configuration;
@@ -15,6 +17,9 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 {
     public class Transmission : TransmissionBase
     {
+        public override string Name => "Transmission";
+        public override bool SupportsLabels => HasClientVersion(4, 0);
+
         public Transmission(ITransmissionProxy proxy,
                             ITorrentFileInfoReader torrentFileInfoReader,
                             IHttpClient httpClient,
@@ -28,9 +33,48 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         {
         }
 
+        public override void MarkItemAsImported(DownloadClientItem downloadClientItem)
+        {
+            if (!SupportsLabels)
+            {
+                throw new NotSupportedException($"{Name} does not support marking items as imported");
+            }
+
+            // set post-import category
+            if (Settings.TvImportedCategory.IsNotNullOrWhiteSpace() &&
+                Settings.TvImportedCategory != Settings.TvCategory)
+            {
+                var hash = downloadClientItem.DownloadId.ToLowerInvariant();
+                var torrent = _proxy.GetTorrents(new[] { hash }, Settings).FirstOrDefault();
+
+                if (torrent == null)
+                {
+                    _logger.Warn("Could not find torrent with hash \"{0}\" in Transmission.", hash);
+                    return;
+                }
+
+                try
+                {
+                    var labels = torrent.Labels.ToHashSet(StringComparer.InvariantCultureIgnoreCase);
+                    labels.Add(Settings.TvImportedCategory);
+
+                    if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+                    {
+                        labels.Remove(Settings.TvCategory);
+                    }
+
+                    _proxy.SetTorrentLabels(hash, labels, Settings);
+                }
+                catch (DownloadClientException ex)
+                {
+                    _logger.Warn(ex, "Failed to set post-import torrent label \"{0}\" for {1} in Transmission.", Settings.TvImportedCategory, downloadClientItem.Title);
+                }
+            }
+        }
+
         protected override ValidationFailure ValidateVersion()
         {
-            var versionString = _proxy.GetClientVersion(Settings);
+            var versionString = _proxy.GetClientVersion(Settings, true);
 
             _logger.Debug("Transmission version information: {0}", versionString);
 
@@ -44,7 +88,5 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 
             return null;
         }
-
-        public override string Name => "Transmission";
     }
 }

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Disk;
@@ -18,6 +19,8 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 {
     public abstract class TransmissionBase : TorrentClientBase<TransmissionSettings>
     {
+        public abstract bool SupportsLabels { get; }
+
         protected readonly ITransmissionProxy _proxy;
 
         public TransmissionBase(ITransmissionProxy proxy,
@@ -37,7 +40,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         public override IEnumerable<DownloadClientItem> GetItems()
         {
             var configFunc = new Lazy<TransmissionConfig>(() => _proxy.GetConfig(Settings));
-            var torrents = _proxy.GetTorrents(Settings);
+            var torrents = _proxy.GetTorrents(null, Settings);
 
             var items = new List<DownloadClientItem>();
 
@@ -45,36 +48,45 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             {
                 var outputPath = new OsPath(torrent.DownloadDir);
 
-                if (Settings.TvDirectory.IsNotNullOrWhiteSpace())
+                if (Settings.TvCategory.IsNotNullOrWhiteSpace() && SupportsLabels && torrent.Labels is { Count: > 0 })
                 {
-                    if (!new OsPath(Settings.TvDirectory).Contains(outputPath))
+                    if (!torrent.Labels.Contains(Settings.TvCategory, StringComparer.InvariantCultureIgnoreCase))
                     {
                         continue;
                     }
                 }
-                else if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+                else
                 {
-                    var directories = outputPath.FullPath.Split('\\', '/');
-                    if (!directories.Contains(Settings.TvCategory))
+                    if (Settings.TvDirectory.IsNotNullOrWhiteSpace())
                     {
-                        continue;
+                        if (!new OsPath(Settings.TvDirectory).Contains(outputPath))
+                        {
+                            continue;
+                        }
+                    }
+                    else if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+                    {
+                        var directories = outputPath.FullPath.Split('\\', '/');
+                        if (!directories.Contains(Settings.TvCategory))
+                        {
+                            continue;
+                        }
                     }
                 }
 
                 outputPath = _remotePathMappingService.RemapRemoteToLocal(Settings.Host, outputPath);
 
-                var item = new DownloadClientItem();
-                item.DownloadId = torrent.HashString.ToUpper();
-                item.Category = Settings.TvCategory;
-                item.Title = torrent.Name;
-
-                item.DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this, false);
-
-                item.OutputPath = GetOutputPath(outputPath, torrent);
-                item.TotalSize = torrent.TotalSize;
-                item.RemainingSize = torrent.LeftUntilDone;
-                item.SeedRatio = torrent.DownloadedEver <= 0 ? 0 :
-                    (double)torrent.UploadedEver / torrent.DownloadedEver;
+                var item = new DownloadClientItem
+                {
+                    DownloadId = torrent.HashString.ToUpper(),
+                    Category = Settings.TvCategory,
+                    Title = torrent.Name,
+                    DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this, Settings.TvImportedCategory.IsNotNullOrWhiteSpace() && SupportsLabels),
+                    OutputPath = GetOutputPath(outputPath, torrent),
+                    TotalSize = torrent.TotalSize,
+                    RemainingSize = torrent.LeftUntilDone,
+                    SeedRatio = torrent.DownloadedEver <= 0 ? 0 : (double)torrent.UploadedEver / torrent.DownloadedEver
+                };
 
                 if (torrent.Eta >= 0)
                 {
@@ -300,7 +312,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         {
             try
             {
-                _proxy.GetTorrents(Settings);
+                _proxy.GetTorrents(null, Settings);
             }
             catch (Exception ex)
             {
@@ -309,6 +321,16 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             }
 
             return null;
+        }
+
+        protected bool HasClientVersion(int major, int minor)
+        {
+            var rawVersion = _proxy.GetClientVersion(Settings);
+
+            var versionResult = Regex.Match(rawVersion, @"(?<!\(|(\d|\.)+)(\d|\.)+(?!\)|(\d|\.)+)").Value;
+            var clientVersion = Version.Parse(versionResult);
+
+            return clientVersion >= new Version(major, minor);
         }
     }
 }

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionSettings.cs
@@ -32,6 +32,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             Host = "localhost";
             Port = 9091;
             UrlBase = "/transmission/";
+            TvCategory = "tv-sonarr";
         }
 
         [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
@@ -59,16 +60,19 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         [FieldDefinition(6, Label = "Category", Type = FieldType.Textbox, HelpText = "DownloadClientSettingsCategorySubFolderHelpText")]
         public string TvCategory { get; set; }
 
-        [FieldDefinition(7, Label = "Directory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientTransmissionSettingsDirectoryHelpText")]
+        [FieldDefinition(7, Label = "PostImportCategory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientSettingsPostImportCategoryHelpText")]
+        public string TvImportedCategory { get; set; }
+
+        [FieldDefinition(8, Label = "Directory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientTransmissionSettingsDirectoryHelpText")]
         public string TvDirectory { get; set; }
 
-        [FieldDefinition(8, Label = "DownloadClientSettingsRecentPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsRecentPriorityEpisodeHelpText")]
+        [FieldDefinition(9, Label = "DownloadClientSettingsRecentPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsRecentPriorityEpisodeHelpText")]
         public int RecentTvPriority { get; set; }
 
-        [FieldDefinition(9, Label = "DownloadClientSettingsOlderPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsOlderPriorityEpisodeHelpText")]
+        [FieldDefinition(10, Label = "DownloadClientSettingsOlderPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsOlderPriorityEpisodeHelpText")]
         public int OlderTvPriority { get; set; }
 
-        [FieldDefinition(10, Label = "DownloadClientSettingsAddPaused", Type = FieldType.Checkbox)]
+        [FieldDefinition(11, Label = "DownloadClientSettingsAddPaused", Type = FieldType.Checkbox)]
         public bool AddPaused { get; set; }
 
         public override NzbDroneValidationResult Validate()

Radarr/Radarr

(cherry picked from commit 675e3cd38a14ea33c27f2d66a4be2bf802e17d88)

src/NzbDrone.Core/Download/Clients/Transmission/Transmission.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Configuration;
@@ -15,6 +17,9 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 {
     public class Transmission : TransmissionBase
     {
+        public override string Name => "Transmission";
+        public override bool SupportsLabels => HasClientVersion(4, 0);
+
         public Transmission(ITransmissionProxy proxy,
                             ITorrentFileInfoReader torrentFileInfoReader,
                             IHttpClient httpClient,
@@ -28,9 +33,48 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         {
         }
 
+        public override void MarkItemAsImported(DownloadClientItem downloadClientItem)
+        {
+            if (!SupportsLabels)
+            {
+                throw new NotSupportedException($"{Name} does not support marking items as imported");
+            }
+
+            // set post-import category
+            if (Settings.MovieImportedCategory.IsNotNullOrWhiteSpace() &&
+                Settings.MovieImportedCategory != Settings.MovieCategory)
+            {
+                var hash = downloadClientItem.DownloadId.ToLowerInvariant();
+                var torrent = _proxy.GetTorrents(new[] { hash }, Settings).FirstOrDefault();
+
+                if (torrent == null)
+                {
+                    _logger.Warn("Could not find torrent with hash \"{0}\" in Transmission.", hash);
+                    return;
+                }
+
+                try
+                {
+                    var labels = torrent.Labels.ToHashSet(StringComparer.InvariantCultureIgnoreCase);
+                    labels.Add(Settings.MovieImportedCategory);
+
+                    if (Settings.MovieCategory.IsNotNullOrWhiteSpace())
+                    {
+                        labels.Remove(Settings.MovieCategory);
+                    }
+
+                    _proxy.SetTorrentLabels(hash, labels, Settings);
+                }
+                catch (DownloadClientException ex)
+                {
+                    _logger.Warn(ex, "Failed to set post-import torrent label \"{0}\" for {1} in Transmission.", Settings.MovieImportedCategory, downloadClientItem.Title);
+                }
+            }
+        }
+
         protected override ValidationFailure ValidateVersion()
         {
-            var versionString = _proxy.GetClientVersion(Settings);
+            var versionString = _proxy.GetClientVersion(Settings, true);
 
             _logger.Debug("Transmission version information: {0}", versionString);
 
@@ -44,7 +88,5 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 
             return null;
         }
-
-        public override string Name => "Transmission";
     }
 }

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Disk;
@@ -18,6 +19,8 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 {
     public abstract class TransmissionBase : TorrentClientBase<TransmissionSettings>
     {
+        public abstract bool SupportsLabels { get; }
+
         protected readonly ITransmissionProxy _proxy;
 
         public TransmissionBase(ITransmissionProxy proxy,
@@ -37,7 +40,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         public override IEnumerable<DownloadClientItem> GetItems()
         {
             var configFunc = new Lazy<TransmissionConfig>(() => _proxy.GetConfig(Settings));
-            var torrents = _proxy.GetTorrents(Settings);
+            var torrents = _proxy.GetTorrents(null, Settings);
 
             var items = new List<DownloadClientItem>();
 
@@ -45,36 +48,45 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             {
                 var outputPath = new OsPath(torrent.DownloadDir);
 
-                if (Settings.MovieDirectory.IsNotNullOrWhiteSpace())
+                if (Settings.MovieCategory.IsNotNullOrWhiteSpace() && SupportsLabels && torrent.Labels is { Count: > 0 })
                 {
-                    if (!new OsPath(Settings.MovieDirectory).Contains(outputPath))
+                    if (!torrent.Labels.Contains(Settings.MovieCategory, StringComparer.InvariantCultureIgnoreCase))
                     {
                         continue;
                     }
                 }
-                else if (Settings.MovieCategory.IsNotNullOrWhiteSpace())
+                else
                 {
-                    var directories = outputPath.FullPath.Split('\\', '/');
-                    if (!directories.Contains(Settings.MovieCategory))
+                    if (Settings.MovieDirectory.IsNotNullOrWhiteSpace())
                     {
-                        continue;
+                        if (!new OsPath(Settings.MovieDirectory).Contains(outputPath))
+                        {
+                            continue;
+                        }
+                    }
+                    else if (Settings.MovieCategory.IsNotNullOrWhiteSpace())
+                    {
+                        var directories = outputPath.FullPath.Split('\\', '/');
+                        if (!directories.Contains(Settings.MovieCategory))
+                        {
+                            continue;
+                        }
                     }
                 }
 
                 outputPath = _remotePathMappingService.RemapRemoteToLocal(Settings.Host, outputPath);
 
-                var item = new DownloadClientItem();
-                item.DownloadId = torrent.HashString.ToUpper();
-                item.Category = Settings.MovieCategory;
-                item.Title = torrent.Name;
-
-                item.DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this, false);
-
-                item.OutputPath = GetOutputPath(outputPath, torrent);
-                item.TotalSize = torrent.TotalSize;
-                item.RemainingSize = torrent.LeftUntilDone;
-                item.SeedRatio = torrent.DownloadedEver <= 0 ? 0 :
-                    (double)torrent.UploadedEver / torrent.DownloadedEver;
+                var item = new DownloadClientItem
+                {
+                    DownloadId = torrent.HashString.ToUpper(),
+                    Category = Settings.MovieCategory,
+                    Title = torrent.Name,
+                    DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this, Settings.MovieImportedCategory.IsNotNullOrWhiteSpace() && SupportsLabels),
+                    OutputPath = GetOutputPath(outputPath, torrent),
+                    TotalSize = torrent.TotalSize,
+                    RemainingSize = torrent.LeftUntilDone,
+                    SeedRatio = torrent.DownloadedEver <= 0 ? 0 : (double)torrent.UploadedEver / torrent.DownloadedEver
+                };
 
                 if (torrent.Eta >= 0)
                 {
@@ -300,7 +312,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         {
             try
             {
-                _proxy.GetTorrents(Settings);
+                _proxy.GetTorrents(null, Settings);
             }
             catch (Exception ex)
             {
@@ -309,6 +321,16 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             }
 
             return null;
+        }
+
+        protected bool HasClientVersion(int major, int minor)
+        {
+            var rawVersion = _proxy.GetClientVersion(Settings);
+
+            var versionResult = Regex.Match(rawVersion, @"(?<!\(|(\d|\.)+)(\d|\.)+(?!\)|(\d|\.)+)").Value;
+            var clientVersion = Version.Parse(versionResult);
+
+            return clientVersion >= new Version(major, minor);
         }
     }
 }

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionSettings.cs
@@ -32,6 +32,7 @@ namespace NzbDrone.Core.Download.Clients.Transmission
             Host = "localhost";
             Port = 9091;
             UrlBase = "/transmission/";
+            MovieCategory = "radarr";
         }
 
         [FieldDefinition(0, Label = "Host", Type = FieldType.Textbox)]
@@ -59,16 +60,19 @@ namespace NzbDrone.Core.Download.Clients.Transmission
         [FieldDefinition(6, Label = "Category", Type = FieldType.Textbox, HelpText = "DownloadClientSettingsCategorySubFolderHelpText")]
         public string MovieCategory { get; set; }
 
-        [FieldDefinition(7, Label = "Directory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientTransmissionSettingsDirectoryHelpText")]
+        [FieldDefinition(7, Label = "PostImportCategory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientSettingsPostImportCategoryHelpText")]
+        public string MovieImportedCategory { get; set; }
+
+        [FieldDefinition(8, Label = "Directory", Type = FieldType.Textbox, Advanced = true, HelpText = "DownloadClientTransmissionSettingsDirectoryHelpText")]
         public string MovieDirectory { get; set; }
 
-        [FieldDefinition(8, Label = "DownloadClientSettingsRecentPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsRecentPriorityMovieHelpText")]
+        [FieldDefinition(9, Label = "DownloadClientSettingsRecentPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsRecentPriorityMovieHelpText")]
         public int RecentMoviePriority { get; set; }
 
-        [FieldDefinition(9, Label = "DownloadClientSettingsOlderPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsOlderPriorityMovieHelpText")]
+        [FieldDefinition(10, Label = "DownloadClientSettingsOlderPriority", Type = FieldType.Select, SelectOptions = typeof(TransmissionPriority), HelpText = "DownloadClientSettingsOlderPriorityMovieHelpText")]
         public int OlderMoviePriority { get; set; }
 
-        [FieldDefinition(10, Label = "DownloadClientSettingsAddPaused", Type = FieldType.Checkbox)]
+        [FieldDefinition(11, Label = "DownloadClientSettingsAddPaused", Type = FieldType.Checkbox)]
         public bool AddPaused { get; set; }
 
         public override NzbDroneValidationResult Validate()