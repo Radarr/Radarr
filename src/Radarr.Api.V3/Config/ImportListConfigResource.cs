205914c59d9d8afb02e6b0fc31fab935afb65e35
Bruce Harrison <leeharrison1984@gmail.com>
2024-12-30 12:10:32 -0600

    update API to accept new property

--- src/Radarr.Api.V3/Config/ImportListConfigResource.cs
+++ src/Radarr.Api.V3/Config/ImportListConfigResource.cs
@@ -6,6 +6,7 @@ namespace Radarr.Api.V3.Config
     public class ImportListConfigResource : RestResource
     {
         public string ListSyncLevel { get; set; }
+        public bool IgnoreRelatedCollectionMoviesDuringListSync { get; set; }
     }
 
     public static class ImportListConfigResourceMapper
@@ -15,6 +16,7 @@ namespace Radarr.Api.V3.Config
             return new ImportListConfigResource
             {
                 ListSyncLevel = model.ListSyncLevel,
+                IgnoreRelatedCollectionMoviesDuringListSync = model.IgnoreRelatedCollectionMoviesDuringListSync
             };
         }
     }
