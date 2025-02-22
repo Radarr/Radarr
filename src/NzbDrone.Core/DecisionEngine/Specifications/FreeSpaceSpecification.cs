b7a46bedb036f6e57a2f2d043ddcbd1659fea82d
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-02-22 21:28:33 +0200

    Fixed: Avoid checking for free space if other specifications fail first

--- src/NzbDrone.Core/DecisionEngine/Specifications/FreeSpaceSpecification.cs
+++ src/NzbDrone.Core/DecisionEngine/Specifications/FreeSpaceSpecification.cs
@@ -21,7 +21,7 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
             _logger = logger;
         }
 
-        public SpecificationPriority Priority => SpecificationPriority.Default;
+        public SpecificationPriority Priority => SpecificationPriority.Disk;
         public RejectionType Type => RejectionType.Permanent;
 
         public DownloadSpecDecision IsSatisfiedBy(RemoteMovie subject, SearchCriteriaBase searchCriteria)
