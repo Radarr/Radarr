bcd4fe1f08cb5af347700a70d087aaeb84ff4d77
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-03-20 13:10:06 +0200

    Fixed: Priority validation for indexers and download clients
    (cherry picked from commit f0e320f3aa501f120721503b8256f464a31be783)

--- src/Radarr.Api.V3/DownloadClient/DownloadClientController.cs
+++ src/Radarr.Api.V3/DownloadClient/DownloadClientController.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using NzbDrone.Core.Download;
 using NzbDrone.SignalR;
 using Radarr.Http;
@@ -13,6 +14,7 @@ namespace Radarr.Api.V3.DownloadClient
         public DownloadClientController(IBroadcastSignalRMessage signalRBroadcaster, IDownloadClientFactory downloadClientFactory)
             : base(signalRBroadcaster, downloadClientFactory, "downloadclient", ResourceMapper, BulkResourceMapper)
         {
+            SharedValidator.RuleFor(c => c.Priority).InclusiveBetween(1, 50);
         }
     }
 }
