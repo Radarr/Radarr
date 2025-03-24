Sonarr/Sonarr

New: Option to treat downloads with non-media extensions as failed
Closes #7369

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -166,6 +166,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                         {
                             trackedDownload.RemoteEpisode.Release.IndexerFlags = flags;
                         }
+
+                        if (downloadHistory != null)
+                        {
+                            trackedDownload.RemoteEpisode.Release.IndexerId = downloadHistory.IndexerId;
+                        }
                     }
                 }
 

Radarr/Radarr

(cherry picked from commit 776143cc813ec1b5fa31fbf8667c3ab174b71f5c)
New: Treat .scr as dangerous file
(cherry picked from commit 103ccd74f30830944e9e9f06d02be096f476ae34)
New: .arj and .lzh extensions are potentially dangerous
(cherry picked from commit a72288a14e67f62b00f921dbeb1a0f57a61e5ba7)
Fixed: Failing dangerous and executable single file downloads
(cherry picked from commit e37684e045310ca543aa6a22b38a325cd8a8e84d)
Fixed: Rejected Imports with no associated release or indexer
(cherry picked from commit 31e02bdeada8c85d67a75b69e57d3e7ea46989c6)
Fixed: Don't return warning in title field for rejected downloads
(cherry picked from commit 1fa532dd3eaaee01ac6a049e43fcdbd44357d617)
Fixed: Improve rejected download handling
(cherry picked from commit 4db43882361232eb8fe9ee5331c3d77ea3aa8dfa)

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -166,6 +166,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                         {
                             trackedDownload.RemoteMovie.Release.IndexerFlags = flags;
                         }
+
+                        if (downloadHistory != null)
+                        {
+                            trackedDownload.RemoteMovie.Release.IndexerId = downloadHistory.IndexerId;
+                        }
                     }
                 }
 