cfd25e974f4e3bd706a5329d201f26b9eaafb618
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-04-15 19:46:59 +0300

    Fixed: Free space and missing for selected root folder value

--- frontend/src/Components/Form/Select/RootFolderSelectInputOption.tsx
+++ frontend/src/Components/Form/Select/RootFolderSelectInputOption.tsx
@@ -18,18 +18,16 @@ interface RootFolderSelectInputOptionProps
   isWindows?: boolean;
 }
 
-function RootFolderSelectInputOption(props: RootFolderSelectInputOptionProps) {
-  const {
-    id,
-    value,
-    freeSpace,
-    isMissing,
-    movieFolder,
-    isMobile,
-    isWindows,
-    ...otherProps
-  } = props;
-
+function RootFolderSelectInputOption({
+  id,
+  value,
+  freeSpace,
+  isMissing,
+  movieFolder,
+  isMobile,
+  isWindows,
+  ...otherProps
+}: RootFolderSelectInputOptionProps) {
   const slashCharacter = isWindows ? '\\' : '/';
 
   return (
