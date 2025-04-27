Sonarr/Sonarr

Convert Table to TypeScript

frontend/src/InteractiveImport/Interactive/InteractiveImportModalContent.tsx
@@ -58,7 +58,7 @@ import {
 } from 'Store/Actions/interactiveImportActions';
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import { SortCallback } from 'typings/callbacks';
-import { SelectStateInputProps } from 'typings/props';
+import { CheckInputChanged } from 'typings/inputs';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
 import hasDifferentItems from 'Utilities/Object/hasDifferentItems';
 import translate from 'Utilities/String/translate';
@@ -431,7 +431,7 @@ function InteractiveImportModalContent(
   }, [previousIsDeleting, isDeleting, deleteError, onModalClose]);
 
   const onSelectAllChange = useCallback(
-    ({ value }: SelectStateInputProps) => {
+    ({ value }: CheckInputChanged) => {
       setSelectState({ type: value ? 'selectAll' : 'unselectAll', items });
     },
     [items, setSelectState]
@@ -449,8 +449,8 @@ function InteractiveImportModalContent(
 
       setWithoutEpisodeFileIdRowsSelected(
         hasEpisodeFileId || !value
-          ? without(withoutEpisodeFileIdRowsSelected, id)
-          : [...withoutEpisodeFileIdRowsSelected, id]
+          ? without(withoutEpisodeFileIdRowsSelected, id as number)
+          : [...withoutEpisodeFileIdRowsSelected, id as number]
       );
     },
     [

Radarr/Radarr

(cherry picked from commit 699120a8fd54be9e70fb9a83298f94c8cb6a80bb)

frontend/src/InteractiveImport/Interactive/InteractiveImportModalContent.tsx
@@ -53,7 +53,7 @@ import {
 } from 'Store/Actions/movieFileActions';
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import { SortCallback } from 'typings/callbacks';
-import { SelectStateInputProps } from 'typings/props';
+import { CheckInputChanged } from 'typings/inputs';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
 import translate from 'Utilities/String/translate';
 import getSelectedIds from 'Utilities/Table/getSelectedIds';
@@ -360,7 +360,7 @@ function InteractiveImportModalContent(
   }, [previousIsDeleting, isDeleting, deleteError, onModalClose]);
 
   const onSelectAllChange = useCallback(
-    ({ value }: SelectStateInputProps) => {
+    ({ value }: CheckInputChanged) => {
       setSelectState({ type: value ? 'selectAll' : 'unselectAll', items });
     },
     [items, setSelectState]
@@ -378,8 +378,8 @@ function InteractiveImportModalContent(
 
       setWithoutMovieFileIdRowsSelected(
         hasMovieFileId || !value
-          ? without(withoutMovieFileIdRowsSelected, id)
-          : [...withoutMovieFileIdRowsSelected, id]
+          ? without(withoutMovieFileIdRowsSelected, id as number)
+          : [...withoutMovieFileIdRowsSelected, id as number]
       );
     },
     [