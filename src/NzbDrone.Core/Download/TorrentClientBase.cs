a10759c7e983ae71980a61f0049d0fa332476872
carrossos <99329036+carrossos@users.noreply.github.com>
2025-05-22 07:57:11 +0700

    Treat HTTP 410 response for failed download similarly to HTTP 404
    (cherry picked from commit 818ae02a7a8f0a8ea0a44e0015e2667d96453332)

--- src/NzbDrone.Core/Download/TorrentClientBase.cs
+++ src/NzbDrone.Core/Download/TorrentClientBase.cs
@@ -172,7 +172,7 @@ namespace NzbDrone.Core.Download
             }
             catch (HttpException ex)
             {
-                if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+                if (ex.Response.StatusCode is HttpStatusCode.NotFound or HttpStatusCode.Gone)
                 {
                     _logger.Error(ex, "Downloading torrent file for movie '{0}' failed since it no longer exists ({1})", remoteMovie.Release.Title, torrentUrl);
                     throw new ReleaseUnavailableException(remoteMovie.Release, "Downloading torrent failed", ex);
--- src/NzbDrone.Core/Download/UsenetClientBase.cs
+++ src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -59,7 +59,7 @@ namespace NzbDrone.Core.Download
             }
             catch (HttpException ex)
             {
-                if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+                if (ex.Response.StatusCode is HttpStatusCode.NotFound or HttpStatusCode.Gone)
                 {
                     _logger.Error(ex, "Downloading nzb file for movie '{0}' failed since it no longer exists ({1})", remoteMovie.Release.Title, url);
                     throw new ReleaseUnavailableException(remoteMovie.Release, "Downloading nzb failed", ex);
