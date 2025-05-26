2235823af313ea1f39fd1189b69a75fc5d380c41
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-26 17:06:46 +0300

    New: Bump to .NET 8
    Co-authored-by: Qstick <qstick@gmail.com>

--- src/Radarr.Http/Authentication/NoAuthenticationHandler.cs
+++ src/Radarr.Http/Authentication/NoAuthenticationHandler.cs
@@ -13,9 +13,8 @@ namespace Radarr.Http.Authentication
     {
         public NoAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
             ILoggerFactory logger,
-            UrlEncoder encoder,
-            ISystemClock clock)
-            : base(options, logger, encoder, clock)
+            UrlEncoder encoder)
+            : base(options, logger, encoder)
         {
         }
 
