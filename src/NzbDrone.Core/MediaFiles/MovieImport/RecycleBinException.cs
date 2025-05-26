bd2ea54c270e28d9564070b7b14a3480dbb82738
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-26 17:06:46 +0300

    New: Bump to .NET 8
    Co-authored-by: Qstick <qstick@gmail.com>

--- src/NzbDrone.Core/MediaFiles/MovieImport/RecycleBinException.cs
+++ src/NzbDrone.Core/MediaFiles/MovieImport/RecycleBinException.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Runtime.Serialization;
 
 namespace NzbDrone.Core.MediaFiles.MovieImport
 {
@@ -17,11 +16,6 @@ namespace NzbDrone.Core.MediaFiles.MovieImport
 
         public RecycleBinException(string message, Exception innerException)
             : base(message, innerException)
-        {
-        }
-
-        protected RecycleBinException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
