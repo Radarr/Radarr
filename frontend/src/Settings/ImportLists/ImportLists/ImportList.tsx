Sonarr/Sonarr

Convert ImportLists to TypeScript

frontend/src/Settings/ImportLists/ImportLists/ImportList.tsx
@@ -1,0 +1,104 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import Card from 'Components/Card';
+import Label from 'Components/Label';
+import ConfirmModal from 'Components/Modal/ConfirmModal';
+import TagList from 'Components/TagList';
+import { kinds } from 'Helpers/Props';
+import { deleteImportList } from 'Store/Actions/settingsActions';
+import useTags from 'Tags/useTags';
+import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
+import translate from 'Utilities/String/translate';
+import EditImportListModal from './EditImportListModal';
+import styles from './ImportList.css';
+
+interface ImportListProps {
+  id: number;
+  name: string;
+  enableAutomaticAdd: boolean;
+  tags: number[];
+  minRefreshInterval: string;
+}
+
+function ImportList({
+  id,
+  name,
+  enableAutomaticAdd,
+  tags,
+  minRefreshInterval,
+}: ImportListProps) {
+  const dispatch = useDispatch();
+  const tagList = useTags();
+
+  const [isEditImportListModalOpen, setIsEditImportListModalOpen] =
+    useState(false);
+
+  const [isDeleteImportListModalOpen, setIsDeleteImportListModalOpen] =
+    useState(false);
+
+  const handleEditImportListPress = useCallback(() => {
+    setIsEditImportListModalOpen(true);
+  }, []);
+
+  const handleEditImportListModalClose = useCallback(() => {
+    setIsEditImportListModalOpen(false);
+  }, []);
+
+  const handleDeleteImportListPress = useCallback(() => {
+    setIsEditImportListModalOpen(false);
+    setIsDeleteImportListModalOpen(true);
+  }, []);
+
+  const handleDeleteImportListModalClose = useCallback(() => {
+    setIsDeleteImportListModalOpen(false);
+  }, []);
+
+  const handleConfirmDeleteImportList = useCallback(() => {
+    dispatch(deleteImportList({ id }));
+  }, [id, dispatch]);
+
+  return (
+    <Card
+      className={styles.list}
+      overlayContent={true}
+      onPress={handleEditImportListPress}
+    >
+      <div className={styles.name}>{name}</div>
+
+      <div className={styles.enabled}>
+        {enableAutomaticAdd ? (
+          <Label kind={kinds.SUCCESS}>{translate('AutomaticAdd')}</Label>
+        ) : null}
+      </div>
+
+      <TagList tags={tags} tagList={tagList} />
+
+      <div className={styles.enabled}>
+        <Label kind={kinds.DEFAULT} title="List Refresh Interval">
+          {`${translate('Refresh')}: ${formatShortTimeSpan(
+            minRefreshInterval
+          )}`}
+        </Label>
+      </div>
+
+      <EditImportListModal
+        id={id}
+        isOpen={isEditImportListModalOpen}
+        onModalClose={handleEditImportListModalClose}
+        onDeleteImportListPress={handleDeleteImportListPress}
+      />
+
+      <ConfirmModal
+        isOpen={isDeleteImportListModalOpen}
+        kind={kinds.DANGER}
+        title={translate('DeleteImportList')}
+        message={translate('DeleteImportListMessageText', { name })}
+        confirmLabel={translate('Delete')}
+        onConfirm={handleConfirmDeleteImportList}
+        onCancel={handleDeleteImportListModalClose}
+      />
+    </Card>
+  );
+}
+
+export default ImportList;

Radarr/Radarr

(cherry picked from commit 10e3a237ef972540abcf4348bb56973d7ee19bd7)

frontend/src/Settings/ImportLists/ImportLists/ImportList.tsx
@@ -1,0 +1,114 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import Card from 'Components/Card';
+import Label from 'Components/Label';
+import ConfirmModal from 'Components/Modal/ConfirmModal';
+import TagList from 'Components/TagList';
+import { kinds } from 'Helpers/Props';
+import { deleteImportList } from 'Store/Actions/settingsActions';
+import useTags from 'Tags/useTags';
+import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
+import translate from 'Utilities/String/translate';
+import EditImportListModal from './EditImportListModal';
+import styles from './ImportList.css';
+
+interface ImportListProps {
+  id: number;
+  name: string;
+  enabled: boolean;
+  enableAuto: boolean;
+  tags: number[];
+  minRefreshInterval: string;
+}
+
+function ImportList({
+  id,
+  name,
+  enabled,
+  enableAuto,
+  tags,
+  minRefreshInterval,
+}: ImportListProps) {
+  const dispatch = useDispatch();
+  const tagList = useTags();
+
+  const [isEditImportListModalOpen, setIsEditImportListModalOpen] =
+    useState(false);
+
+  const [isDeleteImportListModalOpen, setIsDeleteImportListModalOpen] =
+    useState(false);
+
+  const handleEditImportListPress = useCallback(() => {
+    setIsEditImportListModalOpen(true);
+  }, []);
+
+  const handleEditImportListModalClose = useCallback(() => {
+    setIsEditImportListModalOpen(false);
+  }, []);
+
+  const handleDeleteImportListPress = useCallback(() => {
+    setIsEditImportListModalOpen(false);
+    setIsDeleteImportListModalOpen(true);
+  }, []);
+
+  const handleDeleteImportListModalClose = useCallback(() => {
+    setIsDeleteImportListModalOpen(false);
+  }, []);
+
+  const handleConfirmDeleteImportList = useCallback(() => {
+    dispatch(deleteImportList({ id }));
+  }, [id, dispatch]);
+
+  return (
+    <Card
+      className={styles.list}
+      overlayContent={true}
+      onPress={handleEditImportListPress}
+    >
+      <div className={styles.name}>{name}</div>
+
+      <div className={styles.enabled}>
+        {enabled ? (
+          <Label kind={kinds.SUCCESS}>{translate('Enabled')}</Label>
+        ) : (
+          <Label kind={kinds.DISABLED} outline={true}>
+            {translate('Disabled')}
+          </Label>
+        )}
+
+        {enableAuto ? (
+          <Label kind={kinds.SUCCESS}>{translate('AutomaticAdd')}</Label>
+        ) : null}
+      </div>
+
+      <TagList tags={tags} tagList={tagList} />
+
+      <div className={styles.enabled}>
+        <Label kind={kinds.DEFAULT} title="List Refresh Interval">
+          {`${translate('Refresh')}: ${formatShortTimeSpan(
+            minRefreshInterval
+          )}`}
+        </Label>
+      </div>
+
+      <EditImportListModal
+        id={id}
+        isOpen={isEditImportListModalOpen}
+        onModalClose={handleEditImportListModalClose}
+        onDeleteImportListPress={handleDeleteImportListPress}
+      />
+
+      <ConfirmModal
+        isOpen={isDeleteImportListModalOpen}
+        kind={kinds.DANGER}
+        title={translate('DeleteImportList')}
+        message={translate('DeleteImportListMessageText', { name })}
+        confirmLabel={translate('Delete')}
+        onConfirm={handleConfirmDeleteImportList}
+        onCancel={handleDeleteImportListModalClose}
+      />
+    </Card>
+  );
+}
+
+export default ImportList;