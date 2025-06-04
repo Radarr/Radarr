Sonarr/Sonarr

Convert Media Management settings to TypeScript

frontend/src/typings/Settings/MediaManagement.ts
@@ -1,0 +1,22 @@
+export default interface MediaManagement {
+  autoUnmonitorPreviouslyDownloadedEpisodes: boolean;
+  recycleBin: string;
+  recycleBinCleanupDays: number;
+  downloadPropersAndRepacks: string;
+  createEmptySeriesFolders: boolean;
+  deleteEmptyFolders: boolean;
+  fileDate: string;
+  rescanAfterRefresh: string;
+  setPermissionsLinux: boolean;
+  chmodFolder: string;
+  chownGroup: string;
+  episodeTitleRequired: string;
+  skipFreeSpaceCheckWhenImporting: boolean;
+  minimumFreeSpaceWhenImporting: number;
+  copyUsingHardlinks: boolean;
+  useScriptImport: boolean;
+  scriptImportPath: string;
+  importExtraFiles: boolean;
+  extraFileExtensions: string;
+  enableMediaInfo: boolean;
+}

Radarr/Radarr

(cherry picked from commit 27f81117ed188712600d8daf3ccb5121f9808458)

frontend/src/typings/Settings/MediaManagement.ts
@@ -1,0 +1,21 @@
+export default interface MediaManagement {
+  autoUnmonitorPreviouslyDownloadedMovies: boolean;
+  recycleBin: string;
+  recycleBinCleanupDays: number;
+  downloadPropersAndRepacks: string;
+  createEmptyMovieFolders: boolean;
+  deleteEmptyFolders: boolean;
+  fileDate: string;
+  rescanAfterRefresh: string;
+  setPermissionsLinux: boolean;
+  chmodFolder: string;
+  chownGroup: string;
+  skipFreeSpaceCheckWhenImporting: boolean;
+  minimumFreeSpaceWhenImporting: number;
+  copyUsingHardlinks: boolean;
+  useScriptImport: boolean;
+  scriptImportPath: string;
+  importExtraFiles: boolean;
+  extraFileExtensions: string;
+  enableMediaInfo: boolean;
+}