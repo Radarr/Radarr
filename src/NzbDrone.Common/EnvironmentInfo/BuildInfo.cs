b6837de9f4bf1ed3e59902310d474530b1a07239
admin <admin@ardentleatherworks.com>
2025-12-17 17:45:37 -0600

    feat: initial project branding and setup

--- src/NzbDrone.Common/EnvironmentInfo/BuildInfo.cs
+++ src/NzbDrone.Common/EnvironmentInfo/BuildInfo.cs
@@ -26,7 +26,7 @@ namespace NzbDrone.Common.EnvironmentInfo
             Release = $"{Version}-{Branch}";
         }
 
-        public static string AppName { get; } = "Radarr";
+        public static string AppName { get; } = "Logarr";
 
         public static Version Version { get; }
         public static string Branch { get; }
