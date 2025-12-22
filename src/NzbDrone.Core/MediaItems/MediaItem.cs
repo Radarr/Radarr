10c333a7d3e5ecc03d51f8e029dc4a0b1f3cc7c1
Cody Kickertz <cody.kickertz@pm.me>
2025-12-21 18:14:41 -0600

    refactor(core): create MediaItem abstract base class (#117)
    * feat(db): add MediaType discriminator to Movies table
    
    Adds foundation for multi-media support by:
    - Adding MediaType column to Movies table (migration 244)
    - Adding MediaType property to Movie entity, defaulting to Movie
    
    Existing movies will have MediaType=1 (Movie) after migration.
    This prepares for future Book and Audiobook media types.
    
    Addresses Issue #1
    
    * refactor(core): create MediaItem abstract base class
    
    Extracts common properties from Movie into a new MediaItem base class:
    - MediaType, Monitored, QualityProfileId
    - Path, RootFolderPath, Added, Tags, LastSearchTime
    
    Movie now inherits from MediaItem and implements abstract methods
    GetTitle() and GetYear() while maintaining backward-compatible
    Title/Year property accessors.
    
    This prepares for Book and Audiobook entities that will share
    the same base structure.
    
    Addresses Issue #1
    
    ---------
    
    Co-authored-by: admin <admin@ardentleatherworks.com>

---
+++ src/NzbDrone.Core/MediaItems/MediaItem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.MediaTypes;
+
+namespace NzbDrone.Core.MediaItems
+{
+    public abstract class MediaItem : ModelBase
+    {
+        protected MediaItem()
+        {
+            Tags = new HashSet<int>();
+        }
+
+        public MediaType MediaType { get; set; }
+        public bool Monitored { get; set; }
+        public int QualityProfileId { get; set; }
+        public string Path { get; set; }
+        public string RootFolderPath { get; set; }
+        public DateTime Added { get; set; }
+        public HashSet<int> Tags { get; set; }
+        public DateTime? LastSearchTime { get; set; }
+
+        public abstract string GetTitle();
+        public abstract int GetYear();
+    }
+}
