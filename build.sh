a8203d47ed14a381275078eb892083ecf0d7e844
realzombee <neoyoda42@protonmail.com>
2026-01-28 01:50:09 +0000

    Fix build (#1)
    Co-authored-by: realzombee <209545148+realzombee@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -377,11 +377,11 @@ then
     Build
     if [[ -z "$RID" || -z "$FRAMEWORK" ]];
     then
-        PackageTests "net8.0" "win-x64"
-        PackageTests "net8.0" "win-x86"
+#        PackageTests "net8.0" "win-x64"
+#        PackageTests "net8.0" "win-x86"
         PackageTests "net8.0" "linux-x64"
         PackageTests "net8.0" "linux-musl-x64"
-        PackageTests "net8.0" "osx-x64"
+#        PackageTests "net8.0" "osx-x64"
         if [ "$ENABLE_EXTRA_PLATFORMS" = "YES" ];
         then
             PackageTests "net8.0" "freebsd-x64"
@@ -412,16 +412,16 @@ then
 
     if [[ -z "$RID" || -z "$FRAMEWORK" ]];
     then
-        Package "net8.0" "win-x64"
-        Package "net8.0" "win-x86"
+#        Package "net8.0" "win-x64"
+#        Package "net8.0" "win-x86"
         Package "net8.0" "linux-x64"
         Package "net8.0" "linux-musl-x64"
         Package "net8.0" "linux-arm64"
         Package "net8.0" "linux-musl-arm64"
         Package "net8.0" "linux-arm"
         Package "net8.0" "linux-musl-arm"
-        Package "net8.0" "osx-x64"
-        Package "net8.0" "osx-arm64"
+#        Package "net8.0" "osx-x64"
+#        Package "net8.0" "osx-arm64"
         if [ "$ENABLE_EXTRA_PLATFORMS" = "YES" ];
         then
             Package "net8.0" "freebsd-x64"
