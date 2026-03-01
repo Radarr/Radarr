dbcb327a1134d26d2feb9b4589ee8c7b2dafb1ae
Yashizzle <16638776+Yashizzle@users.noreply.github.com>
2026-03-02 04:48:15 +1100

    New: Include External IDs for movies with links
    (cherry picked from commit 7f971d47ac032c8f5731303b3a29dac5cf90c9d3)

--- frontend/src/Movie/Details/MovieDetailsLinks.tsx
+++ frontend/src/Movie/Details/MovieDetailsLinks.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Label from 'Components/Label';
+import React, { useMemo } from 'react';
+import Label, { LabelProps } from 'Components/Label';
+import ClipboardButton from 'Components/Link/ClipboardButton';
 import Link from 'Components/Link/Link';
 import { kinds, sizes } from 'Helpers/Props';
 import Movie from 'Movie/Movie';
@@ -11,116 +12,99 @@ type MovieDetailsLinksProps = Pick<
   'tmdbId' | 'imdbId' | 'youTubeTrailerId'
 >;
 
+interface MovieDetailsLink {
+  externalId: string | number;
+  name: string;
+  url: string;
+  kind?: LabelProps['kind'];
+}
+
 function MovieDetailsLinks(props: MovieDetailsLinksProps) {
   const { tmdbId, imdbId, youTubeTrailerId } = props;
 
+  const links = useMemo(() => {
+    const validLinks: MovieDetailsLink[] = [];
+
+    if (tmdbId) {
+      validLinks.push(
+        {
+          externalId: tmdbId,
+          name: 'TMDb',
+          url: `https://www.themoviedb.org/movie/${tmdbId}`,
+        },
+        {
+          externalId: tmdbId,
+          name: 'Letterboxd',
+          url: `https://letterboxd.com/tmdb/${tmdbId}`,
+        }
+      );
+    }
+
+    if (imdbId) {
+      validLinks.push(
+        {
+          externalId: imdbId,
+          name: 'IMDb',
+          url: `https://imdb.com/title/${imdbId}/`,
+        },
+        {
+          externalId: tmdbId,
+          name: 'Trakt',
+          url: `https://trakt.tv/movies/${imdbId}`,
+        },
+        {
+          externalId: imdbId,
+          name: 'Movie Chat',
+          url: `https://moviechat.org/${imdbId}/`,
+        },
+        {
+          externalId: imdbId,
+          name: 'MDBList',
+          url: `https://mdblist.com/movie/${imdbId}`,
+        },
+        {
+          externalId: imdbId,
+          name: 'Blu-ray',
+          url: `https://www.blu-ray.com/search/?quicksearch=1&quicksearch_keyword=${imdbId}&section=theatrical`,
+        }
+      );
+    }
+
+    if (youTubeTrailerId) {
+      validLinks.push({
+        externalId: youTubeTrailerId,
+        name: translate('Trailer'),
+        url: `https://www.youtube.com/watch?v=${youTubeTrailerId}`,
+        kind: kinds.DANGER,
+      });
+    }
+
+    return validLinks;
+  }, [tmdbId, imdbId, youTubeTrailerId]);
+
   return (
     <div className={styles.links}>
-      <Link
-        className={styles.link}
-        to={`https://www.themoviedb.org/movie/${tmdbId}`}
-      >
-        <Label
-          className={styles.linkLabel}
-          kind={kinds.INFO}
-          size={sizes.LARGE}
-        >
-          {translate('TMDb')}
-        </Label>
-      </Link>
-
-      {imdbId ? (
-        <Link className={styles.link} to={`https://trakt.tv/movies/${imdbId}`}>
-          <Label
-            className={styles.linkLabel}
-            kind={kinds.INFO}
-            size={sizes.LARGE}
-          >
-            {translate('Trakt')}
-          </Label>
-        </Link>
-      ) : null}
-
-      <Link
-        className={styles.link}
-        to={`https://letterboxd.com/tmdb/${tmdbId}`}
-      >
-        <Label
-          className={styles.linkLabel}
-          kind={kinds.INFO}
-          size={sizes.LARGE}
-        >
-          {translate('Letterboxd')}
-        </Label>
-      </Link>
-
-      {imdbId ? (
-        <>
-          <Link
-            className={styles.link}
-            to={`https://imdb.com/title/${imdbId}/`}
-          >
+      {links.map((link) => (
+        <div key={link.name} className={styles.linkBlock}>
+          <Link className={styles.link} to={link.url}>
             <Label
               className={styles.linkLabel}
-              kind={kinds.INFO}
+              kind={link.kind ?? kinds.INFO}
               size={sizes.LARGE}
             >
-              {translate('IMDb')}
+              {link.name}
             </Label>
           </Link>
 
-          <Link className={styles.link} to={`https://moviechat.org/${imdbId}/`}>
-            <Label
-              className={styles.linkLabel}
-              kind={kinds.INFO}
-              size={sizes.LARGE}
-            >
-              {translate('MovieChat')}
-            </Label>
-          </Link>
-
-          <Link
-            className={styles.link}
-            to={`https://mdblist.com/movie/${imdbId}`}
-          >
-            <Label
-              className={styles.linkLabel}
-              kind={kinds.INFO}
-              size={sizes.LARGE}
-            >
-              MDBList
-            </Label>
-          </Link>
-
-          <Link
-            className={styles.link}
-            to={`https://www.blu-ray.com/search/?quicksearch=1&quicksearch_keyword=${imdbId}&section=theatrical`}
-          >
-            <Label
-              className={styles.linkLabel}
-              kind={kinds.INFO}
-              size={sizes.LARGE}
-            >
-              Blu-ray
-            </Label>
-          </Link>
-        </>
-      ) : null}
-
-      {youTubeTrailerId ? (
-        <Link
-          className={styles.link}
-          to={`https://www.youtube.com/watch?v=${youTubeTrailerId}`}
-        >
-          <Label
-            className={styles.linkLabel}
-            kind={kinds.DANGER}
-            size={sizes.LARGE}
-          >
-            {translate('Trailer')}
-          </Label>
-        </Link>
-      ) : null}
+          <ClipboardButton
+            value={`${link.externalId}`}
+            title={translate('CopyToClipboard')}
+            kind={kinds.DEFAULT}
+            size={sizes.SMALL}
+            label={link.externalId}
+          />
+        </div>
+      ))}
     </div>
   );
 }
