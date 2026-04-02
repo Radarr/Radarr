45a8537dc4f6096bbfdf633d7c60199e04f7d61d
scphantm <scphantm@gmail.com>
2026-04-01 22:01:49 -0400

    Refactor ImportListSyncService to adjust auto-import logic

--- src/NzbDrone.Core/ImportLists/ImportListSyncService.cs
+++ src/NzbDrone.Core/ImportLists/ImportListSyncService.cs
@@ -76,7 +76,7 @@ namespace NzbDrone.Core.ImportLists
 
         private void ProcessMovieReport(ImportListDefinition importList, ImportListMovie report, List<ImportListExclusion> listExclusions, List<int> dbMovies, List<Movie> moviesToAdd)
         {
-            if (report.TmdbId == 0 || !importList.EnableAuto)
+            if (report.TmdbId == 0)
             {
                 return;
             }
@@ -86,6 +86,12 @@ namespace NzbDrone.Core.ImportLists
             {
                 RetroApplyTags(importList, report);
                 _logger.Debug("{0} [{1}] Rejected, Movie Exists in DB", report.TmdbId, report.Title);
+                return;
+            }
+
+            // Now that retro-tags are applied, end if auto import is disabled
+            if (!importList.EnableAuto)
+            {
                 return;
             }
 
