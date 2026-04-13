658cb30ebcb75e9b8e6d53885df27a29a2ff7a95
Foireatout <foireatout@gmail.com>
2026-04-13 15:16:48 +0200

    Update TorrentFreeboxDownload.cs

--- src/NzbDrone.Core/Download/Clients/FreeboxDownload/TorrentFreeboxDownload.cs
+++ src/NzbDrone.Core/Download/Clients/FreeboxDownload/TorrentFreeboxDownload.cs
@@ -126,15 +126,17 @@ namespace NzbDrone.Core.Download.Clients.FreeboxDownload
 
             return queueItems;
         }
-
+        
         protected override string AddFromMagnetLink(RemoteMovie remoteMovie, string hash, string magnetLink)
         {
             return _proxy.AddTaskFromUrl(magnetLink, GetDownloadDirectory(remoteMovie).EncodeBase64(), ToBePaused(), ToBeQueuedFirst(remoteMovie), GetSeedRatio(remoteMovie), Settings);
         }
+        
         protected override string AddFromTorrentFile(RemoteMovie remoteMovie, string hash, string filename, byte[] fileContent)
         {
             return _proxy.AddTaskFromFile(filename, fileContent, GetDownloadDirectory(remoteMovie).EncodeBase64(), ToBePaused(), ToBeQueuedFirst(remoteMovie), GetSeedRatio(remoteMovie), Settings);
         }
+        
         public override void RemoveItem(DownloadClientItem item, bool deleteData)
         {
             _proxy.DeleteTask(item.DownloadId, deleteData, Settings);
