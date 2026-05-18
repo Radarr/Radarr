Sonarr/Sonarr

New: Simkl Anime List integration
Co-authored-by: iceypotato <nickyjedi@gmail.com>
Closes #5635

src/NzbDrone.Core/ImportLists/Simkl/SimklParser.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Net;
+using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Instrumentation;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.ImportLists.Exceptions;
 using NzbDrone.Core.Parser.Model;
@@ -10,6 +12,11 @@ namespace NzbDrone.Core.ImportLists.Simkl
     public class SimklParser : IParseImportListResponse
     {
         private ImportListResponse _importResponse;
+        private static readonly Logger Logger = NzbDroneLogger.GetLogger(typeof(SimklParser));
+
+        public SimklParser()
+        {
+        }
 
         public virtual IList<ImportListItemInfo> ParseResponse(ImportListResponse importResponse)
         {
@@ -22,7 +29,7 @@ namespace NzbDrone.Core.ImportLists.Simkl
                 return series;
             }
 
-            var jsonResponse = STJson.Deserialize<SimklResponse>(_importResponse.Content);
+            var jsonResponse = Json.Deserialize<SimklResponse>(_importResponse.Content);
 
             // no shows were return
             if (jsonResponse == null)
@@ -30,14 +37,40 @@ namespace NzbDrone.Core.ImportLists.Simkl
                 return series;
             }
 
-            foreach (var show in jsonResponse.Shows)
+            if (jsonResponse.Anime != null)
             {
-                series.AddIfNotNull(new ImportListItemInfo()
+                foreach (var show in jsonResponse.Anime)
                 {
-                    Title = show.Show.Title,
-                    TvdbId = int.TryParse(show.Show.Ids.Tvdb, out var tvdbId) ? tvdbId : 0,
-                    ImdbId = show.Show.Ids.Imdb
-                });
+                    var tentativeTvdbId = int.TryParse(show.Show.Ids.Tvdb, out var tvdbId) ? tvdbId : 0;
+
+                    if (tentativeTvdbId > 0 && show.AnimeType == SimklAnimeType.Tv)
+                    {
+                        series.AddIfNotNull(new ImportListItemInfo()
+                        {
+                            Title = show.Show.Title,
+                            ImdbId = show.Show.Ids.Imdb,
+                            TvdbId = tvdbId,
+                            MalId = int.TryParse(show.Show.Ids.Mal, out var malId) ? malId : 0
+                        });
+                    }
+                    else
+                    {
+                        Logger.Warn("Skipping info grabbing for '{0}' because it is a movie or it is not the first season of the show", show.Show.Title);
+                    }
+                }
+            }
+
+            if (jsonResponse.Shows != null)
+            {
+                foreach (var show in jsonResponse.Shows)
+                {
+                    series.AddIfNotNull(new ImportListItemInfo()
+                    {
+                        Title = show.Show.Title,
+                        TvdbId = int.TryParse(show.Show.Ids.Tvdb, out var tvdbId) ? tvdbId : 0,
+                        ImdbId = show.Show.Ids.Imdb
+                    });
+                }
             }
 
             return series;

src/NzbDrone.Core/ImportLists/Simkl/User/SimklUserRequestGenerator.cs
@@ -21,7 +21,7 @@ namespace NzbDrone.Core.ImportLists.Simkl.User
 
         private IEnumerable<ImportListRequest> GetSeriesRequest()
         {
-            var link = $"{Settings.BaseUrl.Trim()}/sync/all-items/shows/{((SimklUserListType)Settings.ListType).ToString().ToLowerInvariant()}";
+            var link = $"{Settings.BaseUrl.Trim()}/sync/all-items/{((SimklUserShowType)Settings.ShowType).ToString().ToLowerInvariant()}/{((SimklUserListType)Settings.ListType).ToString().ToLowerInvariant()}";
 
             var request = new ImportListRequest(link, HttpAccept.Json);
 

Radarr/Radarr

(cherry picked from commit b779c92200b8e14398061199535bb3eb0841f626)
Fixed: Importing additional Anime Types from Simkl
(cherry picked from commit cac101bdeea0dec54e17e418d7ca024ce4516fc4)

src/NzbDrone.Core/ImportLists/Simkl/SimklParser.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Net;
+using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Instrumentation;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.ImportLists.Exceptions;
 using NzbDrone.Core.ImportLists.ImportListMovies;
@@ -10,41 +12,61 @@ namespace NzbDrone.Core.ImportLists.Simkl
     public class SimklParser : IParseImportListResponse
     {
         private ImportListResponse _importResponse;
-
-        public SimklParser()
-        {
-        }
+        private static readonly Logger Logger = NzbDroneLogger.GetLogger(typeof(SimklParser));
 
         public virtual IList<ImportListMovie> ParseResponse(ImportListResponse importResponse)
         {
             _importResponse = importResponse;
 
-            var movie = new List<ImportListMovie>();
+            var movies = new List<ImportListMovie>();
 
             if (!PreProcess(_importResponse))
             {
-                return movie;
+                return movies;
             }
 
-            var jsonResponse = STJson.Deserialize<SimklResponse>(_importResponse.Content);
+            var jsonResponse = Json.Deserialize<SimklResponse>(_importResponse.Content);
 
-            // no shows were return
+            // no movies were returned
             if (jsonResponse == null)
             {
-                return movie;
+                return movies;
             }
 
-            foreach (var show in jsonResponse.Movies)
+            if (jsonResponse.Anime != null)
             {
-                movie.AddIfNotNull(new ImportListMovie()
+                foreach (var movie in jsonResponse.Anime)
                 {
-                    Title = show.Movie.Title,
-                    TmdbId = int.TryParse(show.Movie.Ids.Tmdb, out var tmdbId) ? tmdbId : 0,
-                    ImdbId = show.Movie.Ids.Imdb
-                });
+                    if (int.TryParse(movie.Show.Ids.Tmdb, out var tmdbId) && tmdbId > 0 && movie.AnimeType is SimklAnimeType.Movie)
+                    {
+                        movies.AddIfNotNull(new ImportListMovie
+                        {
+                            Title = movie.Show.Title,
+                            ImdbId = movie.Show.Ids.Imdb,
+                            TmdbId = tmdbId,
+                        });
+                    }
+                    else
+                    {
+                        Logger.Warn("Skipping info grabbing for '{0}' because it is an unsupported content type.", movie.Show.Title);
+                    }
+                }
             }
 
-            return movie;
+            if (jsonResponse.Movies != null)
+            {
+                foreach (var movie in jsonResponse.Movies)
+                {
+                    movies.AddIfNotNull(new ImportListMovie
+                    {
+                        Title = movie.Movie.Title,
+                        TmdbId = int.TryParse(movie.Movie.Ids.Tmdb, out var tmdbId) ? tmdbId : 0,
+                        ImdbId = movie.Movie.Ids.Imdb,
+                    });
+                }
+            }
+
+            return movies;
         }
 
         protected virtual bool PreProcess(ImportListResponse netImportResponse)

src/NzbDrone.Core/ImportLists/Simkl/User/SimklUserRequestGenerator.cs
@@ -10,10 +10,6 @@ namespace NzbDrone.Core.ImportLists.Simkl.User
 
         public string ClientId { get; set; }
 
-        public SimklUserRequestGenerator()
-        {
-        }
-
         public virtual ImportListPageableRequestChain GetMovies()
         {
             var pageableRequests = new ImportListPageableRequestChain();
@@ -25,7 +21,7 @@ namespace NzbDrone.Core.ImportLists.Simkl.User
 
         private IEnumerable<ImportListRequest> GetSeriesRequest()
         {
-            var link = $"{Settings.BaseUrl.Trim()}/sync/all-items/movies/{((SimklUserListType)Settings.ListType).ToString().ToLowerInvariant()}";
+            var link = $"{Settings.BaseUrl.Trim()}/sync/all-items/{((SimklUserMovieType)Settings.MovieType).ToString().ToLowerInvariant()}/{((SimklUserListType)Settings.ListType).ToString().ToLowerInvariant()}";
 
             var request = new ImportListRequest(link, HttpAccept.Json);
 