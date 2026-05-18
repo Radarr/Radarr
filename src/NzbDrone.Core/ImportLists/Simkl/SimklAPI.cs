Sonarr/Sonarr

Fixed: Anime Lists from Simkl
Closes #8574

src/NzbDrone.Core/ImportLists/Simkl/SimklAPI.cs
@@ -66,6 +66,9 @@ namespace NzbDrone.Core.ImportLists.Simkl
     {
         [JsonProperty("tv_shows")]
         public SimklTvSyncActivityResource TvShows { get; set; }
+
+        [JsonProperty("anime")]
+        public SimklTvSyncActivityResource Anime { get; set; }
     }
 
     public class SimklTvSyncActivityResource

src/NzbDrone.Core/ImportLists/Simkl/SimklImportBase.cs
@@ -4,6 +4,7 @@ using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.ImportLists.Simkl.User;
 using NzbDrone.Core.Localization;
 using NzbDrone.Core.Parser;
 using NzbDrone.Core.Parser.Model;
@@ -110,7 +111,16 @@ namespace NzbDrone.Core.ImportLists.Simkl
 
                 if (response?.Resource != null)
                 {
-                    return response.Resource.TvShows.All;
+                    var showType = SimklUserShowType.Shows;
+
+                    if (Settings is SimklUserSettings userSettings)
+                    {
+                        showType = (SimklUserShowType)userSettings.ShowType;
+                    }
+
+                    return showType == SimklUserShowType.Shows
+                        ? response.Resource.TvShows.All
+                        : response.Resource.Anime.All;
                 }
             }
             catch (HttpException)

Radarr/Radarr

(cherry picked from commit 931f8d5162609770a01a931233a353cc9b86b474)

src/NzbDrone.Core/ImportLists/Simkl/SimklAPI.cs
@@ -70,7 +70,11 @@ namespace NzbDrone.Core.ImportLists.Simkl
 
     public class SimklSyncActivityResource
     {
+        [JsonProperty("movies")]
         public SimklMoviesSyncActivityResource Movies { get; set; }
+
+        [JsonProperty("anime")]
+        public SimklMoviesSyncActivityResource Anime { get; set; }
     }
 
     public class SimklMoviesSyncActivityResource

src/NzbDrone.Core/ImportLists/Simkl/SimklImportBase.cs
@@ -4,6 +4,7 @@ using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.ImportLists.Simkl.User;
 using NzbDrone.Core.Parser;
 using NzbDrone.Core.Validation;
 
@@ -104,7 +105,16 @@ namespace NzbDrone.Core.ImportLists.Simkl
 
                 if (response?.Resource != null)
                 {
-                    return response.Resource.Movies.All;
+                    var movieType = SimklUserMovieType.Movies;
+
+                    if (Settings is SimklUserSettings userSettings)
+                    {
+                        movieType = (SimklUserMovieType)userSettings.MovieType;
+                    }
+
+                    return movieType == SimklUserMovieType.Movies
+                        ? response.Resource.Movies.All
+                        : response.Resource.Anime.All;
                 }
             }
             catch (HttpException)