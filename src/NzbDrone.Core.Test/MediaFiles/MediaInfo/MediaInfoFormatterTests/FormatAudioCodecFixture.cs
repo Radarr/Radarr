7fd1005c547e8a5426d2da1b02cd946993569634
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-06-02 21:32:59 +0300

    Fix filename builder for files without streams
    (cherry picked from commit edba5ce84cf8016ba9b945396d155f4ab2ead66e)

--- src/NzbDrone.Core.Test/MediaFiles/MediaInfo/MediaInfoFormatterTests/FormatAudioCodecFixture.cs
+++ src/NzbDrone.Core.Test/MediaFiles/MediaInfo/MediaInfoFormatterTests/FormatAudioCodecFixture.cs
@@ -8,48 +8,46 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo.MediaInfoFormatterTests
     [TestFixture]
     public class FormatAudioCodecFixture : TestBase
     {
-        private static string sceneName = "My.Series.S01E01-Sonarr";
+        private const string SceneName = "My.Series.S01E01-Sonarr";
 
-        [TestCase("mp2, ,  ", "droned.s01e03.swedish.720p.hdtv.x264-prince", "MP2")]
-        [TestCase("vorbis, ,  ", "DB Super HDTV", "Vorbis")]
-        [TestCase("pcm_s16le, ,  ", "DW DVDRip XviD-idTV, ", "PCM")]
-        [TestCase("truehd, ,  ", "", "TrueHD")]
-        [TestCase("truehd, ,  ", "TrueHD", "TrueHD")]
-        [TestCase("truehd, thd+,  ", "Atmos", "TrueHD Atmos")]
-        [TestCase("truehd, thd+,  ", "TrueHD.Atmos.7.1", "TrueHD Atmos")]
-        [TestCase("truehd, thd+,  ", "", "TrueHD Atmos")]
-        [TestCase("wmav1, , ", "Droned.wmv", "WMA")]
-        [TestCase("wmav2, , ", "B.N.S04E18.720p.WEB-DL", "WMA")]
-        [TestCase("opus, ,  ", "Roadkill Ep3x11 - YouTube.webm", "Opus")]
-        [TestCase("mp3, ,  ", "climbing.mp4", "MP3")]
-        [TestCase("dts, , DTS-HD MA", "DTS-HD.MA", "DTS-HD MA")]
-        [TestCase("dts, , DTS:X", "DTS-X", "DTS-X")]
-        [TestCase("dts, , DTS-HD MA", "DTS-HD.MA", "DTS-HD MA")]
-        [TestCase("dts, , DTS-ES", "DTS-ES", "DTS-ES")]
-        [TestCase("dts, , DTS-ES", "DTS", "DTS-ES")]
-        [TestCase("dts, , DTS-HD HRA", "DTSHD-HRA", "DTS-HD HRA")]
-        [TestCase("dts, , DTS", "DTS", "DTS")]
-        [TestCase("eac3, ec+3,  ", "EAC3.Atmos", "EAC3 Atmos")]
-        [TestCase("eac3, ,  ", "DDP5.1", "EAC3")]
-        [TestCase("ac3, ,  ", "DD5.1", "AC3")]
-        [TestCase("adpcm_ima_qt, ,  ", "Custom?", "PCM")]
-        [TestCase("adpcm_ms, ,  ", "Custom", "PCM")]
-
-        public void should_format_audio_format(string audioFormatPack, string sceneName, string expectedFormat)
+        [TestCase(new[] { "mp2", "", "" }, "droned.s01e03.swedish.720p.hdtv.x264-prince", "MP2")]
+        [TestCase(new[] { "vorbis", "", "" }, "DB Super HDTV", "Vorbis")]
+        [TestCase(new[] { "pcm_s16le", "", "" }, "DW DVDRip XviD-idTV, ", "PCM")]
+        [TestCase(new[] { "truehd", "", "" }, "", "TrueHD")]
+        [TestCase(new[] { "truehd", "", "" }, "TrueHD", "TrueHD")]
+        [TestCase(new[] { "truehd", "thd+", "" }, "Atmos", "TrueHD Atmos")]
+        [TestCase(new[] { "truehd", "thd+", "" }, "TrueHD.Atmos.7.1", "TrueHD Atmos")]
+        [TestCase(new[] { "truehd", "thd+", "" }, "", "TrueHD Atmos")]
+        [TestCase(new[] { "wmav1", "", "" }, "Droned.wmv", "WMA")]
+        [TestCase(new[] { "wmav2", "", "" }, "B.N.S04E18.720p.WEB-DL", "WMA")]
+        [TestCase(new[] { "opus", "", "" }, "Roadkill Ep3x11 - YouTube.webm", "Opus")]
+        [TestCase(new[] { "mp3", "", "" }, "climbing.mp4", "MP3")]
+        [TestCase(new[] { "dts", "", "DTS-HD MA" }, "DTS-HD.MA", "DTS-HD MA")]
+        [TestCase(new[] { "dts", "", "DTS:X" }, "DTS-X", "DTS-X")]
+        [TestCase(new[] { "dts", "", "DTS-HD MA" }, "DTS-HD.MA", "DTS-HD MA")]
+        [TestCase(new[] { "dts", "", "DTS-ES" }, "DTS-ES", "DTS-ES")]
+        [TestCase(new[] { "dts", "", "DTS-ES" }, "DTS", "DTS-ES")]
+        [TestCase(new[] { "dts", "", "DTS-HD HRA" }, "DTSHD-HRA", "DTS-HD HRA")]
+        [TestCase(new[] { "dts", "", "DTS" }, "DTS", "DTS")]
+        [TestCase(new[] { "eac3", "ec+3", "" }, "EAC3.Atmos", "EAC3 Atmos")]
+        [TestCase(new[] { "eac3", "", "" }, "DDP5.1", "EAC3")]
+        [TestCase(new[] { "ac3", "", "" }, "DD5.1", "AC3")]
+        [TestCase(new[] { "adpcm_ima_qt", "", "" }, "Custom?", "PCM")]
+        [TestCase(new[] { "adpcm_ms", "", "" }, "Custom", "PCM")]
+        public void should_format_audio_format(string[] audioFormat, string sceneName, string expectedFormat)
         {
-            var split = audioFormatPack.Split(new string[] { ", " }, System.StringSplitOptions.None);
             var mediaInfoModel = new MediaInfoModel
             {
-                AudioFormat = split[0],
-                AudioCodecID = split[1],
-                AudioProfile = split[2]
+                AudioFormat = audioFormat[0],
+                AudioCodecID = audioFormat[1],
+                AudioProfile = audioFormat[2]
             };
 
             MediaInfoFormatter.FormatAudioCodec(mediaInfoModel, sceneName).Should().Be(expectedFormat);
         }
 
         [Test]
-        public void should_return_AudioFormat_by_default()
+        public void should_return_audio_format_by_default()
         {
             var mediaInfoModel = new MediaInfoModel
             {
@@ -57,7 +55,13 @@ namespace NzbDrone.Core.Test.MediaFiles.MediaInfo.MediaInfoFormatterTests
                 AudioCodecID = "Other Audio Codec"
             };
 
-            MediaInfoFormatter.FormatAudioCodec(mediaInfoModel, sceneName).Should().Be(mediaInfoModel.AudioFormat);
+            MediaInfoFormatter.FormatAudioCodec(mediaInfoModel, SceneName).Should().Be(mediaInfoModel.AudioFormat);
+        }
+
+        [Test]
+        public void should_return_null_if_audio_stream_is_null()
+        {
+            MediaInfoFormatter.FormatAudioCodec(null, SceneName).Should().Be(string.Empty);
         }
     }
 }
