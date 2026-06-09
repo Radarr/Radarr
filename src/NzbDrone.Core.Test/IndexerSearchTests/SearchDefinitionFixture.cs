c795dddca1991894de16b3dcd671637ae3e4c88e
Andy Ye <35905412+TurboTheTurtle@users.noreply.github.com>
2026-06-08 21:11:21 -0700

    Fixed: Preserve dotted title separators in searches

--- src/NzbDrone.Core.Test/IndexerSearchTests/SearchDefinitionFixture.cs
+++ src/NzbDrone.Core.Test/IndexerSearchTests/SearchDefinitionFixture.cs
@@ -13,12 +13,16 @@ namespace NzbDrone.Core.Test.IndexerSearchTests
         [TestCase("Star Wars: The Clone Wars", "Star+Wars+The+Clone+Wars")]
         [TestCase("Hawaii Five-0", "Hawaii+Five+0")]
         [TestCase("Franklin & Bash", "Franklin+and+Bash")]
-        [TestCase("Chicago P.D.", "Chicago+PD")]
+        [TestCase("Chicago P.D.", "Chicago+P+D")]
         [TestCase("Kourtney And Khlo\u00E9 Take The Hamptons", "Kourtney+And+Khloe+Take+The+Hamptons")]
         [TestCase("Betty White`s Off Their Rockers", "Betty+Whites+Off+Their+Rockers")]
         [TestCase("Betty White\u00b4s Off Their Rockers", "Betty+Whites+Off+Their+Rockers")]
         [TestCase("Betty White‘s Off Their Rockers", "Betty+Whites+Off+Their+Rockers")]
         [TestCase("Betty White’s Off Their Rockers", "Betty+Whites+Off+Their+Rockers")]
+        [TestCase("Snake Eyes: G.I. Joe Origins", "Snake+Eyes+G+I+Joe+Origins")]
+        [TestCase("Sniper: G.R.I.T. - Global Response & Intelligence Team", "Sniper+G+R+I+T+Global+Response+and+Intelligence+Team")]
+        [TestCase("Ghost in the Shell 2.0", "Ghost+in+the+Shell+2+0")]
+        [TestCase("G.O.A.T", "G+O+A+T")]
         public void should_replace_some_special_characters(string input, string expected)
         {
             Subject.SceneTitles = new List<string> { input };
