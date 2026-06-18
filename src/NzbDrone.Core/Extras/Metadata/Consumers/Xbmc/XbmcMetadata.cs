ee240037055206a8977d8a546dfdb791148e15a1
darksol736 <darksol736@gmail.com>
2026-06-18 18:39:16 -0400

    Fixed: Reduced the Kodi metadata frame rate decimals to 3

--- src/NzbDrone.Core/Extras/Metadata/Consumers/Xbmc/XbmcMetadata.cs
+++ src/NzbDrone.Core/Extras/Metadata/Consumers/Xbmc/XbmcMetadata.cs
@@ -339,7 +339,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Xbmc
                     video.Add(new XElement("aspect", (float)movieFile.MediaInfo.Width / (float)movieFile.MediaInfo.Height));
                     video.Add(new XElement("bitrate", movieFile.MediaInfo.VideoBitrate));
                     video.Add(new XElement("codec", MediaInfoFormatter.FormatVideoCodec(movieFile.MediaInfo, sceneName)));
-                    video.Add(new XElement("framerate", movieFile.MediaInfo.VideoFps));
+                    video.Add(new XElement("framerate", movieFile.MediaInfo.VideoFps.ToString("0.###")));
                     video.Add(new XElement("height", movieFile.MediaInfo.Height));
                     video.Add(new XElement("scantype", movieFile.MediaInfo.ScanType));
                     video.Add(new XElement("width", movieFile.MediaInfo.Width));
