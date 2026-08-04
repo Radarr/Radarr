9acfecd9e35abca31c03dd11dd2e51a43fb8057f
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-08-05 02:14:24 +0300

    Fixed: Ignore invalid languages during Manual Import
    (cherry picked from commit 143a007cec0b7e8408ffd1b8ea3282f1cfe76cb9)

--- src/NzbDrone.Core/Languages/Language.cs
+++ src/NzbDrone.Core/Languages/Language.cs
@@ -239,5 +239,15 @@ namespace NzbDrone.Core.Languages
 
             return language;
         }
+
+        public bool IsValid(bool throwOnMissing = true)
+        {
+            return Lookup.ContainsKey(Id) switch
+            {
+                false when throwOnMissing => throw new InvalidOperationException("ID does not match a known language"),
+                false => false,
+                _ => true
+            };
+        }
     }
 }
