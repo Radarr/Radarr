Sonarr/Sonarr

New: Add hostname validation

src/NzbDrone.Core.Test/Configuration/AllowedHostsParserFixture.cs
@@ -1,0 +1,87 @@
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.Configuration
+{
+    [TestFixture]
+    public class AllowedHostsParserFixture : TestBase
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void should_return_empty_list_for_null_or_empty(string value)
+        {
+            AllowedHostsParser.Parse(value).Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_split_on_comma_and_semicolon_and_trim_whitespace()
+        {
+            var result = AllowedHostsParser.Parse("sonarr.local, 192.168.1.5; myhost");
+
+            result.Should().BeEquivalentTo("sonarr.local", "192.168.1.5", "myhost");
+        }
+
+        [Test]
+        public void should_wrap_bare_ipv6_addresses_in_brackets()
+        {
+            var result = AllowedHostsParser.Parse("::1, 2001:db8::1, [fe80::1]");
+
+            result.Should().BeEquivalentTo("[::1]", "[2001:db8::1]", "[fe80::1]");
+        }
+
+        [Test]
+        public void should_not_duplicate_bare_and_bracketed_ipv6_addresses()
+        {
+            var result = AllowedHostsParser.Parse("::1,[::1]");
+
+            result.Should().BeEquivalentTo("[::1]");
+        }
+
+        [Test]
+        public void should_remove_empty_entries_and_duplicates()
+        {
+            var result = AllowedHostsParser.Parse("a,,b, a ");
+
+            result.Should().BeEquivalentTo("a", "b");
+        }
+
+        [TestCase("sonarr.local")]
+        [TestCase("sonarr.example.co.uk")]
+        [TestCase("my-host")]
+        [TestCase("192.168.1.5")]
+        [TestCase("[::1]")]
+        [TestCase("[2001:db8::1]")]
+        [TestCase("2001:db8::1")]
+        [TestCase("*.example.com")]
+        [TestCase("*.example.co.uk")]
+        [TestCase("*.subdomain.example.com")]
+        [TestCase("*.subdomain.example.co.uk")]
+        public void should_be_valid_host(string host)
+        {
+            AllowedHostsParser.IsValidHost(host).Should().BeTrue();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        [TestCase("http://sonarr.local")]
+        [TestCase("sonarr.local:8989")]
+        [TestCase("sonarr local")]
+        [TestCase("sonarr.*")]
+        [TestCase("*")]
+        [TestCase("*.")]
+        [TestCase("*.192.168.1.5")]
+        [TestCase("*.[::1]")]
+        [TestCase("*.*.example.com")]
+        [TestCase("[not-an-address]")]
+        [TestCase(".example.com")]
+        [TestCase(".example.co.uk")]
+        public void should_not_be_valid_host(string host)
+        {
+            AllowedHostsParser.IsValidHost(host).Should().BeFalse();
+        }
+    }
+}

src/NzbDrone.Core.Test/HealthCheck/Checks/AllowedHostsCheckFixture.cs
@@ -1,0 +1,71 @@
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Core.Authentication;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Localization;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class AllowedHostsCheckFixture : CoreTest<AllowedHostsCheck>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mocker.GetMock<ILocalizationService>()
+                  .Setup(s => s.GetLocalizedString(It.IsAny<string>()))
+                  .Returns("Some Warning Message");
+
+            GivenAllowedHosts(string.Empty);
+            GivenAuthenticationRequired(AuthenticationRequiredType.Enabled);
+        }
+
+        private void GivenAllowedHosts(string allowedHosts)
+        {
+            Mocker.GetMock<IConfigFileProvider>()
+                  .SetupGet(s => s.AllowedHosts)
+                  .Returns(allowedHosts);
+        }
+
+        private void GivenAuthenticationRequired(AuthenticationRequiredType required)
+        {
+            Mocker.GetMock<IConfigFileProvider>()
+                  .SetupGet(s => s.AuthenticationRequired)
+                  .Returns(required);
+        }
+
+        [Test]
+        public void should_return_ok_when_authentication_is_required()
+        {
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_warning_when_authentication_is_not_required_for_local_addresses()
+        {
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+        }
+
+        [Test]
+        public void should_return_ok_when_allowed_hosts_is_configured()
+        {
+            GivenAllowedHosts("sonarr.local");
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_warning_when_allowed_hosts_is_whitespace()
+        {
+            GivenAllowedHosts(" , ");
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+        }
+    }
+}

Radarr/Radarr

(cherry picked from commit 1579a8bb7f89803d0850369f3ff7980d6edaf31d)

src/NzbDrone.Core.Test/Configuration/AllowedHostsParserFixture.cs
@@ -1,0 +1,87 @@
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.Configuration
+{
+    [TestFixture]
+    public class AllowedHostsParserFixture : TestBase
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void should_return_empty_list_for_null_or_empty(string value)
+        {
+            AllowedHostsParser.Parse(value).Should().BeEmpty();
+        }
+
+        [Test]
+        public void should_split_on_comma_and_semicolon_and_trim_whitespace()
+        {
+            var result = AllowedHostsParser.Parse("radarr.local, 192.168.1.5; myhost");
+
+            result.Should().BeEquivalentTo("radarr.local", "192.168.1.5", "myhost");
+        }
+
+        [Test]
+        public void should_wrap_bare_ipv6_addresses_in_brackets()
+        {
+            var result = AllowedHostsParser.Parse("::1, 2001:db8::1, [fe80::1]");
+
+            result.Should().BeEquivalentTo("[::1]", "[2001:db8::1]", "[fe80::1]");
+        }
+
+        [Test]
+        public void should_not_duplicate_bare_and_bracketed_ipv6_addresses()
+        {
+            var result = AllowedHostsParser.Parse("::1,[::1]");
+
+            result.Should().BeEquivalentTo("[::1]");
+        }
+
+        [Test]
+        public void should_remove_empty_entries_and_duplicates()
+        {
+            var result = AllowedHostsParser.Parse("a,,b, a ");
+
+            result.Should().BeEquivalentTo("a", "b");
+        }
+
+        [TestCase("radarr.local")]
+        [TestCase("radarr.example.co.uk")]
+        [TestCase("my-host")]
+        [TestCase("192.168.1.5")]
+        [TestCase("[::1]")]
+        [TestCase("[2001:db8::1]")]
+        [TestCase("2001:db8::1")]
+        [TestCase("*.example.com")]
+        [TestCase("*.example.co.uk")]
+        [TestCase("*.subdomain.example.com")]
+        [TestCase("*.subdomain.example.co.uk")]
+        public void should_be_valid_host(string host)
+        {
+            AllowedHostsParser.IsValidHost(host).Should().BeTrue();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        [TestCase("http://radarr.local")]
+        [TestCase("radarr.local:8989")]
+        [TestCase("radarr local")]
+        [TestCase("radarr.*")]
+        [TestCase("*")]
+        [TestCase("*.")]
+        [TestCase("*.192.168.1.5")]
+        [TestCase("*.[::1]")]
+        [TestCase("*.*.example.com")]
+        [TestCase("[not-an-address]")]
+        [TestCase(".example.com")]
+        [TestCase(".example.co.uk")]
+        public void should_not_be_valid_host(string host)
+        {
+            AllowedHostsParser.IsValidHost(host).Should().BeFalse();
+        }
+    }
+}

src/NzbDrone.Core.Test/HealthCheck/Checks/AllowedHostsCheckFixture.cs
@@ -1,0 +1,71 @@
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Core.Authentication;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Localization;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class AllowedHostsCheckFixture : CoreTest<AllowedHostsCheck>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mocker.GetMock<ILocalizationService>()
+                  .Setup(s => s.GetLocalizedString(It.IsAny<string>()))
+                  .Returns("Some Warning Message");
+
+            GivenAllowedHosts(string.Empty);
+            GivenAuthenticationRequired(AuthenticationRequiredType.Enabled);
+        }
+
+        private void GivenAllowedHosts(string allowedHosts)
+        {
+            Mocker.GetMock<IConfigFileProvider>()
+                  .SetupGet(s => s.AllowedHosts)
+                  .Returns(allowedHosts);
+        }
+
+        private void GivenAuthenticationRequired(AuthenticationRequiredType required)
+        {
+            Mocker.GetMock<IConfigFileProvider>()
+                  .SetupGet(s => s.AuthenticationRequired)
+                  .Returns(required);
+        }
+
+        [Test]
+        public void should_return_ok_when_authentication_is_required()
+        {
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_warning_when_authentication_is_not_required_for_local_addresses()
+        {
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+        }
+
+        [Test]
+        public void should_return_ok_when_allowed_hosts_is_configured()
+        {
+            GivenAllowedHosts("radarr.local");
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_warning_when_allowed_hosts_is_whitespace()
+        {
+            GivenAllowedHosts(" , ");
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+        }
+    }
+}