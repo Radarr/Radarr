vtorres-t/Sonarr

Improve Emby/Jellyfin connection test

src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowserService.cs
@@ -62,8 +62,7 @@ namespace NzbDrone.Core.Notifications.Emby
             try
             {
                 _logger.Debug("Testing connection to Emby/Jellyfin : {0}", settings.Address);
-
-                Notify(settings, "Test from Sonarr", "Success! MediaBrowser has been successfully configured!");
+                _proxy.TestConnection(settings);
             }
             catch (HttpException ex)
             {
@@ -71,11 +70,35 @@ namespace NzbDrone.Core.Notifications.Emby
                 {
                     return new ValidationFailure("ApiKey", _localizationService.GetLocalizedString("NotificationsValidationInvalidApiKey"));
                 }
+                else
+                {
+                    _logger.Trace(ex, "Error when connecting to Emby/Jellyfin");
+                    return new ValidationFailure("Host", _localizationService.GetLocalizedString("NotificationsValidationUnableToSendTestMessage", new Dictionary<string, object> { { "exceptionMessage", ex.Message } }));
+                }
             }
             catch (Exception ex)
             {
                 _logger.Error(ex, "Unable to send test message");
                 return new ValidationFailure("Host", _localizationService.GetLocalizedString("NotificationsValidationUnableToSendTestMessage", new Dictionary<string, object> { { "exceptionMessage", ex.Message } }));
+            }
+
+            if (settings.Notify)
+            {
+                try
+                {
+                    Notify(settings, "Test from Sonarr", "Success! MediaBrowser has been successfully configured!");
+                }
+                catch (HttpException ex)
+                {
+                    if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        return new ValidationFailure("Notify", "Unable to send notification to Emby. If you're using Jellyfin disable 'Send Notifications'");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
             }
 
             return null;

Radarr/Radarr

Resolves #11570
(cherry picked from commit c5d68bd9788af24afd9ffd9344eea77c22245bc4)

src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowserService.cs
@@ -60,8 +60,7 @@ namespace NzbDrone.Core.Notifications.Emby
             try
             {
                 _logger.Debug("Testing connection to Emby/Jellyfin : {0}", settings.Address);
-
-                Notify(settings, "Test from Radarr", "Success! MediaBrowser has been successfully configured!");
+                _proxy.TestConnection(settings);
             }
             catch (HttpException ex)
             {
@@ -69,11 +68,35 @@ namespace NzbDrone.Core.Notifications.Emby
                 {
                     return new ValidationFailure("ApiKey", _localizationService.GetLocalizedString("NotificationsValidationInvalidApiKey"));
                 }
+                else
+                {
+                    _logger.Trace(ex, "Error when connecting to Emby/Jellyfin");
+                    return new ValidationFailure("Host", _localizationService.GetLocalizedString("NotificationsValidationUnableToSendTestMessage", new Dictionary<string, object> { { "exceptionMessage", ex.Message } }));
+                }
             }
             catch (Exception ex)
             {
                 _logger.Error(ex, "Unable to send test message");
                 return new ValidationFailure("Host", _localizationService.GetLocalizedString("NotificationsValidationUnableToSendTestMessage", new Dictionary<string, object> { { "exceptionMessage", ex.Message } }));
+            }
+
+            if (settings.Notify)
+            {
+                try
+                {
+                    Notify(settings, "Test from Radarr", "Success! MediaBrowser has been successfully configured!");
+                }
+                catch (HttpException ex)
+                {
+                    if (ex.Response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        return new ValidationFailure("Notify", "Unable to send notification to Emby. If you're using Jellyfin disable 'Send Notifications'");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
             }
 
             return null;