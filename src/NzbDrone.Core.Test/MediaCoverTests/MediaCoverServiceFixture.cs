Sonarr/Sonarr

Fixed: Don't cache media covers that aren't downloaded

src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
@@ -38,9 +38,75 @@ namespace NzbDrone.Core.Test.MediaCoverTests
                 new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
             };
 
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
             Subject.ConvertToLocalUrls(12, covers);
 
             covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?h=a6210a45e2b93963ad9e");
+        }
+
+        [Test]
+        public void should_convert_cover_urls_to_local_without_hash_if_cover_has_not_been_downloaded()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(12, covers);
+
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+        }
+
+        [Test]
+        public void should_only_check_if_cover_exists_on_disk_once()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.ConvertToLocalUrls(12, covers);
+            Subject.ConvertToLocalUrls(12, covers);
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.FileExists(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void should_add_hash_to_cover_url_once_cover_exists()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Poster, RemoteUrl = "https://artworks.examples.com/posters/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(_series.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_series.Id}/poster.jpg");
+
+            Mocker.GetMock<ICoverExistsSpecification>()
+                  .Setup(v => v.AlreadyExists(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.HandleAsync(new SeriesUpdatedEvent(_series));
+            Subject.ConvertToLocalUrls(_series.Id, covers);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_series.Id}/poster.jpg?h=2a57c239a7baaae159e7");
         }
 
         [Test]

Radarr/Radarr

(cherry picked from commit 70829703c9598249ac6cb4df17d67a213e662327)

src/NzbDrone.Core.Test/MediaCoverTests/MediaCoverServiceFixture.cs
@@ -26,6 +26,7 @@ namespace NzbDrone.Core.Test.MediaCoverTests
 
             _movie = Builder<Movie>.CreateNew()
                 .With(v => v.Id = 2)
+                .With(v => v.Added = DateTime.UtcNow)
                 .With(v => v.MovieMetadata.Value.Images = new List<MediaCover.MediaCover> { new(MediaCoverTypes.Poster, "") })
                 .Build();
         }
@@ -38,9 +39,75 @@ namespace NzbDrone.Core.Test.MediaCoverTests
                 new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
             };
 
-            Subject.ConvertToLocalUrls(12, covers);
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow);
 
             covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?h=a6210a45e2b93963ad9e");
+        }
+
+        [Test]
+        public void should_convert_cover_urls_to_local_without_hash_if_cover_has_not_been_downloaded()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow);
+
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+        }
+
+        [Test]
+        public void should_only_check_if_cover_exists_on_disk_once()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow);
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow);
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.FileExists(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void should_add_hash_to_cover_url_once_cover_exists()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Poster, RemoteUrl = "https://artworks.examples.com/posters/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(_movie.Id, covers, _movie.Added);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_movie.Id}/poster.jpg");
+
+            Mocker.GetMock<ICoverExistsSpecification>()
+                  .Setup(v => v.AlreadyExists(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(true);
+
+            Subject.HandleAsync(new MovieUpdatedEvent(_movie));
+            Subject.ConvertToLocalUrls(_movie.Id, covers, _movie.Added);
+
+            covers.Single().Url.Should().Be($"/MediaCover/{_movie.Id}/poster.jpg?h=2a57c239a7baaae159e7");
         }
 
         [Test]
@@ -51,9 +118,29 @@ namespace NzbDrone.Core.Test.MediaCoverTests
                     new() { CoverType = MediaCoverTypes.Banner }
                 };
 
-            Subject.ConvertToLocalUrls(12, covers);
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow);
 
             covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg");
+        }
+
+        [Test]
+        public void should_not_check_if_cover_exists_for_movie_added_more_than_a_day_ago()
+        {
+            var covers = new List<MediaCover.MediaCover>
+            {
+                new() { CoverType = MediaCoverTypes.Banner, RemoteUrl = "https://artworks.examples.com/banners/1.jpg" }
+            };
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Setup(v => v.FileExists(It.IsAny<string>()))
+                  .Returns(false);
+
+            Subject.ConvertToLocalUrls(12, covers, DateTime.UtcNow.AddDays(-2));
+
+            covers.Single().Url.Should().Be("/MediaCover/12/banner.jpg?h=a6210a45e2b93963ad9e");
+
+            Mocker.GetMock<IDiskProvider>()
+                  .Verify(v => v.FileExists(It.IsAny<string>()), Times.Never());
         }
 
         [Test]